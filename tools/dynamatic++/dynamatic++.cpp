2f2d03470ee765833a351cee52eec01a4674a983
Lucas Ramirez <lucas.rami@proton.me>
2023-01-20 22:17:58 +0100

    [Build] Basic CMake setup for dynamatic++ tool

---
+++ tools/dynamatic++/dynamatic++.cpp
@@ -0,0 +1,12 @@
+//===- dynamatic++.cpp - The utility for performing dynamic HLS -----------===//
+//
+// This file implements 'dynamatic++'.
+//
+//===----------------------------------------------------------------------===//
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+  std::cout << "Hello world!" << std::endl;
+  return 0;
+}
