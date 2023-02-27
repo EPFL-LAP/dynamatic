af4a8f4065300bffb00aa4439adcc7944662a802
Lucas Ramirez <lucas.rami@proton.me>
2023-02-27 14:07:10 +0100

    [Build] Cleaner build script and VSCode config
    This commit overhauls the project's build script to improve its
    readability and make its output slightly cleaner. In addition, the
    script's default behavior is changed to not build and run tests for
    subprojects and superproject (provide the --check flag to run tests
    while building). Finally, the dynamatic-opt binary gets symlinked from
    the bin/ directory, along other useful binaries.
    
    The commit also improves the proposed VSCode config for the project by
    adding include paths related to Dynamatic and deleting a useless CMake
    flag in the settings file.

--- build.sh
+++ build.sh
@@ -3,19 +3,41 @@
 # Kill the whole script on Ctrl+C
 trap "exit" INT
 
+# Directory where the script is being ran from (must be directory where script
+# is located!)
+SCRIPT_CWD=$PWD
+
+#### Helper functions ####
+
 # Display list of possible options and exit
 print_help_and_exit () {
     echo -e \
 "./build.sh [options]
 
 List of options:
-    --disable-build-opt | -o    : don't use clang/lld/ccache to speed up builds
-    --disable-tests | -t        : don't run tests during build
-    --force-rebuild | -f        : force rebuild everything
-    --release | -r              : build in \"Release\" mode (default is \"Debug\")
-    --help | -h                 : display this help message
+  --disable-build-opt | -o    : don't use clang/lld/ccache to speed up builds
+  --force-rebuild | -f        : force rebuild everything
+  --release | -r              : build in \"Release\" mode (default is \"Debug\")
+  --check | -c                : run tests during build
+  --help | -h                 : display this help message
 "
     exit
+}
+
+# Helper function to print large section title text
+echo_section() {
+    echo ""
+    echo "# ===----------------------------------------------------------------------=== #"
+    echo "# $1"
+    echo "# ===----------------------------------------------------------------------=== #"
+    echo ""
+}
+
+# Helper function to print subsection title text
+echo_subsection() {
+    echo ""
+    echo "# ===--- $1 ---==="
+    echo ""
 }
 
 # Helper function to exit script on failed command
@@ -24,9 +46,14 @@ exit_on_fail() {
         if [[ ! -z $1 ]]; then
             echo -e "\n$1"
         fi
-        echo -e "\n-------------- Build failed ------------"
+        echo_section "Build failed!"
         exit 1
     fi
+}
+
+# Helper function to create build directory and cd to it
+create_build_directory() {
+    cd "${SCRIPT_CWD}" && mkdir -p ${1} && cd ${1}
 }
 
 # Create symbolic link from the bin/ directory to an executable file built by
@@ -45,10 +72,10 @@ create_symlink() {
 #### Parse arguments ####
 
 # Loop over command line arguments and update script variables
-CMAKE_FLAGS_EXT="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
+CMAKE_FLAGS_SUPER="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
     -DLLVM_ENABLE_LLD=ON" 
-CMAKE_FLAGS_LLVM="$CMAKE_FLAGS_EXT -DLLVM_CCACHE_BUILD=ON" 
-DISABLE_TESTS=0
+CMAKE_FLAGS_LLVM="$CMAKE_FLAGS_SUPER -DLLVM_CCACHE_BUILD=ON" 
+ENABLE_TESTS=0
 FORCE_REBUILD=0
 BUILD_TYPE="Debug"
 for arg in "$@"; 
@@ -56,16 +83,16 @@ do
     case "$arg" in 
         "--disable-build-opt" | "-o")
             CMAKE_FLAGS_LLVM=""
-            CMAKE_FLAGS_EXT=""
-            ;;
-        "--disable-tests" | "-t")
-            DISABLE_TESTS=1
+            CMAKE_FLAGS_SUPER=""
             ;;
         "--force-rebuild" | "-f")
             FORCE_REBUILD=1
             ;;
         "--release" | "-r")
             BUILD_TYPE="Release"
+            ;;
+        "--check" | "-c")
+            ENABLE_TESTS=1
             ;;
         "--help" | "-h")
             print_help_and_exit
@@ -77,26 +104,33 @@ do
     esac
 done
 
+# Path to build directories
+POLYGEIST_LLVM_BUILD_DIR="polygeist/llvm-project/build"
+POLYGEIST_BUILD_DIR="polygeist/build"
+CIRCT_LLVM_BUILD_DIR="circt/llvm/build"
+CIRCT_BUILD_DIR="circt/build"
+DYNAMATIC_BUILD_DIR="build"
+
 # Delete build folders if forcing rebuild
 if [[ $FORCE_REBUILD -ne 0 ]]; then
-    rm -rf polygeist/llvm-project/build polygeist/build # Polygeist
-    rm -rf circt/llvm/build circt/build # CIRCT
-    rm -rf build # Dynamatic++
+    rm -rf "${POLYGEIST_LLVM_BUILD_DIR}" "${POLYGEIST_BUILD_DIR}" # Polygeist
+    rm -rf "${CIRCT_LLVM_BUILD_DIR}" "${CIRCT_BUILD_DIR}" # CIRCT
+    rm -rf "${DYNAMATIC_BUILD_DIR}" # Dynamatic
 fi
 
-echo -e \
-"\n----------------------------------------
----------- Building Polygeist ----------
-----------------------------------------\n"
+#### Build the project (submodules and superproject) ####
 
-cd polygeist
 
-echo -e "\n---- Building polygeist/llvm-project ---\n"
+# Print header
+echo "################################################################################"
+echo "############# DYNAMATIC - DHLS COMPILER INFRASTRUCTURE - EPFL/LAP ##############"
+echo "################################################################################"
 
-# Create build directory and cd to it
-mkdir -p llvm-project/build && cd llvm-project/build
+echo_section "Building Polygeist"
+echo_subsection "Building LLVM submodule"
+create_build_directory "${POLYGEIST_LLVM_BUILD_DIR}"
 
-# Build
+# CMake
 cmake -G Ninja ../llvm \
     -DLLVM_ENABLE_PROJECTS="mlir;clang" \
     -DLLVM_TARGETS_TO_BUILD="host" \
@@ -105,48 +139,43 @@ cmake -G Ninja ../llvm \
     ${CMAKE_FLAGS_LLVM}
 exit_on_fail "Failed to cmake polygeist/llvm-project"
 
+# Build
 ninja
 exit_on_fail "Failed to build polygeist/llvm-project"
-if [[ DISABLE_TESTS -eq 0 ]]; then
+if [[ ENABLE_TESTS -eq 1 ]]; then
     ninja check-mlir
     exit_on_fail "Tests for polygeist/llvm-project failed"
 fi
 
-echo -e "\n---------- Building polygeist ----------\n"
+echo_subsection "Building superproject"
+create_build_directory "${POLYGEIST_BUILD_DIR}"
 
-# Create build directory and cd to it
-cd ../.. && mkdir -p build && cd build
-
-# Build
+# CMake
 cmake -G Ninja .. \
     -DMLIR_DIR=$PWD/../llvm-project/build/lib/cmake/mlir \
     -DCLANG_DIR=$PWD/../llvm-project/build/lib/cmake/clang \
     -DLLVM_TARGETS_TO_BUILD="host" \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
     -DLLVM_ENABLE_ASSERTIONS=ON \
-    $CMAKE_FLAGS_EXT
+    -Wno-dev \
+    $CMAKE_FLAGS_SUPER
 exit_on_fail "Failed to cmake polygeist"
 
+# Build
 ninja
 exit_on_fail "Failed to build polygeist"
-if [[ DISABLE_TESTS -eq 0 ]]; then
+if [[ ENABLE_TESTS -eq 1 ]]; then
     ninja check-polygeist-opt
     exit_on_fail "Tests for polygeist failed"
     ninja check-cgeist
     exit_on_fail "Tests for polygeist failed"
 fi
 
-echo -e \
-"\n----------------------------------------
------------- Building CIRCT ------------
-----------------------------------------\n"
+echo_section "Building CIRCT"
+echo_subsection "Building LLVM submodule"
+create_build_directory "${CIRCT_LLVM_BUILD_DIR}"
 
-echo -e "\n---------- Building circt/llvm ---------\n"
-
-# Create build directory and cd to it
-cd ../../circt && mkdir -p llvm/build && cd llvm/build
-
-# Build
+# CMake
 cmake -G Ninja ../llvm \
     -DLLVM_ENABLE_PROJECTS="mlir" \
     -DLLVM_TARGETS_TO_BUILD="host" \
@@ -156,44 +185,41 @@ cmake -G Ninja ../llvm \
     $CMAKE_FLAGS_LLVM
 exit_on_fail "Failed to cmake circt/llvm"
 
+# Build
 ninja
 exit_on_fail "Failed to build circt-llvm"
-if [[ DISABLE_TESTS -eq 0 ]]; then
+if [[ ENABLE_TESTS -eq 1 ]]; then
     ninja check-mlir
     exit_on_fail "Tests for circt/llvm failed"
 fi
 
-echo -e "\n------------ Building circt ------------\n"
+echo_subsection "Building superproject"
+create_build_directory "${CIRCT_BUILD_DIR}"
 
-# Create build directory and cd to it
-cd ../.. && mkdir -p build && cd build
-
+# CMake
 cmake -G Ninja .. \
     -DMLIR_DIR=$PWD/../llvm/build/lib/cmake/mlir \
     -DLLVM_DIR=$PWD/../llvm/build/lib/cmake/llvm \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
     -DLLVM_ENABLE_ASSERTIONS=ON \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-    $CMAKE_FLAGS_EXT
+    $CMAKE_FLAGS_SUPER
 exit_on_fail "Failed to cmake circt"
 
+# Build
 ninja
 exit_on_fail "Failed to build circt"
-if [[ DISABLE_TESTS -eq 0 ]]; then
+if [[ ENABLE_TESTS -eq 1 ]]; then
     ninja check-circt
     exit_on_fail "Tests for circt failed"
     ninja check-circt-integration
-    exit_on_fail "Tests for circt failed"
+    exit_on_fail "Integration tests for circt failed"
 fi
 
-echo -e \
-"\n----------------------------------------
---------- Building Dynamatic++ ---------
-----------------------------------------\n"
+echo_section "Building Dynamatic"
+create_build_directory "${DYNAMATIC_BUILD_DIR}"
 
-# Create build directory and cd to it
-cd ../.. && mkdir -p build && cd build
-
+# CMake
 cmake -G Ninja .. \
     -DCIRCT_DIR=$PWD/../circt/build/lib/cmake/circt \
     -DMLIR_DIR=$PWD/../circt/llvm/build/lib/cmake/mlir \
@@ -201,36 +227,29 @@ cmake -G Ninja .. \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
     -DLLVM_ENABLE_ASSERTIONS=ON \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-    $CMAKE_FLAGS_LLVM
-exit_on_fail "Failed to cmake dynamatic++"
+    $CMAKE_FLAGS_SUPER
+exit_on_fail "Failed to cmake dynamatic"
 
+# Build
 ninja
-exit_on_fail "Failed to build dynamatic++"
+exit_on_fail "Failed to build dynamatic"
+if [[ ENABLE_TESTS -eq 1 ]]; then
+    ninja check-dynamatic
+    exit_on_fail "Tests for dynamatic failed"
+fi
 
-echo -e \
-"\n----------------------------------------
-------------- Building CLI -------------
-----------------------------------------\n"
-
-cd ../cli
-cargo build
-exit_on_fail "Failed to build CLI"
-
-echo -e \
-"\n----------------------------------------
-------- Creating symbolic links --------
-----------------------------------------\n"
+echo_section "Creating symbolic links"
 
 # Create bin/ directory at the project's root
-cd .. && mkdir -p bin 
+cd "${SCRIPT_CWD}" && mkdir -p bin 
 
 # Create symbolic links to all binaries we use from subfolders
 create_symlink polygeist/build/bin/cgeist
 create_symlink polygeist/build/bin/polygeist-opt
 create_symlink polygeist/llvm-project/build/bin/mlir-opt
 create_symlink circt/build/bin/circt-opt
-create_symlink cli/target/debug/cli
+create_symlink build/bin/dynamatic-opt
 create_symlink build/bin/dynamatic++
 
-echo -e "\n----------- Build successful -----------"
+echo_section "Build successful!"
  
