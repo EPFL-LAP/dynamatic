dad22894fdb0fc511612cb9c057d9c0fbc1e1e6a
yuxwang <yuxwang.1999@hotmail.com>
2023-03-27 16:10:29 +0200

    update bits optimize initialization

--- lib/Transforms/BitsOptimize.cpp
+++ lib/Transforms/BitsOptimize.cpp
@@ -6,12 +6,13 @@
 
 #include "dynamatic/Transforms/BitsOptimize.h"
 #include "dynamatic/Transforms/PassDetails.h"
-#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "dynamatic/Transforms/Passes.h"
 #include "circt/Dialect/Handshake/HandshakeOps.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/IR/BuiltinTypes.h"
-#include "llvm/IR/InstIterator.h"
 #include "mlir/IR/OperationSupport.h"
-#include "mlir/IR/PatternMatch.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Support/IndentedOstream.h"
 
 
@@ -20,49 +21,74 @@ using namespace circt::handshake;
 using namespace mlir;
 using namespace dynamatic;
 
-static LogicalResult bitsOptimize(handshake::FuncOp funcOp) {
-  llvm::errs() << funcOp->getNumRegions() << '\n';
 
-  llvm::errs() << "visiting regions: \n";
-  for (auto &region : funcOp->getRegions()){
-    // llvm::errs() << region << '\n';
-    
-    llvm::errs() << region.getBlocks().size()  << '\n';
-
+/// A type converter for convert bit widths of integer type 
+class WidthTypeConverter : public TypeConverter {
+public:
+  WidthTypeConverter(){
+    /// addConversion
+    /// addTargetMaterialization
   }
 
-  llvm::errs() << "visiting op: \n";
+
+};
+
+static LogicalResult convertType(handshake::FuncOp funcOp,
+                         PatternRewriter &rewriter) {
   for (auto &op : funcOp.getOps()){
-    // validate the operation is legalized or not
-
-    // functions of determining operators and results type
-    
-    // determine whether success
-
     llvm::errs() << op << '\n';
-    llvm::errs() << op.getOperandTypes() << '\n';
-    for(NamedAttribute attr : op.getAttrs())
-      llvm::errs() << " - '" << attr.getName() << "' : '" << attr.getValue()
-                      << "'\n";
-    llvm::errs() << op.getResultTypes() << "\n\n";
+    llvm::errs() << op.getOperand(0) << '\n';
   }
-  
   return success();
 }
+
+/// Custom conversion target used to mark functions dynamically legal after
+/// we've applied the conversion pattern to them.
+class BitsOptimForwardTarget : public ConversionTarget {
+public:
+  explicit BitsOptimForwardTarget(MLIRContext &context)
+      : ConversionTarget(context) {
+    addLegalOp<arith::AddIOp>();
+    addLegalOp<handshake::ConstantOp>();
+    // more supported Operations need to be marked as legal
+  }
+};
+
+struct BitsOptimForward : public OpConversionPattern<handshake::FuncOp> {
+  // using OpConversionPattern::OpConversionPattern;
+
+  BitsOptimForward(BitsOptimForwardTarget &target, MLIRContext *ctx)
+      : OpConversionPattern<handshake::FuncOp>(ctx), target(target) {}
+
+  LogicalResult
+  matchAndRewrite(handshake::FuncOp op, OpAdaptor /*adaptor*/,
+                  ConversionPatternRewriter &rewriter) const override {
+
+    // Convert legal operator type
+    LogicalResult res = failure();
+    rewriter.updateRootInPlace(op,
+                               [&] { res = convertType(op, rewriter); });
+
+    return res;
+  }
+
+private:
+  BitsOptimForwardTarget &target;
+};
 
 namespace {
 struct HandshakeBitsOptimizePass
     : public HandshakeBitsOptimizeBase<HandshakeBitsOptimizePass> {
 
   void runOnOperation() override {
-    ModuleOp m = getOperation();
+    auto *ctx = &getContext();
+
+    BitsOptimForwardTarget target(*ctx);
+    RewritePatternSet patterns{ctx};
+    patterns.add<BitsOptimForward>(target, ctx);
 
     llvm::errs() << "Attemp to debug\n";
-    for (auto funcOp : m.getOps<handshake::FuncOp>()){
-      llvm::errs() << "Attemp to debug funcOp\n";
-      if (failed(bitsOptimize(funcOp)))
-        return signalPassFailure();
-    }
+
   };
 
 };
