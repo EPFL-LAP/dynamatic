74bc62852a355165ebc40f81fa7eccda509151d0
yuxuanWang <yuxwang.1999@hotmail.com>
2023-03-28 09:02:29 -0400

    methods to convert operation type

--- lib/Transforms/BitsOptimize.cpp
+++ lib/Transforms/BitsOptimize.cpp
@@ -10,6 +10,11 @@
 #include "circt/Dialect/Handshake/HandshakeOps.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/IR/BuiltinTypes.h"
+
+// #include "mlir/IR/BuiltinAttributes.h"
+// #include "mlir/IR/BuiltinDialect.h"
+// #include "TypeDetail.h"
+
 #include "mlir/IR/OperationSupport.h"
 #include "mlir/Support/LogicalResult.h"
 #include "mlir/Transforms/DialectConversion.h"
@@ -19,6 +24,7 @@
 using namespace circt;
 using namespace circt::handshake;
 using namespace mlir;
+ using namespace mlir::detail;
 using namespace dynamatic;
 
 
@@ -34,13 +40,66 @@ public:
 };
 
 static LogicalResult convertType(handshake::FuncOp funcOp,
+                          MLIRContext *ctx) {
+  bool optimizeProcess = true;
+
+  for (auto &op : funcOp.getOps()){
+    llvm::errs() << op << '\n';
+    
+    // https://mlir.llvm.org/doxygen/classmlir_1_1Operation.html
+    // first get the operands type
+    const auto &opType = op.getOpOperands();
+    // llvm::errs() << opType << '\n';
+
+    // get get the type attribute of the operators;
+    SmallVector<Value> vecOperands;
+    for (auto Operand : op.getOperands()){
+      vecOperands.push_back(Operand);
+    }
+
+    // functions to be implemented for forward pass
+    // input: opType, vecOperands
+    // return newOpType: type attributes of the results
+    IntegerType::SignednessSemantics ifSign;
+    llvm::errs() << "Number of operators : " << op.getNumOperands() << '\n';
+    for (auto optimOp : op.getOperands()){
+      llvm::errs() << "Operator type " << optimOp.getType() << " ; ";
+      if (auto validType = optimOp.getType() ; isa<IntegerType>(validType))
+        ifSign = dyn_cast<IntegerType>(validType).getSignedness();
+    }
+    llvm::errs() << " \n";
+    auto newOutType = IntegerType::get(ctx, 16,ifSign);
+
+    // https://mlir.llvm.org/doxygen/IR_2BuiltinTypes_8cpp_source.html
+
+
+
+    llvm::errs() << "Number of result Operators : " << op.getNumResults() << '\n';
+    for (auto resultOp : op.getResults()){
+      llvm::errs() << "Result type " << resultOp.getType() << " ; ";
+      if (isa<IntegerType>(resultOp.getType()))
+        resultOp.setType(newOutType);
+        break; //currently only set the first result
+    }
+      
+    llvm::errs() << " \n\n ";
+    // Value optimOp = op.getOpResults();
+    // change results type
+    // https://mlir.llvm.org/doxygen/classmlir_1_1Value.html
+    // optimOp.setType(newOpType)
+    // llvm::errs() << op.getOperand(0) << '\n';
+  }
+  return success();
+}
+
+static LogicalResult convertType(handshake::FuncOp funcOp,
                          PatternRewriter &rewriter) {
   for (auto &op : funcOp.getOps()){
     llvm::errs() << op << '\n';
     
     // https://mlir.llvm.org/doxygen/classmlir_1_1Operation.html
     // first get the operands type
-    // const auto &opType = op.getOpOperands();
+    const auto &opType = op.getOpOperands();
 
     // get get the type attribute of the operators;
     SmallVector<Value> vecOperands;
@@ -52,7 +111,8 @@ static LogicalResult convertType(handshake::FuncOp funcOp,
     // input: opType, vecOperands
     // return newOpType: type attributes of the results
 
-
+    for (auto optimOp : op.getOpResults())
+      llvm::errs() << optimOp << "\n\n";
     // Value optimOp = op.getOpResults();
     // change results type
     // https://mlir.llvm.org/doxygen/classmlir_1_1Value.html
@@ -69,6 +129,8 @@ public:
   explicit BitsOptimForwardTarget(MLIRContext &context)
       : ConversionTarget(context) {
     addLegalOp<arith::AddIOp>();
+    addLegalDialect<mlir::arith::ArithDialect>();
+    addLegalDialect<handshake::HandshakeDialect>();
     addLegalOp<handshake::ConstantOp>();
     // more supported Operations need to be marked as legal
   }
@@ -84,6 +146,7 @@ struct BitsOptimForward : public OpConversionPattern<handshake::FuncOp> {
   matchAndRewrite(handshake::FuncOp op, OpAdaptor /*adaptor*/,
                   ConversionPatternRewriter &rewriter) const override {
 
+    llvm::errs() << "Attemp to debug forward\n";
     // Convert legal operator type
     LogicalResult res = failure();
     rewriter.updateRootInPlace(op,
@@ -102,12 +165,18 @@ struct HandshakeBitsOptimizePass
 
   void runOnOperation() override {
     auto *ctx = &getContext();
-
-    BitsOptimForwardTarget target(*ctx);
-    RewritePatternSet patterns{ctx};
-    patterns.add<BitsOptimForward>(target, ctx);
+    ModuleOp m = getOperation();
 
     llvm::errs() << "Attemp to debug\n";
+    for (auto funcOp : m.getOps<handshake::FuncOp>())
+      if(failed(convertType(funcOp, ctx)))
+        return signalPassFailure();
+
+    // BitsOptimForwardTarget target(*ctx);
+    // RewritePatternSet patterns{ctx};
+    // patterns.add<BitsOptimForward>(target, ctx);
+
+    llvm::errs() << "End of debug\n";
 
   };
 
