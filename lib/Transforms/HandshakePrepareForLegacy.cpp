1a948371f9e07195ea97fce5aadb22d96d14d7b9
Lucas Ramirez <lucas.rami@proton.me>
2023-05-02 12:42:26 +0200

    [PrepareForLegacy] Downgrade cmerges to merges
    This commit adds a conversion pattern to the HandshakePrepareForLegacy
    pass that downgrades cmerges to merges when possible. This is to address
    the fact that legacy Dynamatic doesn't seem to support cmerges with a
    single input value.

--- lib/Transforms/HandshakePrepareForLegacy.cpp
+++ lib/Transforms/HandshakePrepareForLegacy.cpp
@@ -1,10 +1,7 @@
 //===- HandshakePrepareForLegacy.h - Prepare for legacy flow ----*- C++ -*-===//
 //
-// This file implements of a preprocessing step for handshake-level IR to make
-// it compatible with the legacy Dynamatic flow (through DOT export). At the
-// moment, this pass only turns unconditional branches (which legacy Dynamatic
-// never generates) into conditional branches with a constant condition input
-// and a sinked false output.
+// This file implements a preprocessing step for handshake-level IR to make it
+// compatible with the legacy Dynamatic flow (through DOT export).
 //
 //===----------------------------------------------------------------------===//
 
@@ -25,6 +22,14 @@ using namespace circt::handshake;
 using namespace mlir;
 using namespace dynamatic;
 
+/// Transfers the "bb" attribute from a source operation to a destination
+/// operation if it exists. TODO: this should probably be a publicly visible
+/// function, make it so when we have better support for "logical basic blocks"
+static void inheritBB(Operation *src, Operation *dst) {
+  if (auto bb = src->getAttr(BB_ATTR))
+    dst->setAttr(BB_ATTR, bb);
+}
+
 /// Creates a corresponding conditional branch for each unconditional branch.
 /// The data input of corresponding branches is the same. A constant true
 /// triggered by the given control signal is created to feed the conditional
@@ -34,17 +39,23 @@ static void createNewBranches(ArrayRef<handshake::BranchOp> branches,
   // Create constant source of true conditions
   rewriter.setInsertionPointAfterValue(ctrl);
   IntegerAttr cond = rewriter.getBoolAttr(true);
-  Value trueCond = rewriter
-                       .create<handshake::ConstantOp>(
-                           ctrl.getLoc(), cond.getType(), cond, ctrl)
-                       .getResult();
+  auto constOp = rewriter.create<handshake::ConstantOp>(
+      ctrl.getLoc(), cond.getType(), cond, ctrl);
 
-  // Insert a conditional branch for every unconditional branch and
-  // replace the latter's result uses with the "true" result of the former
+  // Try to set the bb attribute on the merge
+  if (auto defOp = ctrl.getDefiningOp())
+    inheritBB(defOp, constOp);
+  else
+    constOp->setAttr(BB_ATTR, rewriter.getUI32IntegerAttr(0));
+
+  // Insert a conditional branch for every unconditional branch and replace the
+  // latter's result uses with the "true" result of the former
+  auto trueCond = constOp.getResult();
   for (auto br : branches) {
     rewriter.setInsertionPointAfter(br);
     auto cbranch = rewriter.create<handshake::ConditionalBranchOp>(
         br.getLoc(), trueCond, br->getOperand(0));
+    inheritBB(br, cbranch);
     br.getResult().replaceAllUsesWith(cbranch.getTrueResult());
   }
 }
@@ -62,37 +73,37 @@ static void convertBranches(handshake::FuncOp funcOp,
   // Iterate over all identified handshake blocks to identify unconditional
   // branches and convert them
   SmallVector<handshake::BranchOp> branchesOutOfBlocks;
-  for (auto &[blockID, ops] : handshakeBlocks.blocks) {
+  for (auto &[blockID, blockOps] : handshakeBlocks.blocks) {
 
     // Identify all unconditional branches in the block, as well as a control
     // merge with dataless inputs if possible
-    SmallVector<handshake::BranchOp> branches;
+    SmallVector<handshake::BranchOp> blockBranchOps;
     handshake::ControlMergeOp cmerge = nullptr;
-    for (auto op : ops)
+    for (auto op : blockOps)
       if (isa<handshake::BranchOp>(op))
-        branches.push_back(dyn_cast<handshake::BranchOp>(op));
+        blockBranchOps.push_back(dyn_cast<handshake::BranchOp>(op));
       else if (isa<handshake::ControlMergeOp>(op)) {
         auto blockCMerge = dyn_cast<handshake::ControlMergeOp>(op);
         if (!cmerge && blockCMerge.isControl())
           cmerge = blockCMerge;
       }
 
-    if (branches.empty())
+    if (blockBranchOps.empty())
       continue;
 
     if (cmerge)
       // If we found a control merge with dataless inputs in the block, use its
       // result as control value for the new conditional branches
-      createNewBranches(branches, cmerge.getResult(), rewriter);
+      createNewBranches(blockBranchOps, cmerge.getResult(), rewriter);
     else if (blockID == 0)
       // If we are in the entry block, we can use the start input of the
       // function (last argument) as our control value
-      createNewBranches(branches, funcOp.getArguments().back(), rewriter);
+      createNewBranches(blockBranchOps, funcOp.getArguments().back(), rewriter);
     else
       // If we did not find a control merge with dataless inputs in the
       // block, we'll simply create an endless source of true conditions
       // outside of all blocks to trigger the conditional branches
-      llvm::copy(branches, std::back_inserter(branchesOutOfBlocks));
+      llvm::copy(blockBranchOps, std::back_inserter(branchesOutOfBlocks));
   }
 
   // Collect all unconditional branches that are out of blocks
@@ -114,27 +125,107 @@ static void convertBranches(handshake::FuncOp funcOp,
   }
 
   // Delete all unconditional branches
-  for (auto op : funcOp.getOps<handshake::BranchOp>())
-    rewriter.eraseOp(op);
+  for (auto brOp : branches)
+    rewriter.eraseOp(brOp);
 }
 
 namespace {
 
-/// Converts simple branches to conditional branches with constant condition.
+/// Custom conversion target for the pass that checks whether the IR is valid
+/// for use in legacy Dynamatic.
+class LegacyDynamatic : public ConversionTarget {
+public:
+  explicit LegacyDynamatic(MLIRContext &context) : ConversionTarget(context) {
+    addLegalDialect<handshake::HandshakeDialect>();
+    // Functions become legal after the ConvertSimpleBranches pattern has
+    // matched them
+    addDynamicallyLegalOp<handshake::FuncOp>(
+        [&](const auto &op) { return convertedFuncOps.contains(op); });
+    // CMerges are only valid when they have at least two operands in legacy
+    // Dynamatic
+    addDynamicallyLegalOp<handshake::ControlMergeOp>(
+        [&](const auto &op) { return op->getNumOperands() >= 2; });
+  }
+  /// The set of function that have already been matched by the
+  /// ConvertSimpleBranches pattern.
+  SmallPtrSet<Operation *, 4> convertedFuncOps;
+};
+
+/// Converts simple branches to conditional branches with a constant condition
+/// input and a sinked false output.
 struct ConvertSimpleBranches : public OpConversionPattern<handshake::FuncOp> {
   using OpConversionPattern::OpConversionPattern;
+
+  ConvertSimpleBranches(LegacyDynamatic &target, MLIRContext *ctx)
+      : OpConversionPattern<handshake::FuncOp>(ctx), target(target) {}
 
   LogicalResult
   matchAndRewrite(handshake::FuncOp op, OpAdaptor /*adaptor*/,
                   ConversionPatternRewriter &rewriter) const override {
     rewriter.updateRootInPlace(op, [&] { convertBranches(op, rewriter); });
+
+    // Mark the function legal
+    target.convertedFuncOps.insert(op);
+    return success();
+  }
+
+private:
+  /// Reference to the conversion target so that we can mark the operation legal
+  /// once we are done converting branches.
+  LegacyDynamatic &target;
+};
+
+/// Simplifies control merges into simple merges when they only have one operand
+/// or when their index result is unused.
+struct SimplifyControlMerges
+    : public OpConversionPattern<handshake::ControlMergeOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(handshake::ControlMergeOp op, OpAdaptor /*adaptor*/,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto numOperands = op->getNumOperands();
+    auto indexUnused = op.getIndex().use_empty();
+
+    if (numOperands == 1 || indexUnused) {
+      // A cmerge with one operands or an unused index result can be downgraded
+      // to a simple merge with the same operands
+      rewriter.setInsertionPoint(op);
+      auto merge = rewriter.create<handshake::MergeOp>(op.getLoc(),
+                                                       op.getDataOperands());
+      inheritBB(op, merge);
+      rewriter.updateRootInPlace(op, [&] {
+        op.getResult().replaceAllUsesWith(merge.getResult());
+
+        if (indexUnused)
+          return;
+        // When the index has users, we must replace it with a constant 0
+        // triggered by the newly created merge
+
+        // Create the attribute for the constant, whose type is derived from the
+        // cmerge's index result (index or integer attribute)
+        auto indexResType = op.getIndex().getType();
+        TypedAttr constantAttr;
+        if (isa<IndexType>(indexResType))
+          constantAttr = rewriter.getIndexAttr(0);
+        else
+          constantAttr = rewriter.getIntegerAttr(indexResType, 0);
+
+        // Create the constant and replace the cmerge's index result
+        auto constantOp = rewriter.create<handshake::ConstantOp>(
+            op.getLoc(), constantAttr.getType(), constantAttr,
+            merge.getResult());
+        inheritBB(op, constantOp);
+        op.getIndex().replaceAllUsesWith(constantOp.getResult());
+      });
+      rewriter.eraseOp(op);
+    }
+
     return success();
   }
 };
 
-/// Simple driver for prepare for legacy pass. Runs a partial conversion by
-/// using a single operation conversion pattern on each handshake::FuncOp in the
-/// module.
+/// Simple driver for prepare for legacy pass.
 struct HandshakePrepareForLegacyPass
     : public HandshakePrepareForLegacyBase<HandshakePrepareForLegacyPass> {
 
@@ -142,8 +233,9 @@ struct HandshakePrepareForLegacyPass
     auto *ctx = &getContext();
 
     RewritePatternSet patterns{ctx};
-    patterns.add<ConvertSimpleBranches>(ctx);
-    ConversionTarget target(*ctx);
+    LegacyDynamatic target(*ctx);
+    patterns.add<ConvertSimpleBranches>(target, ctx);
+    patterns.add<SimplifyControlMerges>(ctx);
 
     if (failed(applyPartialConversion(getOperation(), target,
                                       std::move(patterns))))
