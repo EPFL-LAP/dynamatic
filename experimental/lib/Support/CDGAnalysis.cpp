3af9898a11cdf5153af5e16164cbcafa04202517
Lucas Ramirez <lucas.rami@proton.me>
2023-07-20 18:29:20 +0200

    Fix failing test

--- experimental/lib/Support/CDGAnalysis.cpp
+++ experimental/lib/Support/CDGAnalysis.cpp
@@ -4,42 +4,42 @@ using namespace dynamatic::experimental;
 
 namespace {
 
-  // Helper struct for CDG analysis that represents the edge (A,B) in CFG.
-  struct CFGEdge {
-    DominanceInfoNode *from; // A
-    DominanceInfoNode *to;   // B
+// Helper struct for CDG analysis that represents the edge (A,B) in CFG.
+struct CFGEdge {
+  DominanceInfoNode *from; // A
+  DominanceInfoNode *to;   // B
 
-    CFGEdge(DominanceInfoNode *from, DominanceInfoNode *to)
-    : from(from), to(to){}
+  CFGEdge(DominanceInfoNode *from, DominanceInfoNode *to)
+      : from(from), to(to) {}
 
-    // Finds the least common ancesstor (LCA) in post-dominator tree 
-    // for nodes A and B of a CFG edge (A,B).
-    DominanceInfoNode *findLCAInPostDomTree() {
-      std::set<DominanceInfoNode *> ancestorsA;
-      unsigned level;
+  // Finds the least common ancesstor (LCA) in post-dominator tree
+  // for nodes A and B of a CFG edge (A,B).
+  DominanceInfoNode *findLCAInPostDomTree() {
+    std::set<DominanceInfoNode *> ancestorsA;
+    unsigned level;
 
-      // Traverse upwards from node A and store its ancestors in the set.
-      level = from->getLevel();
-      for (DominanceInfoNode *curr = from; level > 0;
-          --level, curr = curr->getIDom()) {
-        ancestorsA.insert(curr);
-      }
-
-      // Traverse upwards from node B until a common ancestor is found.
-      level = to->getLevel();
-      for (DominanceInfoNode *curr = to; level > 0;
-          --level, curr = curr->getIDom()) {
-        if (ancestorsA.find(curr) != ancestorsA.end()) {
-          // Lowest common ancestor
-          return curr;
-        }
-      }
-
-      return nullptr;
+    // Traverse upwards from node A and store its ancestors in the set.
+    level = from->getLevel();
+    for (DominanceInfoNode *curr = from; level > 0;
+         --level, curr = curr->getIDom()) {
+      ancestorsA.insert(curr);
     }
-  };
 
-}
+    // Traverse upwards from node B until a common ancestor is found.
+    level = to->getLevel();
+    for (DominanceInfoNode *curr = to; level > 0;
+         --level, curr = curr->getIDom()) {
+      if (ancestorsA.find(curr) != ancestorsA.end()) {
+        // Lowest common ancestor
+        return curr;
+      }
+    }
+
+    return nullptr;
+  }
+};
+
+} // namespace
 
 // Preorder post-dominance tree traversal
 static void PostDomTreeTraversal(DominanceInfoNode *node, unsigned level) {
@@ -59,7 +59,7 @@ static void PostDomTreeTraversal(DominanceInfoNode *node, unsigned level) {
 
 // Traversal of the CFG that creates set of graph edges (A,B) so A is NOT
 // post-dominated by B.
-static void CFGTraversal(Block *rootBlock, std::set<Block *> *visitedSet, 
+static void CFGTraversal(Block *rootBlock, std::set<Block *> *visitedSet,
                          std::set<CFGEdge *> *edgeSet,
                          PostDominanceInfo *postDomInfo,
                          llvm::DominatorTreeBase<Block, true> &postDomTree) {
@@ -77,7 +77,8 @@ static void CFGTraversal(Block *rootBlock, std::set<Block *> *visitedSet,
     // Check if curr is not post-dominated by his successor.
     if (postDomInfo->properlyPostDominates(successor, curr)) {
       // Every node must be marked as visited for the CDG construction purposes.
-      // We will use this set to easly traverse each node and create corresponing CDGNode object.
+      // We will use this set to easly traverse each node and create
+      // corresponing CDGNode object.
       visitedSet->insert(successor);
       continue;
     }
@@ -99,7 +100,7 @@ static void CFGTraversal(Block *rootBlock, std::set<Block *> *visitedSet,
   }
 }
 
-static std::string toString(Block* block) {
+static std::string toString(Block *block) {
   std::string result;
   llvm::raw_string_ostream ss(result);
   block->printAsOperand(ss);
@@ -107,8 +108,9 @@ static std::string toString(Block* block) {
 }
 
 // CDG traversal function
-static void CDGTraversal(CDGNode<Block> *node, std::set<Block*> &visitedSet) {
-  if (!node) return;
+static void CDGTraversal(CDGNode<Block> *node, std::set<Block *> &visitedSet) {
+  if (!node)
+    return;
 
   // visit node
   visitedSet.insert(node->getBB());
@@ -118,30 +120,29 @@ static void CDGTraversal(CDGNode<Block> *node, std::set<Block*> &visitedSet) {
 
   ss << "CDG Node: ";
   if (node->getBB()) {
-   node->getBB()->printAsOperand(ss);
-  }
-  else {
+    node->getBB()->printAsOperand(ss);
+  } else {
     ss << "<<entry>>";
   }
   ss << " Successors: [";
   for (auto it = node->beginSucc(); it != node->endSucc(); ++it) {
-    CDGNode<Block>* successor = *it;
+    CDGNode<Block> *successor = *it;
     successor->getBB()->printAsOperand(ss);
     ss << " ";
   }
-  ss << "]\n";
+  ss << "]";
 
   if (node->getBB()) {
-    Operation* termOp = node->getBB()->getTerminator();
+    Operation *termOp = node->getBB()->getTerminator();
     OpBuilder builder(termOp->getContext());
     termOp->setAttr("CD", builder.getStringAttr(ss.str()));
   }
 
   // end visit
-  
+
   for (auto it = node->beginSucc(); it != node->endSucc(); ++it) {
-    CDGNode<Block>* successor = *it;
-    
+    CDGNode<Block> *successor = *it;
+
     if (visitedSet.find(successor->getBB()) != visitedSet.end()) {
       // Successor is already visited.
       continue;
@@ -151,8 +152,8 @@ static void CDGTraversal(CDGNode<Block> *node, std::set<Block*> &visitedSet) {
   }
 }
 
-CDGNode<Block>* dynamatic::experimental::CDGAnalysis(func::FuncOp funcOp,
-                                                   MLIRContext *ctx) {
+CDGNode<Block> *dynamatic::experimental::CDGAnalysis(func::FuncOp funcOp,
+                                                     MLIRContext *ctx) {
 
   Region &funcReg = funcOp.getRegion();
   Block &rootBlockCFG = funcReg.getBlocks().front();
@@ -174,45 +175,46 @@ CDGNode<Block>* dynamatic::experimental::CDGAnalysis(func::FuncOp funcOp,
   // Memory for edges is allocated on the heap.
   CFGTraversal(&rootBlockCFG, &visitedSet, &edgeSet, &postDomInfo, postDomTree);
 
-  std::unordered_map<Block*, CDGNode<Block>*> blockToCDGNodeMap;
-  for (Block* block : visitedSet) {
+  std::unordered_map<Block *, CDGNode<Block> *> blockToCDGNodeMap;
+  for (Block *block : visitedSet) {
     blockToCDGNodeMap[block] = new CDGNode<Block>(block);
   }
 
   // Process each edge from the set.
   for (CFGEdge *edge : edgeSet) {
-    // Find the least common ancesstor (LCA) in post-dominator tree 
+    // Find the least common ancesstor (LCA) in post-dominator tree
     // of A and B for each CFG edge (A,B).
     DominanceInfoNode *LCA = edge->findLCAInPostDomTree();
 
     Block *controlBlock = edge->from->getBlock(); /*A*/
-    CDGNode<Block>* controlNode = blockToCDGNodeMap[controlBlock];
+    CDGNode<Block> *controlNode = blockToCDGNodeMap[controlBlock];
 
     // LCA = parent(A) or LCA = A.
     // All nodes on the path (LCA, B] are control dependant on A.
 
-    for (DominanceInfoNode* curr = edge->to /*B*/; curr != LCA; curr = curr->getIDom() /*parent*/) {
-      Block* dependantBlock = curr->getBlock();
-      CDGNode<Block>* dependantNode = blockToCDGNodeMap[dependantBlock];
+    for (DominanceInfoNode *curr = edge->to /*B*/; curr != LCA;
+         curr = curr->getIDom() /*parent*/) {
+      Block *dependantBlock = curr->getBlock();
+      CDGNode<Block> *dependantNode = blockToCDGNodeMap[dependantBlock];
 
       dependantNode->addPredecessor(controlNode);
       controlNode->addSuccessor(dependantNode);
     } // for end
-  } // process edge end
+  }   // process edge end
 
   // Deallocate memory for CFGEdge objects.
   for (auto edge : edgeSet) {
-      delete edge;
+    delete edge;
   }
   edgeSet.clear();
 
   // Entry point in the CDG.
-  CDGNode<Block>* entryCDGNode = new CDGNode<Block>(nullptr);
+  CDGNode<Block> *entryCDGNode = new CDGNode<Block>(nullptr);
   // Connect all detached root CDG nodes to the entry CDG node.
-  for (auto const& pair : blockToCDGNodeMap) {
-    Block* key = pair.first;
-    CDGNode<Block>* node = pair.second;
-    
+  for (auto const &pair : blockToCDGNodeMap) {
+    Block *key = pair.first;
+    CDGNode<Block> *node = pair.second;
+
     if (node->isRoot()) {
       entryCDGNode->addSuccessor(node);
       node->addPredecessor(entryCDGNode);
