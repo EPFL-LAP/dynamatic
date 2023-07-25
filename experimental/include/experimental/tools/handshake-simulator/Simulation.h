e25780a070303eee3eda7df260d1a57bd91dcac2
Polymeth <rayan.boucheny@epfl.ch>
2023-07-25 11:30:13 +0200

    [handshake-sim] Added header file for Simulation

---
+++ experimental/include/experimental/tools/handshake-simulator/Simulation.h
@@ -0,0 +1,34 @@
+//===- Simulation.h - Handshake MLIR Operations -----------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains functions used to execute a restricted form of the
+// standard dialect, and the handshake dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_HANDSHAKE_SIMULATION_H
+#define CIRCT_DIALECT_HANDSHAKE_SIMULATION_H
+
+#include "circt/Support/JSON.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/MLIRContext.h"
+#include <string>
+
+namespace dynamatic {
+namespace experimental {
+
+bool simulate(llvm::StringRef toplevelFunction,
+              llvm::ArrayRef<std::string> inputArgs,
+              mlir::OwningOpRef<mlir::ModuleOp> &module,
+              mlir::MLIRContext &context,
+              llvm::json::Value &config);
+} // namespace dynamatic
+} // namespace experimental
+
+
+#endif
