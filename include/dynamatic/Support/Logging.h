5400c8dc4345542110f0960242a53be4c529b174
Lucas Ramirez <lucas.rami@proton.me>
2023-09-25 22:37:56 +0200

    [Logger] Logging support infrastructure
    This commit introduces a support data structure to help create and write
    to log files, automatically managing the underlying file descriptor and
    output stream using RAII.

---
+++ include/dynamatic/Support/Logging.h
@@ -0,0 +1,50 @@
+//===- Logging.h - Utilities to create and manage log files -----*- C++ -*-===//
+//
+// Utilities to facilitate interactions with log files by automatically managing
+// underlying resources.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DYNAMATIC_SUPPORT_LOGGING_H
+#define DYNAMATIC_SUPPORT_LOGGING_H
+
+#include "dynamatic/Support/LLVM.h"
+#include "mlir/Support/IndentedOstream.h"
+#include "llvm/Support/Path.h"
+#include <filesystem>
+
+namespace dynamatic {
+
+/// Manages a file descriptor and exposes a writer stream to it, both of which
+/// are allocated on object creation and destroyed when the object goes out of
+/// scope. Useful, for example, for logging debugging information to a file as
+/// part of a pass.
+class Logger {
+public:
+  /// Creates a logger at the given filepath. The path must include a filename
+  /// thay may be preceded by a sequence of directories, which will be created
+  /// if they do not exist. On error, `ec` will contain a non-zero error code
+  /// and the logger should not be used.
+  Logger(StringRef filepath, std::error_code &ec);
+
+  /// Returns a reference to an underlying indented writer stream to the log
+  /// file.
+  mlir::raw_indented_ostream &operator*() { return *logStream; }
+
+  Logger(const Logger &other) = delete;
+  Logger operator=(const Logger &other) = delete;
+
+  /// Deallocates the indented writer stream and closes the log file's
+  /// descriptor.
+  ~Logger();
+
+private:
+  /// Writer stream to the log file's descriptor.
+  llvm::raw_fd_ostream *logFile = nullptr;
+  /// Indented writer stream to the log file's descriptor.
+  mlir::raw_indented_ostream *logStream = nullptr;
+};
+
+} // namespace dynamatic
+
+#endif // DYNAMATIC_SUPPORT_LOGGING_H
---
+++ lib/Support/Logging.cpp
@@ -0,0 +1,38 @@
+//===- Logging.cpp - Utilities to create and manage log files ---*- C++ -*-===//
+//
+// Implementation of logging infrastructure.
+//
+//===----------------------------------------------------------------------===//
+
+#include "dynamatic/Support/Logging.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Path.h"
+
+using namespace llvm::sys;
+using namespace dynamatic;
+
+Logger::Logger(StringRef filepath, std::error_code &ec) {
+  // Check whether the path contains a filename
+  if (!path::has_filename(filepath)) {
+    ec = std::error_code(errno, std::generic_category());
+    return;
+  }
+
+  // Potentially create nested directories to create the logging file in
+  if (ec = fs::create_directories(path::parent_path(filepath)); ec.value() != 0)
+    return;
+
+  // Create the log file and store its file descriptor for writing
+  if (logFile = new llvm::raw_fd_ostream(filepath, ec); ec.value() != 0)
+    return;
+
+  logStream = new mlir::raw_indented_ostream(*logFile);
+}
+
+Logger::~Logger() {
+  // First delete the stream, the the file descriptor
+  if (logStream)
+    delete logStream;
+  if (logFile)
+    delete logFile;
+}
