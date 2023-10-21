e0a5a1663b883447010b4e6d4dfce0f1752a1add
Albert Fares <albert_fares@hotmail.com>
2023-10-21 15:29:38 +0200

    added description

--- experimental/visual-dataflow/src/DOTReformat.h
+++ experimental/visual-dataflow/src/DOTReformat.h
@@ -1,14 +1,20 @@
+//===- DOTReformat.h - Reformats a .dot file -------------------*- C++ -*-===//
 //
-// Created by Albert Fares on 02.10.2023.
-//
-
+// The DOTReformat class is a temporary fix to the dot file being formatted wr-
+// onfully so it can be parsed correctly.
+//===----------------------------------------------------------------------===//
 
 #include <string>
 
-void putPosOnSameLine(const std::string& inputFileName, const std::string& outputFileName);
-void insertNewlineBeforeStyle(const std::string& inputFileName, const std::string& outputFileName);
-void removeBackslashWithSpaceFromPos(const std::string& inputFileName, const std::string& outputFileName);
-void removeEverythingAfterApostropheComma(const std::string& inputFileName, const std::string& outputFileName);
-void removeEverythingAfterCommaInStyle(const std::string& inputFileName, const std::string& outputFileName);
-void reformatDot(const std::string& inputFileName, const std::string& outputFileName);
-
+void putPosOnSameLine(const std::string &inputFileName,
+                      const std::string &outputFileName);
+void insertNewlineBeforeStyle(const std::string &inputFileName,
+                              const std::string &outputFileName);
+void removeBackslashWithSpaceFromPos(const std::string &inputFileName,
+                                     const std::string &outputFileName);
+void removeEverythingAfterApostropheComma(const std::string &inputFileName,
+                                          const std::string &outputFileName);
+void removeEverythingAfterCommaInStyle(const std::string &inputFileName,
+                                       const std::string &outputFileName);
+void reformatDot(const std::string &inputFileName,
+                 const std::string &outputFileName);
