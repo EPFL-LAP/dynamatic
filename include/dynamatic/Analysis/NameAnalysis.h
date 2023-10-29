ab8bb3cde89c0d1ba27345cc1fda9e47d9793edd
Lucas Ramirez <lucas.rami@proton.me>
2023-10-29 17:22:18 +0100

    [NameAnalysis] New analysis managing unique op names
    This commit starts to address #45 by introducing an analysis
    data-structure (`NameAnalysis`) meant to be loaded by Dynamatic passes
    to query and set unique names for all operations in the IR, as well as
    verify the uniqueness and persistence of names throughout the IR's
    im-memory lifetime. A next commit will make most current Dynamatic
    passes use this analysis internally to ensure that any operation they
    introduce is named.

---
+++ include/dynamatic/Analysis/NameAnalysis.h
@@ -0,0 +1,193 @@
+//===- NameAnalysis.h - Uniquely name all IR operations ---------*- C++ -*-===//
+//
+// Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Name analysis infrastructure, allocating and giving access to unique names
+// for all IR operations. This is meant to be be used from Dynamatic passes as a
+// queryable analysis on the top-level module operation (`mlir::ModuleOp`) using
+// `getAnalysis<NameAnalysis>()`.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DYNAMATIC_ANALYSIS_NAMEANALYSIS
+#define DYNAMATIC_ANALYSIS_NAMEANALYSIS
+
+#include "circt/Dialect/Handshake/HandshakeOps.h"
+#include "dynamatic/Support/LLVM.h"
+#include "mlir/Pass/AnalysisManager.h"
+
+namespace dynamatic {
+
+/// Analysis to manage unique operation names throughout the IR's lifetime.
+/// Query at the beginning of Dynamatic passes using
+/// `getAnalysis<NameAnalysis>()` and cache it to avoid recomputations for
+/// further passes using `markAnalysesPreserved<NameAnalysis>()` at the end.
+///
+/// This analysis provides an easy way to manage unique names for all IR
+/// operations (besides `func` and `handshake` level functions as well as the
+/// top-level MLIR module itself): performing caching, verification, providing
+/// unique names to yet unnamed operations, etc. At construction time and when
+/// invoking the `walk` method, the entire IR nested under the
+/// constructor-provided top-level operation is walked, establishing a
+/// bidirectionnal mapping between operation and their names as well as checking
+/// the analysis's invariants, which are:
+/// - uniqueness: no two operations can hold the same name
+/// - persistence: the name of an operation cannot change once set
+/// - completeness (optional, opt-in): all name-able operations are named
+/// In general, it is not recommended to set operation names without going
+/// through this analysis's API; doing so may break the analysis's invariants
+/// and lead to undefined behavior.
+///
+/// Operation names are internally stored in an operation attribute of type
+/// `circt::handhsake::NameAttr` attached to the operation being named under the
+/// attibute name "name"; the attribute is a simple wrapper around a
+/// `mlir::StringAttr`. Channel names are determined based on the names of the
+/// "producing" and "consuming" operations.
+class NameAnalysis {
+public:
+  /// Constructor called automatically by `getAnalysis<NameAnalysis>()` if the
+  /// analysis is not already cached. Simply stores the top-level pass operation
+  /// it is passed and walks the IR to establish mappings between already named
+  /// operations and their names. If `nameAllOps` is true, unnamed operationss
+  /// are named along the way. Users should call the `areNamesValid` method
+  /// after the constructor returns to verify that the analysis's invariants
+  /// are not violated by the operations nested under the passed operation.
+  NameAnalysis(Operation *op, bool nameAllOps = false) : topLevelOp(op) {
+    // Explictly discard the result since we can't return anything from here
+    (void)walk(nameAllOps ? UnnamedBehavior::NAME
+                          : UnnamedBehavior::DO_NOTHING);
+  };
+
+  /// Whether the last walk through the IR revealed that one of the analysis's
+  /// invariants is broken. Setting new operation names despite a broken
+  /// invariant will lead to undefined behavior.
+  bool areNamesValid() { return namesValid; }
+
+  /// Whether the last walk through the IR found no unnamed operation it did not
+  /// name. If this returns true right after the analysis walked the IR, all
+  /// operations may be assumed to have names (which may not be unique, if the
+  /// IR is in an invalid state).
+  bool areAllOpsNamed() { return allOpsNamed; }
+
+  /// Whether the operation has a name.
+  bool hasName(Operation *op) { return opToName.contains(op); }
+
+  /// If the operation has a name, stores a reference to it in `name` and
+  /// succeeds, fails otherwise.
+  LogicalResult getName(Operation *op, StringRef &name);
+
+  /// Derives a unique name for the provided operand, which is based on the
+  /// respective names of its "producer" and "consumer" operations (therefore,
+  /// both of them need to be named, or a failure will be produced) as well as
+  /// operation-specific result/operand names if available (if not, their
+  /// respective index). On success, sets `name` to the derived name.
+  LogicalResult getName(OpOperand oprd, std::string &name);
+
+  /// Return the operation that has this name, if it exists.
+  Operation *getOp(StringRef name) { return nameToOp[name]; };
+
+  /// Sets a unique name for the operation and returns a reference to it. The
+  /// name is based on the operation's type. If the operation already had a
+  /// name, just return a reference to this name.
+  StringRef setName(Operation *op);
+
+  /// Attempts to name the operation with the provided name. Fails if the
+  /// operation was already named or if another operation already holds this
+  /// name, succeeds otherwise. If the `uniqueWhenTaken` flag is set, succeeds
+  /// even if the provided name is already taken by deriving a unique name from
+  /// the provided one for the operation.
+  LogicalResult setName(Operation *op, StringRef name,
+                        bool uniqueWhenTaken = false);
+
+  /// Attempts to uniquely name the operation by concatenating a unique name
+  /// based on the operation's type with the name of an "ascending" operation
+  /// that is logically related to the operation being named. This is useful for
+  /// debugging when one may want to showcase operation filiations through their
+  /// name. Fails if the operation was already named, if the ascending operation
+  /// does not have a name, or if another operation already holds the derived
+  /// name. If the `uniqueWhenTaken` flag is set, succeeds even if the provided
+  /// name is already taken by deriving a unique name from the "normal" one for
+  /// the operation.
+  LogicalResult setName(Operation *op, Operation *ascendant,
+                        bool uniqueWhenTaken = false);
+
+  /// Tells IR walks what to do when encountering an unnamed operation.
+  enum class UnnamedBehavior {
+    /// Do nothing (further calls to `areAllOpsNamed` will return false until
+    /// the next walk).
+    DO_NOTHING,
+    /// Set a unique name for the unnamed operation.
+    NAME,
+    /// Report an error on standard error and make the walk fail (further calls
+    /// to `areAllOpsNamed` will return false until the next walk).
+    FAIL
+  };
+
+  /// Walks the IR to update known mappings between operations and their names.
+  /// The argument controls the walk's behavior when encountering an unnamed
+  /// operation. Fails if the IR breaks one of the analysis's invariants (in
+  /// which case setting new operation names will lead to undefined behavior) or
+  /// if the argument was `UnnamedBehavior::FAIL` and there exists ar least one
+  /// unnamed operation in the IR.
+  LogicalResult walk(UnnamedBehavior onUnnamed);
+
+  /// Invalidation hook to keep the analysis cached across passes. Returns true
+  /// if the analysis should be invalidated and fully reconstructed the next
+  /// time it is queried.
+  bool isInvalidated(const mlir::AnalysisManager::PreservedAnalyses &pa) {
+    return !pa.isPreserved<NameAnalysis>();
+  }
+
+  /// Names can't be guaranteed to be unique if the analysis is copied.
+  NameAnalysis(const NameAnalysis &other) = delete;
+  /// Names can't be guaranteed to be unique if the analysis is copied.
+  NameAnalysis operator=(const NameAnalysis &other) = delete;
+
+private:
+  /// Top-level operation on which the analysis operates. This should be the
+  /// top-level MLIR module to ensure unique names across an entire module.
+  Operation *topLevelOp;
+  /// Operation counters, used to generate unique names on-demand based on the
+  /// operation's type.
+  DenseMap<mlir::OperationName, unsigned> counters;
+  /// Maps all operations whose the analysis knows have names to a copy of their
+  /// name. Note that an operation may still be present in the map but no longer
+  /// in the IR.
+  mlir::DenseMap<Operation *, std::string> opToName;
+  /// Maps all known operation names to their owning operation. Note that an
+  /// operation may still be present in the map but no longer in the IR. This is
+  /// intentional and is meant so that the analysis never assigns a name which
+  /// belonged to an erased operation to a new operation.
+  llvm::StringMap<Operation *> nameToOp;
+  /// Whether the last walk through the IR revealed that one of the analysis's
+  /// invariants is broken. Reset to true at the beginning of each walk and
+  /// potentially set to false during it.
+  bool namesValid = true;
+  /// Whether the last walk through the IR revealed found no unnamed operation
+  /// it did not name. Reset to true at the beginning of each walk and
+  /// potentially set to false during it.
+  bool allOpsNamed = true;
+
+  /// Generate a unique name based on the type of an operation (using the
+  /// counters).
+  std::string genUniqueName(mlir::OperationName opName);
+
+  /// Derive a unique name from a base name by appending an increasing "counter"
+  /// string at the end (e.g., "baseName0" -> "baseName1" -> ...).
+  std::string deriveUniqueName(StringRef base);
+
+  /// Adds the bidirectional mapping between an operation and its name to our
+  /// maps. It's assumed that the operation/name pair was unknown before.
+  void addMapping(Operation *op, StringRef name) {
+    nameToOp[name] = op;
+    opToName[op] = name;
+  }
+};
+
+} // namespace dynamatic
+
+#endif // DYNAMATIC_ANALYSIS_NAMEANALYSIS
---
+++ lib/Analysis/NameAnalysis.cpp
@@ -0,0 +1,249 @@
+//===- NameAnalysis.cpp - Uniquely name all IR operations -------*- C++ -*-===//
+//
+// Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Implements the name analysis infrastructure.
+//
+//===----------------------------------------------------------------------===//
+
+#include "dynamatic/Analysis/NameAnalysis.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/OperationSupport.h"
+#include "mlir/IR/Value.h"
+#include "mlir/Support/LogicalResult.h"
+#include "llvm/ADT/STLExtras.h"
+#include <string>
+
+using namespace mlir;
+using namespace circt;
+using namespace dynamatic;
+
+inline static handshake::NameAttr getNameAttr(Operation *op) {
+  return op->getAttrOfType<handshake::NameAttr>(
+      handshake::NameAttr::getMnemonic());
+}
+
+/// Returns the name of a result which is either provided by the
+/// handshake::NamedIOInterface interface or, failing that, is its index.
+static std::string getResultName(Operation *op, size_t resIdx) {
+  std::string oprName;
+  if (auto namedIO = dyn_cast<handshake::NamedIOInterface>(op))
+    return namedIO.getResultName(resIdx);
+  return std::to_string(resIdx);
+}
+
+/// Returns the name of an operand which is either provided by the
+/// handshake::NamedIOInterface interface  or, failing that, is its index.
+static std::string getOperandName(Operation *op, size_t oprdIdx) {
+  std::string oprName;
+  if (auto namedIO = dyn_cast<handshake::NamedIOInterface>(op))
+    return namedIO.getOperandName(oprdIdx);
+  return std::to_string(oprdIdx);
+}
+
+LogicalResult NameAnalysis::getName(Operation *op, StringRef &name) {
+  auto nameIt = opToName.find(op);
+  if (nameIt == opToName.end())
+    return failure();
+  name = nameIt->getSecond();
+  return success();
+}
+
+LogicalResult NameAnalysis::getName(OpOperand oprd, std::string &name) {
+  // The defining operation must have a name (if the defining operation is a
+  // Handshake function, the function symbol is used instead)
+  std::string defName, resName;
+  Value val = oprd.get();
+  if (Operation *defOp = val.getDefiningOp()) {
+    if (auto defNameIt = opToName.find(defOp); defNameIt != opToName.end()) {
+      defName = defNameIt->second;
+      resName = getResultName(defOp, cast<OpResult>(val).getResultNumber());
+    } else {
+      return failure();
+    }
+  } else {
+    // If the parent is some kind of function, use the function name as the
+    // channel source name
+    BlockArgument arg = cast<BlockArgument>(val);
+    Operation *parentOp = arg.getParentBlock()->getParentOp();
+    if (auto funcOp = dyn_cast<handshake::FuncOp>(parentOp)) {
+      defName = funcOp.getNameAttr().str();
+      resName = funcOp.getResName(arg.getArgNumber()).str();
+    }
+    if (auto funcOp = dyn_cast<func::FuncOp>(parentOp)) {
+      defName = funcOp.getNameAttr().str();
+      resName = std::to_string(arg.getArgNumber());
+    }
+  }
+
+  // The user operation must have a name
+  std::string userName, oprName;
+  Operation *userOp = oprd.getOwner();
+  if (auto userNameIt = opToName.find(userOp); userNameIt != opToName.end()) {
+    userName = userNameIt->second;
+    oprName = getOperandName(userOp, oprd.getOperandNumber());
+  } else {
+    return failure();
+  }
+
+  name = defName + "_" + resName + "_" + oprName + "_" + userName;
+  return success();
+}
+
+StringRef NameAnalysis::setName(Operation *op) {
+  assert(namesValid && "analysis invariant is broken");
+  StringRef existingName;
+  if (succeeded(getName(op, existingName)))
+    // If the operation already has a name, do nothing and return this one
+    return existingName;
+
+  // Set the attribute on the operation and update our mapping
+  std::string name = genUniqueName(op->getName());
+  auto newAttr = handshake::NameAttr::get(op->getContext(), name);
+  op->setAttr(handshake::NameAttr::getMnemonic(), newAttr);
+  addMapping(op, name);
+  return opToName[op];
+}
+
+LogicalResult NameAnalysis::setName(Operation *op, StringRef name,
+                                    bool uniqueWhenTaken) {
+  assert(namesValid && "analysis invariant is broken");
+  if (handshake::NameAttr attr = getNameAttr(op))
+    // If the operation already has a name, this is a failure
+    return failure();
+
+  std::string uniqueName = name.str();
+  if (nameToOp.contains(name)) {
+    if (!uniqueWhenTaken)
+      return failure();
+    uniqueName = deriveUniqueName(uniqueName);
+  }
+
+  // Set the attribute on the operation and update our mapping
+  auto newAttr = handshake::NameAttr::get(op->getContext(), uniqueName);
+  op->setAttr(handshake::NameAttr::getMnemonic(), newAttr);
+  addMapping(op, uniqueName);
+  return success();
+}
+
+LogicalResult NameAnalysis::setName(Operation *op, Operation *ascendant,
+                                    bool uniqueWhenTaken) {
+  assert(namesValid && "analysis invariant is broken");
+  if (handshake::NameAttr attr = getNameAttr(op))
+    // If the operation already has a name, this is a failure
+    return failure();
+
+  auto fromNameIt = opToName.find(ascendant);
+  if (fromNameIt == opToName.end())
+    // If the ascendant operation doesn't have a name, this is a failure
+    return failure();
+
+  std::string uniqueName = genUniqueName(op->getName()) + fromNameIt->second;
+  if (nameToOp.contains(uniqueName)) {
+    if (!uniqueWhenTaken)
+      return failure();
+    uniqueName = deriveUniqueName(uniqueName);
+  }
+
+  // Set the attribute on the operation and update our mapping
+  auto newAttr = handshake::NameAttr::get(op->getContext(), uniqueName);
+  op->setAttr(handshake::NameAttr::getMnemonic(), newAttr);
+  addMapping(op, uniqueName);
+  return success();
+}
+
+LogicalResult NameAnalysis::walk(UnnamedBehavior onUnnamed) {
+  // Reset the flags
+  allOpsNamed = true;
+  namesValid = true;
+
+  topLevelOp->walk([&](Operation *nestedOp) {
+    // Do not name the top-level module
+    if (isa<mlir::ModuleOp>(nestedOp))
+      return;
+
+    // Functions already have a unique name; store it in our mapping so that we
+    // avoid naming conflicts in case a smart cookie decides one day to name
+    // their function "merge0"
+    if (auto funcOp = dyn_cast<func::FuncOp>(nestedOp)) {
+      addMapping(funcOp, funcOp.getNameAttr());
+      return;
+    }
+    if (auto funcOp = dyn_cast<handshake::FuncOp>(nestedOp)) {
+      addMapping(funcOp, funcOp.getNameAttr());
+      return;
+    }
+
+    handshake::NameAttr attr = getNameAttr(nestedOp);
+    if (!attr) {
+      // Check what we must do when we encounter an unnamed operation
+      switch (onUnnamed) {
+      case UnnamedBehavior::DO_NOTHING:
+        allOpsNamed = false;
+        break;
+      case UnnamedBehavior::NAME:
+        setName(nestedOp);
+        break;
+      case UnnamedBehavior::FAIL:
+        allOpsNamed = false;
+        // All operations must be named
+        nestedOp->emitError()
+            << "Operation has no name. All operations must be uniquely named.";
+        break;
+      }
+      return;
+    }
+
+    StringRef name = attr.getName();
+
+    // Names cannot change during operations' lifetimes
+    if (auto nameIt = opToName.find(nestedOp); nameIt != opToName.end()) {
+      if (nameIt->getSecond() != name) {
+        nestedOp->emitError()
+            << "Operation's name was '" << nameIt->second << "' but now is '"
+            << name
+            << "'. The name of an operation cannot change during its lifetime.";
+        namesValid = false;
+        return;
+      }
+    }
+
+    // Names must be unique
+    if (nameToOp.contains(name)) {
+      nestedOp->emitError() << "Operation has name '" << name
+                            << "' but another operation already has this name. "
+                               "Names must be unique.";
+      namesValid = false;
+      return;
+    }
+
+    // Store the bi-directional mapping and advance
+    addMapping(nestedOp, name);
+  });
+
+  return success(namesValid &&
+                 (onUnnamed != UnnamedBehavior::FAIL || allOpsNamed));
+}
+
+std::string NameAnalysis::genUniqueName(mlir::OperationName opName) {
+  std::string prefix = opName.stripDialect().str();
+  std::string candidate;
+  do {
+    candidate = prefix + std::to_string(counters[opName]++);
+  } while (nameToOp.contains(candidate));
+  return candidate;
+}
+
+std::string NameAnalysis::deriveUniqueName(StringRef base) {
+  unsigned counter = 0;
+  std::string candidate;
+  do {
+    candidate = base.str() + std::to_string(counter++);
+  } while (nameToOp.contains(candidate));
+  return candidate;
+}
