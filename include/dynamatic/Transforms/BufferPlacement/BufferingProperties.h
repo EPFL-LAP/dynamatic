cb631d49d1e8ce2b8f8b7fdec5379c9cc720fc8e
Lucas Ramirez <lucas.rami@proton.me>
2023-10-31 17:35:23 +0100

    [OperandAttribute] Support for generic operand attributes (#46)
    This commit adds support for working with operanf attributes: attributes
    that are semantically attached to MLIR operands rather than operations.
    Since this is not natively supported by MLIR, we introduce support
    functions to allow one to work with operand attributes with almost the
    same ease as regular attributes.
    
    Dynamatic already used "channel attributes" prior to this change in the
    form of channel-specific buffering properties. This commit, however,
    1. makes the implementation general to work with any user-provided
       operand attribute type.
    2. makes operation attributes attach of the channel's "owning" operation
       (the one which the channel is an operand of) instead of the
       "defining" operation.
    3. moves the templated implementation to a `Support` header.
    The original/partial implementation of operand attributes is thus
    deleted in favor of the new one.

--- include/dynamatic/Transforms/BufferPlacement/BufferingProperties.h
+++ include/dynamatic/Transforms/BufferPlacement/BufferingProperties.h
@@ -10,28 +10,6 @@
 // are used by the buffer placement logic to constrain the nature and
 // positions of buffers in dataflow circuits.
 //
-// Buffering properties are fundamentally an attribute of MLIR values
-// (equivalently, in buffer placement jargon, of channels). However, since it is
-// impossible to attach attributes to MLIR SSA values directly, we opt to attach
-// these "value attributes" to the operation that defines them instead. This
-// implies that an operation may contain buffering properties for multiple
-// values if it defines multiple values. We special case the handshake::FuncOp
-// operation, for which it's understood that any set of buffering properties
-// refer to its arguments instead of its results. Internally, operations keep
-// track of which buffering properties are attached to which of the values they
-// define using their index. The API defined in this file largely tries to hide
-// those cumbersome index management steps and exposes functions to manage
-// buffering properties for a specific operation result or function argument
-// directly. None of the functions declared in this file make any attempt at
-// preventing the addition of unsatisfiable buffering properties to a channel
-// i.e., it is the responsibility of the callers to check the properties'
-// satisifability if they care about it.
-//
-// Since the underlying attribute that stores buffering properties maps
-// properties to values using their indices, modifying the list of
-// results/function arguments after having added channel buffering properties
-// will break this mapping and lead to undefined behavior.
-//
 //===----------------------------------------------------------------------===//
 
 #ifndef DYNAMATIC_TRANSFORMS_BUFFERPLACEMENT_BUFFERINGPROPERTIES_H
@@ -133,9 +111,9 @@ struct Channel {
   /// SSA value representing the channel.
   Value value;
   /// Channel's producer.
-  Operation &producer;
+  Operation *producer;
   /// Channel's consumer.
-  Operation &consumer;
+  Operation *consumer;
   /// Lazily-loaded channel-specific buffering properties.
   /// NOTE: Modifying these properties will cause the IR to be updated.
   LazyChannelBufProps props;
@@ -143,18 +121,14 @@ struct Channel {
   /// Constructs a channel from its assoicated SSA value, the value's producer,
   /// and one of its comsumers. To maximize flexibility, the constructor doesn't
   /// check in any way that the provided producer and consumer correspond to the
-  /// SSA value.
-  inline Channel(Value value, Operation &producer, Operation &consumer,
-                 bool updateProps = false)
+  /// SSA value. Use at your own risk.
+  Channel(Value value, Operation *producer, Operation *consumer,
+          bool updateProps = false)
       : value(value), producer(producer), consumer(consumer),
         props(value, updateProps){};
 
-  /// Constructs a channel from its associated SSA value alone. Fails if the
-  /// value's producer is invalid or if does not have at least one consumer. Use
-  /// at your own risk.
-  inline Channel(Value value, bool updateProps = false)
-      : value(value), producer(*getChannelProducer(value)),
-        consumer(**value.getUsers().begin()), props(value, updateProps){};
+  /// Constructs a channel from its associated SSA value alone.
+  Channel(Value value, bool updateProps = false);
 
   /// The lazily-loaded channel buffering properties cannot be safely copied, so
   /// neither can the channel.
@@ -169,35 +143,6 @@ struct Channel {
     return isa<circt::handshake::FuncOp>(producer);
   }
 };
-
-// Attribute name under which buffering properties for an operation's output
-// channels are expected to be stored.
-const std::string BUF_PROPS_ATTR = "bufProps";
-
-/// Returns a mapping between the results of an operation that has channel
-/// buffering properties to their properties. If an operation's result is not in
-/// the returned map, it means it has no specific buffering properties. If the
-/// provided operation is a handshake::FuncOp, the return value maps each
-/// function argument to its buffering properties instead.
-mlir::DenseMap<Value, ChannelBufProps> getAllBufProps(Operation *op);
-
-/// Returns buffering properties associated to a channel, if any are defined.
-std::optional<ChannelBufProps> getBufProps(Value channel);
-
-/// Adds buffering properties to a channel that must either be an operation's
-/// result or a Handshake function argument. Fails and does nothing if the
-/// channel's producer already specified buffering properties for the channel.
-LogicalResult addBufProps(Value channel, ChannelBufProps &props);
-
-/// Adds or replaces the buffering properties of a channel that must either be
-/// an operation's result or a Handshake function argument. If the channel's
-/// producer already had buffering properties specified for it and the last
-/// argument isn't nullptr, the latter is set to true.
-LogicalResult replaceBufProps(Value channel, ChannelBufProps &props,
-                              bool *replaced = nullptr);
-
-/// Clears the buffering properties attached to an operation's results.
-void clearBufProps(Operation *op);
 
 } // namespace buffer
 } // namespace dynamatic
--- lib/Transforms/BufferPlacement/BufferingProperties.cpp
+++ lib/Transforms/BufferPlacement/BufferingProperties.cpp
@@ -13,6 +13,7 @@
 
 #include "dynamatic/Transforms/BufferPlacement/BufferingProperties.h"
 #include "circt/Dialect/Handshake/HandshakeOps.h"
+#include "dynamatic/Support/OperandAttribute.h"
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/Value.h"
 #include "mlir/Support/LogicalResult.h"
@@ -25,144 +26,17 @@ using namespace circt::handshake;
 using namespace dynamatic;
 using namespace dynamatic::buffer;
 
-//===----------------------------------------------------------------------===//
-// Utility functions
-//===----------------------------------------------------------------------===//
-
-/// Converts the attribute's name to an unsigned number. If building in debug
-/// mode, asserts if the attribute name doesn't represent a valid index.
-static inline size_t toIdx(const NamedAttribute &attr) {
-  std::string str = attr.getName().str();
-  assert(std::all_of(str.begin(), str.end(),
-                     [](char c) { return std::isdigit(c); }) &&
-         "invalid idx");
-  return stoi(str);
-}
-
-/// Converts the attribute's value to channel buffering properties. Asserts if
-/// the attribte value doesn't represent channel buffering properties.
-static inline ChannelBufProps toBufProps(const NamedAttribute &attr) {
-  return attr.getValue().cast<ChannelBufPropsAttr>().getProps();
-}
-
-/// Returns buffering properties associated to a channel, if any are defined.
-/// Additionally, if validChannel is not nullptr, sets it to false if the
-/// channel cannot have buffering properties attached to it.
-static std::optional<ChannelBufProps> getProps(Value channel,
-                                               bool *validChannel = nullptr) {
-  size_t idx = 0;
-  Operation *op = getChannelProducer(channel, &idx);
-  if (!op) {
-    if (validChannel)
-      *validChannel = false;
-    return {};
+Channel::Channel(Value value, bool updateProps)
+    : value(value), consumer(*value.getUsers().begin()),
+      props(value, updateProps) {
+  if (OpResult res = dyn_cast<OpResult>(value)) {
+    producer = value.getDefiningOp();
+    return;
   }
-
-  // Check if the attribute containing the properties is defined
-  auto opPropsAttr = op->getAttrOfType<OpBufPropsAttr>(BUF_PROPS_ATTR);
-  if (!opPropsAttr)
-    return {};
-
-  // Look for buffering properties attached to the channel
-  for (const NamedAttribute &attr : opPropsAttr.getChannelProperties())
-    if (toIdx(attr) == idx)
-      return toBufProps(attr);
-  return {};
-}
-
-/// Adds or replaces a set of channel buffering properties in the map maintained
-/// by the channel's defining operation. Fails if the channel is not
-/// bufferizable (i.e, not an operation's result or a function argument), or if
-/// stopOnOverwrite is true and buffering properties already exist for the
-/// channel. Otherwise, succeeds and sets the last argument to true if it is not
-/// nullptr and buffering properties already exist for the channel
-static LogicalResult setProps(Value channel, ChannelBufProps &props,
-                              bool stopOnOverwrite, bool *replaced = nullptr) {
-  size_t idx = 0;
-  Operation *op = getChannelProducer(channel, &idx);
-  if (!op)
-    return op->emitError() << "expected block argument's parent operation "
-                              "to be Handshake function";
-
-  MLIRContext *ctx = channel.getContext();
-  SmallVector<NamedAttribute> channelProperties;
-  channelProperties.push_back(
-      NamedAttribute(StringAttr::get(ctx, std::to_string(idx)),
-                     ChannelBufPropsAttr::get(ctx, props)));
-
-  // Get the attribute if it exists. If yes, copy its existing channel buffering
-  // properties while potentially replacing the one given as argument, then
-  // remove the attribute
-  if (auto attr = op->getAttrOfType<OpBufPropsAttr>(buffer::BUF_PROPS_ATTR)) {
-    for (const NamedAttribute &attr : attr.getChannelProperties()) {
-      if (toIdx(attr) == idx) {
-        if (replaced)
-          *replaced = true;
-        if (stopOnOverwrite)
-          return failure();
-      } else
-        channelProperties.push_back(attr);
-    }
-    op->removeAttr(buffer::BUF_PROPS_ATTR);
-  }
-
-  // Recreate the entire attribute
-  op->setAttr(buffer::BUF_PROPS_ATTR,
-              OpBufPropsAttr::get(ctx, channelProperties));
-  return success();
-}
-
-//===----------------------------------------------------------------------===//
-// Method definitions
-//===----------------------------------------------------------------------===//
-
-bool LazyChannelBufProps::updateIR() {
-  bool updated = updateIRIfNecessary();
-  if (updated)
-    unchangedProps = props;
-  return updated;
-}
-
-ChannelBufProps &LazyChannelBufProps::operator*() {
-  if (!props.has_value())
-    readAttribute();
-  return props.value();
-  ;
-}
-
-ChannelBufProps *LazyChannelBufProps::operator->() {
-  if (!props.has_value())
-    readAttribute();
-  return &props.value();
-}
-
-LazyChannelBufProps::~LazyChannelBufProps() {
-  if (updateOnDestruction)
-    updateIRIfNecessary();
-}
-
-void LazyChannelBufProps::readAttribute() {
-  bool validChannel = true;
-  std::optional<ChannelBufProps> optProps = getProps(val, &validChannel);
-  assert(validChannel && "channel cannot have buffering properties attached");
-  if (optProps.has_value())
-    props = optProps.value();
-  else
-    props = ChannelBufProps();
-  unchangedProps = props;
-}
-
-bool LazyChannelBufProps::updateIRIfNecessary() {
-  if (!props.has_value() || props.value() == unchangedProps.value())
-    return false;
-  assert(succeeded(buffer::replaceBufProps(val, props.value())) &&
-         "failed to replace buffering properties");
-  return true;
-}
-
-//===----------------------------------------------------------------------===//
-// Public functions to manipulate channel buffering properties
-//===----------------------------------------------------------------------===//
+  // Channel must be a block argument: make the parent operation the "producer"
+  BlockArgument arg = cast<BlockArgument>(value);
+  producer = arg.getParentBlock()->getParentOp();
+};
 
 Operation *dynamatic::buffer::getChannelProducer(Value channel, size_t *idx) {
   if (OpResult res = dyn_cast<OpResult>(channel)) {
@@ -182,49 +56,42 @@ Operation *dynamatic::buffer::getChannelProducer(Value channel, size_t *idx) {
   return nullptr;
 }
 
-DenseMap<Value, ChannelBufProps>
-dynamatic::buffer::getAllBufProps(Operation *op) {
-  DenseMap<Value, ChannelBufProps> props;
-  // Check if the attribute containing the properties is defined
-  auto opPropsAttr = op->getAttrOfType<OpBufPropsAttr>(BUF_PROPS_ATTR);
-  if (!opPropsAttr)
-    return props;
-
-  if (handshake::FuncOp funcOp = dyn_cast<handshake::FuncOp>(op)) {
-    // Map each function argument to the corresponding value
-    for (const NamedAttribute &attr : opPropsAttr.getChannelProperties())
-      props.insert(
-          std::make_pair(funcOp.getArgument(toIdx(attr)), toBufProps(attr)));
-    return props;
-  }
-
-  // Map each result index to the corresponding value
-  for (const NamedAttribute &attr : opPropsAttr.getChannelProperties())
-    props.insert(std::make_pair(op->getResult(toIdx(attr)), toBufProps(attr)));
-
-  return props;
+bool LazyChannelBufProps::updateIR() {
+  bool updated = updateIRIfNecessary();
+  if (updated)
+    unchangedProps = props;
+  return updated;
 }
 
-std::optional<ChannelBufProps> dynamatic::buffer::getBufProps(Value channel) {
-  return getProps(channel, nullptr);
+ChannelBufProps &LazyChannelBufProps::operator*() {
+  if (!props.has_value())
+    readAttribute();
+  return *props;
+  ;
 }
 
-LogicalResult dynamatic::buffer::addBufProps(Value channel,
-                                             ChannelBufProps &props) {
-  bool replaced = false;
-  return failure(failed(setProps(channel, props, true, &replaced)) || replaced);
+ChannelBufProps *LazyChannelBufProps::operator->() {
+  if (!props.has_value())
+    readAttribute();
+  return &*props;
 }
 
-LogicalResult dynamatic::buffer::replaceBufProps(Value channel,
-                                                 ChannelBufProps &props,
-                                                 bool *replaced) {
-  return setProps(channel, props, false, replaced);
+LazyChannelBufProps::~LazyChannelBufProps() {
+  if (updateOnDestruction)
+    updateIRIfNecessary();
 }
 
-void dynamatic::buffer::clearBufProps(Operation *op) {
-  // Check if the attribute containing the properties is defined
-  if (!op->hasAttrOfType<OpBufPropsAttr>(BUF_PROPS_ATTR))
-    return;
+void LazyChannelBufProps::readAttribute() {
+  ChannelBufPropsAttr optProps =
+      getOperandAttr<ChannelBufPropsAttr>(*val.getUses().begin());
+  props = optProps ? optProps.getProps() : ChannelBufProps();
+  unchangedProps = props;
+}
 
-  op->removeAttr(BUF_PROPS_ATTR);
+bool LazyChannelBufProps::updateIRIfNecessary() {
+  if (!props.has_value() || *props == *unchangedProps)
+    return false;
+  setOperandAttr(*val.getUses().begin(),
+                 ChannelBufPropsAttr::get(val.getContext(), *props));
+  return true;
 }
