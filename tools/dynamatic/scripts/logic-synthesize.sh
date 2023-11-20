20a691fa9d0b447b942c4fcd95fa8198057c35ff
Lucas Ramirez <lucas.rami@proton.me>
2023-11-20 14:01:40 -0500

    [dynamatic] Fix Vivado script in logic synthesis step
    This fixes two issues in the Vivado script generated by our logic
    synthesis frontend script.
    1. Removes the invokation to `read_verilog` when no LSQs are present.
        While this may seem unnecessary, it makes Vivado hand when no Verilog
        files are present. In our case, only LSQs are written in Verilog,
        therefore we skip the invokation when there are none.
    2. Fix typo in name of report files generated by Vivado.
    3. Save standard output of Vivado invokation to a report file.

--- tools/dynamatic/scripts/logic-synthesize.sh
+++ tools/dynamatic/scripts/logic-synthesize.sh
@@ -12,11 +12,12 @@ KERNEL_NAME=$3
 # Generated directories/files
 SYNTH_DIR="$OUTPUT_DIR/synth"
 HDL_DIR="$SYNTH_DIR/hdl"
+F_REPORT="$SYNTH_DIR/report.txt"
 F_SCRIPT="$SYNTH_DIR/synthesize.tcl"
 F_PERIOD="$SYNTH_DIR/period_4.xdc"
 F_UTILIZATION_SYN="$SYNTH_DIR/utilization_post_syn.rpt"
 F_TIMING_SYN="$SYNTH_DIR/timing_post_syn.rpt"
-F_UTILIZATION_SYN="$SYNTH_DIR/utilization_post_pr.rpt"
+F_UTILIZATION_PR="$SYNTH_DIR/utilization_post_pr.rpt"
 F_TIMING_PR="$SYNTH_DIR/timing_post_pr.rpt"
 
 
@@ -46,14 +47,20 @@ rm -rf "$SYNTH_DIR" && mkdir -p "$SYNTH_DIR"
 # Copy all synthesizable components to specific folder for Vivado
 mkdir -p "$HDL_DIR"
 cp "$OUTPUT_DIR/$KERNEL_NAME.vhd" "$HDL_DIR"
-cp "$OUTPUT_DIR/"LSQ*.v "$HDL_DIR" 2> /dev/null
 cp "$LEGACY_DIR"/components/*.vhd "$HDL_DIR"
 
-# Generate synthesization scripts
+# See if we should include any LSQ in the synthesis script
+READ_VERILOG=""
+if ls "$OUTPUT_DIR"/LSQ*.v 1> /dev/null 2>&1; then
+  cp "$OUTPUT_DIR/"LSQ*.v "$HDL_DIR"
+  READ_VERILOG="read_verilog [glob $SYNTH_DIR/hdl/*.v]"
+fi
+
+# Generate synthesis script
 echo -e \
 "set_param general.maxThreads 8
 read_vhdl -vhdl2008 [glob $SYNTH_DIR/hdl/*.vhd]
-read_verilog [glob $SYNTH_DIR/hdl/*.v]
+$READ_VERILOG
 read_xdc "$F_PERIOD"
 synth_design -top $KERNEL_NAME -part xc7k160tfbg484-2 -no_iobuf -mode out_of_context
 report_utilization > $F_UTILIZATION_SYN
@@ -63,7 +70,7 @@ place_design
 phys_opt_design
 route_design
 phys_opt_design
-report_utilization > $F_UTILIZATION_SYN
+report_utilization > $F_UTILIZATION_PR
 report_timing > $F_TIMING_PR
 exit" > "$F_SCRIPT"
 
@@ -76,7 +83,7 @@ set_property HD.CLK_SRC BUFGCTRL_X0Y0 [get_ports clk]
 
 echo_info "Created synthesization scripts"
 echo_info "Launching Vivado synthesis"
-vivado -mode tcl -source "$F_SCRIPT" > /dev/null
+vivado -mode tcl -source "$F_SCRIPT" > "$F_REPORT"
 RET=$?
 rm -rf *.jou *.log .Xil
 if [[ $RET -ne 0 ]]; then
