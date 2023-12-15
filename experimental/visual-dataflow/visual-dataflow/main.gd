4d2b8b42c5fdc2cd29a7eac3c3f3c0307406d4e9
Quentin Gross <quentin.gross@epfl.ch>
2023-12-15 13:56:14 +0100

    onClick godot

--- experimental/visual-dataflow/visual-dataflow/main.gd
+++ experimental/visual-dataflow/visual-dataflow/main.gd
@@ -26,25 +26,33 @@ var color4 = Color(0, 0.8, 0.8, 1)
 func _ready():
 	legend.hide()
 	timeline.hide()
-	
+
+
 func _process(delta):
 	if is_playing:
 		elapsed_time += delta
 		if elapsed_time >= time_interval:
 			nextCycle()
 			elapsed_time = 0
-	
+
+
 func _input(event: InputEvent):
 	if event.is_action_pressed("ui_right"):
 		_on_next_pressed()
 	if event.is_action_pressed("ui_left"):
 		_on_prev_pressed()
-		
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			onClick(event.position)
+
+
 func _on_next_pressed():
 	nextCycle()
 
+
 func _on_prev_pressed():
 	previousCycle()
+
 
 func _on_h_slider_value_changed(value):
 	changeCycle(value)
