38364a0f37e8d18d8229e79f4f87a88d3ccd6875
Lucas Ramirez <lucas.rami@proton.me>
2024-01-12 16:34:48 +0100

    [integration-test] Add 7 memory-oriented kernels
    This adds 7 new integration test to the repository that are aimed to
    test memory-access/interface-related behaviors. These existed on
    legacy Dynamatic's repository already, and are very useful to observe
    Dynamatic's behavior in the presence of various memory access patterns.

---
+++ integration-test/test_memory_12/test_memory_12.c
@@ -0,0 +1,25 @@
+#include "test_memory_12.h"
+#include "../integration_utils.h"
+#include <stdlib.h>
+
+void test_memory_12(int a[N]) {
+  for (unsigned i = 0; i < N; i++) {
+    a[0] = 0;
+    a[1] = 1;
+    a[2] = 2;
+    if (i & 1)
+      a[3] = 3;
+  }
+}
+
+int main(void) {
+  inout_int_t a[N];
+
+  srand(13);
+  for (unsigned i = 0; i < N; ++i) {
+    a[i] = (rand() % 100) - 50;
+  }
+
+  CALL_KERNEL(test_memory_12, a);
+  return 0;
+}
---
+++ integration-test/test_memory_15/test_memory_15.c
@@ -0,0 +1,29 @@
+#include "test_memory_15.h"
+#include "../integration_utils.h"
+#include <stdlib.h>
+
+void test_memory_15(inout_int_t a[N], in_int_t b[N]) {
+  for (unsigned i = 0; i < N; i++) {
+    int x = a[i];
+    if (b[i] > 5) {
+      x = x + 1;
+      b[i] = 0;
+    } else
+      x = x - 1;
+    a[i] = x;
+  }
+}
+
+int main(void) {
+  inout_int_t a[N];
+  in_int_t b[N];
+
+  srand(13);
+  for (unsigned j = 0; j < N; ++j) {
+    a[j] = (rand() % 50);
+    b[j] = (rand() % 50);
+  }
+
+  CALL_KERNEL(test_memory_15, a, b);
+  return 0;
+}
