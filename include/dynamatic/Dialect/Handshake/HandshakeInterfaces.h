581a6a18f0f13438c84eb3a19f4eb1efb1cf45c3
Lucas Ramirez <lucas.rami@proton.me>
2024-01-25 14:49:05 +0100

    [Handshake] Proper LLVM licence notice in file headers
    I think files that originates mostly from CIRCT need to retain the exact
    same licence notice at the top of the file. This is now the case.

--- include/dynamatic/Dialect/Handshake/HandshakeInterfaces.h
+++ include/dynamatic/Dialect/Handshake/HandshakeInterfaces.h
@@ -1,6 +1,6 @@
 //===- HandshakeInterfaces.h - Handshake interfaces -------------*- C++ -*-===//
 //
-// Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
