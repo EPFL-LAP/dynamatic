fc23749ad2f84ae7287ba7d7f34e272dc6e40e9e
Jiahui Xu <jxu@ethz.ch>
2024-02-11 20:12:41 +0100

    [Experimental] add a simple pass that directly adds a buffer at the output of a unit
    This pass facilitates prototyping a custom buffer placement analysis
    externally, e.g., in python, for example, we can add a buffer by
    specifying the follows:
    
    dynamatic-opt handshake_transformed.mlir \
    	--handshake-placebuffers-custom="pred=mux2 outid=0 slots=5 type=oehb" \
    	--handshake-placebuffers-custom="pred=mux2 outid=0 slots=5 type=tehb" \

---
+++ experimental/include/experimental/Transforms/HandshakePlaceBuffersCustom.h
@@ -0,0 +1,53 @@
+//===- HandshakePlaceBuffers.h - Place buffers in DFG -----------*- C++ -*-===//
+//
+// Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the --handshake-place-buffers-custom pass
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DYNAMATIC_TRANSFORMS_BUFFERPLACEMENT_PLACEBUFFERS_CUSTOM_H
+#define DYNAMATIC_TRANSFORMS_BUFFERPLACEMENT_PLACEBUFFERS_CUSTOM_H
+
+#include "dynamatic/Dialect/Handshake/HandshakeOps.h"
+#include "dynamatic/Support/DynamaticPass.h"
+#include "dynamatic/Support/LLVM.h"
+#include "dynamatic/Support/Logging.h"
+#include "dynamatic/Support/TimingModels.h"
+#include "dynamatic/Transforms/BufferPlacement/BufferPlacementMILP.h"
+#include "dynamatic/Transforms/BufferPlacement/BufferingSupport.h"
+#include "dynamatic/Transforms/BufferPlacement/CFDFC.h"
+#include "dynamatic/Transforms/HandshakeMaterialize.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "llvm/ADT/StringRef.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/OperationSupport.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Support/IndentedOstream.h"
+#include <string>
+
+
+namespace dynamatic {
+namespace experimental {
+namespace buffer {
+
+std::unique_ptr<dynamatic::DynamaticPass> createHandshakePlaceBuffersCustom(
+		const std::string &pred="", const unsigned &outid=1, const
+		unsigned &slots=1, const std::string &type="oehb");
+
+#define GEN_PASS_DECL_HANDSHAKEPLACEBUFFERSCUSTOM
+#define GEN_PASS_DEF_HANDSHAKEPLACEBUFFERSCUSTOM
+#include "experimental/Transforms/Passes.h.inc"
+
+
+} // namespace buffer
+} // namespace experimental
+} // namespace dynamatic
+
+#endif // DYNAMATIC_TRANSFORMS_BUFFERPLACEMENT_PLACEBUFFERS_CUSTOM_H
---
+++ experimental/lib/Transforms/HandshakePlaceBuffersCustom.cpp
@@ -0,0 +1,94 @@
+//===- HandshakePlaceBuffersCustom.cpp - Place buffers in DFG ---------*- C++
+//-*-===//
+//
+// Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Buffer placement in Handshake functions, taking a list of
+// predecessor:successor pairs, for example "mux1:add1 mux2:add2"
+//
+//===----------------------------------------------------------------------===//
+
+#include "experimental/Transforms/HandshakePlaceBuffersCustom.h"
+#include "dynamatic/Analysis/NameAnalysis.h"
+#include "dynamatic/Dialect/Handshake/HandshakeDialect.h"
+#include "dynamatic/Dialect/Handshake/HandshakeOps.h"
+#include "dynamatic/Support/Attribute.h"
+#include "dynamatic/Support/CFG.h"
+#include "dynamatic/Support/Logging.h"
+
+using namespace llvm;
+using namespace dynamatic;
+using namespace dynamatic::experimental;
+using namespace dynamatic::experimental::buffer;
+
+namespace {
+
+struct HandshakePlaceBuffersCustomPass
+    : public dynamatic::experimental::buffer::impl::
+          HandshakePlaceBuffersCustomBase<HandshakePlaceBuffersCustomPass> {
+
+  /// Trivial field-by-field constructor.
+  HandshakePlaceBuffersCustomPass(const std::string &pred, const int &outid,
+                                  const int &slots, const std::string &type) {
+    this->pred = pred;
+    this->outid = outid;
+    this->slots = slots;
+    this->type = type;
+  }
+
+  /// Called on the MLIR module provided as input.
+  void runDynamaticPass() override {
+    mlir::ModuleOp modOp = getOperation();
+    MLIRContext *ctx = &getContext();
+    if (failed(verifyIRMaterialized(modOp))) {
+      modOp->emitError() << ERR_NON_MATERIALIZED_MOD;
+      return;
+    }
+
+    OpBuilder builder(ctx);
+    for (handshake::FuncOp funcOp : modOp.getOps<handshake::FuncOp>())
+      for (Operation &op : funcOp.getOps()) {
+        if (getUniqueName(&op).str() == pred) {
+          assert(outid <= op.getNumResults() &&
+                 "The output id exceeds the number of output ports!");
+          Value channel = op.getResult(outid);
+          // Set the insertion point to be before the original successor of the
+          // channel
+          Operation *succ = channel.getUses().begin()->getOwner();
+          builder.setInsertionPoint(succ);
+          if (type == "oehb") {
+            auto bufOp = builder.create<handshake::OEHBOp>(channel.getLoc(),
+                                                           channel, slots);
+            inheritBB(succ, bufOp);
+            Value bufferRes = bufOp.getResult();
+            succ->replaceUsesOfWith(channel, bufferRes);
+          } else {
+            auto bufOp = builder.create<handshake::TEHBOp>(channel.getLoc(),
+                                                           channel, slots);
+            inheritBB(succ, bufOp);
+            Value bufferRes = bufOp.getResult();
+            succ->replaceUsesOfWith(channel, bufferRes);
+          }
+          return;
+        }
+      }
+
+    llvm::errs() << "The unit " << pred
+                 << "does not exist in the dataflow graph!"
+                 << "\n";
+    return signalPassFailure();
+  }
+};
+} // namespace
+
+std::unique_ptr<dynamatic::DynamaticPass>
+dynamatic::experimental::buffer::createHandshakePlaceBuffersCustom(
+    const std::string &pred, const unsigned &outid, const unsigned &slots,
+    const std::string &type) {
+  return std::make_unique<HandshakePlaceBuffersCustomPass>(pred, outid, slots,
+                                                           type);
+}
