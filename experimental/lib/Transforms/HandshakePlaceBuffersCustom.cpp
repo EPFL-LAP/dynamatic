b441f25432e1365639bfb63bfbabcb873602a17c
Lucas Ramirez <RamirezLucas@users.noreply.github.com>
2024-02-16 12:51:25 +0100

    Fix header comment

--- experimental/lib/Transforms/HandshakePlaceBuffersCustom.cpp
+++ experimental/lib/Transforms/HandshakePlaceBuffersCustom.cpp
@@ -1,5 +1,4 @@
-//===- HandshakePlaceBuffersCustom.cpp - Place buffers in DFG ---------*- C++
-//-*-===//
+//===- HandshakePlaceBuffersCustom.cpp - Place buffers in DFG ---*- C++ -*-===//
 //
 // Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
