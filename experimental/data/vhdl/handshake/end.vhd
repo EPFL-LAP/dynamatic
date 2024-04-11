523a8cd01e5e50608d40b52ae86846d1fa35e220
Lucas Ramirez <lucas.rami@proton.me>
2024-04-11 16:47:15 +0200

    [Backend] New control/store-less memory controller
    This adds a version of the VHDL entity representing the memory
    controller without any store or control-related ports. We need this
    version to model Handshake memory controller operations that also have
    no such ports; using the existing VHDL implementation would have led
    to unbound IOs when instantiating the VHDL component from the new
    backend (bi-dimensional vectors for control and store IOs would need to
    have at least size 1, however we would not have any valid signal to
    assign them to). The interface between the end synchronizer and the
    memory controller---through the `memDone`--- signal, is also modified
    to be a proper dataflow channel (though only the `valid` signal is
    acttually used).

--- experimental/data/vhdl/handshake/end.vhd
+++ experimental/data/vhdl/handshake/end.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.types.all;
 
 entity end_sync is
   generic (
@@ -10,14 +11,15 @@ entity end_sync is
   port (
     -- inputs
     clk, rst      : in std_logic;
-    ins           : in std_logic_vector(BITWIDTH - 1 downto 0);
-    ins_valid     : in std_logic;
+    ins           : in data_array(0 downto 0)(BITWIDTH - 1 downto 0);
+    ins_valid     : in std_logic_vector(0 downto 0);
+    memDone       : in data_array(MEM_INPUTS - 1 downto 0)(0 downto 0);
     memDone_valid : in std_logic_vector(MEM_INPUTS - 1 downto 0) := (others => '1');
-    outs_ready    : in std_logic;
+    outs_ready    : in std_logic_vector(0 downto 0);
     -- outputs
-    outs          : out std_logic_vector(BITWIDTH - 1 downto 0);
-    outs_valid    : out std_logic;
-    ins_ready     : out std_logic;
+    outs          : out data_array(0 downto 0)(BITWIDTH - 1 downto 0);
+    outs_valid    : out std_logic_vector(0 downto 0);
+    ins_ready     : out std_logic_vector(0 downto 0);
     memDone_ready : out std_logic_vector(MEM_INPUTS - 1 downto 0)
   );
 end entity;
@@ -39,14 +41,14 @@ begin
     variable tmp_data_out  : unsigned(BITWIDTH - 1 downto 0);
     variable tmp_valid_out : std_logic;
   begin
-    tmp_data_out  := unsigned(ins);
+    tmp_data_out  := unsigned((ins(0)));
     tmp_valid_out := '0';
-    if (ins_valid = '1') then
-      tmp_data_out  := unsigned(ins);
-      tmp_valid_out := ins_valid;
+    if (ins_valid(0) = '1') then
+      tmp_data_out  := unsigned(ins(0));
+      tmp_valid_out := ins_valid(0);
     end if;
-    outs  <= std_logic_vector(resize(tmp_data_out, BITWIDTH));
-    valid <= tmp_valid_out;
+    outs(0) <= std_logic_vector(resize(tmp_data_out, BITWIDTH));
+    valid   <= tmp_valid_out;
   end process;
 
   mem_and : entity work.and_n(vanilla) generic map (MEM_INPUTS)
@@ -55,13 +57,13 @@ begin
   j : entity work.join(arch) generic map(2)
     port map(
     (valid, mem_valid),
-      outs_ready,
+      outs_ready(0),
       joinValid,
       out_array);
-  outs_valid <= joinValid;
+  outs_valid(0) <= joinValid;
 
   process (joinReady)
   begin
-    ins_ready <= joinReady;
+    ins_ready(0) <= joinReady;
   end process;
 end architecture;
--- experimental/data/vhdl/handshake/mem_controller_no_store.vhd
+++ experimental/data/vhdl/handshake/mem_controller_no_store.vhd
@@ -3,37 +3,26 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.types.all;
 
-entity mem_controller is
+entity mem_controller_no_store is
   generic (
-    CTRL_COUNT    : integer;
     LOAD_COUNT    : integer;
-    STORE_COUNT   : integer;
     DATA_BITWIDTH : integer;
     ADDR_BITWIDTH : integer
   );
   port (
     -- inputs
     loadData      : in std_logic_vector(DATA_BITWIDTH downto 0);
-    ctrl          : in data_array (CTRL_COUNT - 1 downto 0)(31 downto 0);
-    ctrl_valid    : in std_logic_vector(CTRL_COUNT - 1 downto 0);
     ldAddr        : in data_array (LOAD_COUNT - 1 downto 0)(ADDR_BITWIDTH - 1 downto 0);
     ldAddr_valid  : in std_logic_vector(LOAD_COUNT - 1 downto 0);
-    stAddr        : in data_array (STORE_COUNT - 1 downto 0)(ADDR_BITWIDTH - 1 downto 0);
-    stAddr_valid  : in std_logic_vector(STORE_COUNT - 1 downto 0);
-    stData        : in data_array (STORE_COUNT - 1 downto 0)(DATA_BITWIDTH - 1 downto 0);
-    stData_valid  : in std_logic_vector(STORE_COUNT - 1 downto 0);
     clk           : in std_logic;
     rst           : in std_logic;
     ldData_ready  : in std_logic_vector(LOAD_COUNT - 1 downto 0);
     memDone_ready : in std_logic;
     -- outputs
-    ctrl_ready    : out std_logic_vector(CTRL_COUNT - 1 downto 0);
     ldAddr_ready  : out std_logic_vector(LOAD_COUNT - 1 downto 0);
-    stAddr_ready  : out std_logic_vector(STORE_COUNT - 1 downto 0);
-    stData_ready  : out std_logic_vector(STORE_COUNT - 1 downto 0);
     ldData        : out data_array (LOAD_COUNT - 1 downto 0)(DATA_BITWIDTH - 1 downto 0);
     ldData_valid  : out std_logic_vector(LOAD_COUNT - 1 downto 0);
-    memDone       : in  std_logic_vector(0 downto 0);
+    memDone       : out std_logic_vector(0 downto 0);
     memDone_valid : out std_logic;
     loadEn        : out std_logic;
     loadAddr      : out std_logic_vector(ADDR_BITWIDTH downto 0);
@@ -43,11 +32,9 @@ entity mem_controller is
   );
 end entity;
 
-architecture arch of mem_controller is
-  signal counter1    : std_logic_vector(31 downto 0);
-  signal valid_WR    : std_logic_vector(STORE_COUNT - 1 downto 0);
-  constant zero_32   : std_logic_vector(31 downto 0)             := (others => '0');
-  constant zero_ctrl : std_logic_vector(CTRL_COUNT - 1 downto 0) := (others => '0');
+architecture arch of mem_controller_no_store is
+  signal counter1  : std_logic_vector(31 downto 0);
+  constant zero_32 : std_logic_vector(31 downto 0) := (others => '0');
 
   signal mcStoreDataOut : std_logic_vector(DATA_BITWIDTH - 1 downto 0);
   signal mcStoreAddrOut : std_logic_vector(ADDR_BITWIDTH - 1 downto 0);
@@ -55,8 +42,6 @@ architecture arch of mem_controller is
   signal mcLoadAddrOut  : std_logic_vector(ADDR_BITWIDTH - 1 downto 0);
 
 begin
-  stData_ready <= stAddr_ready;
-
   storeData    <= std_logic_vector (resize(unsigned(mcStoreDataOut), storeData'length));
   storeAddr    <= std_logic_vector (resize(unsigned(mcStoreAddrOut), storeAddr'length));
   mcLoadDataIn <= std_logic_vector (resize(unsigned(loadData), mcLoadDataIn'length));
@@ -82,47 +67,16 @@ begin
       data_from_memory => mcLoadDataIn
     );
 
-  write_arbiter : entity work.write_memory_arbiter
-    generic map(
-      ARBITER_SIZE => STORE_COUNT,
-      ADDR_WIDTH   => ADDR_BITWIDTH,
-      DATA_WIDTH   => DATA_BITWIDTH
-    )
-    port map(
-      rst            => rst,
-      clk            => clk,
-      pValid         => stAddr_valid,
-      ready          => stAddr_ready,
-      address_in     => stAddr,
-      data_in        => stData,
-      nReady         => (others => '1'),
-      valid          => valid_WR,
-      write_enable   => storeEn,
-      write_address  => mcStoreAddrOut,
-      data_to_memory => mcStoreDataOut
-    );
-
   Counterp : process (CLK)
     variable counter : std_logic_vector(31 downto 0);
   begin
     if (rst = '1') then
       counter := (31 downto 0 => '0');
-
     elsif rising_edge(CLK) then
-      for I in 0 to CTRL_COUNT - 1 loop
-        if (ctrl_valid(I) = '1') then
-          counter := std_logic_vector(unsigned(counter) + unsigned(ctrl(I)));
-        end if;
-      end loop;
-      if (storeEn = '1') then
-        counter := std_logic_vector(unsigned(counter) - 1);
-      end if;
-
       counter1 <= counter;
     end if;
   end process;
 
-  memDone_valid <= '1' when (counter1 = zero_32 and (ctrl_valid = zero_ctrl)) else '0';
-  ctrl_ready    <= (others => '1');
+  memDone_valid <= '1' when (counter1 = zero_32) else '0';
 
 end architecture;
