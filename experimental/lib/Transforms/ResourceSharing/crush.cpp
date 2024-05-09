6cdb61a0578b9a5e1a6e9f76c863cdd69a13fd49
Jiahui Xu <jxu@ethz.ch>
2024-05-09 17:50:59 +0200

    [ResourceSharing] Wrapper class for buffering

--- experimental/lib/Transforms/ResourceSharing/crush.cpp
+++ experimental/lib/Transforms/ResourceSharing/crush.cpp
@@ -9,13 +9,49 @@
 #include "experimental/Transforms/ResourceSharing/crush.h"
 #include "dynamatic/Support/DynamaticPass.h"
 #include "dynamatic/Support/LLVM.h"
+#include "dynamatic/Transforms/BufferPlacement/BufferingSupport.h"
+#include "dynamatic/Transforms/BufferPlacement/CFDFC.h"
+#include "dynamatic/Transforms/BufferPlacement/HandshakePlaceBuffers.h"
 #include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include <map>
 #include <string>
 
 using namespace llvm;
 using namespace dynamatic;
 using namespace dynamatic::experimental;
 using namespace dynamatic::experimental::sharing;
+
+using namespace dynamatic::buffer;
+
+// extracted data from buffer placement
+struct ThroughputInfo {
+
+  // For each CFDFC, the obtained throughput
+  std::map<CFDFC, double> cfdfcThroughput;
+};
+
+namespace {
+
+// An wrapper class that applies buffer p
+// extracts the report.
+struct BufferPlacementWrapperPass : public HandshakePlaceBuffersPass {
+  BufferPlacementWrapperPass(ThroughputInfo &info, StringRef algorithm,
+                             StringRef frequencies, StringRef timingModels,
+                             bool firstCFDFC, double targetCP, unsigned timeout,
+                             bool dumpLogs)
+      : HandshakePlaceBuffersPass(algorithm, frequencies, timingModels,
+                                  firstCFDFC, targetCP, timeout, dumpLogs),
+        info(info){};
+  ThroughputInfo &info;
+
+protected:
+  void runDynamaticPass() override {
+    HandshakePlaceBuffersPass::runDynamaticPass();
+  }
+};
+
+} // namespace
 
 namespace {
 struct CreditBasedSharingPass
@@ -40,6 +76,23 @@ struct CreditBasedSharingPass
 
 void CreditBasedSharingPass::runDynamaticPass() {
   llvm::errs() << "***** Resource Sharing *****\n";
+
+  ModuleOp modOp = getOperation();
+
+  ThroughputInfo data;
+
+  TimingDatabase timingDB(&getContext());
+  if (failed(TimingDatabase::readFromJSON(timingModels, timingDB)))
+    return signalPassFailure();
+
+  // running buffer placement on current module
+  mlir::PassManager pm(&getContext());
+  pm.addPass(std::make_unique<BufferPlacementWrapperPass>(
+      data, algorithm, frequencies, timingModels, firstCFDFC, targetCP, timeout,
+      dumpLogs));
+  if (failed(pm.run(modOp))) {
+    return signalPassFailure();
+  }
 }
 
 namespace dynamatic {
