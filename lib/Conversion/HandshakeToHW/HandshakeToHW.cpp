0dcf717ca916ed3f97250a9e34f27b26a6c83d71
Lucas Ramirez <lucas.rami@proton.me>
2024-06-21 13:49:46 +0200

    [HW] Move `PortNameGenerator` to dialect's library
    The `PortNameGenerator` utility class, which derives port names for all
    supported operations in the IR, was previously a private part of the
    `HandshakeToHW` conversion pass and was therefore not usable by other
    parts of the codebase. It may however be useful in different contexts,
    and was therefore moved to the HW's dialect library.

--- lib/Conversion/HandshakeToHW/HandshakeToHW.cpp
+++ lib/Conversion/HandshakeToHW/HandshakeToHW.cpp
@@ -114,123 +114,7 @@ private:
   SmallVector<hw::ModulePort> outputs;
 };
 
-/// Provides an opaque interface for generating the port names of an operation;
-/// handshake operations generate names by the `handshake::NamedIOInterface`;
-/// and other operations, such as arith ops, are assigned default names.
-class PortNameGenerator {
-public:
-  /// Does nohting; no port name will be generated.
-  PortNameGenerator() = default;
-
-  /// Derives port names for the operation on object creation.
-  PortNameGenerator(Operation *op);
-
-  /// Returs the port name of the input at the specified index.
-  StringRef getInputName(unsigned idx) { return inputs[idx]; }
-
-  /// Returs the port name of the output at the specified index.
-  StringRef getOutputName(unsigned idx) { return outputs[idx]; }
-
-private:
-  /// Maps the index of an input or output to its port name.
-  using IdxToStrF = const std::function<std::string(unsigned)> &;
-
-  /// Infers port names for the operation using the provided callbacks.
-  void infer(Operation *op, IdxToStrF &inF, IdxToStrF &outF);
-
-  /// Infers default port names when nothing better can be achieved.
-  void inferDefault(Operation *op);
-
-  /// Infers port names for an operation implementing the
-  /// `handshake::NamedIOInterface` interface.
-  void inferFromNamedOpInterface(handshake::NamedIOInterface namedIO);
-
-  /// Infers port names for a Handshake function.
-  void inferFromFuncOp(handshake::FuncOp funcOp);
-
-  /// List of input port names.
-  SmallVector<std::string> inputs;
-  /// List of output port names.
-  SmallVector<std::string> outputs;
-};
 } // namespace
-
-PortNameGenerator::PortNameGenerator(Operation *op) {
-  assert(op && "cannot generate port names for null operation");
-  if (auto namedOpInterface = dyn_cast<handshake::NamedIOInterface>(op))
-    inferFromNamedOpInterface(namedOpInterface);
-  else if (auto funcOp = dyn_cast<handshake::FuncOp>(op))
-    inferFromFuncOp(funcOp);
-  else
-    inferDefault(op);
-}
-
-void PortNameGenerator::infer(Operation *op, IdxToStrF &inF, IdxToStrF &outF) {
-  for (size_t idx = 0, e = op->getNumOperands(); idx < e; ++idx)
-    inputs.push_back(inF(idx));
-  for (size_t idx = 0, e = op->getNumResults(); idx < e; ++idx)
-    outputs.push_back(outF(idx));
-
-  // The Handshake terminator forwards its non-memory inputs to its outputs, so
-  // it needs port names for them
-  if (handshake::EndOp endOp = dyn_cast<handshake::EndOp>(op)) {
-    handshake::FuncOp funcOp = endOp->getParentOfType<handshake::FuncOp>();
-    assert(funcOp && "end must be child of handshake function");
-    size_t numResults = funcOp.getFunctionType().getNumResults();
-    for (size_t idx = 0, e = numResults; idx < e; ++idx)
-      outputs.push_back(endOp.getDefaultResultName(idx));
-  }
-}
-
-void PortNameGenerator::inferDefault(Operation *op) {
-  llvm::TypeSwitch<Operation *, void>(op)
-      .Case<arith::AddFOp, arith::AddIOp, arith::AndIOp, arith::CmpIOp,
-            arith::CmpFOp, arith::DivFOp, arith::DivSIOp, arith::DivUIOp,
-            arith::MaximumFOp, arith::MinimumFOp, arith::MulFOp, arith::MulIOp,
-            arith::OrIOp, arith::ShLIOp, arith::ShRSIOp, arith::ShRUIOp,
-            arith::SubFOp, arith::SubIOp, arith::XOrIOp>([&](auto) {
-        infer(
-            op, [](unsigned idx) { return idx == 0 ? "lhs" : "rhs"; },
-            [](unsigned idx) { return "result"; });
-      })
-      .Case<arith::ExtSIOp, arith::ExtUIOp, arith::NegFOp, arith::TruncIOp>(
-          [&](auto) {
-            infer(
-                op, [](unsigned idx) { return "ins"; },
-                [](unsigned idx) { return "outs"; });
-          })
-      .Case<arith::SelectOp>([&](auto) {
-        infer(
-            op,
-            [](unsigned idx) {
-              if (idx == 0)
-                return "condition";
-              if (idx == 1)
-                return "trueValue";
-              return "falseValue";
-            },
-            [](unsigned idx) { return "result"; });
-      })
-      .Default([&](auto) {
-        infer(
-            op, [](unsigned idx) { return "in" + std::to_string(idx); },
-            [](unsigned idx) { return "out" + std::to_string(idx); });
-      });
-}
-
-void PortNameGenerator::inferFromNamedOpInterface(
-    handshake::NamedIOInterface namedIO) {
-  auto inF = [&](unsigned idx) { return namedIO.getOperandName(idx); };
-  auto outF = [&](unsigned idx) { return namedIO.getResultName(idx); };
-  infer(namedIO, inF, outF);
-}
-
-void PortNameGenerator::inferFromFuncOp(handshake::FuncOp funcOp) {
-  llvm::transform(funcOp.getArgNames(), std::back_inserter(inputs),
-                  [](Attribute arg) { return cast<StringAttr>(arg).str(); });
-  llvm::transform(funcOp.getResNames(), std::back_inserter(outputs),
-                  [](Attribute res) { return cast<StringAttr>(res).str(); });
-}
 
 namespace {
 /// Aggregates information to convert a Handshake memory interface into a
@@ -246,7 +130,7 @@ struct MemLoweringState {
   FuncMemoryPorts ports;
   /// Generates and stores the interface's port names before starting the
   /// conversion, when those are still queryable.
-  PortNameGenerator portNames;
+  hw::PortNameGenerator portNames;
   /// Backedges to the containing module's `hw::OutputOp` operation, which
   /// must be set, in order, with the memory interface's results that connect
   /// to the top-level module IO.
@@ -294,7 +178,7 @@ struct ModuleLoweringState {
   llvm::DenseMap<handshake::MemoryOpInterface, MemLoweringState> memInterfaces;
   /// Generates and stores the end operations's port names before starting the
   /// conversion, when those are still queryable.
-  PortNameGenerator endPorts;
+  hw::PortNameGenerator endPorts;
   /// Backedges to the containing module's `hw::OutputOp` operation, which
   /// must be set, in order, with the results of the `hw::InstanceOp`
   /// operation to which the `handshake::EndOp` operation was converted to.
@@ -1014,7 +898,7 @@ static void addMemIO(ModuleBuilder &modBuilder, handshake::FuncOp funcOp,
 hw::ModulePortInfo getFuncPortInfo(handshake::FuncOp funcOp,
                                    ModuleLoweringState &state) {
   ModuleBuilder modBuilder(funcOp.getContext());
-  PortNameGenerator portNames(funcOp);
+  hw::PortNameGenerator portNames(funcOp);
 
   // Add all function outputs to the module
   for (auto [idx, res] : llvm::enumerate(funcOp.getResultTypes()))
@@ -1177,7 +1061,7 @@ LogicalResult ConvertExternalFunc::matchAndRewrite(
 
   StringAttr name = rewriter.getStringAttr(funcOp.getName());
   ModuleBuilder modBuilder(funcOp.getContext());
-  PortNameGenerator portNames(funcOp);
+  hw::PortNameGenerator portNames(funcOp);
 
   // Add all function outputs to the module
   for (auto [idx, res] : llvm::enumerate(funcOp.getResultTypes()))
@@ -1397,7 +1281,7 @@ template <typename T>
 LogicalResult ConvertToHWInstance<T>::matchAndRewrite(
     T op, OpAdaptor adaptor, ConversionPatternRewriter &rewriter) const {
   HWConverter converter(this->getContext());
-  PortNameGenerator portNames(op);
+  hw::PortNameGenerator portNames(op);
 
   // Add all operation operands to the inputs
   for (auto [idx, oprd] : llvm::enumerate(adaptor.getOperands()))
