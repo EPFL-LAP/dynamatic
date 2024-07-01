3b5dabdcb9874bef00b180732201d302438bcbe7
Jiahui Xu <jxu@ethz.ch>
2024-07-01 23:18:01 +0200

    [experimental] fixing cond branch
    The impl of cond branch is quite confusing
    and also incorrect (true/false ports are swapped).

--- experimental/data/vhdl/handshake/dataless/cond_br.vhd
+++ experimental/data/vhdl/handshake/dataless/cond_br.vhd
@@ -23,27 +23,24 @@ end entity;
 
 architecture arch of cond_br_dataless is
   signal joinValid, brReady : std_logic;
-  signal out_array          : std_logic_vector(1 downto 0);
-  signal out2_array         : std_logic_vector(1 downto 0);
 begin
-  data_ready      <= out_array(0);
-  condition_ready <= out_array(1);
-  trueOut_valid   <= out2_array(0);
-  falseOut_valid  <= out2_array(1);
 
   j : entity work.join(arch) generic map(2)
     port map(
-    (data_valid, condition_valid),
-      brReady,
-      joinValid,
-      out_array);
+    ins_valid(0) => data_valid,
+    ins_valid(1) => condition_valid,
+    outs_ready => brReady,
+    outs_valid => joinValid,
+    ins_ready(0) => data_ready, 
+    ins_ready(1) => condition_ready);
 
   cond_brp : entity work.branch_simple(arch)
     port map(
-      condition(0),
-      joinValid,
-      (trueOut_ready,
-      falseOut_ready),
-      out2_array,
-      brReady);
+      condition => condition(0),
+      valid => joinValid,
+      trueOut_ready => trueOut_ready,
+      falseOut_ready => falseOut_ready,
+      trueOut_valid => trueOut_valid,
+      falseOut_valid => falseOut_valid,
+      ins_ready => brReady);
 end architecture;
--- experimental/data/vhdl/support/branch_simple.vhd
+++ experimental/data/vhdl/support/branch_simple.vhd
@@ -4,18 +4,20 @@ use ieee.std_logic_1164.all;
 entity branch_simple is
   port (
     -- inputs
-    condition  : in std_logic;
-    valid      : in std_logic;
-    outs_ready : in std_logic_vector(1 downto 0);
+    condition      : in std_logic;
+    valid          : in std_logic;
+    trueOut_ready  : in std_logic;
+    falseOut_ready : in std_logic;
     -- outputs
-    ins_valid : out std_logic_vector(1 downto 0);
+    trueOut_valid  : out std_logic;
+    falseOut_valid : out std_logic;
     ins_ready : out std_logic
   );
 end branch_simple;
 
 architecture arch of branch_simple is
 begin
-  ins_valid(1) <= (not condition) and valid;
-  ins_valid(0) <= condition and valid;
-  ins_ready    <= (outs_ready(1) and not condition) or (outs_ready(0) and condition);
+  falseOut_valid <= (not condition) and valid;
+  trueOut_valid  <= condition and valid;
+  ins_ready      <= (falseOut_ready and not condition) or (trueOut_ready and condition);
 end arch;
