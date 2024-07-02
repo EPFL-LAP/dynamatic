2c0e582b1b5dd12ac9b34581c2514f4935ccc280
Lucas Ramirez <lucas.rami@proton.me>
2024-07-02 02:27:02 +0200

    [VHDL] Fix and clean-up many VHDL components
    This commit is a debugging/clean up pass over VHDL components in which
    bugs were introduced when adapting them to the new backend. The fixes
    have been confirmed to make simulation successful on all of our
    integration tests (with simple buffers), pending upcoming changes to
    the testbench generation logic.

--- experimental/data/vhdl/handshake/mux.vhd
+++ experimental/data/vhdl/handshake/mux.vhd
@@ -28,50 +28,48 @@ entity mux is
 end entity;
 
 architecture arch of mux is
-
-  signal tehb_data_in : std_logic_vector(DATA_WIDTH - 1 downto 0);
-  signal tehb_pvalid  : std_logic;
-  signal tehb_ready   : std_logic;
-
+  signal tehb_ins                       : std_logic_vector(DATA_WIDTH - 1 downto 0);
+  signal tehb_ins_valid, tehb_ins_ready : std_logic;
 begin
-  process (ins, ins_valid, outs_ready, index, tehb_ready)
-    variable tmp_data_out  : unsigned(DATA_WIDTH - 1 downto 0);
-    variable tmp_valid_out : std_logic;
+  process (ins, ins_valid, outs_ready, index, index_valid, tehb_ins_ready)
+    variable selectedData                   : std_logic_vector(DATA_WIDTH - 1 downto 0);
+    variable selectedData_valid, indexEqual : std_logic;
   begin
-    tmp_data_out  := unsigned(ins(0));
-    tmp_valid_out := '0';
-    for I in SIZE - 1 downto 0 loop
-      if (unsigned(index) = to_unsigned(I, index'length) and index_valid = '1' and ins_valid(I) = '1') then
-        tmp_data_out  := unsigned(ins(I));
-        tmp_valid_out := '1';
-      end if;
+    selectedData       := ins(0);
+    selectedData_valid := '0';
 
-      if ((unsigned(index) = to_unsigned(I, index'length) and index_valid = '1' and tehb_ready = '1' and ins_valid(I) = '1') or ins_valid(I) = '0') then
-        ins_ready(I) <= '1';
+    for i in SIZE - 1 downto 0 loop
+      if unsigned(index) = to_unsigned(i, index'length) then
+        indexEqual := '1';
       else
-        ins_ready(I) <= '0';
+        indexEqual := '0';
       end if;
+      if indexEqual and index_valid and ins_valid(i) then
+        selectedData       := ins(i);
+        selectedData_valid := '1';
+      end if;
+      ins_ready(i) <= (indexEqual and index_valid and ins_valid(i) and tehb_ins_ready) or (not ins_valid(i));
     end loop;
 
-    if (index_valid = '0' or (tmp_valid_out = '1' and tehb_ready = '1')) then
-      index_ready <= '1';
-    else
-      index_ready <= '0';
-    end if;
-
-    tehb_data_in <= std_logic_vector(resize(tmp_data_out, DATA_WIDTH));
-    tehb_pvalid  <= tmp_valid_out;
+    index_ready    <= (not index_valid) or (selectedData_valid and tehb_ins_ready);
+    tehb_ins       <= selectedData;
+    tehb_ins_valid <= selectedData_valid;
   end process;
 
-  tehb : entity work.tehb(arch) generic map (DATA_WIDTH)
+  tehb : entity work.tehb(arch)
+    generic map(
+      DATA_WIDTH => DATA_WIDTH
+    )
     port map(
-      clk        => clk,
-      rst        => rst,
-      ins_valid  => tehb_pvalid,
-      outs_ready => outs_ready,
+      clk => clk,
+      rst => rst,
+      -- input channel
+      ins       => tehb_ins,
+      ins_valid => tehb_ins_valid,
+      ins_ready => tehb_ins_ready,
+      -- output channel
+      outs       => outs,
       outs_valid => outs_valid,
-      ins_ready  => tehb_ready,
-      ins        => tehb_data_in,
-      outs       => outs
+      outs_ready => outs_ready
     );
 end architecture;
--- experimental/data/vhdl/support/eager_fork_register_block.vhd
+++ experimental/data/vhdl/support/eager_fork_register_block.vhd
@@ -3,33 +3,31 @@ use ieee.std_logic_1164.all;
 
 entity eager_fork_register_block is
   port (
+    clk, rst : in std_logic;
     -- inputs
-    clk, reset            : in std_logic;
-    p_valid               : in std_logic;
-    n_stop                : in std_logic;
-    p_valid_and_fork_stop : in std_logic;
+    ins_valid    : in std_logic;
+    outs_ready   : in std_logic;
+    backpressure : in std_logic;
     -- outputs
-    valid      : out std_logic;
-    block_stop : out std_logic
+    outs_valid : out std_logic;
+    blockStop  : out std_logic
   );
 end entity;
 
 architecture arch of eager_fork_register_block is
-  signal reg_value, reg_in, block_stop_internal : std_logic;
+  signal transmitValue, keepValue : std_logic;
 begin
-  block_stop_internal <= n_stop and reg_value;
-  block_stop          <= block_stop_internal;
-  reg_in              <= block_stop_internal or (not p_valid_and_fork_stop);
-  valid               <= reg_value and p_valid;
+  keepValue <= (not outs_ready) and transmitValue;
 
-  reg : process (clk, reset, reg_in)
+  process (rst, clk)
   begin
-    if (reset = '1') then
-      reg_value <= '1'; --contains a "stop" signal - must be 1 at reset
-    else
-      if (rising_edge(clk)) then
-        reg_value <= reg_in;
-      end if;
+    if (rst = '1') then
+      transmitValue <= '1';
+    elsif (rising_edge(clk)) then
+      transmitValue <= keepValue or (not backpressure);
     end if;
-  end process reg;
+  end process;
+
+  outs_valid <= transmitValue and ins_valid;
+  blockStop  <= keepValue;
 end architecture;
