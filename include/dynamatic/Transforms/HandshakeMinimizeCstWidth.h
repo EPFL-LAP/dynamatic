e8c954ed761fbcaa0017257c3971e2a13ffdc8e9
Lucas Ramirez <lucas.rami@proton.me>
2024-07-26 12:33:14 +0200

    Delete `ConstantAnalysis` compile unit
    The compile unit was incorrectly treated as an MLIR analysis depsite it
    only containing helper methods related to Handshake constants. This
    moves these methods to the few passes using them, and deletes the
    compile unit.

--- include/dynamatic/Transforms/HandshakeMinimizeCstWidth.h
+++ include/dynamatic/Transforms/HandshakeMinimizeCstWidth.h
@@ -13,17 +13,20 @@
 #ifndef DYNAMATIC_TRANSFORMS_HANDSHAKEMINIMIZECSTWIDTH_H
 #define DYNAMATIC_TRANSFORMS_HANDSHAKEMINIMIZECSTWIDTH_H
 
+#include "dynamatic/Dialect/Handshake/HandshakeOps.h"
 #include "dynamatic/Support/DynamaticPass.h"
 #include "dynamatic/Support/LLVM.h"
-#include "mlir/Dialect/Arith/IR/Arith.h"
-#include "mlir/IR/DialectRegistry.h"
-#include "mlir/Pass/Pass.h"
 
 namespace dynamatic {
 
 #define GEN_PASS_DECL_HANDSHAKEMINIMIZECSTWIDTH
 #define GEN_PASS_DEF_HANDSHAKEMINIMIZECSTWIDTH
 #include "dynamatic/Transforms/Passes.h.inc"
+
+/// Attempts to find an equivalent constant to the one provided in the circuit,
+/// that is a constant with the same control and same value attribute. Returns
+/// such an equivalent constant if it finds one, nullptr otherwise.
+handshake::ConstantOp findEquivalentCst(handshake::ConstantOp cstOp);
 
 /// Computes the minimum required bitwidth needed to store the provided integer.
 unsigned computeRequiredBitwidth(APInt val);
