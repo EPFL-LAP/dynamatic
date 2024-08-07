ad842e14f3cf8b0f8f1226d80d5d0ecb2c9350fb
Liudmila-Paskonova <paskonovalv@gmail.com>
2024-08-07 17:01:27 +0200

    HandshakeSimulator] Major update
    - updated easy_sum example: now, given 4 int numbers, a, b, cst,c, it computes (a * b * cst + c) to show binary components with different latencies
    - Working simulator for the tests similar to easy_sum: sink, tehb and binary parametric integer components (AddI, MulI,... etc)
    - The simulator accepts values from cmd line
    - The results of the simulation are printed to stdio
    - Value -> OpOperand* refactoring for future components
    - Previos comments are taken into consideration

---
+++ integration-test/easy_sum/easy_sum.h
@@ -0,0 +1,11 @@
+#ifndef EASY_SUM_EASY_SUM_H
+#define EASY_SUM_EASY_SUM_H
+
+#define N 1000
+#define N_DEC 999
+
+typedef int in_int_t;
+
+int easy_sum(in_int_t a, in_int_t b, in_int_t cst, in_int_t c);
+
+#endif // EASY_SUM_EASY_SUM_H
