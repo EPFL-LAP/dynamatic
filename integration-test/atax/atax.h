2427c54e8028a058ab1cee436fa936ac43f0f848
Jiahui Xu <jxu@ethz.ch>
2024-08-20 18:20:32 +0200

    [integration-test] add atax benchmark

---
+++ integration-test/atax/atax.h
@@ -0,0 +1,8 @@
+typedef float in_int_t;
+typedef float out_int_t;
+typedef float inout_int_t;
+
+#define N 20
+
+void atax(in_int_t A[N][N], in_int_t x[N], inout_int_t y[N],
+          inout_int_t tmp[N]);
