44890671a1f705716596ed1941e48719ad293fa9
Jiahui Xu <jxu@ethz.ch>
2024-08-21 00:13:19 +0200

    [integration-test] cosmetic improvement

--- integration-test/atax/atax.h
+++ integration-test/atax/atax.h
@@ -1,3 +1,6 @@
+#ifndef ATAX_ATAX_FLOAT
+#define ATAX_ATAX_FLOAT
+
 typedef float in_int_t;
 typedef float out_int_t;
 typedef float inout_int_t;
@@ -6,3 +9,4 @@ typedef float inout_int_t;
 
 void atax(in_int_t A[N][N], in_int_t x[N], inout_int_t y[N],
           inout_int_t tmp[N]);
+#endif // ATAX_ATAX_FLOAT
