41ce1f12f6fd20a7bacd003a4d83a3a1276ccd59
Jiahui Xu <jxu@ethz.ch>
2024-08-21 20:52:31 +0200

    [integration] matching.c: new benchmark

---
+++ integration-test/matching/matching.h
@@ -0,0 +1,8 @@
+#ifndef MATCHING_MATCHING_H
+#define MATCHING_MATCHING_H
+typedef int in_int_t;
+typedef int inout_int_t;
+
+float matching(in_int_t edges[1000], inout_int_t vertices[1000],
+               in_int_t num_edges);
+#endif // MATCHING_MATCHING_H
