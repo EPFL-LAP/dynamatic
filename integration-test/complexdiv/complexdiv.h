fb7632071d06013b2aec827f733a6075091dac59
Jiahui Xu <jxu@ethz.ch>
2024-08-21 21:35:29 +0200

    [integration] complexdiv.c: new benchmark

---
+++ integration-test/complexdiv/complexdiv.h
@@ -0,0 +1,9 @@
+#ifndef COMPLEXDIV_COMPLEXDIV_H
+#define COMPLEXDIV_COMPLEXDIV_H
+
+typedef int in_int_t;
+typedef int out_int_t;
+void complexdiv(in_int_t a_i[1000], in_int_t a_r[1000], in_int_t b_i[1000],
+                in_int_t b_r[1000], out_int_t c_i[1000], out_int_t c_r[1000]);
+
+#endif // COMPLEXDIV_COMPLEXDIV_H
