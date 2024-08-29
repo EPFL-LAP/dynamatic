6a9cf46b9b04f5f4de2c9ec24f66a499e95217b8
Jiahui Xu <jxu@ethz.ch>
2024-08-29 15:54:26 +0200

    [Frontend] dynamatic/bin/clang++ -> clang++
    On non-Debian-based distros, the clang++ built from source cannot
    correctly find all the std headers. To make dynamatic compatible for
    more distros, we use the c++ compiler shipped by the distro itself.
    
    This change can be reverted if we can point clang++ to the needed header
    correctly.

--- tools/dynamatic/scripts/simulate.sh
+++ tools/dynamatic/scripts/simulate.sh
@@ -24,7 +24,7 @@ IO_GEN_BIN="$SIM_DIR/C_SRC/$KERNEL_NAME-io-gen"
 
 # Shortcuts
 HDL_DIR="$OUTPUT_DIR/hdl"
-CLANGXX_BIN="$DYNAMATIC_DIR/bin/clang++"
+CLANGXX_BIN="clang++"
 HLS_VERIFIER_BIN="$DYNAMATIC_DIR/bin/hls-verifier"
 RESOURCE_DIR="$DYNAMATIC_DIR/tools/hls-verifier/resources"
 
@@ -40,7 +40,7 @@ mkdir -p "$C_SRC_DIR" "$C_OUT_DIR" "$VHDL_SRC_DIR" "$VHDL_OUT_DIR" \
     "$INPUT_VECTORS_DIR" "$HLS_VERIFY_DIR"
 
 # Copy integration header to a C source subdirectory so that its relative path
-# is correct with respect to the source file containing the kernel 
+# is correct with respect to the source file containing the kernel
 DYN_INCLUDE_DIR="$C_SRC_DIR/dynamatic"
 mkdir "$DYN_INCLUDE_DIR"
 cp "$DYNAMATIC_DIR/include/dynamatic/Integration.h" "$DYN_INCLUDE_DIR"
@@ -50,7 +50,7 @@ cp "$HDL_DIR/"*.vhd "$VHDL_SRC_DIR" 2> /dev/null
 cp "$HDL_DIR/"*.v "$VHDL_SRC_DIR" 2> /dev/null
 
 # Copy sources to dedicated folder
-cp "$SRC_DIR/$KERNEL_NAME.c" "$C_SRC_DIR" 
+cp "$SRC_DIR/$KERNEL_NAME.c" "$C_SRC_DIR"
 cp "$SRC_DIR/$KERNEL_NAME.h" "$C_SRC_DIR"
 
 # Compile kernel's main function to generate inputs and golden outputs for the
@@ -58,11 +58,11 @@ cp "$SRC_DIR/$KERNEL_NAME.h" "$C_SRC_DIR"
 "$CLANGXX_BIN" "$SRC_DIR/$KERNEL_NAME.c" -D HLS_VERIFICATION \
   -DHLS_VERIFICATION_PATH="$SIM_DIR" -I "$DYNAMATIC_DIR/include" \
   -Wno-deprecated -o "$IO_GEN_BIN"
-exit_on_fail "Failed to build kernel for IO gen." "Built kernel for IO gen." 
+exit_on_fail "Failed to build kernel for IO gen." "Built kernel for IO gen."
 
 # Generate IO
 "$IO_GEN_BIN"
-exit_on_fail "Failed to run kernel for IO gen." "Ran kernel for IO gen." 
+exit_on_fail "Failed to run kernel for IO gen." "Ran kernel for IO gen."
 
 # Simulate and verify design
 echo_info "Launching Modelsim simulation"
