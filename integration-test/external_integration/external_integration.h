40361ef55441915e155f2b664cfbddcea86f42b4
Carmine Rizzi <crizzi@ethz.ch>
2024-09-06 17:00:13 +0200

    Added new test benchmark to test external integration

---
+++ integration-test/external_integration/external_integration.h
@@ -0,0 +1,11 @@
+#ifndef EXTERNAL_EXTERNAL_H
+#define EXTERNAL_EXTERNAL_H
+
+typedef int in_int_t;
+
+int port_A(int x);
+int port_B(int x);
+int external_integration(in_int_t a);
+
+#endif // EXTERNAL_EXTERNAL_H
+
