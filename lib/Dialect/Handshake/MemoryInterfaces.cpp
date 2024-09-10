a0b6aeead09a3840b9d0c36aaaf5780a0687e48a
Patrick Schuster <patrickschuster@protonmail.com>
2024-09-10 10:33:49 +0200

    set unused lsq depth param to max of store deth and load depth

--- lib/Dialect/Handshake/MemoryInterfaces.cpp
+++ lib/Dialect/Handshake/MemoryInterfaces.cpp
@@ -369,6 +369,7 @@ void LSQGenerationInfo::fromPorts(FuncMemoryPorts &ports) {
   if(lsqSizeAttr) {
     depthLoad = lsqSizeAttr.getLoadQueueSize();
     depthStore = lsqSizeAttr.getStoreQueueSize();
+    depth = std::max(depthLoad, depthStore); // TODO parameter should not be necessary in theory, but lsq generator would need to be changed to not use it
   } else {
     depthLoad = 16;
     depthStore = 16;
