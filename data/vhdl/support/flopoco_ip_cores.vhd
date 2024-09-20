1c036d31e86b2e6d69a9b5dc5bcd0da8012cdd3c
Jiahui Xu <jxu@ethz.ch>
2024-09-20 21:30:57 +0200

    Update 64 bit float comparator logic

--- data/vhdl/support/flopoco_ip_cores.vhd
+++ data/vhdl/support/flopoco_ip_cores.vhd
@@ -3884,3 +3884,389 @@ begin
    R <= sX & expR & fracR; 
 end architecture;
 
+
+--------------------------------------------------------------------------------
+--                       IntComparator_63_111_F500_uid5
+-- VHDL generated for Kintex7 @ 500MHz
+-- This operator is part of the Infinite Virtual Library FloPoCoLib
+-- All rights reserved 
+-- Authors: Florent de Dinechin (2021)
+--------------------------------------------------------------------------------
+-- Pipeline depth: 0 cycles
+-- Clock period (ns): 2
+-- Target frequency (MHz): 500
+-- Input signals: X Y
+-- Output signals: XltY XeqY XgtY
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+library std;
+use std.textio.all;
+library work;
+
+entity IntComparator_63_111_F500_uid5 is
+    port (clk : in std_logic;
+          X : in  std_logic_vector(62 downto 0);
+          Y : in  std_logic_vector(62 downto 0);
+          XltY : out  std_logic;
+          XeqY : out  std_logic;
+          XgtY : out  std_logic   );
+end entity;
+
+architecture arch of IntComparator_63_111_F500_uid5 is
+signal XltYi :  std_logic;
+signal XeqYi :  std_logic;
+signal XgtYi :  std_logic;
+begin
+   XltYi <= '1' when X<Y else '0';
+   XeqYi <= '1' when X=Y else '0';
+   XgtYi <= not (XeqYi or XltYi);
+   XltY <= XltYi;
+   XeqY <= XeqYi;
+   XgtY <= XgtYi;
+end architecture;
+
+--------------------------------------------------------------------------------
+--                        FPComparator_double_precision
+-- VHDL generated for Kintex7 @ 500MHz
+-- This operator is part of the Infinite Virtual Library FloPoCoLib
+-- All rights reserved 
+-- Authors: Florent de Dinechin (2021)
+--------------------------------------------------------------------------------
+-- Pipeline depth: 1 cycles
+-- Clock period (ns): 2
+-- Target frequency (MHz): 500
+-- Input signals: X Y
+-- Output signals: unordered XltY XeqY XgtY XleY XgeY
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+library std;
+use std.textio.all;
+library work;
+
+entity FPComparator_double_precision is
+    port (clk : in std_logic;
+          X : in  std_logic_vector(11+52+2 downto 0);
+          Y : in  std_logic_vector(11+52+2 downto 0);
+          unordered : out  std_logic;
+          XltY : out  std_logic;
+          XeqY : out  std_logic;
+          XgtY : out  std_logic;
+          XleY : out  std_logic;
+          XgeY : out  std_logic   );
+end entity;
+
+architecture arch of FPComparator_double_precision is
+   component IntComparator_63_111_F500_uid5 is
+      port ( clk : in std_logic;
+             X : in  std_logic_vector(62 downto 0);
+             Y : in  std_logic_vector(62 downto 0);
+             XltY : out  std_logic;
+             XeqY : out  std_logic;
+             XgtY : out  std_logic   );
+   end component;
+
+signal excX :  std_logic_vector(1 downto 0);
+signal excY :  std_logic_vector(1 downto 0);
+signal signX :  std_logic;
+signal signY :  std_logic;
+signal ExpFracX :  std_logic_vector(62 downto 0);
+signal ExpFracY :  std_logic_vector(62 downto 0);
+signal isZeroX, isZeroX_d1 :  std_logic;
+signal isZeroY, isZeroY_d1 :  std_logic;
+signal isNormalX, isNormalX_d1 :  std_logic;
+signal isNormalY, isNormalY_d1 :  std_logic;
+signal isInfX, isInfX_d1 :  std_logic;
+signal isInfY, isInfY_d1 :  std_logic;
+signal isNaNX :  std_logic;
+signal isNaNY :  std_logic;
+signal negativeX, negativeX_d1 :  std_logic;
+signal positiveX, positiveX_d1 :  std_logic;
+signal negativeY, negativeY_d1 :  std_logic;
+signal positiveY, positiveY_d1 :  std_logic;
+signal ExpFracXeqExpFracY :  std_logic;
+signal ExpFracXltExpFracY, ExpFracXltExpFracY_d1 :  std_logic;
+signal ExpFracXgtExpFracY, ExpFracXgtExpFracY_d1 :  std_logic;
+signal sameSign :  std_logic;
+signal XeqYNum, XeqYNum_d1 :  std_logic;
+signal XltYNum :  std_logic;
+signal XgtYNum :  std_logic;
+signal unorderedR, unorderedR_d1 :  std_logic;
+signal XltYR :  std_logic;
+signal XeqYR :  std_logic;
+signal XgtYR :  std_logic;
+signal XleYR :  std_logic;
+signal XgeYR :  std_logic;
+begin
+   process(clk)
+      begin
+         if clk'event and clk = '1' then
+            isZeroX_d1 <=  isZeroX;
+            isZeroY_d1 <=  isZeroY;
+            isNormalX_d1 <=  isNormalX;
+            isNormalY_d1 <=  isNormalY;
+            isInfX_d1 <=  isInfX;
+            isInfY_d1 <=  isInfY;
+            negativeX_d1 <=  negativeX;
+            positiveX_d1 <=  positiveX;
+            negativeY_d1 <=  negativeY;
+            positiveY_d1 <=  positiveY;
+            ExpFracXltExpFracY_d1 <=  ExpFracXltExpFracY;
+            ExpFracXgtExpFracY_d1 <=  ExpFracXgtExpFracY;
+            XeqYNum_d1 <=  XeqYNum;
+            unorderedR_d1 <=  unorderedR;
+         end if;
+      end process;
+   excX <= X(65 downto 64);
+   excY <= Y(65 downto 64);
+   signX <= X(63);
+   signY <= Y(63);
+   ExpFracX <= X(62 downto 0);
+   ExpFracY <= Y(62 downto 0);
+   -- Comparing (as integers) excX & ExpFracX with excY & ExpFracY would almost work 
+   --  since indeed inf>normal>0	
+   -- However we wouldn't capture infinity equality in cases when the infinities have different ExpFracs (who knows)...	 
+   -- Besides, expliciting the isXXX bits will help factoring code with a comparator for IEEE format (some day)
+   isZeroX <= '1' when excX="00" else '0' ;
+   isZeroY <= '1' when excY="00" else '0' ;
+   isNormalX <= '1' when excX="01" else '0' ;
+   isNormalY <= '1' when excY="01" else '0' ;
+   isInfX <= '1' when excX="10" else '0' ;
+   isInfY <= '1' when excY="10" else '0' ;
+   isNaNX <= '1' when excX="11" else '0' ;
+   isNaNY <= '1' when excY="11" else '0' ;
+   -- Just for readability of the formulae below
+   negativeX <= signX ;
+   positiveX <= not signX ;
+   negativeY <= signY ;
+   positiveY <= not signY ;
+   -- expfrac comparisons 
+   ExpFracCmp: IntComparator_63_111_F500_uid5
+      port map ( clk  => clk,
+                 X => ExpFracX,
+                 Y => ExpFracY,
+                 XeqY => ExpFracXeqExpFracY,
+                 XgtY => ExpFracXgtExpFracY,
+                 XltY => ExpFracXltExpFracY);
+   -- -- and now the logic
+   sameSign <= not (signX xor signY) ;
+   XeqYNum <= 
+         (isZeroX and isZeroY) -- explicitely stated by IEEE 754
+      or (isInfX and isInfY and sameSign)  -- bizarre but also explicitely stated by IEEE 754
+      or (isNormalX and isNormalY and sameSign and ExpFracXeqExpFracY)   ;
+   XltYNum <=     -- case enumeration on Y
+         ( (not (isInfX_d1 and positiveX_d1)) and (isInfY_d1  and positiveY_d1)) 
+      or ((negativeX_d1 or isZeroX_d1) and (isNormalY_d1 and positiveY_d1)) 
+      or ((negativeX_d1 and not isZeroX_d1) and isZeroY_d1) 
+      or (isNormalX_d1 and isNormalY_d1 and positiveX_d1 and positiveY_d1 and ExpFracXltExpFracY_d1)
+      or (isNormalX_d1 and isNormalY_d1 and negativeX_d1 and negativeY_d1 and ExpFracXgtExpFracY_d1)
+      or ((isInfX_d1 and negativeX_d1) and (not (isInfY_d1 and negativeY_d1)))    ;
+   XgtYNum <=     -- case enumeration on X
+         ( (not (isInfY_d1 and positiveY_d1)) and (isInfX_d1  and positiveX_d1)) 
+      or ((negativeY_d1 or isZeroY_d1) and (isNormalX_d1 and positiveX_d1)) 
+      or ((negativeY_d1 and not isZeroY_d1) and isZeroX_d1) 
+      or (isNormalX_d1 and isNormalY_d1 and positiveY_d1 and positiveX_d1 and ExpFracXgtExpFracY_d1)
+      or (isNormalX_d1 and isNormalY_d1 and negativeY_d1 and negativeX_d1 and ExpFracXltExpFracY_d1)
+      or ((isInfY_d1 and negativeY_d1) and (not (isInfX_d1 and negativeX_d1)))    ;
+   unorderedR <=  isNaNX or isNaNY;
+   XltYR <= XltYNum and not unorderedR_d1;
+   XeqYR <= XeqYNum and not unorderedR;
+   XgtYR <= XgtYNum and not unorderedR_d1;
+   XleYR <= (XeqYNum_d1 or XltYNum)	 and not unorderedR_d1;
+   XgeYR <= (XeqYNum_d1 or XgtYNum)	 and not unorderedR_d1;
+   unordered <= unorderedR;
+   XltY <= XltYR;
+   XeqY <= XeqYR;
+   XgtY <= XgtYR;
+   XleY <= XleYR;
+   XgeY <= XgeYR;
+end architecture;
+
+--------------------------------------------------------------------------------
+--                       IntComparator_31_111_F500_uid9
+-- VHDL generated for Kintex7 @ 500MHz
+-- This operator is part of the Infinite Virtual Library FloPoCoLib
+-- All rights reserved 
+-- Authors: Florent de Dinechin (2021)
+--------------------------------------------------------------------------------
+-- Pipeline depth: 0 cycles
+-- Clock period (ns): 2
+-- Target frequency (MHz): 500
+-- Input signals: X Y
+-- Output signals: XltY XeqY XgtY
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+library std;
+use std.textio.all;
+library work;
+
+entity IntComparator_31_111_F500_uid9 is
+    port (clk : in std_logic;
+          X : in  std_logic_vector(30 downto 0);
+          Y : in  std_logic_vector(30 downto 0);
+          XltY : out  std_logic;
+          XeqY : out  std_logic;
+          XgtY : out  std_logic   );
+end entity;
+
+architecture arch of IntComparator_31_111_F500_uid9 is
+signal XltYi :  std_logic;
+signal XeqYi :  std_logic;
+signal XgtYi :  std_logic;
+begin
+   XltYi <= '1' when X<Y else '0';
+   XeqYi <= '1' when X=Y else '0';
+   XgtYi <= not (XeqYi or XltYi);
+   XltY <= XltYi;
+   XeqY <= XeqYi;
+   XgtY <= XgtYi;
+end architecture;
+
+--------------------------------------------------------------------------------
+--                        FPComparator_single_precision
+-- VHDL generated for Kintex7 @ 500MHz
+-- This operator is part of the Infinite Virtual Library FloPoCoLib
+-- All rights reserved 
+-- Authors: Florent de Dinechin (2021)
+--------------------------------------------------------------------------------
+-- Pipeline depth: 0 cycles
+-- Clock period (ns): 2
+-- Target frequency (MHz): 500
+-- Input signals: X Y
+-- Output signals: unordered XltY XeqY XgtY XleY XgeY
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+library std;
+use std.textio.all;
+library work;
+
+entity FPComparator_single_precision is
+    port (clk : in std_logic;
+          X : in  std_logic_vector(8+23+2 downto 0);
+          Y : in  std_logic_vector(8+23+2 downto 0);
+          unordered : out  std_logic;
+          XltY : out  std_logic;
+          XeqY : out  std_logic;
+          XgtY : out  std_logic;
+          XleY : out  std_logic;
+          XgeY : out  std_logic   );
+end entity;
+
+architecture arch of FPComparator_single_precision is
+   component IntComparator_31_111_F500_uid9 is
+      port ( clk : in std_logic;
+             X : in  std_logic_vector(30 downto 0);
+             Y : in  std_logic_vector(30 downto 0);
+             XltY : out  std_logic;
+             XeqY : out  std_logic;
+             XgtY : out  std_logic   );
+   end component;
+
+signal excX :  std_logic_vector(1 downto 0);
+signal excY :  std_logic_vector(1 downto 0);
+signal signX :  std_logic;
+signal signY :  std_logic;
+signal ExpFracX :  std_logic_vector(30 downto 0);
+signal ExpFracY :  std_logic_vector(30 downto 0);
+signal isZeroX :  std_logic;
+signal isZeroY :  std_logic;
+signal isNormalX :  std_logic;
+signal isNormalY :  std_logic;
+signal isInfX :  std_logic;
+signal isInfY :  std_logic;
+signal isNaNX :  std_logic;
+signal isNaNY :  std_logic;
+signal negativeX :  std_logic;
+signal positiveX :  std_logic;
+signal negativeY :  std_logic;
+signal positiveY :  std_logic;
+signal ExpFracXeqExpFracY :  std_logic;
+signal ExpFracXltExpFracY :  std_logic;
+signal ExpFracXgtExpFracY :  std_logic;
+signal sameSign :  std_logic;
+signal XeqYNum :  std_logic;
+signal XltYNum :  std_logic;
+signal XgtYNum :  std_logic;
+signal unorderedR :  std_logic;
+signal XltYR :  std_logic;
+signal XeqYR :  std_logic;
+signal XgtYR :  std_logic;
+signal XleYR :  std_logic;
+signal XgeYR :  std_logic;
+begin
+   excX <= X(33 downto 32);
+   excY <= Y(33 downto 32);
+   signX <= X(31);
+   signY <= Y(31);
+   ExpFracX <= X(30 downto 0);
+   ExpFracY <= Y(30 downto 0);
+   -- Comparing (as integers) excX & ExpFracX with excY & ExpFracY would almost work 
+   --  since indeed inf>normal>0	
+   -- However we wouldn't capture infinity equality in cases when the infinities have different ExpFracs (who knows)...	 
+   -- Besides, expliciting the isXXX bits will help factoring code with a comparator for IEEE format (some day)
+   isZeroX <= '1' when excX="00" else '0' ;
+   isZeroY <= '1' when excY="00" else '0' ;
+   isNormalX <= '1' when excX="01" else '0' ;
+   isNormalY <= '1' when excY="01" else '0' ;
+   isInfX <= '1' when excX="10" else '0' ;
+   isInfY <= '1' when excY="10" else '0' ;
+   isNaNX <= '1' when excX="11" else '0' ;
+   isNaNY <= '1' when excY="11" else '0' ;
+   -- Just for readability of the formulae below
+   negativeX <= signX ;
+   positiveX <= not signX ;
+   negativeY <= signY ;
+   positiveY <= not signY ;
+   -- expfrac comparisons 
+   ExpFracCmp: IntComparator_31_111_F500_uid9
+      port map ( clk  => clk,
+                 X => ExpFracX,
+                 Y => ExpFracY,
+                 XeqY => ExpFracXeqExpFracY,
+                 XgtY => ExpFracXgtExpFracY,
+                 XltY => ExpFracXltExpFracY);
+   -- -- and now the logic
+   sameSign <= not (signX xor signY) ;
+   XeqYNum <= 
+         (isZeroX and isZeroY) -- explicitely stated by IEEE 754
+      or (isInfX and isInfY and sameSign)  -- bizarre but also explicitely stated by IEEE 754
+      or (isNormalX and isNormalY and sameSign and ExpFracXeqExpFracY)   ;
+   XltYNum <=     -- case enumeration on Y
+         ( (not (isInfX and positiveX)) and (isInfY  and positiveY)) 
+      or ((negativeX or isZeroX) and (isNormalY and positiveY)) 
+      or ((negativeX and not isZeroX) and isZeroY) 
+      or (isNormalX and isNormalY and positiveX and positiveY and ExpFracXltExpFracY)
+      or (isNormalX and isNormalY and negativeX and negativeY and ExpFracXgtExpFracY)
+      or ((isInfX and negativeX) and (not (isInfY and negativeY)))    ;
+   XgtYNum <=     -- case enumeration on X
+         ( (not (isInfY and positiveY)) and (isInfX  and positiveX)) 
+      or ((negativeY or isZeroY) and (isNormalX and positiveX)) 
+      or ((negativeY and not isZeroY) and isZeroX) 
+      or (isNormalX and isNormalY and positiveY and positiveX and ExpFracXgtExpFracY)
+      or (isNormalX and isNormalY and negativeY and negativeX and ExpFracXltExpFracY)
+      or ((isInfY and negativeY) and (not (isInfX and negativeX)))    ;
+   unorderedR <=  isNaNX or isNaNY;
+   XltYR <= XltYNum and not unorderedR;
+   XeqYR <= XeqYNum and not unorderedR;
+   XgtYR <= XgtYNum and not unorderedR;
+   XleYR <= (XeqYNum or XltYNum)	 and not unorderedR;
+   XgeYR <= (XeqYNum or XgtYNum)	 and not unorderedR;
+   unordered <= unorderedR;
+   XltY <= XltYR;
+   XeqY <= XeqYR;
+   XgtY <= XgtYR;
+   XleY <= XleYR;
+   XgeY <= XgeYR;
+end architecture;
+
