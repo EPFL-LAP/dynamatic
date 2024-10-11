3bd0d035b27f504a49b8e36f475c090118bc9b44
shundroid <shundroid@users.noreply.github.com>
2024-10-11 10:11:17 +0200

    added buffers to some vhdl files

--- experimental/data/vhdl/speculation/speculating_branch_wrapper.vhd
+++ experimental/data/vhdl/speculation/speculating_branch_wrapper.vhd
@@ -1,6 +1,69 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+
+entity tehb_for_speculating_branch is
+  generic (
+    DATA_TYPE : integer
+  );
+  port (
+    clk, rst : in std_logic;
+    -- input channel
+    ins       : in  std_logic_vector(DATA_TYPE - 1 downto 0);
+    ins_valid : in  std_logic;
+    ins_spec_tag : in std_logic;
+    ins_ready : out std_logic;
+    -- output channel
+    outs       : out std_logic_vector(DATA_TYPE - 1 downto 0);
+    outs_valid : out std_logic;
+    outs_spec_tag : out std_logic;
+    outs_ready : in  std_logic
+  );
+end entity;
+
+architecture arch of tehb_for_speculating_branch is
+  signal outs_inner : std_logic_vector(DATA_TYPE - 1 downto 0);
+  signal outs_valid_inner : std_logic;
+  signal outs_spec_tag_inner : std_logic;
+  signal outs_ready_inner : std_logic;
+begin
+  tehb0 : entity work.tehb_with_tag(arch)
+    generic map(
+      DATA_TYPE => DATA_TYPE
+    )
+    port map(
+      clk => clk,
+      rst => rst,
+      ins => ins,
+      ins_valid => ins_valid,
+      ins_spec_tag => ins_spec_tag,
+      ins_ready => ins_ready,
+      outs => outs_inner,
+      outs_valid => outs_valid_inner,
+      outs_spec_tag => outs_spec_tag_inner,
+      outs_ready => outs_ready_inner
+    );
+  tehb1 : entity work.tehb_with_tag(arch)
+    generic map(
+      DATA_TYPE => DATA_TYPE
+    )
+    port map(
+      clk => clk,
+      rst => rst,
+      ins => outs_inner,
+      ins_valid => outs_valid_inner,
+      ins_spec_tag => outs_spec_tag_inner,
+      ins_ready => outs_ready_inner,
+      outs => outs,
+      outs_valid => outs_valid,
+      outs_spec_tag => outs_spec_tag,
+      outs_ready => outs_ready
+    );
+end architecture;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 use work.types.all;
 
 entity speculating_branch_wrapper_with_tag is
@@ -44,14 +107,55 @@ architecture arch of speculating_branch_wrapper_with_tag is
   signal specOutArray : data_array(1 downto 0)(DATA_TYPE - 1 downto 0);
   signal validArray : std_logic_vector(1 downto 0);
   signal nReadyArray : std_logic_vector(1 downto 0);
+
+  signal data_inner : std_logic_vector(DATA_TYPE - 1 downto 0);
+  signal data_valid_inner : std_logic;
+  signal data_spec_tag_inner : std_logic;
+  signal data_ready_inner : std_logic;
+  signal spec_tag_data_inner : std_logic_vector(SPEC_TAG_DATA_TYPE - 1 downto 0);
+  signal spec_tag_data_valid_inner : std_logic;
+  signal spec_tag_data_spec_tag_inner : std_logic;
+  signal spec_tag_data_ready_inner : std_logic;
 begin
+  tehb0 : entity work.tehb_for_speculating_branch(arch)
+    generic map(
+      DATA_TYPE => DATA_TYPE
+    )
+    port map(
+      clk => clk,
+      rst => rst,
+      ins => data,
+      ins_valid => data_valid,
+      ins_spec_tag => data_spec_tag,
+      ins_ready => data_ready,
+      outs => data_inner,
+      outs_valid => data_valid_inner,
+      outs_spec_tag => data_spec_tag_inner,
+      outs_ready => data_ready_inner
+    );
+  tehb1 : entity work.tehb_for_speculating_branch(arch)
+    generic map(
+      DATA_TYPE => SPEC_TAG_DATA_TYPE
+    )
+    port map(
+      clk => clk,
+      rst => rst,
+      ins => spec_tag_data,
+      ins_valid => spec_tag_data_valid,
+      ins_spec_tag => spec_tag_data_spec_tag,
+      ins_ready => spec_tag_data_ready,
+      outs => spec_tag_data_inner,
+      outs_valid => spec_tag_data_valid_inner,
+      outs_spec_tag => spec_tag_data_spec_tag_inner,
+      outs_ready => spec_tag_data_ready_inner
+    );
   condition_inner(0)(0) <= '0';
   dataInArray(0) <= data;
-  specInArray(0)(0) <= data_spec_tag;
-  specInArray(1)(0) <= spec_tag_data_spec_tag;
-  pValidArray <= spec_tag_data_valid & data_valid;
-  spec_tag_data_ready <= readyArray(1);
-  data_ready <= readyArray(0);
+  specInArray(0)(0) <= data_spec_tag_inner;
+  specInArray(1)(0) <= spec_tag_data_spec_tag_inner;
+  pValidArray <= spec_tag_data_valid_inner & data_valid_inner;
+  spec_tag_data_ready_inner <= readyArray(1);
+  data_ready_inner <= readyArray(0);
   trueOut <= dataOutArray(0);
   falseOut <= dataOutArray(1);
   trueOut_spec_tag <= specOutArray(0)(0);
