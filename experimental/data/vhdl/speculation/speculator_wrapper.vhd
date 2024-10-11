75d174bcf76cf64379416b2027eb84d3be3fd557
shundroid <shundroid@users.noreply.github.com>
2024-10-11 10:11:17 +0200

    added buffers to some vhdl files

--- experimental/data/vhdl/speculation/speculator_wrapper.vhd
+++ experimental/data/vhdl/speculation/speculator_wrapper.vhd
@@ -1,6 +1,69 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+
+entity two_tehb_with_tag is
+  generic (
+    DATA_TYPE : integer
+  );
+  port (
+    clk, rst : in std_logic;
+    -- input channel
+    ins       : in  std_logic_vector(DATA_TYPE - 1 downto 0);
+    ins_valid : in  std_logic;
+    ins_spec_tag : in std_logic;
+    ins_ready : out std_logic;
+    -- output channel
+    outs       : out std_logic_vector(DATA_TYPE - 1 downto 0);
+    outs_valid : out std_logic;
+    outs_spec_tag : out std_logic;
+    outs_ready : in  std_logic
+  );
+end entity;
+
+architecture arch of two_tehb_with_tag is
+  signal outs_inner : std_logic_vector(DATA_TYPE - 1 downto 0);
+  signal outs_valid_inner : std_logic;
+  signal outs_spec_tag_inner : std_logic;
+  signal outs_ready_inner : std_logic;
+begin
+  tehb0 : entity work.tehb_with_tag(arch)
+    generic map(
+      DATA_TYPE => DATA_TYPE
+    )
+    port map(
+      clk => clk,
+      rst => rst,
+      ins => ins,
+      ins_valid => ins_valid,
+      ins_spec_tag => ins_spec_tag,
+      ins_ready => ins_ready,
+      outs => outs_inner,
+      outs_valid => outs_valid_inner,
+      outs_spec_tag => outs_spec_tag_inner,
+      outs_ready => outs_ready_inner
+    );
+  tehb1 : entity work.tehb_with_tag(arch)
+    generic map(
+      DATA_TYPE => DATA_TYPE
+    )
+    port map(
+      clk => clk,
+      rst => rst,
+      ins => outs_inner,
+      ins_valid => outs_valid_inner,
+      ins_spec_tag => outs_spec_tag_inner,
+      ins_ready => outs_ready_inner,
+      outs => outs,
+      outs_valid => outs_valid,
+      outs_spec_tag => outs_spec_tag,
+      outs_ready => outs_ready
+    );
+end architecture;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 use work.types.all;
 
 entity speculator_wrapper_with_tag is
@@ -62,6 +125,32 @@ architecture arch of speculator_wrapper_with_tag is
   signal scBranchOutArray: data_array(0 downto 0)(0 downto 0);
   signal nReadyArray: std_logic_vector(5 downto 0);
   signal validArray: std_logic_vector(5 downto 0);
+
+  signal outs_inner: std_logic_vector(DATA_TYPE - 1 downto 0);
+  signal outs_valid_inner: std_logic;
+  signal outs_spec_tag_inner: std_logic;
+  signal outs_ready_inner: std_logic;
+
+  signal ctrl_save_inner: std_logic_vector(0 downto 0);
+  signal ctrl_save_valid_inner: std_logic;
+  signal ctrl_save_spec_tag_inner: std_logic;
+  signal ctrl_save_ready_inner: std_logic;
+  signal ctrl_commit_inner: std_logic_vector(0 downto 0);
+  signal ctrl_commit_valid_inner: std_logic;
+  signal ctrl_commit_spec_tag_inner: std_logic;
+  signal ctrl_commit_ready_inner: std_logic;
+  signal ctrl_sc_save_inner: std_logic_vector(2 downto 0);
+  signal ctrl_sc_save_valid_inner: std_logic;
+  signal ctrl_sc_save_spec_tag_inner: std_logic;
+  signal ctrl_sc_save_ready_inner: std_logic;
+  signal ctrl_sc_commit_inner: std_logic_vector(2 downto 0);
+  signal ctrl_sc_commit_valid_inner: std_logic;
+  signal ctrl_sc_commit_spec_tag_inner: std_logic;
+  signal ctrl_sc_commit_ready_inner: std_logic;
+  signal ctrl_sc_branch_inner: std_logic_vector(0 downto 0);
+  signal ctrl_sc_branch_valid_inner: std_logic;
+  signal ctrl_sc_branch_spec_tag_inner: std_logic;
+  signal ctrl_sc_branch_ready_inner: std_logic;
 begin
   dataInArray(0) <= ins;
   specInArray(0)(0) <= ins_spec_tag;
@@ -69,31 +158,31 @@ begin
   pValidArray <= enable_valid & ins_valid;
   enable_ready <= readyArray(1);
   ins_ready <= readyArray(0);
-  outs <= dataOutArray(0);
-  outs_spec_tag <= specOutArray(0)(0);
-  ctrl_save <= saveOutArray(0);
-  ctrl_commit <= commitOutArray(0);
-  ctrl_sc_save <= scOut0Array(0);
-  ctrl_sc_commit <= scOut1Array(0);
-  ctrl_sc_branch <= scBranchOutArray(0);
-  nReadyArray(5) <= ctrl_save_ready;
-  nReadyArray(4) <= ctrl_commit_ready;
-  nReadyArray(3) <= ctrl_sc_commit_ready;
-  nReadyArray(2) <= ctrl_sc_save_ready;
-  nReadyArray(1) <= ctrl_sc_branch_ready;
-  nReadyArray(0) <= outs_ready;
-  ctrl_save_valid <= validArray(5);
-  ctrl_commit_valid <= validArray(4);
-  ctrl_sc_commit_valid <= validArray(3);
-  ctrl_sc_save_valid <= validArray(2);
-  ctrl_sc_branch_valid <= validArray(1);
-  outs_valid <= validArray(0);
+  outs_inner <= dataOutArray(0);
+  outs_spec_tag_inner <= specOutArray(0)(0);
+  ctrl_save_inner <= saveOutArray(0);
+  ctrl_commit_inner <= commitOutArray(0);
+  ctrl_sc_save_inner <= scOut0Array(0);
+  ctrl_sc_commit_inner <= scOut1Array(0);
+  ctrl_sc_branch_inner <= scBranchOutArray(0);
+  nReadyArray(5) <= ctrl_save_ready_inner;
+  nReadyArray(4) <= ctrl_commit_ready_inner;
+  nReadyArray(3) <= ctrl_sc_commit_ready_inner;
+  nReadyArray(2) <= ctrl_sc_save_ready_inner;
+  nReadyArray(1) <= ctrl_sc_branch_ready_inner;
+  nReadyArray(0) <= outs_ready_inner;
+  ctrl_save_valid_inner <= validArray(5);
+  ctrl_commit_valid_inner <= validArray(4);
+  ctrl_sc_commit_valid_inner <= validArray(3);
+  ctrl_sc_save_valid_inner <= validArray(2);
+  ctrl_sc_branch_valid_inner <= validArray(1);
+  outs_valid_inner <= validArray(0);
   -- temp
-  ctrl_save_spec_tag <= '0';
-  ctrl_commit_spec_tag <= '0';
-  ctrl_sc_save_spec_tag <= '0';
-  ctrl_sc_commit_spec_tag <= '0';
-  ctrl_sc_branch_spec_tag <= '0';
+  ctrl_save_spec_tag_inner <= '0';
+  ctrl_commit_spec_tag_inner <= '0';
+  ctrl_sc_save_spec_tag_inner <= '0';
+  ctrl_sc_commit_spec_tag_inner <= '0';
+  ctrl_sc_branch_spec_tag_inner <= '0';
   speculator : entity work.speculator(arch)
     generic map(
       DATA_SIZE_IN => DATA_TYPE,
@@ -117,5 +206,101 @@ begin
       scBranchOutArray => scBranchOutArray,
       validArray => validArray,
       nReadyArray => nReadyArray
+    );
+  tehb_outs : entity work.two_tehb_with_tag(arch)
+    generic map(
+      DATA_TYPE => DATA_TYPE
+    )
+    port map(
+      clk => clk,
+      rst => rst,
+      ins => outs_inner,
+      ins_valid => outs_valid_inner,
+      ins_spec_tag => outs_spec_tag_inner,
+      ins_ready => outs_ready_inner,
+      outs => outs,
+      outs_valid => outs_valid,
+      outs_spec_tag => outs_spec_tag,
+      outs_ready => outs_ready
+    );
+  tehb_ctrl_save : entity work.two_tehb_with_tag(arch)
+    generic map(
+      DATA_TYPE => 1
+    )
+    port map(
+      clk => clk,
+      rst => rst,
+      ins => ctrl_save_inner,
+      ins_valid => ctrl_save_valid_inner,
+      ins_spec_tag => ctrl_save_spec_tag_inner,
+      ins_ready => ctrl_save_ready_inner,
+      outs => ctrl_save,
+      outs_valid => ctrl_save_valid,
+      outs_spec_tag => ctrl_save_spec_tag,
+      outs_ready => ctrl_save_ready
+    );
+  tehb_ctrl_commit : entity work.two_tehb_with_tag(arch)
+    generic map(
+      DATA_TYPE => 1
+    )
+    port map(
+      clk => clk,
+      rst => rst,
+      ins => ctrl_commit_inner,
+      ins_valid => ctrl_commit_valid_inner,
+      ins_spec_tag => ctrl_commit_spec_tag_inner,
+      ins_ready => ctrl_commit_ready_inner,
+      outs => ctrl_commit,
+      outs_valid => ctrl_commit_valid,
+      outs_spec_tag => ctrl_commit_spec_tag,
+      outs_ready => ctrl_commit_ready
+    );
+  tehb_ctrl_sc_save : entity work.two_tehb_with_tag(arch)
+    generic map(
+      DATA_TYPE => 3
+    )
+    port map(
+      clk => clk,
+      rst => rst,
+      ins => ctrl_sc_save_inner,
+      ins_valid => ctrl_sc_save_valid_inner,
+      ins_spec_tag => ctrl_sc_save_spec_tag_inner,
+      ins_ready => ctrl_sc_save_ready_inner,
+      outs => ctrl_sc_save,
+      outs_valid => ctrl_sc_save_valid,
+      outs_spec_tag => ctrl_sc_save_spec_tag,
+      outs_ready => ctrl_sc_save_ready
+    );
+  tehb_ctrl_sc_commit : entity work.two_tehb_with_tag(arch)
+    generic map(
+      DATA_TYPE => 3
+    )
+    port map(
+      clk => clk,
+      rst => rst,
+      ins => ctrl_sc_commit_inner,
+      ins_valid => ctrl_sc_commit_valid_inner,
+      ins_spec_tag => ctrl_sc_commit_spec_tag_inner,
+      ins_ready => ctrl_sc_commit_ready_inner,
+      outs => ctrl_sc_commit,
+      outs_valid => ctrl_sc_commit_valid,
+      outs_spec_tag => ctrl_sc_commit_spec_tag,
+      outs_ready => ctrl_sc_commit_ready
+    );
+  tehb_ctrl_sc_branch : entity work.two_tehb_with_tag(arch)
+    generic map(
+      DATA_TYPE => 1
+    )
+    port map(
+      clk => clk,
+      rst => rst,
+      ins => ctrl_sc_branch_inner,
+      ins_valid => ctrl_sc_branch_valid_inner,
+      ins_spec_tag => ctrl_sc_branch_spec_tag_inner,
+      ins_ready => ctrl_sc_branch_ready_inner,
+      outs => ctrl_sc_branch,
+      outs_valid => ctrl_sc_branch_valid,
+      outs_spec_tag => ctrl_sc_branch_spec_tag,
+      outs_ready => ctrl_sc_branch_ready
     );
 end architecture;
