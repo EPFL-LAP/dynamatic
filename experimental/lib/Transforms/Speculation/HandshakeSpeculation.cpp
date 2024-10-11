bcf8f45ecc9151213fe04676717c474d3b3383d0
shundroid <shundroid@users.noreply.github.com>
2024-10-11 10:12:07 +0200

    fixed a bug on speculator ctrl signal

--- experimental/lib/Transforms/Speculation/HandshakeSpeculation.cpp
+++ experimental/lib/Transforms/Speculation/HandshakeSpeculation.cpp
@@ -342,7 +342,7 @@ std::optional<Value> findControlInputToBB(Operation *op) {
   for (const BBArc &arc : bbToPredecessorArcs[bb]) {
     // Iterate the operands in the edge
     for (mlir::OpOperand *p : arc.edges) {
-      Value inEdge = p->get();
+      Value inEdge = p->getOwner()->getResult(0);
       // The control signal should be the only control input to the BB
       if (inEdge.getType().isa<handshake::ControlType>())
         return inEdge;
