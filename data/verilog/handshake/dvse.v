962df149539d022a6f4a5582a8e82aebce9710aa
QinYuan2000 <1553654728@qq.com>
2024-10-24 02:43:11 +0200

    DVSE Verilog

---
+++ data/verilog/handshake/dvse.v
@@ -0,0 +1,49 @@
+`timescale 1ns/1ps
+
+module dvse #(
+  parameter integer DEPTH = 4,
+  parameter integer DATA_TYPE = 32
+)(
+  input  clk,
+  input  rst,
+  // Input channel
+  input  [DATA_TYPE - 1 : 0] ins,
+  input  ins_valid,
+  output ins_ready,
+  // Output channel
+  output [DATA_TYPE - 1 : 0] outs,
+  output outs_valid,
+  input  outs_ready
+);
+
+  // Internal signals
+  wire regEn, inputReady;
+  reg [DATA_TYPE - 1 : 0] Memory [0 : DEPTH - 1];
+  
+  // Instance of dvse_dataless to manage handshaking
+  dvse_dataless #(
+    .DEPTH(DEPTH)
+  ) control (
+    .clk        (clk        ),
+    .rst        (rst        ),
+    .ins_valid  (ins_valid  ),
+    .ins_ready  (inputReady ),
+    .outs_valid (outs_valid ),
+    .outs_ready (outs_ready )
+  );
+  
+  integer i;
+  always @(posedge clk) begin
+    if (regEn) begin
+      for (i = DEPTH - 1; i > 0; i = i - 1) begin
+        Memory[i] <= Memory[i - 1];
+      end
+      Memory[0] <= ins;
+    end
+  end
+  
+  assign regEn     = inputReady;
+  assign ins_ready = inputReady;
+  assign outs      = Memory[DEPTH - 1];
+
+endmodule
