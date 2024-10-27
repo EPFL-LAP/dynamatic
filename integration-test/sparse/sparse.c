2516055c72e53e2ca847d89c256c82a39afc81da
shundroid <shundroid@users.noreply.github.com>
2024-10-27 14:52:03 +0100

    added integration test from Haoran's thesis

---
+++ integration-test/sparse/sparse.c
@@ -0,0 +1,29 @@
+#include "sparse.h"
+#include "dynamatic/Integration.h"
+#include "stdlib.h"
+
+float sparse(in_float_t a[N], in_float_t x[N]) {
+  float sum = 0.0f;
+  int i = 0;
+  float mul;
+  while (sum >= 0.0f) {
+    mul = a[i] * x[i];
+    sum += mul;
+    i++;
+  }
+  return sum;
+}
+
+int main(void) {
+  in_float_t a[N];
+  in_float_t x[N];
+
+  srand(13);
+  for (int j = 0; j < N; ++j) {
+    a[j] = 1.2f - j;
+    x[j] = j;
+  }
+
+  CALL_KERNEL(sparse, a, x);
+  return 0;
+}
