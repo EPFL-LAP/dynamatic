5b69fdc977bda33aa62df37a036aef4d25a91c4c
QinYuan2000 <qinyuan123123@gmail.com>
2024-10-27 20:32:05 +0100

    small update

--- tools/dynamatic/dynamatic.cpp
+++ tools/dynamatic/dynamatic.cpp
@@ -570,7 +570,7 @@ CommandResult Compile::execute(CommandArguments &args) {
 
   if (auto it = args.options.find(BUFFER_ALGORITHM); it != args.options.end()) {
     if (it->second == "on-merges" || it->second == "fpga20" ||
-        it->second == "fpl22") {
+        it->second == "fpl22" || it->second == "costaware") {
       buffers = it->second;
     } else {
       llvm::errs()
