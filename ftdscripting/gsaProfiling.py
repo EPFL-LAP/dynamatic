f2d678866817ba37866962e5b4e00ef2a898d336
Giacomo Sansone <giacomo.sansone@yahoo.com>
2024-11-01 14:17:06 +0100

    [FTD][RP] Computing mux token delays

--- ftdscripting/gsaProfiling.py
+++ ftdscripting/gsaProfiling.py
@@ -1,17 +1,51 @@
+import argparse
 import csv
 import re
 import subprocess
-from functools import cmp_to_key
 from io import StringIO
 from typing import List
 
-KERNEL_NAME = "if_loop_mul"
-LOOP_INFO_FILE_NAME = "ftdscripting/loopinfo.txt"
-PATH_TO_OUT = f"integration-test/{KERNEL_NAME}/out"
-PATH_TO_COMP = f"{PATH_TO_OUT}/comp"
-PATH_TO_SIM = f"{PATH_TO_OUT}/sim"
-PATH_TO_WLF = f"{PATH_TO_SIM}/HLS_VERIFY/vsim.wlf"
-PATH_TO_WLF2CSV = "./bin/wlf2csv"
+KERNEL_NAME = ""
+LOOP_INFO_FILE_NAME = ""
+PATH_TO_OUT = ""
+PATH_TO_COMP = ""
+PATH_TO_SIM = ""
+PATH_TO_WLF = ""
+PATH_TO_WLF2CSV = ""
+DST_COMP = "dst_component"
+DST_PORT = "dst_port"
+CC = "cycle"
+STATE = "state"
+
+
+def get_cli_arguments():
+    parser = argparse.ArgumentParser(
+        prog="GSA profiling",
+        description="""The script is in charge of profiling the execution 
+        of an elastic circuit, getting information about the timing of the 
+        multiplexers involved""",
+    )
+
+    parser.add_argument("--kernel_name", type=str, required=True)
+    return parser.parse_args()
+
+
+def set_paths(kernel_name: str):
+    global KERNEL_NAME
+    global LOOP_INFO_FILE_NAME
+    global PATH_TO_OUT
+    global PATH_TO_COMP
+    global PATH_TO_SIM
+    global PATH_TO_WLF
+    global PATH_TO_WLF2CSV
+    KERNEL_NAME = kernel_name
+
+    LOOP_INFO_FILE_NAME = "ftdscripting/loopinfo.txt"
+    PATH_TO_OUT = f"integration-test/{KERNEL_NAME}/out"
+    PATH_TO_COMP = f"{PATH_TO_OUT}/comp"
+    PATH_TO_SIM = f"{PATH_TO_OUT}/sim"
+    PATH_TO_WLF = f"{PATH_TO_SIM}/HLS_VERIFY/vsim.wlf"
+    PATH_TO_WLF2CSV = "./bin/wlf2csv"
 
 
 # Information for a GSA multiplxer, including its name, type, name of the innermost loop
@@ -98,31 +132,6 @@ class ForLoopNode:
             result = [node_mux_list + elem for elem in result]
 
         return result
-
-
-class MuxesReportTable:
-    def __init__(self, muxes_list: list[MuxInfo]):
-        self.muxes_list: list[MuxInfo] = muxes_list
-        self.table: list[str] = []
-        self.compact_table: dict[str, int] = {}
-
-    def add_row(self, row: str) -> None:
-        if len(row) != len(self.muxes_list):
-            raise Exception(
-                f"Cannot add row {row} to table of mux due to size inconsistency"
-            )
-        self.table.append(row)
-
-    def print(self) -> None:
-        print([mux.mux_name for mux in self.muxes_list], sep=", ")
-        print(self.compact_table)
-
-    def build_compact_table(self) -> None:
-        for row in self.table:
-            if row in self.compact_table.keys():
-                self.compact_table[row] += 1
-            else:
-                self.compact_table[row] = 1
 
 
 # Obtain all the paths from root to leaves for all the trees in the
@@ -221,27 +230,9 @@ def get_sim_transactions():
     # Remove usless rows from the csv
     final_csv = []
     for row in reader:
-        if (
-            ("mux" in row["dst_component"] and "0" in row["dst_port"])
-            or ("mux" in row["src_component"])
-        ) and row["state"] == "accept":
-            del row["src_port"]
-            del row["dst_port"]
+        if "mux" in row[DST_COMP]:
             final_csv.append(row)
 
-    def custom_sort(a, b):
-        if int(a["cycle"]) > int(b["cycle"]):
-            return 1
-        if int(a["cycle"]) < int(b["cycle"]):
-            return -1
-        else:
-            if "mux" in a["dst_component"]:
-                return -1
-            else:
-                return 1
-
-    custom_sort_key = cmp_to_key(custom_sort)
-    final_csv.sort(key=custom_sort_key)
     return final_csv
 
 
@@ -264,7 +255,7 @@ def remove_unknown_muxes(
             # Keep the mux only if exists one transaction having that same mux as destination component
             to_keep = False
             for row in transactions:
-                if row["dst_component"] == mux.mux_name:
+                if row[DST_COMP] == mux.mux_name:
                     to_keep = True
                     break
 
@@ -275,77 +266,92 @@ def remove_unknown_muxes(
     return new_muxes_to_correlate
 
 
-def get_table_muxes_conditions(
-    muxes_to_correlate: list[MuxInfo], transactions
-) -> MuxesReportTable:
+# Get the delay of each token at each input of each mux in the circuit
+def analyze_delay_muxes(muxes_to_correlate, transactions):
 
-    LAST_USED = "last_index_used"
-    NEXT_TO_USE = "last_index_received"
-    muxes_last_value = {}
-    table = MuxesReportTable(muxes_to_correlate)
-    last_cycle = 0
+    # Some constants
+    LTY = "last_type"
+    LTM = "last_time"
+    NTR = "number_of_transactions"
+    DLY = "delay"
+    TRN = "transfer"
+    STL = "stall"
+    ACC = "accept"
 
-    innermost_loop_index = muxes_to_correlate[-1].loop_depth
-    muxes_in_table = [mux.mux_name for mux in muxes_to_correlate]
-    muxes_in_innermost_loop = [
-        mux.mux_name
-        for mux in muxes_to_correlate
-        if mux.loop_depth == innermost_loop_index
-    ]
-    updated_muxes_innermost = 0
+    # A dictionary is used to store the information about each mux.
+    # The available keys are:
+    #   - `last_type`: the last type of transaction, it can be either "accept", "stall" or "transfer"
+    #   - `last_time`: time of the last transaction received
+    #   - `number_of_transactions`: how many transactions received per port
+    #   - `delay`: list of all the delays of tokens at each input port, kept in order
+    #
+    # The data are available for each of the three input port, with:
+    #   - `cond` being port 0;
+    #   - `false` being port 1;
+    #   - `true` being port 2;
+    transactions_per_mux = {}
+    all_muxes = []
+    for table in muxes_to_correlate:
+        for mux in table:
+            all_muxes.append(mux.mux_name)
+            # New dictionary for each mux
+            transactions_per_mux[mux.mux_name] = {
+                LTY: [ACC, ACC, ACC],
+                LTM: [0, 0, 0],
+                NTR: [0, 0, 0],
+                DLY: [[], [], []],
+            }
 
-    for tr in transactions:
-        mux_name = tr["dst_component"]
-        mux_name_src = tr["src_component"]
-        mux_data = int(float(tr["data"]) if tr["data"] != "" else 0)
-        mux_cycle = int(tr["cycle"])
-        trans_has_mux_dst = "mux" in mux_name
-        trans_has_mux_src = "mux" in mux_name_src
+    # For each transaction
+    for transaction in transactions:
+        # Get the mux
+        mux = transaction[DST_COMP]
 
-        if trans_has_mux_dst and mux_name not in muxes_in_table:
+        # Ignore if the mux is not to analyze
+        if mux not in all_muxes:
             continue
 
-        if trans_has_mux_src and mux_name_src not in muxes_in_table:
-            continue
+        # Get transaction type
+        tt = transaction[STATE]
+        # Get transaction port
+        port = int(transaction[DST_PORT])
+        # Get the type of the last transaction on the same port
+        last_tt = transactions_per_mux[mux][LTY][port]
 
-        if mux_cycle == 0:
-            if trans_has_mux_dst:
-                muxes_last_value[mux_name] = {
-                    LAST_USED: -1,
-                    NEXT_TO_USE: mux_data,
-                }
-            continue
+        # Consider a token to be waiting if the previous transaction was `accept` and the current one
+        # is either `stall` or `transfer`
+        if (last_tt == ACC and tt == STL) or (last_tt == ACC and tt == TRN):
+            transactions_per_mux[mux][LTM][port] = int(transaction[CC])
+            transactions_per_mux[mux][LTY][port] = tt
 
-        if (
-            updated_muxes_innermost == len(muxes_in_innermost_loop)
-            and last_cycle != mux_cycle
-        ):
-            new_row = ""
-            for mux in muxes_in_table:
-                new_row += str(muxes_last_value[mux][LAST_USED])
-            table.add_row(new_row)
-            updated_muxes_innermost = 0
+        # Consider a token to be delivered if the previous transaction was either `stall` or `transfer`
+        # and the current one is `accept`
+        if (last_tt == TRN and tt == ACC) or (last_tt == STL and tt == ACC):
+            # One more transaction
+            transactions_per_mux[mux][NTR][port] += 1
+            # Compute the delay
+            delay = int(transaction[CC]) - int(transactions_per_mux[mux][LTM][port])
+            # Add the delay to the list of delays, and mark the last transaction on that port as accepted
+            transactions_per_mux[mux][DLY][port].append(delay)
+            transactions_per_mux[mux][LTY][port] = ACC
 
-        if trans_has_mux_dst:
-            muxes_last_value[mux_name][NEXT_TO_USE] = mux_data
-
-        if trans_has_mux_src:
-            muxes_last_value[mux_name_src][LAST_USED] = muxes_last_value[mux_name_src][
-                NEXT_TO_USE
-            ]
-            muxes_last_value[mux_name_src][NEXT_TO_USE] = -1
-
-            if mux_name_src in muxes_in_innermost_loop:
-                updated_muxes_innermost += 1
-
-        last_cycle = mux_cycle
-
-    table.build_compact_table()
-
-    return table
+    # TEMP: print data
+    for mux, data in transactions_per_mux.items():
+        print("=================================")
+        print(mux)
+        print(f"\tNumber Transasctions on condition input: {data[NTR][0]};")
+        print(f"\tAVG delay: {sum(data[DLY][0])/data[NTR][0]}")
+        print(f"\tNumber Transasctions on input 0: {data[NTR][1]};")
+        print(f"\tAVG delay: {sum(data[DLY][1])/data[NTR][1]}")
+        print(f"\tNumber Transasctions on input 1: {data[NTR][2]};")
+        print(f"\tAVG delay: {sum(data[DLY][2])/data[NTR][2]}")
+        print("=================================")
 
 
 def main():
+
+    cli_args = get_cli_arguments()
+    set_paths(cli_args.kernel_name)
 
     # Read file with loop information
     mux_list = parse_mux_info(LOOP_INFO_FILE_NAME)
@@ -361,21 +367,11 @@ def main():
 
     # Extract the muxes transactions
     transactions = get_sim_transactions()
-    for transaction in transactions:
-        print(transaction)
 
     # Remove muxes which are not present in the simulation
     muxes_to_correlate = remove_unknown_muxes(muxes_to_correlate, transactions)
 
-    # Obtain tables with values of the conditions of each set of muxes to correlate
-    tables_muxes_conditions = []
-    for muxes_list in muxes_to_correlate:
-        tables_muxes_conditions.append(
-            get_table_muxes_conditions(muxes_list, transactions)
-        )
-
-    for table in tables_muxes_conditions:
-        table.print()
+    analyze_delay_muxes(muxes_to_correlate, transactions)
 
     return
 
