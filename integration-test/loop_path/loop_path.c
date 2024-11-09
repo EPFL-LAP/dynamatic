ba1a1613fd55c5066593da4d466c8501f3938bd8
shundroid <shundroid@users.noreply.github.com>
2024-11-09 11:19:41 +0100

    updated speculation integration tests

--- integration-test/loop_path/loop_path.c
+++ integration-test/loop_path/loop_path.c
@@ -1,18 +1,19 @@
 #include "loop_path.h"
 #include "dynamatic/Integration.h"
+#include "stdbool.h"
 #include "stdlib.h"
 
 void loop_path(in_int_t a[N], in_int_t b[N], inout_int_t c[N]) {
-  int i;
+  int i = 0;
+  bool break_flag = false;
 
-  for (i = 0; i < N; i++) {
+  do {
     int temp = a[i] + b[i];
     int x = 5;
     c[i] = temp;
-    if ((1000 - temp) <= x * temp) {
-      break;
-    }
-  }
+    i++;
+    break_flag = (1000 - temp) <= x * temp;
+  } while (i < N && !break_flag);
 }
 
 int main(void) {
@@ -22,8 +23,8 @@ int main(void) {
 
   srand(13);
   for (int j = 0; j < N; ++j) {
-    a[j] = rand() % 10;
-    b[j] = rand() % 10;
+    a[j] = j;
+    b[j] = 1;
   }
 
   CALL_KERNEL(loop_path, a, b, c);
