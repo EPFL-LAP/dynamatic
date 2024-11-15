d8341c32202cce12831f94541dc03a821292b58f
shundroid <shundroid@users.noreply.github.com>
2024-11-15 17:42:59 +0100

    updated integration-tests

---
+++ integration-test/if_convert/if_convert.h
@@ -0,0 +1,12 @@
+#ifndef LOOP_PATH_H
+#define LOOP_PATH_H
+
+#define N 20
+#define N2 19
+
+typedef int in_int_t;
+typedef int inout_int_t;
+
+void if_convert(in_int_t a[N], inout_int_t b[N]);
+
+#endif
