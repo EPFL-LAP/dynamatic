e573ab8a4dd93cb3b19e077f8e5498dc2ec43863
Gioele Gottardo <giogot2000@gmail.com>
2024-11-19 15:31:42 +0100

    Add file

---
+++ experimental/include/experimental/Support/RigidificationSupport.h
@@ -0,0 +1,10 @@
+//===- RigidificationSupport.h --- FTD conversion support ---*- C++ -*-===//
+//
+// Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
