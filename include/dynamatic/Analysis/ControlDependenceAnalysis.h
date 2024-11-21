6a2fcbc16a156ef29ab314a60dbc7792c25f33cd
Giacomo Sansone <giacomo.sansone@yahoo.com>
2024-11-21 09:32:51 +0100

    [Analysis] Control Dependence Analysis (#186)
    The control dependence analysis is a further step into #177. 
    The analysis answers the following question: "Given a block `bb` in the
    CFG, which are the blocks whose control decisions impact the execution
    of `bb`"?
    The analysis can be applied on an IR with an underlying CFG structure.
    
    The implementation comes directly from its definition in the following
    paper:
    _Jeanne Ferrante, Karl J. Ottenstein, and Joe D. Warren. 1987. The
    program dependence graph and its use in optimization. ACM Trans.
    Program. Lang. Syst. 9, 3 (July 1987), 319–349.
    https://doi.org/10.1145/24039.24041_

---
+++ include/dynamatic/Analysis/ControlDependenceAnalysis.h
@@ -0,0 +1,78 @@
+//===- ControlDependenceAnalysis.h - Control dependence analyis *--- C++-*-===//
+//
+// Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Declares some utility functions useful to analyzing the control dependencies
+// between basic blocks of the CFG.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DYNAMATIC_ANALYSIS_CONTROLDEPENDENCEANALYSIS_H
+#define DYNAMATIC_ANALYSIS_CONTROLDEPENDENCEANALYSIS_H
+
+#include "dynamatic/Support/LLVM.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Pass/AnalysisManager.h"
+#include <optional>
+
+namespace dynamatic {
+
+/// Analysis to obtain the control dependence analysis of each block in the CFG
+class ControlDependenceAnalysis {
+
+public:
+  /// This structure contains all the control dependencies of a block, also
+  /// separating the forward ones
+  struct BlockControlDeps {
+    DenseSet<Block *> allControlDeps;
+    DenseSet<Block *> forwardControlDeps;
+  };
+
+  /// Store the dependencies of each block into a map
+  using BlockControlDepsMap = DenseMap<Block *, BlockControlDeps>;
+
+  // Constructor for the analysis pass
+  ControlDependenceAnalysis(Operation *operation);
+
+  // Given a BB, return all its control dependencies
+  std::optional<DenseSet<Block *>> getBlockAllControlDeps(Block *block) const;
+
+  // Given a BB, return all its forward control dependencies
+  std::optional<DenseSet<Block *>>
+  getBlockForwardControlDeps(Block *block) const;
+
+  // Return the map of the control dependencies as
+  BlockControlDepsMap getAllBlockDeps() const;
+
+  /// Invalidation hook to keep the analysis cached across passes. Returns true
+  /// if the analysis should be invalidated and fully reconstructed the next
+  /// time it is queried. This is useful in case the analysis is integrated in
+  /// the MLIR conversion pass
+  bool isInvalidated(const mlir::AnalysisManager::PreservedAnalyses &pa) const {
+    return !pa.isPreserved<ControlDependenceAnalysis>();
+  }
+
+  /// Print all the dependencies
+  void printAllBlocksDeps() const;
+
+private:
+  BlockControlDepsMap blocksControlDeps;
+
+  /// Get all the control dependencies of a block
+  void identifyAllControlDeps(mlir::func::FuncOp &funcOp);
+
+  // Get the forward dependencies only of a block
+  void identifyForwardControlDeps(mlir::func::FuncOp &funcOp);
+
+  /// Modify the control dependencies to include the dependencies of each
+  /// dependent block too
+  void addDepsOfDeps(mlir::func::FuncOp &funcOp);
+};
+
+} // namespace dynamatic
+
+#endif // DYNAMATIC_ANALYSIS_CONTROLDEPENDENCEANALYSIS_H
