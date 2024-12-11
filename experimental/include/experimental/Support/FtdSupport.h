5747c9e8f1e543d0be559a1982841cada59f9ee9
Giacomo Sansone <giacomo.sansone@yahoo.com>
2024-12-11 13:41:29 +0100

    [TYPO] Fixed typo

--- experimental/include/experimental/Support/FtdSupport.h
+++ experimental/include/experimental/Support/FtdSupport.h
@@ -1,4 +1,4 @@
-//===- FtdSupport.cpp - FTD conversion support -----------------*- C++ -*-===//
+//===- FtdSupport.h - FTD conversion support -------------------*- C++ -*-===//
 //
 // Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
