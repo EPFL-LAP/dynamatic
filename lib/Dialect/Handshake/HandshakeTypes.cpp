f68b0b5a1b002d99626da09079712df2f5a0d4a3
shundroid <shundroid@users.noreply.github.com>
2024-12-20 12:12:52 +0100

    removed ExtraSignal::Storage

--- lib/Dialect/Handshake/HandshakeTypes.cpp
+++ lib/Dialect/Handshake/HandshakeTypes.cpp
@@ -360,14 +360,8 @@ Type ChannelType::removeExtraSignal(const std::string &name) const {
 // ExtraSignal
 //===----------------------------------------------------------------------===//
 
-ExtraSignal::Storage::Storage(StringRef name, mlir::Type type, bool downstream)
-    : name(name), type(type), downstream(downstream) {}
-
 ExtraSignal::ExtraSignal(StringRef name, mlir::Type type, bool downstream)
     : name(name), type(type), downstream(downstream) {}
-
-ExtraSignal::ExtraSignal(const ExtraSignal::Storage &storage)
-    : name(storage.name), type(storage.type), downstream(storage.downstream) {}
 
 unsigned ExtraSignal::getBitWidth() const {
   return type.getIntOrFloatBitWidth();
