2159c7692085a2998a45b68ad5951803f183bc20
Eugen Bosnjak <eugen.bosnjak@epfl.ch>
2025-01-29 19:15:31 +0100

    Fixed Python style to follow PEP8

--- tools/integration/run_integration.py
+++ tools/integration/run_integration.py
@@ -1,3 +1,6 @@
+"""
+Script for running Dynamatic integration tests.
+"""
 import argparse
 import os
 import sys
@@ -5,32 +8,44 @@ import shutil
 import subprocess
 import re
 
+
 class CLIHandler:
-  def __init__(self):
-    self.parser = argparse.ArgumentParser()
-    self.add_arguments()
-
-  def add_arguments(self):
-    self.parser.add_argument(
-      "-l", "--list", 
-      help="Path to a text file with names of tests to run. If not given, runs all tests."
-    )
-    self.parser.add_argument(
-      "-i", "--ignore",
-      help="Path to a text file with names of tests to ignore (i.e. skip)."
-    )
-    self.parser.add_argument(
-      "-t", "--timeout",
-      help="Custom timeout value for a single test. If not given, 500 seconds is used."
-    )
-
-  def parse_args(self, args=None):
     """
-    Parses the command-line arguments.
-    :param args: List of arguments to parse (default: sys.argv).
-    :return: Parsed arguments namespace.
+    This class parses the script's command line arguments.
     """
-    return self.parser.parse_args(args)
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.add_arguments()
+
+    def add_arguments(self):
+        """
+        Configures all available command line arguments.
+        """
+        self.parser.add_argument(
+            "-l", "--list",
+            help="Path to a text file with names of tests to run. If not given, runs all tests."
+        )
+        self.parser.add_argument(
+            "-i", "--ignore",
+            help="Path to a text file with names of tests to ignore (i.e. skip)."
+        )
+        self.parser.add_argument(
+            "-t", "--timeout",
+            help="Custom timeout value for a single test. If not given, 500 seconds is used."
+        )
+
+    def parse_args(self, args=None):
+        """
+        Parses the command-line arguments.
+
+        Arguments:
+        `args` -- List of arguments to parse (default: sys.argv)
+
+        Returns: Parsed arguments namespace
+        """
+        return self.parser.parse_args(args)
+
 
 INTEGRATION_FOLDER = "../../integration-test/"
 SCRIPT_CONTENT = """set-dynamatic-path ../..
@@ -47,151 +62,237 @@ DYN_FILE = "../../build/run_test.dyn"
 DYNAMATIC_COMMAND = "../../bin/dynamatic --exit-on-failure --run {script_path}"
 
 # Class to have different colors while writing in terminal
-class bcolors:
-  HEADER = "\033[95m"
-  OKBLUE = "\033[94m"
-  OKCYAN = "\033[96m"
-  OKGREEN = "\033[92m"
-  WARNING = "\033[93m"
-  FAIL = "\033[91m"
-  ENDC = "\033[0m"
-  BOLD = "\033[1m"
-  UNDERLINE = "\033[4m"
 
-# Print with a certain color
+
+class TermColors:
+    """
+    Contains ANSI color escape sequences for colored terminal output.
+    """
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
 def color_print(string: str, color: str):
-  print(f"{color}{string}{bcolors.ENDC}")
+    """
+    Prints colored text to stdout using ANSI escape sequences.
+
+    Arguments:
+    `string` -- The text to be outputted
+    `color` -- ANSI escape seq. for the desired color; use TermColors constants
+    """
+    print(f"{color}{string}{TermColors.ENDC}")
+
 
 def find_files_ext(directory, ext):
-  c_files = []
-  for root, dirs, files in os.walk(directory):
-    for file in files:
-      if file.endswith(ext) and root.find("/out/") == -1:
-        c_files.append(os.path.join(root, file))
-  return c_files
+    """
+    Finds all files with a given extension in a directory.
+
+    Arguments:
+    `directory` -- Path of the directory
+    `ext` -- File extension to be searched for
+
+    Returns: List of paths to all files in directory with extension ext
+    """
+    c_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith(ext) and root.find("/out/") == -1:
+                c_files.append(os.path.join(root, file))
+    return c_files
+
 
 def write_string_to_file(content, file_path):
-  with open(file_path, "w") as file:
-    file.write(content)
+    """
+    Opens a text file and writes a string to it.
+
+    Arguments:
+    `content` -- String to be written
+    `file_path` -- Path of the file to be written to
+    """
+    with open(file_path, "w", encoding="UTF-8") as file:
+        file.write(content)
 
 
 def read_file(file_path):
-  with open(file_path, "r") as file:
-    return file.read()
+    """
+    Opens a text file and reads all of its contents.
+
+    Arguments:
+    `file_path` -- Path to the file to be read from
+
+    Returns: File contents as a string
+    """
+    with open(file_path, "r", encoding="UTF-8") as file:
+        return file.read()
+
 
 def run_command_with_timeout(command, timeout=500):
-  try:
-    proc = subprocess.run(
-      command,
-      shell=True,
-      timeout=timeout,
-      check=True,
-      stdout=subprocess.PIPE,  # Suppress standard output
-      stderr=subprocess.PIPE,  # Suppress standard error
-    )
-    return 0
-  except subprocess.CalledProcessError:
-    # Test returned non-zero
-    return 1
-  except subprocess.TimeoutExpired:
-    # Test timed out
-    return 2
+    """
+    Runs a command with a time limit for execution.
 
-def replace_filename_with(file_path, to_add):
-  directory = os.path.dirname(file_path)
-  new_path = os.path.join(directory, to_add)
-  return new_path
+    Arguments:
+    `command` -- Shell command to be executed
+    `timeout` -- Execution time limit
+
+    Returns: An integer representing the execution result
+    0 -- if process completed without errors
+    1 -- if process returned a non-zero exit code
+    2 -- if process execution time exceeded timeout
+    """
+    try:
+        subprocess.run(
+            command,
+            shell=True,
+            timeout=timeout,
+            check=True,
+            stdout=subprocess.PIPE,  # Suppress standard output
+            stderr=subprocess.PIPE,  # Suppress standard error
+        )
+        return 0
+    except subprocess.CalledProcessError:
+        # Test returned non-zero
+        return 1
+    except subprocess.TimeoutExpired:
+        # Test timed out
+        return 2
+
+
+def replace_filename_with(file_path, suffix):
+    """
+    Replaces the file name in the path with another string.
+
+    Arguments:
+    `file_path` -- A path to a file
+    `suffix` -- String to replace the file name with
+
+    Returns: A new path with the file name replaced with suffix.
+
+    Example: If called with "/home/user/a.txt" and "b.py", returns "/home/user/b.py"
+    """
+    directory = os.path.dirname(file_path)
+    new_path = os.path.join(directory, suffix)
+    return new_path
+
 
 def get_sim_time(log_path):
-  # Regular expression to match the desired line format
-  pattern = re.compile(r"Time: (\d+) ns")
-    
-  last_time = None
+    """
+    Returns the simulation time from a Modelsim log file.
 
-  # Open the file and read it in reverse line order
-  with open(log_path, "r") as file:
-    for line in reversed(file.readlines()):
-      match = pattern.search(line)
-      if match:
-        last_time = int(match.group(1))
-        break
+    Arguments:
+    `log_path` -- Path to the Modelsim simulation log.
 
-  if last_time is not None:
-    return last_time
-  else:
-    raise ValueError("Log file does not contain simulation time!")
+    Returns:
+    The simulation duration found in the log file.
+    If this value is not present in the log file, throws a `ValueError`.
+    """
+
+    # Regular expression to match the desired line format
+    pattern = re.compile(r"Time: (\d+) ns")
+
+    last_time = None
+
+    # Open the file and read it in reverse line order
+    with open(log_path, "r", encoding="UTF-8") as file:
+        for line in reversed(file.readlines()):
+            match = pattern.search(line)
+            if match:
+                last_time = int(match.group(1))
+                break
+
+    if last_time is not None:
+        return last_time
+    else:
+        raise ValueError("Log file does not contain simulation time!")
+
 
 def main():
-  cli = CLIHandler()
-  args = cli.parse_args()  # Parse the CLI arguments
+    """
+    Entry point for the script.
+    """
+    cli = CLIHandler()
+    args = cli.parse_args()  # Parse the CLI arguments
 
-  c_files = []
-  if args.list:
-    test_names = read_file(args.list).strip().split("\n")
-    c_files = [os.path.join(INTEGRATION_FOLDER, name, f"{name}.c") for name in test_names]
-  else:
-    c_files = find_files_ext(INTEGRATION_FOLDER, ".c")
-
-  ignored_tests = []
-  if args.ignore:
-    test_names = read_file(args.ignore).strip().split("\n")
-    ignored_tests = [os.path.join(INTEGRATION_FOLDER, name, f"{name}.c") for name in test_names]
-
-  print("========= INTEGRATION TEST =========")
-
-  test_cnt = 0
-  passed_cnt = 0
-  ignored_cnt = 0
-
-  for c_file in c_files:
-    # Write .dyn script with appropriate source file name
-    write_string_to_file(SCRIPT_CONTENT.format(src_path=c_file), DYN_FILE)
-
-    # Get out dir name
-    out_dir = replace_filename_with(c_file, "out")
-
-    # Check if test is supposed to be ignored
-    if c_file in ignored_tests:
-      ignored_cnt += 1
-      color_print(f"[IGNORED] {c_file}", bcolors.OKGREEN)
-      continue
-
-    # One more test to handle
-    test_cnt += 1
-
-    # Remove previous out directory
-    if os.path.isdir(out_dir):
-      shutil.rmtree(out_dir)
-    
-    # Run test and output result
-    if args.timeout:
-      result = run_command_with_timeout(DYNAMATIC_COMMAND.format(script_path=DYN_FILE), timeout=int(args.timeout))
+    c_files = []
+    if args.list:
+        test_names = read_file(args.list).strip().split("\n")
+        c_files = [os.path.join(INTEGRATION_FOLDER, name, f"{
+                                name}.c") for name in test_names]
     else:
-      result = run_command_with_timeout(DYNAMATIC_COMMAND.format(script_path=DYN_FILE))
-    
-    if result == 0:
-      sim_log_path = os.path.join(out_dir, "sim", "report.txt")
-      try:
-        sim_time = get_sim_time(sim_log_path)
-        color_print(f"[PASS] {c_file} (simulation duration: {round(sim_time / 4)} cycles)", bcolors.OKGREEN)
-      except ValueError:
-        # This should never happen
-        color_print(f"[PASS] {c_file} (simulation duration: NOT FOUND)", bcolors.OKGREEN)
-      
-      passed_cnt += 1
-    elif result == 1:
-      color_print(f"[FAIL] {c_file}", bcolors.FAIL)
+        c_files = find_files_ext(INTEGRATION_FOLDER, ".c")
+
+    ignored_tests = []
+    if args.ignore:
+        test_names = read_file(args.ignore).strip().split("\n")
+        ignored_tests = [os.path.join(INTEGRATION_FOLDER, name, f"{
+                                      name}.c") for name in test_names]
+
+    print("========= INTEGRATION TEST =========")
+
+    test_cnt = 0
+    passed_cnt = 0
+    ignored_cnt = 0
+
+    for c_file in c_files:
+        # Write .dyn script with appropriate source file name
+        write_string_to_file(SCRIPT_CONTENT.format(src_path=c_file), DYN_FILE)
+
+        # Get out dir name
+        out_dir = replace_filename_with(c_file, "out")
+
+        # Check if test is supposed to be ignored
+        if c_file in ignored_tests:
+            ignored_cnt += 1
+            color_print(f"[IGNORED] {c_file}", TermColors.OKGREEN)
+            continue
+
+        # One more test to handle
+        test_cnt += 1
+
+        # Remove previous out directory
+        if os.path.isdir(out_dir):
+            shutil.rmtree(out_dir)
+
+        # Run test and output result
+        if args.timeout:
+            result = run_command_with_timeout(DYNAMATIC_COMMAND.format(
+                script_path=DYN_FILE), timeout=int(args.timeout))
+        else:
+            result = run_command_with_timeout(
+                DYNAMATIC_COMMAND.format(script_path=DYN_FILE))
+
+        if result == 0:
+            sim_log_path = os.path.join(out_dir, "sim", "report.txt")
+            try:
+                sim_time = get_sim_time(sim_log_path)
+                color_print(f"[PASS] {c_file} (simulation duration: {
+                            round(sim_time / 4)} cycles)", TermColors.OKGREEN)
+            except ValueError:
+                # This should never happen
+                color_print(
+                    f"[PASS] {c_file} (simulation duration: NOT FOUND)", TermColors.OKGREEN)
+
+            passed_cnt += 1
+        elif result == 1:
+            color_print(f"[FAIL] {c_file}", TermColors.FAIL)
+        else:
+            color_print(f"[TIMEOUT] {c_file}", TermColors.WARNING)
+
+        sys.stdout.flush()
+
+    print(f"** Integration testing finished: passed {passed_cnt}/{test_cnt} tests ({
+          100 * passed_cnt / test_cnt: .2f}% ), {ignored_cnt} ignored **")
+    if passed_cnt == test_cnt:
+        sys.exit(0)
     else:
-      color_print(f"[TIMEOUT] {c_file}", bcolors.WARNING)
-
-    sys.stdout.flush()
-
-  print(f"** Integration testing finished: passed {passed_cnt}/{test_cnt} tests ({100 * passed_cnt / test_cnt : .2f}% ), {ignored_cnt} ignored **")
-  if passed_cnt == test_cnt:
-    sys.exit(0)
-  else:
-    sys.exit(1)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-  main()
+    main()
