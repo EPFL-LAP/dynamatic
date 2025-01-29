1d194948196881c7f6ac2288b072aa0e2ef0a27d
Gioele Gottardo <giogot2000@gmail.com>
2025-01-29 11:12:40 +0100

    Improve utils functions

--- experimental/tools/unit-generators/smv/generators/support/utils.py
+++ experimental/tools/unit-generators/smv/generators/support/utils.py
@@ -2,26 +2,29 @@ import re
 
 
 def mlir_type_to_smv_type(type):
-  pattern = r"^!handshake\.channel<i(\d+)>$"
+  pattern = r"^!handshake\.channel<[u]?i(\d+)>$"
   match = re.match(pattern, type)
 
   if match:
-    type_width = int(match.group(1))
+    signed = not match.group(1).startswith("u")
+    type_width = int(match.group(2))
     if type_width == 1:
       return "boolean"
+    elif signed:
+      return f"signed word [{type_width}]"
     else:
       return f"unsigned word [{type_width}]"
   else:
     raise ValueError(f"Type {type} doesn't correspond to any SMV type")
 
 
-def smv_init_data_type(smv_type):
-  pattern = r"^unsigned word \[(\d+)\]$"
+def smv_format_constant(const_value, smv_type):
+  pattern = r"^[un]?signed word \[(\d+)\]$"
   match = re.match(pattern, smv_type)
 
   if smv_type == "boolean":
-    return "FALSE"
+    return "TRUE" if bool(const_value) else "FALSE"
   elif match:
-    return 0
+    return int(const_value)
   else:
     raise ValueError(f"Type {smv_type} isn't supported")
