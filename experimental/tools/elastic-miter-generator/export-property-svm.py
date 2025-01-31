6cdc66f5dc7005d3afec1f238b270263d1706736
Fabio Cappellini <fcappellini@ethz.ch>
2025-01-31 18:49:53 +0100

    Add property generation

--- experimental/tools/elastic-miter-generator/export-property-svm.py
+++ experimental/tools/elastic-miter-generator/export-property-svm.py
@@ -1,7 +1,40 @@
 import json
 from pprint import pprint
 
-with  open("tools/elastic-miter/out/comp/elastic-miter-config.json") as json_data:
-  d = json.load(json_data)
+# TODO add as argument
+with open("experimental/tools/elastic-miter-generator/out/comp/elastic-miter-config.json") as json_data:
+  config = json.load(json_data)
 
-pprint(d)
+# pprint(config)
+
+
+def create_eq_properties(eq_ops):
+  for op in eq_ops:
+    prop = f"AG ({op}.valid0 -> {op}.dataOut0)"
+    print('add_property -c -p "' + prop + '"')
+
+
+
+create_eq_properties(config["eq"])
+
+
+output_prop = ""
+# TODO clean this up or change json
+for buf in sum(config["output_buffers"], []):
+  output_prop += f"{buf}.num = 0 & "
+
+output_prop = output_prop[:-3]
+# print(output_prop)
+
+input_prop = ""
+for lhs_buffer, rhs_buffer in config["input_buffers"]:
+  input_prop += f"{lhs_buffer}.num = {rhs_buffer}.num & "
+
+input_prop = input_prop[:-3]
+# print(input_prop)
+
+
+final_buffer_prop = "AF AG " + input_prop + " & " + output_prop
+
+print('add_property -c -p "' + final_buffer_prop + '"')
+
--- experimental/tools/elastic-miter-generator/miter.sh
+++ experimental/tools/elastic-miter-generator/miter.sh
@@ -3,7 +3,7 @@ DYNAMATIC_DIR=.
 source "$DYNAMATIC_DIR/tools/dynamatic/scripts/utils.sh"
 DYNAMATIC_OPT_BIN="$DYNAMATIC_DIR/bin/dynamatic-opt"
 
-OUT_DIR="experimental/tools/elastic-miter-generator/out/"
+OUT_DIR="experimental/tools/elastic-miter-generator/out"
 F_HW="$OUT_DIR/hw.mlir"
 
 COMP_DIR="$OUT_DIR/comp"
@@ -13,4 +13,8 @@ DOT="$COMP_DIR/miter.dot"
 "bin/export-dot" $F_HANDSHAKE_MITER "--edge-style=spline" > $DOT
 dot -Tpng $DOT > $COMP_DIR/visual.png
 
-# python3 "../dot2smv/dot2smv" "./miter.dot"
+python3 "../dot2smv/dot2smv" $DOT
+
+python3 experimental/tools/elastic-miter-generator/export-property-svm.py > experimental/tools/elastic-miter-generator/out/comp/add_props.cmd
+
+nuXmv -source $COMP_DIR/prove.cmd
