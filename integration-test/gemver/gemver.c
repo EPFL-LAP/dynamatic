aafe422891f4fba657db2e22e57d95e9cbbb1a01
AyaElAkhras <ayatallah.elakhras@epfl.ch>
2025-01-31 20:36:25 +0100

    Made memory deps explicit in gemver

--- integration-test/gemver/gemver.c
+++ integration-test/gemver/gemver.c
@@ -15,27 +15,30 @@ int gemver(in_int_t alpha, in_int_t beta, in_int_t u1[N], in_int_t v1[N],
            in_int_t u2[N], in_int_t v2[N], in_int_t y[N], in_int_t z[N],
            inout_int_t a[N][N], inout_int_t w[N], inout_int_t x[N]) {
   for (unsigned i = 0; i < N; i++)
-    for (unsigned j = 0; j < N; j++)
+    for (unsigned j = 0; j < N; j++) {
       a[i][j] = a[i][j] + u1[i] * v1[j] + u2[i] * v2[j];
+    }
 
+  int tmp;
   for (unsigned i = 0; i < N; i++) {
-    int tmp = x[i];
+    tmp = x[i];
     for (unsigned j = 0; j < N; j++)
       tmp = tmp + beta * a[j][i] * y[j];
     x[i] = tmp;
   }
-  unsigned i;
-  for (i = 0; i < N; i++)
-    x[i] = x[i] + z[i];
+  unsigned ii;
+  for (ii = 0; ii < N; ii++) {
+    x[ii] = x[ii] + z[ii] + tmp;
+  }
 
-  for (i = 0; i < N; i++) {
+  for (unsigned i = 0; i < N; i++) {
     int tmp = w[i];
     for (unsigned j = 0; j < N; j++)
-      tmp = tmp + alpha * a[i][j] * x[j];
+      tmp = tmp + alpha * a[i][j] * x[j] + ii;
     w[i] = tmp;
   }
 
-  return y[i];
+  return y[0];
 }
 
 int main(void) {
