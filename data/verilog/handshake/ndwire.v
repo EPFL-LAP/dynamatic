97cc2b24efe22d7ec9cc04632f989dc7a074ebed
Fabio Cappellini <fcappellini@ethz.ch>
2025-02-03 12:18:18 +0100

    Adding dataless variants of NDWire

--- data/verilog/handshake/ndwire.v
+++ data/verilog/handshake/ndwire.v
@@ -14,40 +14,17 @@ module ndwire #(
   input  outs_ready
 );
 
-  typedef enum logic {SLEEPING, RUNNING} nd_state_t;
-
-  // This is the source of non-determism.
-  // E.g. it needs to be set to a primary input in a formal tool
-  nd_state_t nd_next_state;
-
-  nd_state_t state, next_state;
-
-
-  always @* begin
-    // If the wire is sleeping it can always switch to the running state.
-    // If (ins_valid && outs_ready) we either have a transaction
-    // and can freely choose the state again.
-    if(state == SLEEPING) begin
-      next_state = nd_next_state;
-    end else if (ins_valid && outs_ready) begin
-      next_state = nd_next_state;
-    end else begin
-      next_state = state;
-    end
-  end
-
-  // The initialization of the state is non-deterministic
-  always @(posedge clk or posedge rst) begin
-    if (rst) begin
-      state <= nd_next_state;
-    end else begin
-      state <= next_state;
-    end
-  end
-
-
-  assign ins_ready  = outs_ready && (state == RUNNING);
-  assign outs_valid = ins_valid  && (state == RUNNING);
+  ndwire_dataless #(
+    .SIZE(DATA_TYPE)
+  ) control (
+    .clk        (clk        ),
+    .rst        (rst        ),
+    .ins_valid  (ins_valid  ),
+    .ins_ready  (ins_ready  ),
+    .outs_valid (outs_valid ),
+    .outs_ready (outs_ready )
+  );
+  
   assign outs = ins;
 
 endmodule
--- data/vhdl/handshake/ndwire.vhd
+++ data/vhdl/handshake/ndwire.vhd
@@ -18,42 +18,18 @@ entity ndwire is
   );
 end entity;
 
-architecture arch of br is
-  type nd_state_t is (SLEEPING, RUNNING);
-  signal state, next_state : nd_state_t;
-
-  -- This is the source of non-determism.
-  -- E.g. it needs to be set to a primary input in a formal tool
-  signal nd_next_state : nd_state_t;
-
+architecture arch of ndwire is
 begin
+  control : entity work.ndwire_dataless
+    port map(
+      clk        => clk,
+      rst        => rst,
+      ins_valid  => ins_valid,
+      ins_ready  => ins_ready,
+      outs_valid => outs_valid,
+      outs_ready => outs_ready
+    );
 
-  process (clk, rst)
-  begin
-    -- The initialization of the state is non-deterministic
-    if rst = '1' then
-      state <= nd_next_state;
-    elsif rising_edge(clk) then
-      state <= next_state;
-    end if;
-  end process;
-
-  process (state, ins_valid, outs_ready)
-  begin
-    -- If the wire is sleeping it can always switch to the running state.
-    -- If (ins_valid and outs_ready) we either have a transaction
-    -- and can freely choose the state again.
-    if (state = SLEEPING) then
-      next_state <= nd_next_state;
-    elsif (ins_valid and outs_ready) then
-      next_state <= nd_next_state;
-    else
-      next_state <= state;
-    end if;
-  end process;
-
-  ins_ready <= outs_ready and (state = RUNNING);
-  outs_valid <= ins_valid and (state = RUNNING);
   outs <= ins;
 
 end architecture;
