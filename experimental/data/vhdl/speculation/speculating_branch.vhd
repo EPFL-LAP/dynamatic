c3933ef4be4a32f942e8e07a455faad186f2aa21
shundroid <shundroid@users.noreply.github.com>
2025-02-03 15:41:59 +0100

    updated speculating_branch

--- experimental/data/vhdl/speculation/speculating_branch.vhd
+++ experimental/data/vhdl/speculation/speculating_branch.vhd
@@ -2,65 +2,63 @@ library ieee;
 use ieee.std_logic_1164.all;
 use work.types.all;
 
-entity speculating_branch is 
-    generic(
-        INPUTS        : integer;  -- assumed always 2
-        OUTPUTS       : integer;
-        DATA_SIZE_IN  : integer;
-        DATA_SIZE_OUT : integer
-    );
-    port (
-        clk, rst     : in  std_logic;
-        condition    : in  data_array(0 downto 0)(0 downto 0);
-        dataInArray  : in  data_array(0 downto 0)(DATA_SIZE_IN - 1 downto 0);
-        specInArray  : in  data_array(1 downto 0)(0 downto 0);
-        pValidArray  : in  std_logic_vector(1 downto 0);  -- (cond, data)
-        readyArray   : out std_logic_vector(1 downto 0);  -- (cond, data)
-        
-        dataOutArray : out data_array(OUTPUTS - 1 downto 0)(DATA_SIZE_OUT - 1 downto 0);
-        specOutArray : out data_array(OUTPUTS - 1 downto 0)(0 downto 0);
-        validArray   : out std_logic_vector(1 downto 0);  -- (branch1, branch0)
-        nReadyArray  : in  std_logic_vector(1 downto 0)   -- (branch1, branch0)
-        
-        
-    );
-end speculating_branch;
+entity speculating_branch_with_tag is
+  generic(
+    DATA_TYPE : integer
+  );
+  port(
+    clk, rst : in std_logic;
+    -- data input channel
+    data       : in  std_logic_vector(DATA_TYPE - 1 downto 0);
+    data_valid : in  std_logic;
+    data_spec_tag : in std_logic;
+    data_ready : out std_logic;
+    -- spec_tag_data used for condition
+    spec_tag_data       : in  std_logic_vector(0 downto 0);
+    spec_tag_data_valid : in  std_logic;
+    spec_tag_data_spec_tag : in std_logic;
+    spec_tag_data_ready : out std_logic;
+    -- true output channel
+    trueOut       : out std_logic_vector(DATA_TYPE - 1 downto 0);
+    trueOut_valid : out std_logic;
+    trueOut_spec_tag : out std_logic;
+    trueOut_ready : in  std_logic;
+    -- false output channel
+    falseOut       : out std_logic_vector(DATA_TYPE - 1 downto 0);
+    falseOut_valid : out std_logic;
+    falseOut_spec_tag : out std_logic;
+    falseOut_ready : in  std_logic
+  );
+end speculating_branch_with_tag;
 
-architecture arch of speculating_branch is
-
-    signal joinValid, branchReady : std_logic;
-
-    signal spec_bit : data_array(0 downto 0)(0 downto 0);
-    signal specdataInArray  : data_array(0 downto 0)(DATA_SIZE_IN+1 - 1 downto 0);
-    signal specdataOutArray : data_array(OUTPUTS - 1 downto 0)(DATA_SIZE_OUT+1 - 1 downto 0);
-
+architecture arch of speculating_branch_with_tag is
+  signal cond_br_condition : std_logic_vector(0 downto 0);
 begin
 
-    -- Speculative bit logic
-    spec_bit(0)(0) <= specInArray(1)(0) or specInArray(0)(0);
-    specdataInArray(0) <= spec_bit(0) & dataInArray(0);
-    -----
-
-    j : entity work.join(arch) generic map(2)
-            port map(   (pValidArray(1), pValidArray(0)),
-                        branchReady,
-                        joinValid,
-                        readyArray);
-
-    br : entity work.branchSimple(arch)
-            port map(   specInArray(1)(0),
-                        joinValid,
-                        nReadyArray,
-                        validArray,
-                        branchReady);
-
-    -- Speculative bit logic
-    process(specdataInArray) begin
-        for I in 0 to OUTPUTS - 1 loop
-            dataOutArray(I)    <= specdataInArray(0)(DATA_SIZE_IN - 1 downto 0);
-            specOutArray(I)(0) <= specdataInArray(0)(DATA_SIZE_IN+1 - 1);
-        end loop;
-    end process;
-    -----
+  cond_br_condition(0) <= spec_tag_data_spec_tag;
+  cond_br : entity work.cond_br_with_tag
+    generic map (
+      DATA_TYPE => DATA_TYPE
+    )
+    port map (
+      clk => clk,
+      rst => rst,
+      data => data,
+      data_valid => data_valid,
+      data_spec_tag => data_spec_tag,
+      data_ready => data_ready,
+      condition => cond_br_condition,
+      condition_valid => spec_tag_data_valid,
+      condition_spec_tag => data_spec_tag,
+      condition_ready => spec_tag_data_ready,
+      trueOut => trueOut,
+      trueOut_valid => trueOut_valid,
+      trueOut_spec_tag => trueOut_spec_tag,
+      trueOut_ready => trueOut_ready,
+      falseOut => falseOut,
+      falseOut_valid => falseOut_valid,
+      falseOut_spec_tag => falseOut_spec_tag,
+      falseOut_ready => falseOut_ready
+    );
 
 end arch;
