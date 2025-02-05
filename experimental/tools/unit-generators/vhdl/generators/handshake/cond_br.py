00698c053ebe25370a351d35a9ec90797584dad4
shundroid <shundroid@users.noreply.github.com>
2025-02-05 17:16:40 +0100

    define two signal managers

--- experimental/tools/unit-generators/vhdl/generators/handshake/cond_br.py
+++ experimental/tools/unit-generators/vhdl/generators/handshake/cond_br.py
@@ -21,7 +21,10 @@ def generate_cond_br(name, params):
   data_type = VhdlScalarType(params["data_type"])
 
   if data_type.has_extra_signals():
-    return _generate_cond_br_signal_manager(name, data_type)
+    if data_type.is_channel():
+      return _generate_cond_br_signal_manager(name, data_type)
+    else:
+      return _generate_cond_br_signal_manager_dataless(name, data_type)
   elif data_type.is_channel():
     return _generate_cond_br_dataless(name)
   else:
@@ -129,43 +132,38 @@ end architecture;
 
   return header + dependencies + entity + architecture
 
+# todo: can be reusable among various unit generators
+extra_signal_logics = {
+  "spec": """
+  trueOut_spec <= data_spec or condition_spec;
+  falseOut_spec <= data_spec or condition_spec;
+""" # todo: generate_normal_spec_logics(["trueOut", "falseOut"], ["data", "condition"])
+}
+
 def _generate_cond_br_signal_manager(name, data_type):
-  dependencies = ""
-  if data_type.is_channel():
-    dependencies = _generate_cond_br(f"{name}_inner", data_type.bitwidth)
-  else:
-    dependencies = _generate_cond_br_dataless(f"{name}_inner")
+  dependencies = _generate_cond_br(f"{name}_inner", data_type.bitwidth)
 
   entity = f"""
 entity {name} is
   port (
     clk : in std_logic;
     rst : in std_logic;
-[POSSIBLE_DATA_PORTS]
 [EXTRA_SIGNAL_PORTS]
+    data : in std_logic_vector({data_type.bitwidth - 1} downto 0);
     data_valid : in std_logic;
     data_ready : out std_logic;
     condition : in std_logic_vector(0 downto 0);
     condition_valid : in std_logic;
     condition_ready : out std_logic
+    trueOut : out std_logic_vector({data_type.bitwidth - 1} downto 0);
     trueOut_valid : out std_logic;
     trueOut_ready : in std_logic;
+    falseOut : out std_logic_vector({data_type.bitwidth - 1} downto 0);
     falseOut_valid : out std_logic;
     falseOut_ready : in std_logic
   );
 end entity;
 """
-
-  # Add data ports if the data type is a channel
-  if data_type.is_channel():
-    entity = entity.replace("[POSSIBLE_DATA_PORTS]\n", f"""
-    -- data ports
-    data : in std_logic_vector({data_type.bitwidth - 1} downto 0);
-    trueOut : out std_logic_vector({data_type.bitwidth - 1} downto 0);
-    falseOut : out std_logic_vector({data_type.bitwidth - 1} downto 0);
-""")
-  else:
-    entity = entity.replace("[POSSIBLE_DATA_PORTS]\n", "")
 
   # Add extra signal ports
   entity = entity.replace("[EXTRA_SIGNAL_PORTS]\n",
@@ -173,14 +171,6 @@ end entity;
       ("data", "in"), ("condition", "in"),
       ("trueOut", "out"), ("falseOut", "out")
     ], data_type.extra_signals))
-
-  # todo: can be reusable among various unit generators
-  extra_signal_logics = {
-    "spec": """
-  trueOut_spec <= data_spec or condition_spec;
-  falseOut_spec <= data_spec or condition_spec;
-""" # todo: generate_normal_spec_logics(["trueOut", "falseOut"], ["data", "condition"])
-  }
 
   for name in data_type.extra_signals:
     if name not in extra_signal_logics:
@@ -197,7 +187,71 @@ begin
     port map(
       clk => clk,
       rst => rst,
-[POSSIBLE_DATA_PORTS]
+      data => data,
+      data_valid => data_valid,
+      data_ready => data_ready,
+      condition => condition,
+      condition_valid => condition_valid,
+      condition_ready => condition_ready,
+      trueOut => trueOut,
+      trueOut_valid => trueOut_valid,
+      trueOut_ready => trueOut_ready,
+      falseOut => falseOut,
+      falseOut_valid => falseOut_valid,
+      falseOut_ready => falseOut_ready
+    );
+"""
+
+  architecture.replace("[EXTRA_SIGNAL_LOGICS]", "\n".join([
+    extra_signal_logics[name] for name in data_type.extra_signals
+  ]))
+
+  return header + dependencies + entity + architecture
+
+def _generate_cond_br_signal_manager_dataless(name, data_type):
+  dependencies = _generate_cond_br_dataless(f"{name}_inner")
+
+  entity = f"""
+entity {name} is
+  port (
+    clk : in std_logic;
+    rst : in std_logic;
+[EXTRA_SIGNAL_PORTS]
+    data_valid : in std_logic;
+    data_ready : out std_logic;
+    condition : in std_logic_vector(0 downto 0);
+    condition_valid : in std_logic;
+    condition_ready : out std_logic
+    trueOut_valid : out std_logic;
+    trueOut_ready : in std_logic;
+    falseOut_valid : out std_logic;
+    falseOut_ready : in std_logic
+  );
+end entity;
+"""
+
+  # Add extra signal ports
+  entity = entity.replace("[EXTRA_SIGNAL_PORTS]\n",
+    generate_extra_signal_ports([
+      ("data", "in"), ("condition", "in"),
+      ("trueOut", "out"), ("falseOut", "out")
+    ], data_type.extra_signals))
+
+  for name in data_type.extra_signals:
+    if name not in extra_signal_logics:
+      raise ValueError(f"Extra signal {name} is not supported")
+
+  architecture = f"""
+architecture arch of {name} is
+begin
+
+  -- list of logics for supported extra signals
+[EXTRA_SIGNAL_LOGICS]
+
+  inner : entity work.{name}_inner(arch)
+    port map(
+      clk => clk,
+      rst => rst,
       data_valid => data_valid,
       data_ready => data_ready,
       condition => condition,
@@ -209,16 +263,6 @@ begin
       falseOut_ready => falseOut_ready
     );
 """
-
-  if data_type.is_channel():
-    architecture = architecture.replace("[POSSIBLE_DATA_PORTS]\n", f"""
-      -- data ports
-      data => data,
-      trueOut => trueOut,
-      falseOut => falseOut,
-""")
-  else:
-    architecture = architecture.replace("[POSSIBLE_DATA_PORTS]\n", "")
 
   architecture.replace("[EXTRA_SIGNAL_LOGICS]", "\n".join([
     extra_signal_logics[name] for name in data_type.extra_signals
