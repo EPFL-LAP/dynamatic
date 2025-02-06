ceb4dec1f4feff2924815ac2a1e27c562497b5ce
Gioele Gottardo <giogot2000@gmail.com>
2025-02-06 17:04:27 +0100

    Add nuXmv to tests

---
+++ experimental/test/tools/unit-generators/smv/prove.cmd
@@ -0,0 +1,19 @@
+set verbose_level 0;
+set pp_list cpp;
+set counter_examples 0;
+set dynamic_reorder 1;
+set on_failure_script_quits;
+set reorder_method sift;
+set enable_sexp2bdd_caching 0;
+set bdd_static_order_heuristics basic;
+set cone_of_influence;
+set use_coi_size_sorting 1;
+read_model -i tmp/main.smv;
+flatten_hierarchy;
+encode_variables;
+build_flat_model;
+build_model -f;
+#check_invar -s forward;
+#show_property -o property.rpt;
+time;
+quit
---
+++ experimental/test/tools/unit-generators/smv/testbench-generator.py
@@ -0,0 +1,20 @@
+import sys
+
+def generate_testbench(n):
+  print(f""" #include "./module.smv"
+
+MODULE main
+  VAR
+  {"\n  ".join([f"v{i} : boolean;" for i in range(n)])}
+  dut : test_module({", ".join([f"v{i}" for i in range(n)])});
+""")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python testbench-generator.py <input-signals>")
+    else:
+        try:
+            n_sig = int(sys.argv[1])
+            generate_testbench(n_sig)
+        except ValueError:
+            print("Please provide a valid integer for N.")
