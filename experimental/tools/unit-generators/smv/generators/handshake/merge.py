1466339535922722f76e339fc0d95b037104e48d
Gioele Gottardo <giogot2000@gmail.com>
2025-02-06 17:42:44 +0100

    Fix merge generation

--- experimental/tools/unit-generators/smv/generators/handshake/merge.py
+++ experimental/tools/unit-generators/smv/generators/handshake/merge.py
@@ -24,8 +24,8 @@ MODULE {name}({", ".join([f"ins_valid_{n}" for n in range(size)])}, outs_ready)
 
   // output
   DEFINE
-  {"\n  ".join([f"ins_ready_{n} := inner_merge.ins_ready{n};" for n in range(size)])}
-  outs_valid = inner_tehb.outs_valid;
+  {"\n  ".join([f"ins_ready_{n} := inner_merge.ins_ready_{n};" for n in range(size)])}
+  outs_valid := inner_tehb.outs_valid;
 
 {generate_merge_notehb(f"{name}__merge_notehb_dataless", size)}
 {generate_buffer(f"{name}__tehb_dataless", {"slots": 1, "timing": "R: 1", "data_type": "!handshake.control<>"})}
@@ -34,14 +34,14 @@ MODULE {name}({", ".join([f"ins_valid_{n}" for n in range(size)])}, outs_ready)
 
 def _generate_merge(name, size, data_type):
   return f"""
-MODULE {name}({", ".join([f"ins_{n}, ins_valid{n}" for n in range(size)])}, outs_ready)
+MODULE {name}({", ".join([f"ins_{n}, ins_valid_{n}" for n in range(size)])}, outs_ready)
   VAR
   inner_tehb : {name}__tehb(inner_merge.outs, inner_merge.outs_valid, outs_ready);
   inner_merge : {name}__merge_notehb({", ".join([f"ins_{n}" for n in range(size)])}, {", ".join([f"ins_valid_{n}" for n in range(size)])}, inner_tehb.ins_ready);
 
   // output
   DEFINE
-  {"\n  ".join([f"ins_ready_{n} := inner_merge.ins_ready{n};" for n in range(size)])}
+  {"\n  ".join([f"ins_ready_{n} := inner_merge.ins_ready_{n};" for n in range(size)])}
   outs := inner_tehb.outs;
   outs_valid := inner_tehb.outs_valid;
 
