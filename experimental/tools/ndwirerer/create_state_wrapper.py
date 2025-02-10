2a2d17df859815218479afc68b399fefaa1ea563
Fabio Cappellini <fcappellini@ethz.ch>
2025-02-10 18:16:31 +0100

    Switch to python instead of bash based flow

--- experimental/tools/ndwirerer/create_state_wrapper.py
+++ experimental/tools/ndwirerer/create_state_wrapper.py
@@ -1,92 +1,5 @@
-import argparse
 import re
 import os
-
-
-parser = argparse.ArgumentParser(
-                    prog='StateWrapper',
-                    description='TODO What the program does',
-                    epilog='TODO Text at the bottom of help')
-
-parser.add_argument("--mlir")
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-N", type=int)
-group.add_argument("--inf", action="store_true")
-
-
-args = parser.parse_args()
-
-with open(args.mlir) as f:
-  mlir = f.read()
-
-# print(mlir)
-
-arg_names_match = re.search(r'argNames\s*=\s*\[([^\]]+)\]', mlir)
-res_names_match = re.search(r'resNames\s*=\s*\[([^\]]+)\]', mlir)
-
-# Extract values and convert to lists
-arg_names = arg_names_match.group(1).replace('"', '').split(', ') if arg_names_match else []
-res_names = res_names_match.group(1).replace('"', '').split(', ') if res_names_match else []
-
-# print(arg_names)
-# print(res_names)
-
-inf = args.inf
-
-# TODO add as argument
-buffer_size = args.N
-
-
-# TODO proper include
-print(f'#include "{os.path.basename(args.mlir)}"')
-print(
-"""
-#ifndef BOOL_INPUT
-#define BOOL_INPUT
-MODULE bool_input(nReady0, max_tokens)
-  VAR dataOut0 : boolean;
-  VAR counter : 0..31;
-  ASSIGN
-  init(counter) := 0;
-  next(counter) := case
-    nReady0 & counter < max_tokens : counter + 1;
-    TRUE : counter;
-  esac;
-  
-  -- bool_input persistent
-  ASSIGN
-  next(dataOut0) := case 
-    valid0 & !nReady0 : dataOut0;
-    TRUE : {TRUE, FALSE};
-  esac;
-  DEFINE valid0 := counter < max_tokens;
-MODULE bool_input_inf(nReady0)
-  VAR dataOut0 : boolean;
-  
-  -- bool_input persistent
-  ASSIGN
-  next(dataOut0) := case 
-    valid0 & !nReady0 : dataOut0;
-    TRUE : {TRUE, FALSE};
-  esac;
-  DEFINE valid0 := TRUE;
-#endif // BOOL_INPUT
-
-MODULE main
-"""
-)
-
-
-for i, arg in enumerate(arg_names):
-  if inf:
-    print(f"VAR seq_generator{i} : bool_input_inf(in_ndw{i}.ready0);")
-  else:
-    print(f"VAR seq_generator{i} : bool_input(in_ndw{i}.ready0, {buffer_size});")
-
-  print(f"VAR in_ndw{i} : ndw_1_1(seq_generator{i}.dataOut0, seq_generator{i}.valid0, miter.{arg}_ready);")
-  # print(f"VAR seq_generator{i} : entry_0_1(miter.{arg}_ready);")
-
-print()
 
 
 def create_miter_call(arg, res):
@@ -95,15 +8,90 @@ def create_miter_call(arg, res):
   miter += ", "
   miter += ", ".join([f"out_ndw{i}.ready0" for i, _ in enumerate(res)])
   miter += ");\n"
-  print(miter)
+  return miter
 
 
-create_miter_call(arg_names, res_names)
+def create_state_wrapper(smv, mlir, N=0, inf=False):
 
-# TODO
-print("-- TODO make sure we have sink_1_0")
-for i, res in enumerate(res_names):
-  print(f"VAR out_ndw{i} : ndw_1_1(miter.{res}_out, miter.{res}_valid, sink{i}.ready0);")
-  print(f"VAR sink{i} : sink_1_0(out_ndw{i}.dataOut0, out_ndw{i}.valid0);")
+  wrapper = ""
+  assert((N == 0 and inf) or (N != 0 and not inf), "Either inf must be set or N>0.")
 
-print()
+  with open(mlir) as f:
+    mlir = f.read()
+
+  # print(mlir)
+
+  arg_names_match = re.search(r'argNames\s*=\s*\[([^\]]+)\]', mlir)
+  res_names_match = re.search(r'resNames\s*=\s*\[([^\]]+)\]', mlir)
+
+  # Extract values and convert to lists
+  arg_names = arg_names_match.group(1).replace('"', '').split(', ') if arg_names_match else []
+  res_names = res_names_match.group(1).replace('"', '').split(', ') if res_names_match else []
+
+  # print(arg_names)
+  # print(res_names)
+
+  # TODO add as argument
+  buffer_size = N
+
+
+  # TODO proper include
+  wrapper += (f'#include "{os.path.basename(smv)}"\n')
+  wrapper += \
+  """
+  #ifndef BOOL_INPUT
+  #define BOOL_INPUT
+  MODULE bool_input(nReady0, max_tokens)
+    VAR dataOut0 : boolean;
+    VAR counter : 0..31;
+    ASSIGN
+    init(counter) := 0;
+    next(counter) := case
+      nReady0 & counter < max_tokens : counter + 1;
+      TRUE : counter;
+    esac;
+    
+    -- bool_input persistent
+    ASSIGN
+    next(dataOut0) := case 
+      valid0 & !nReady0 : dataOut0;
+      TRUE : {TRUE, FALSE};
+    esac;
+    DEFINE valid0 := counter < max_tokens;
+  MODULE bool_input_inf(nReady0)
+    VAR dataOut0 : boolean;
+    
+    -- bool_input persistent
+    ASSIGN
+    next(dataOut0) := case 
+      valid0 & !nReady0 : dataOut0;
+      TRUE : {TRUE, FALSE};
+    esac;
+    DEFINE valid0 := TRUE;
+  #endif // BOOL_INPUT
+
+  MODULE main
+  """
+
+
+  for i, arg in enumerate(arg_names):
+    if inf:
+      wrapper += (f"VAR seq_generator{i} : bool_input_inf(in_ndw{i}.ready0);\n")
+    else:
+      wrapper += (f"VAR seq_generator{i} : bool_input(in_ndw{i}.ready0, {buffer_size});\n")
+
+    wrapper += (f"VAR in_ndw{i} : ndw_1_1(seq_generator{i}.dataOut0, seq_generator{i}.valid0, miter.{arg}_ready);\n")
+    # print(f"VAR seq_generator{i} : entry_0_1(miter.{arg}_ready);")
+
+  wrapper += "\n"
+
+
+  wrapper += create_miter_call(arg_names, res_names)
+
+  # TODO
+  wrapper += ("-- TODO make sure we have sink_1_0\n")
+  for i, res in enumerate(res_names):
+    wrapper += (f"VAR out_ndw{i} : ndw_1_1(miter.{res}_out, miter.{res}_valid, sink{i}.ready0);\n")
+    wrapper += (f"VAR sink{i} : sink_1_0(out_ndw{i}.dataOut0, out_ndw{i}.valid0);\n")
+
+  return wrapper
--- experimental/tools/ndwirerer/get_states.py
+++ experimental/tools/ndwirerer/get_states.py
@@ -1,78 +1,66 @@
 
-
-import argparse
-
-
-parser = argparse.ArgumentParser(
-                    prog='StateComparer',
-                    description='TODO What the program does',
-                    epilog='TODO Text at the bottom of help')
-
-parser.add_argument("inf")
-parser.add_argument("fin")
-
-args = parser.parse_args()
+def get_states(inf_file, fin_file):
 
 
-with open(args.inf) as f:
-  inf = f.readlines()
+  with open(inf_file) as f:
+    inf = f.readlines()
 
-with open(args.fin) as f:
-  fin = f.readlines()
+  with open(fin_file) as f:
+    fin = f.readlines()
 
-state = 0
-states_inf = {}
-for line in inf:
-  # print(line)
-  line = line.strip()
-  if "-------" in line:
-    if state > 0:
-      states_inf[state] = "\n".join(states_inf[state])
-    state += 1
-    states_inf[state] = []
-    continue
-  if state == 0:
-    continue
-  if not line.startswith("miter."):
-    continue
-  states_inf[state].append(line)
+  state = 0
+  states_inf = {}
+  for line in inf:
+    # print(line)
+    line = line.strip()
+    if "-------" in line:
+      if state > 0:
+        states_inf[state] = "\n".join(states_inf[state])
+      state += 1
+      states_inf[state] = []
+      continue
+    if state == 0:
+      continue
+    if not line.startswith("miter."):
+      continue
+    states_inf[state].append(line)
 
-del states_inf[state]
-
-
-state = 0
-states_3 = {}
-for line in fin:
-  # print(line)
-  line = line.strip()
-  if "-------" in line:
-    if state > 0:
-      states_3[state] = "\n".join(states_3[state])
-    state += 1
-    states_3[state] = []
-    continue
-  if state == 0:
-    continue
-  if not line.startswith("miter."):
-    continue
-  states_3[state].append(line)
-
-del states_3[state]
-
-set_inf = set(states_inf.values())
-set_3 = set(states_3.values())
+  del states_inf[state]
 
 
-diff = set_inf - set_3
+  state = 0
+  states_fin = {}
+  for line in fin:
+    # print(line)
+    line = line.strip()
+    if "-------" in line:
+      if state > 0:
+        states_fin[state] = "\n".join(states_fin[state])
+      state += 1
+      states_fin[state] = []
+      continue
+    if state == 0:
+      continue
+    if not line.startswith("miter."):
+      continue
+    states_fin[state].append(line)
 
-for state in diff:
-  print("--------------")
-  for number, text in states_inf.items():
-    if text == state:
-        print(number)
-  print(state) 
+  del states_fin[state]
+
+  set_inf = set(states_inf.values())
+  set_fin = set(states_fin.values())
 
 
-print(len(diff))
+  diff = set_inf - set_fin
 
-exit(len(diff))
+  # for state in diff:
+  #   print("--------------")
+  #   for number, text in states_inf.items():
+  #     if text == state:
+  #         print(number)
+  #   print(state) 
+
+
+  print(len(diff))
+
+  return len(diff)
