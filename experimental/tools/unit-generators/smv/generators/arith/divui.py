6d7f4abac69c0a03b096057ca2105d165a2bfa2b
Gioele Gottardo <giogot2000@gmail.com>
2025-02-10 16:31:15 +0100

    Implement divsi and divui

---
+++ experimental/tools/unit-generators/smv/generators/arith/divui.py
@@ -0,0 +1,24 @@
+from generators.support.binary_op_handshake_manager import generate_binary_op_handshake_manager
+from generators.support.utils import SmvScalarType
+
+
+def generate_divui(name, params):
+  latency = params["latency"]
+  data_type = SmvScalarType(params["data_type"])
+
+  return _generate_divui(name, latency, data_type)
+
+
+def _generate_divui(name, latency, data_type):
+  return f"""
+MODULE {name}(lhs, lhs_valid, rhs, rhs_valid, result_ready)
+  VAR inner_handshake_manager : {name}__handshake_manager(lhs_valid, rhs_valid, outs_ready);
+
+  // output
+  DEFINE lhs_ready := inner_handshake_manager.lhs_ready;
+  DEFINE rhs_ready := inner_handshake_manager.rhs_ready;
+  DEFINE outs_valid := inner_handshake_manager.outs_valid;
+  DEFINE outs := lhs / rhs;
+  
+  {generate_binary_op_handshake_manager(f"{name}__handshake_manager", latency)}
+"""
