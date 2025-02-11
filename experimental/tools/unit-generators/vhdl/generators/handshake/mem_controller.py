fb7ec945d58e6fe17f861998b4e354832df4d41d
shundroid <shundroid@users.noreply.github.com>
2025-02-11 18:23:02 +0100

    added mem_controller (todo)

---
+++ experimental/tools/unit-generators/vhdl/generators/handshake/mem_controller.py
@@ -0,0 +1,8 @@
+def generate_mem_controller(name, params):
+  num_controls = params["num_controls"]
+  num_loads = params["num_loads"]
+  num_stores = params["num_stores"]
+  data_width = params["data_width"]
+  address_width = params["address_width"]
+
+  return "" # todo
