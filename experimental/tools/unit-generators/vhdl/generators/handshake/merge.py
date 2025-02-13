8a457822953fed2630060f062887cb43b8eb23a6
shundroid <shundroid@users.noreply.github.com>
2025-02-13 21:05:47 +0100

    fixed merge

--- experimental/tools/unit-generators/vhdl/generators/handshake/merge.py
+++ experimental/tools/unit-generators/vhdl/generators/handshake/merge.py
@@ -224,7 +224,7 @@ end architecture;
 
   ins_conversion = []
   for i in range(size):
-    ins_conversion.append(generate_ins_concat_statements(f"ins_{i}", f"ins_inner({i})", extra_signal_mapping, custom_data_name=f"ins({i})"))
+    ins_conversion.append(generate_ins_concat_statements(f"ins_{i}", f"ins_inner({i})", extra_signal_mapping, bitwidth, custom_data_name=f"ins({i})"))
   outs_conversion = generate_outs_concat_statements("outs", "outs_inner", extra_signal_mapping, bitwidth)
 
   architecture = architecture.replace(
