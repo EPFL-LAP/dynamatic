9c9b4c1bbd2a5208cdf4061c310588925ca8da97
shundroid <shundroid@users.noreply.github.com>
2025-02-14 16:03:16 +0100

    added mem_controller

--- experimental/tools/unit-generators/vhdl/vhdl-unit-generator.py
+++ experimental/tools/unit-generators/vhdl/vhdl-unit-generator.py
@@ -10,6 +10,7 @@ import generators.handshake.control_merge as control_merge
 import generators.handshake.extsi as extsi
 import generators.handshake.fork as fork
 import generators.handshake.load as load
+import generators.handshake.mem_controller as mem_controller
 import generators.handshake.mux as mux
 import generators.handshake.spec_commit as spec_commit
 
@@ -35,6 +36,8 @@ def generate_code(name, mod_type, parameters):
       return fork.generate_fork(name, parameters)
     case "load":
       return load.generate_load(name, parameters)
+    case "mem_controller":
+      return mem_controller.generate_mem_controller(name, parameters)
     case "mux":
       return mux.generate_mux(name, parameters)
     case "spec_commit":
