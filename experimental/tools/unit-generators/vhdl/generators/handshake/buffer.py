35142baf060b2dd01e3aaf7f2ec35f8f213a7deb
shundroid <shundroid@users.noreply.github.com>
2025-02-14 12:20:35 +0100

    updated buffer

--- experimental/tools/unit-generators/vhdl/generators/handshake/buffer.py
+++ experimental/tools/unit-generators/vhdl/generators/handshake/buffer.py
@@ -1,18 +1,16 @@
-import ast
+import re
 
-from generators.support.utils import VhdlScalarType
 from generators.handshake.tfifo import generate_tfifo
 from generators.handshake.tehb import generate_tehb
 from generators.handshake.ofifo import generate_ofifo
 from generators.handshake.oehb import generate_oehb
 
 def generate_buffer(name, params):
-  timing = ast.literal_eval(params["timing"])
   num_slots = int(params["num_slots"])
 
-  timing_r = bool(timing["ready_latency"])
-  timing_d = bool(timing["data_latency"])
-  timing_v = bool(timing["valid_latency"])
+  timing_r = bool(re.search(r"R: (\d+)", params["timing"]))
+  timing_d = bool(re.search(r"D: (\d+)", params["timing"]))
+  timing_v = bool(re.search(r"V: (\d+)", params["timing"]))
   transparent = timing_r and not (timing_d or timing_v)
 
   if transparent and num_slots > 1:
