9d58138b2a7829751e4097fab1f94cbe6b2a8575
Fabio Cappellini <fcappellini@ethz.ch>
2025-02-15 19:34:43 +0100

    Unifying wrapper creation

--- experimental/tools/elastic-miter-generator/CreateWrappers.cpp
+++ experimental/tools/elastic-miter-generator/CreateWrappers.cpp
@@ -9,6 +9,7 @@
 #include <llvm/Support/raw_ostream.h>
 #include <sstream>
 #include <string>
+#include <utility>
 
 #include "../experimental/tools/elastic-miter-generator/ElasticMiterFabricGeneration.h"
 #include "dynamatic/Dialect/Handshake/HandshakeOps.h"
@@ -19,6 +20,7 @@ using namespace mlir;
 
 namespace dynamatic::experimental {
 
+// TODO pass names of in and output
 std::string createMiterCall(const SmallVector<std::string> &args,
                             const SmallVector<std::string> &res) {
   std::ostringstream miter;
@@ -27,7 +29,8 @@ std::string createMiterCall(const SmallVector<std::string> &args,
   for (size_t i = 0; i < args.size(); ++i) {
     if (i > 0)
       miter << ", ";
-    miter << "in_ndw" << i << ".dataOut0, in_ndw" << i << ".valid0";
+    miter << "seq_generator" << i << ".dataOut0, seq_generator" << i
+          << ".valid0";
   }
 
   miter << ", ";
@@ -35,100 +38,49 @@ std::string createMiterCall(const SmallVector<std::string> &args,
   for (size_t i = 0; i < res.size(); ++i) {
     if (i > 0)
       miter << ", ";
-    miter << "out_ndw" << i << ".ready0";
+    miter << "sink" << i << ".ready0";
   }
 
   miter << ");\n";
   return miter.str();
 }
 
-FailureOr<std::string> createReachableStateWrapper(ModuleOp mlir, int n = 0,
-                                                   bool inf = false) {
+std::string createMiterProperties(
+    const SmallVector<std::pair<std::string, std::string>> &inputBuffers,
+    const SmallVector<std::string> &outputBuffers,
+    const SmallVector<std::string> &results) {
+  std::ostringstream properties;
 
-  auto failOrFuncOp = dynamatic::experimental::getModuleFuncOpAndCheck(mlir);
-  if (failed(failOrFuncOp))
-    return failure();
-
-  FuncOp funcOp = failOrFuncOp.value();
-
-  std::string funcName = funcOp.getNameAttr().str();
-  // TODO maybe do this properly
-  funcName = "model";
-
-  SmallVector<std::string> argNames;
-  for (Attribute attr : funcOp.getArgNames()) {
-    auto strAttr = attr.dyn_cast<StringAttr>();
-    argNames.push_back(strAttr.getValue().str());
+  for (const auto &result : results) {
+    properties << "CTLSPEC AG (miter." + result + "_valid -> miter." + result +
+                      "_out)\n";
   }
 
-  SmallVector<std::string> resNames;
-  for (Attribute attr : funcOp.getResNames()) {
-    auto strAttr = attr.dyn_cast<StringAttr>();
-    resNames.push_back(strAttr.getValue().str());
+  std::string outputProp;
+  for (const auto &buffer : outputBuffers) {
+    outputProp += "miter." + buffer + ".num = 0 & ";
   }
 
-  std::ostringstream wrapper;
-  wrapper << "#include \"" << funcName << ".smv\"\n";
-  wrapper << "\n#ifndef BOOL_INPUT\n#define BOOL_INPUT\n";
-  wrapper << "MODULE bool_input(nReady0, max_tokens)\n"
-             "    VAR dataOut0 : boolean;\n"
-             "    VAR counter : 0..31;\n"
-             "    ASSIGN\n"
-             "    init(counter) := 0;\n"
-             "    next(counter) := case\n"
-             "      nReady0 & counter < max_tokens : counter + 1;\n"
-             "      TRUE : counter;\n"
-             "    esac;\n"
-             "    \n"
-             "    -- bool_input persistent\n"
-             "    ASSIGN\n"
-             "    next(dataOut0) := case \n"
-             "      valid0 & !nReady0 : dataOut0;\n"
-             "      TRUE : {TRUE, FALSE};\n"
-             "    esac;\n"
-             "    DEFINE valid0 := counter < max_tokens;\n"
-             "\n"
-             "MODULE bool_input_inf(nReady0)\n"
-             "    VAR dataOut0 : boolean;\n"
-             "    \n"
-             "    -- bool_input persistent\n"
-             "    ASSIGN\n"
-             "    next(dataOut0) := case \n"
-             "      valid0 & !nReady0 : dataOut0;\n"
-             "      TRUE : {TRUE, FALSE};\n"
-             "    esac;\n"
-             "    DEFINE valid0 := TRUE;\n"
-             "#endif // BOOL_INPUT\n"
-             "\n"
-             "MODULE main\n";
+  if (!outputProp.empty())
+    outputProp = outputProp.substr(0, outputProp.size() - 3);
 
-  for (size_t i = 0; i < argNames.size(); ++i) {
-    if (inf) {
-      wrapper << "  VAR seq_generator" << i << " : bool_input_inf(in_ndw" << i
-              << ".ready0);\n";
-    } else {
-      wrapper << "  VAR seq_generator" << i << " : bool_input(in_ndw" << i
-              << ".ready0, " << n << ");\n";
-    }
-    wrapper << "  VAR in_ndw" << i << " : ndw_1_1(seq_generator" << i
-            << ".dataOut0, seq_generator" << i << ".valid0, miter."
-            << argNames[i] << "_ready);\n";
+  std::string inputProp;
+  for (const auto &bufferPair : inputBuffers) {
+    inputProp += "miter." + bufferPair.first + ".num = miter." +
+                 bufferPair.second + ".num & ";
   }
 
-  wrapper << "\n  " << createMiterCall(argNames, resNames) << "\n";
-  wrapper << "  -- TODO make sure we have sink_1_0\n";
+  if (!inputProp.empty())
+    inputProp = inputProp.substr(0, inputProp.size() - 3);
 
-  for (size_t i = 0; i < resNames.size(); ++i) {
-    wrapper << "  VAR out_ndw" << i << " : ndw_1_1(miter." << resNames[i]
-            << "_out, miter." << resNames[i] << "_valid, sink" << i
-            << ".ready0);\n";
-    wrapper << "  VAR sink" << i << " : sink_1_0(out_ndw" << i
-            << ".dataOut0, out_ndw" << i << ".valid0);\n";
-  }
+  std::string finalBufferProp =
+      "AF (AG (" + inputProp + " & " + outputProp + "))";
+  properties << "CTLSPEC " + finalBufferProp + "\n";
 
-  return wrapper.str();
+  return properties.str();
 }
 
+// TODO make this cleaner
 LogicalResult createMiterWrapper(const std::filesystem::path &wrapperPath,
                                  const std::filesystem::path &jsonPath,
                                  const std::string &modelSmvName,
@@ -155,116 +107,88 @@ LogicalResult createMiterWrapper(const std::filesystem::path &wrapperPath,
     return failure();
   }
 
-  std::string output;
-  output += "#include \"" + modelSmvName + "\"\n";
-  output += "#ifndef BOOL_INPUT\n"
-            "#define BOOL_INPUT\n"
-            "MODULE bool_input(nReady0, max_tokens)\n"
-            "  VAR dataOut0 : boolean;\n"
-            "  VAR counter : 0..31;\n"
-            "  ASSIGN\n"
-            "    init(counter) := 0;\n"
-            "    next(counter) := case\n"
-            "      nReady0 & counter < max_tokens : counter + 1;\n"
-            "      TRUE : counter;\n"
-            "    esac;\n"
-            "\n"
-            "  -- bool_input persistent\n"
-            "  ASSIGN\n"
-            "    next(dataOut0) := case\n"
-            "      valid0 & !nReady0 : dataOut0;\n"
-            "      TRUE : {TRUE, FALSE};\n"
-            "    esac;\n"
-            "\n"
-            "  DEFINE valid0 := counter < max_tokens;\n"
-            "#endif // BOOL_INPUT\n"
-            "\n"
-            "MODULE main\n";
-
+  SmallVector<std::string> argNames;
   if (auto *args = config->getArray("arguments")) {
-    for (size_t i = 0; i < args->size(); ++i) {
-      output += "VAR seq_generator" + std::to_string(i) +
-                " : bool_input(miter." +
-                (*args)[i].getAsString().value_or("").str() + "_ready, " +
-                std::to_string(nrOfTokens) + ");\n";
+    for (const auto &arg : *args) {
+      argNames.push_back(arg.getAsString()->str());
     }
-  } else {
-    llvm::errs() << "No arguments in JSON\n";
-    return failure();
   }
-  output += "\n";
 
-  output += "VAR miter : elastic_miter(";
-  if (auto *args = config->getArray("arguments")) {
-    for (size_t i = 0; i < args->size(); ++i) {
-      if (i > 0)
-        output += ", ";
-      output += "seq_generator" + std::to_string(i) +
-                ".dataOut0, seq_generator" + std::to_string(i) + ".valid0";
+  SmallVector<std::string> resNames;
+  if (auto *args = config->getArray("results")) {
+    for (const auto &arg : *args) {
+      resNames.push_back(arg.getAsString()->str());
     }
   }
-  output += ", ";
-  if (auto *res = config->getArray("results")) {
-    for (size_t i = 0; i < res->size(); ++i) {
-      if (i > 0)
-        output += ", ";
-      output += "sink" + std::to_string(i) + ".ready0";
-    }
-  } else {
-    llvm::errs() << "No results in JSON\n";
-    return failure();
-  }
-  output += ");\n\n";
 
-  output += "-- TODO make sure we have sink_1_0\n";
-  if (auto *res = config->getArray("results")) {
-    for (size_t i = 0; i < res->size(); ++i) {
-      output += "VAR sink" + std::to_string(i) + " : sink_1_0(miter." +
-                (*res)[i].getAsString().value_or("").str() + "_out, miter." +
-                (*res)[i].getAsString().value_or("").str() + "_valid);\n";
+  SmallVector<std::string> outputBufferNames;
+  if (auto *args = config->getArray("output_buffers")) {
+    for (const auto &arg : *args) {
+      outputBufferNames.push_back(arg.getAsString()->str());
     }
   }
-  output += "\n";
 
-  if (auto *res = config->getArray("results")) {
-    for (const auto &result : *res) {
-      std::string resStr = result.getAsString().value_or("").str();
-      output += "CTLSPEC AG (miter." + resStr + "_valid -> miter." + resStr +
-                "_out)\n";
-    }
-  }
-  output += "\n";
-
-  std::string outputProp;
-  if (auto *outBufs = config->getArray("output_buffers")) {
-    for (const auto &bufArray : *outBufs) {
-      if (auto *bufList = bufArray.getAsArray()) {
-        for (const auto &buf : *bufList) {
-          outputProp +=
-              "miter." + buf.getAsString().value_or("").str() + ".num = 0 & ";
-        }
-      }
-    }
-  }
-  if (!outputProp.empty())
-    outputProp = outputProp.substr(0, outputProp.size() - 3);
-
+  SmallVector<std::pair<std::string, std::string>> inputBufferNames;
   std::string inputProp;
   if (auto *inBufs = config->getArray("input_buffers")) {
     for (const auto &bufPair : *inBufs) {
       if (auto *pair = bufPair.getAsArray()) {
-        inputProp += "miter." + (*pair)[0].getAsString().value_or("").str() +
-                     ".num = miter." +
-                     (*pair)[1].getAsString().value_or("").str() + ".num & ";
+        inputBufferNames.push_back(std::make_pair(
+            (*pair)[0].getAsString()->str(), (*pair)[1].getAsString()->str()));
       }
     }
   }
-  if (!inputProp.empty())
-    inputProp = inputProp.substr(0, inputProp.size() - 3);
 
-  std::string finalBufferProp =
-      "AF (AG (" + inputProp + " & " + outputProp + "))";
-  output += "CTLSPEC " + finalBufferProp + "\n";
+  std::string output;
+  std::ostringstream wrapper;
+  wrapper << "#include \"" + modelSmvName + "\"\n";
+  wrapper << "#ifndef BOOL_INPUT\n"
+             "#define BOOL_INPUT\n"
+             "MODULE bool_input(nReady0, max_tokens)\n"
+             "  VAR dataOut0 : boolean;\n"
+             "  VAR counter : 0..31;\n"
+             "  ASSIGN\n"
+             "    init(counter) := 0;\n"
+             "    next(counter) := case\n"
+             "      nReady0 & counter < max_tokens : counter + 1;\n"
+             "      TRUE : counter;\n"
+             "    esac;\n"
+             "\n"
+             "  -- bool_input persistent\n"
+             "  ASSIGN\n"
+             "    next(dataOut0) := case\n"
+             "      valid0 & !nReady0 : dataOut0;\n"
+             "      TRUE : {TRUE, FALSE};\n"
+             "    esac;\n"
+             "\n"
+             "  DEFINE valid0 := counter < max_tokens;\n"
+             "#endif // BOOL_INPUT\n"
+             "\n"
+             "MODULE main\n";
+
+  for (size_t i = 0; i < argNames.size(); ++i) {
+    if (nrOfTokens == 0) {
+      wrapper << "  VAR seq_generator" << i << " : bool_input_inf(miter." << i
+              << "_ready);\n";
+    } else {
+      wrapper << "  VAR seq_generator" << i << " : bool_input(miter." << i
+              << "_ready, " << nrOfTokens << ");\n";
+    }
+  }
+  wrapper << "\n";
+
+  wrapper << "\n  " << createMiterCall(argNames, resNames) << "\n";
+  wrapper << "  -- TODO make sure we have sink_1_0\n";
+
+  for (size_t i = 0; i < resNames.size(); ++i) {
+    wrapper << "  VAR sink" << i << " : sink_1_0(miter." << resNames[i]
+            << "_out, miter." << resNames[i] << "_valid);\n";
+  }
+
+  wrapper << "\n";
+
+  wrapper << createMiterProperties(inputBufferNames, outputBufferNames,
+                                   resNames);
 
   std::ofstream mainFile(wrapperPath);
   mainFile << output;
