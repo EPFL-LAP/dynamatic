5bf26457b8506be4b56124d4611a3abca79d5596
Gioele Gottardo <giogot2000@gmail.com>
2025-02-19 15:26:39 +0100

    Use attr constant

--- experimental/tools/unit-generators/smv/generators/arith/cmpi.py
+++ experimental/tools/unit-generators/smv/generators/arith/cmpi.py
@@ -25,7 +25,7 @@ def _generate_cmpi(name, latency, symbol, data_type):
 {generate_binary_op_header(name)}
   DEFINE outs := lhs {symbol} rhs;
   
-  {generate_binary_op_handshake_manager(f"{name}__handshake_manager", {"latency": latency})}
+  {generate_binary_op_handshake_manager(f"{name}__handshake_manager", {ATTR_LATENCY: latency})}
 """
 
 
@@ -34,7 +34,7 @@ def _generate_cmpi_cast(name, latency, symbol, modifier, data_type):
 {generate_binary_op_header(name)}
   DEFINE outs := ({modifier})lhs {symbol} ({modifier})rhs;
   
-  {generate_binary_op_handshake_manager(f"{name}__handshake_manager", {"latency": latency})}
+  {generate_binary_op_handshake_manager(f"{name}__handshake_manager", {ATTR_LATENCY: latency})}
 """
 
 
