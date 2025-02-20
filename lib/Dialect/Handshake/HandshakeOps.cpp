bff4dfc1679a254742debeff819c068270da1343
shundroid <shundroid@users.noreply.github.com>
2025-02-20 10:54:55 +0100

    fixed ops

--- lib/Dialect/Handshake/HandshakeOps.cpp
+++ lib/Dialect/Handshake/HandshakeOps.cpp
@@ -1682,6 +1682,31 @@ LogicalResult SpeculatorOp::verify() {
 }
 
 //===----------------------------------------------------------------------===//
+// SpecCommitOp
+//===----------------------------------------------------------------------===//
+
+LogicalResult SpecCommitOp::inferReturnTypes(
+    MLIRContext *context, std::optional<Location> location, ValueRange operands,
+    DictionaryAttr attributes, mlir::OpaqueProperties properties,
+    mlir::RegionRange regions,
+    SmallVectorImpl<mlir::Type> &inferredReturnTypes) {
+
+  OpBuilder builder(context);
+
+  auto dataInType = cast<ExtraSignalsTypeInterface>(operands.front().getType());
+  SmallVector<ExtraSignal> newExtraSignals;
+  for (const ExtraSignal &signal : dataInType.getExtraSignals()) {
+    if (signal.name != "spec") {
+      newExtraSignals.push_back(signal);
+    }
+  }
+  inferredReturnTypes.push_back(
+      dataInType.copyWithExtraSignals(newExtraSignals));
+
+  return success();
+}
+
+//===----------------------------------------------------------------------===//
 // BundleOp
 //===----------------------------------------------------------------------===//
 
