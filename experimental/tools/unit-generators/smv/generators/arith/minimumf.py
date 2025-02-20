8c651a86f22c13aca5b458e44503589bbd8f33d6
Gioele Gottardo <giogot2000@gmail.com>
2025-02-20 11:00:21 +0100

    Fix generation

--- experimental/tools/unit-generators/smv/generators/arith/minimumf.py
+++ experimental/tools/unit-generators/smv/generators/arith/minimumf.py
@@ -1,4 +1,4 @@
-from generators.support.arith_utils import generate_abstract_unary_op
+from generators.support.arith_utils import generate_abstract_binary_op
 from generators.support.utils import *
 
 
@@ -8,6 +8,6 @@ def generate_minimumf(name, params):
   abstract_data = params[ATTR_ABSTRACT_DATA]
 
   if abstract_data:
-    return generate_abstract_unary_op(name, latency, data_type)
+    return generate_abstract_binary_op(name, latency, data_type)
   else:
     raise ValueError("Floating point operations support abstract data only")
