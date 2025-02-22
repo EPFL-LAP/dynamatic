1be4a411f37ddecde7c72f3fe60edf99276f13c9
shundroid <shundroid@users.noreply.github.com>
2025-02-22 21:15:14 +0100

    [Speculation] Type Verification for Speculation (#300)
    Building on #225, I added additional type verification constraints
    specific to speculation. One newly introduced constraint, however,
    remains general: `AllExtraSignalsMatchExcept`.
    
    - This constraint functions like `AllExtraSignalsMatch`, but ignores a
    specified extra signal. For example: `AllExtraSignalsMatchExcept<"spec",
    ["dataIn", "dataOut"]>`.
    - I implemented `AllExtraSignalsMatchExcept` in `HandshakeTypes.cpp`
    instead of `HandshakeTypeInterfaces.td`, as I found this improves
    readability. If needed, I can move the implementation of other
    constraints to the `.cpp` file for consistency.
    
    Other changes:
    
    - Implemented two speculation-specific constraints: `HasSpecTag` and
    `LacksSpecTag`.
    - Applied these constraints to speculative units, leading to a slight
    change in their assembly format due to the removal of `AllTypesMatch`
    constraints.
    - Added unit tests for the new constraints.
    
    I will update the documentation in the following pull request!

--- lib/Dialect/Handshake/HandshakeOps.cpp
+++ lib/Dialect/Handshake/HandshakeOps.cpp
@@ -1643,44 +1643,6 @@ void SpeculatorOp::print(OpAsmPrinter &p) {
     << ")";
 }
 
-LogicalResult SpeculatorOp::inferReturnTypes(
-    MLIRContext *context, std::optional<Location> location, ValueRange operands,
-    DictionaryAttr attributes, mlir::OpaqueProperties properties,
-    mlir::RegionRange regions,
-    SmallVectorImpl<mlir::Type> &inferredReturnTypes) {
-
-  inferredReturnTypes.push_back(operands.front().getType());
-
-  OpBuilder builder(context);
-  ChannelType ctrlType = ChannelType::get(builder.getIntegerType(1));
-  ChannelType wideControlType = ChannelType::get(builder.getIntegerType(3));
-  inferredReturnTypes.push_back(ctrlType);
-  inferredReturnTypes.push_back(ctrlType);
-  inferredReturnTypes.push_back(wideControlType);
-  inferredReturnTypes.push_back(wideControlType);
-  inferredReturnTypes.push_back(ctrlType);
-  return success();
-}
-
-LogicalResult SpeculatorOp::verify() {
-  Type refBoolType = getSaveCtrl().getType();
-  if (refBoolType != getCommitCtrl().getType() ||
-      refBoolType != getSCBranchCtrl().getType()) {
-    return emitError() << "expected $saveCtrl, $commitCtrl, and $SCBranchCtrl "
-                          "to have the same type, but got "
-                       << refBoolType << ", " << getCommitCtrl().getType()
-                       << ", and " << getSCBranchCtrl().getType();
-  }
-
-  if (getSCSaveCtrl().getType() != getSCCommitCtrl().getType()) {
-    return emitError() << "expected $SCSaveCtrl and $SCCommitCtrl to have the "
-                          "same type, but got "
-                       << getSCSaveCtrl().getType() << " and "
-                       << getSCCommitCtrl().getType();
-  }
-  return success();
-}
-
 //===----------------------------------------------------------------------===//
 // BundleOp
 //===----------------------------------------------------------------------===//
