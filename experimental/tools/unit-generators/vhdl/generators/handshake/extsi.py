42f9a74b7a9c5d2a41419da79703f45b59d66af1
shundroid <shundroid@users.noreply.github.com>
2025-03-11 20:42:33 +0100

    use normal signal manager more

--- experimental/tools/unit-generators/vhdl/generators/handshake/extsi.py
+++ experimental/tools/unit-generators/vhdl/generators/handshake/extsi.py
@@ -1,4 +1,4 @@
-from generators.support.utils import generate_extra_signal_ports
+from generators.support.signal_manager import generate_signal_manager
 
 
 def generate_extsi(name, params):
@@ -48,69 +48,18 @@ end architecture;
   return entity + architecture
 
 
-extra_signal_logic = {
-    "spec": """
-  outs_spec <= ins_spec;
-"""
-}
-
-
 def _generate_extsi_signal_manager(name, input_bitwidth, output_bitwidth, extra_signals):
-  inner_name = f"{name}_inner"
-
-  dependencies = _generate_extsi(inner_name, input_bitwidth, output_bitwidth)
-
-  entity = f"""
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
--- Entity of extsi signal manager
-entity {name} is
-  port (
-    clk, rst : in std_logic;
-    [EXTRA_SIGNAL_PORTS]
-    -- input channel
-    ins       : in  std_logic_vector({input_bitwidth} - 1 downto 0);
-    ins_valid : in  std_logic;
-    ins_ready : out std_logic;
-    -- output channel
-    outs       : out std_logic_vector({output_bitwidth} - 1 downto 0);
-    outs_valid : out std_logic;
-    outs_ready : in  std_logic
-  );
-end entity;
-"""
-
-  # Add extra signal ports
-  extra_signal_ports = generate_extra_signal_ports([
-      ("ins", "in"),
-      ("outs", "out")
-  ], extra_signals)
-  entity = entity.replace("    [EXTRA_SIGNAL_PORTS]\n", extra_signal_ports)
-
-  architecture = f"""
--- Architecture of extsi signal manager
-architecture arch of {name} is
-begin
-  [EXTRA_SIGNAL_LOGIC]
-
-  inner : entity work.{inner_name}(arch)
-    port map(
-      clk        => clk,
-      rst        => rst,
-      ins        => ins,
-      ins_valid  => ins_valid,
-      ins_ready  => ins_ready,
-      outs       => outs,
-      outs_valid => outs_valid,
-      outs_ready => outs_ready
-    );
-end architecture;
-"""
-
-  architecture = architecture.replace("  [EXTRA_SIGNAL_LOGIC]", "\n".join([
-      extra_signal_logic[name] for name in extra_signals
-  ]))
-
-  return dependencies + entity + architecture
+  return generate_signal_manager(name, {
+      "type": "normal",
+      "in_ports": [{
+          "name": "ins",
+          "bitwidth": input_bitwidth,
+          "extra_signals": extra_signals
+      }],
+      "out_ports": [{
+          "name": "outs",
+          "bitwidth": output_bitwidth,
+          "extra_signals": extra_signals
+      }],
+      "extra_signals": extra_signals
+  }, lambda name: _generate_extsi(name, input_bitwidth, output_bitwidth))
