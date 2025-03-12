aea56a36bb4f3a77f51f0488215e8909d472e83f
shundroid <shundroid@users.noreply.github.com>
2025-03-12 14:32:03 +0100

    use new methods for load signal manager

--- experimental/tools/unit-generators/vhdl/generators/support/utils.py
+++ experimental/tools/unit-generators/vhdl/generators/support/utils.py
@@ -1,17 +1,3 @@
-def generate_extra_signal_ports(ports: list[tuple[str, str]], extra_signals: dict[str, int]) -> str:
-  if not extra_signals:
-    return ""
-  return "    -- extra signal ports\n" + "\n".join([
-      "\n".join([
-          f"    {port}_{name} : {inout} std_logic_vector({bitwidth - 1} downto 0);"
-          for name, bitwidth in extra_signals.items()
-      ])
-      for port, inout in ports
-  ])
-
-# For concat-type signal managers (e.g., tehb, fork)
-
-
 class ExtraSignalMapping:
   # List of tuples of (extra_signal_name, (msb, lsb))
   mapping: list[tuple[str, tuple[int, int]]]
@@ -34,75 +20,6 @@ class ExtraSignalMapping:
 
   def get(self, name: str):
     return self.mapping[[name for name, _ in self.mapping].index(name)]
-
-
-def generate_ins_concat_statements(in_name: str, in_inner_name: str, extra_signal_mapping: ExtraSignalMapping, bitwidth: int, indent=2, custom_data_name=None) -> str:
-  """
-  Generates the input signal concatenation statement.
-  in_name: The name of the input signal. (e.g., "ins")
-  in_inner_name: The name of the inner input signal. (e.g., "ins_inner")
-  extra_signal_mapping: An ExtraSignalMapping object.
-  bitwidth: The bitwidth of the data signal.
-  e.g., ins_inner(31 downto 0) <= ins;
-  ins_inner(32 downto 32) <= ins_spec;
-  ins_inner(40 downto 33) <= ins_tag;
-  """
-  indent_str = " " * indent
-  if custom_data_name is None:
-    custom_data_name = in_name
-  return f"{indent_str}{in_inner_name}({bitwidth - 1} downto 0) <= {custom_data_name};\n" + \
-      generate_ins_concat_statements_dataless(
-      in_name, in_inner_name, extra_signal_mapping, indent)
-
-
-def generate_ins_concat_statements_dataless(in_name: str, in_inner_name: str, extra_signal_mapping: ExtraSignalMapping, indent=2) -> str:
-  """
-  Generates the input signal concatenation statement.
-  in_name: The name of the input signal. (e.g., "ins")
-  in_inner_name: The name of the inner input signal. (e.g., "ins_inner")
-  extra_signal_mapping: An ExtraSignalMapping object.
-  e.g., ins_inner(0 downto 0) <= ins_spec;
-  ins_inner(8 downto 1) <= ins_tag;
-  """
-  indent_str = " " * indent
-  return "\n".join([
-      f"{indent_str}{in_inner_name}({msb} downto {lsb}) <= {in_name}_{name};" for name, (msb, lsb) in extra_signal_mapping.mapping
-  ]) + "\n"
-
-
-def generate_outs_concat_statements(out_name: str, out_inner_name: str, extra_signal_mapping: ExtraSignalMapping, bitwidth: int, indent=2, custom_data_name=None) -> str:
-  """
-  Generates the output signal concatenation statement.
-  out_name: The name of the output signal. (e.g., "outs")
-  out_inner_name: The name of the inner output signal. (e.g., "outs_inner")
-  extra_signal_mapping: An ExtraSignalMapping object.
-  bitwidth: The bitwidth of the data signal.
-  e.g., outs <= outs_inner(31 downto 0)
-  outs_spec <= outs_inner(32 downto 32)
-  outs_tag <= outs_inner(40 downto 33)
-  """
-  indent_str = " " * indent
-  if custom_data_name is None:
-    custom_data_name = out_name
-  return f"{indent_str}{custom_data_name} <= {out_inner_name}({bitwidth - 1} downto 0);\n" + \
-      generate_outs_concat_statements_dataless(
-      out_name, out_inner_name, extra_signal_mapping, indent)
-
-
-def generate_outs_concat_statements_dataless(out_name: str, out_inner_name: str, extra_signal_mapping: ExtraSignalMapping, indent=2) -> str:
-  """
-  Generates the output signal concatenation statement.
-  out_name: The name of the output signal. (e.g., "outs")
-  out_inner_name: The name of the inner output signal. (e.g., "outs_inner")
-  extra_signal_mapping: An ExtraSignalMapping object.
-  bitwidth: The bitwidth of the data signal.
-  e.g., outs_spec <= outs_inner(0 downto 0)
-  outs_tag <= outs_inner(8 downto 1)
-  """
-  indent_str = " " * indent
-  return "\n".join([
-      f"{indent_str}{out_name}_{name} <= {out_inner_name}({msb} downto {lsb});" for name, (msb, lsb) in extra_signal_mapping.mapping
-  ]) + "\n"
 
 
 extra_signal_default_values = {
