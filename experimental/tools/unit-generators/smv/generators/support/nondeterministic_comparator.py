ca3ef4e1d90573d84b00c104fb29d84eb98245f4
Gioele Gottardo <72650449+gioelegott@users.noreply.github.com>
2025-03-12 16:02:43 +0100

    [SMV] Arith unit generator (#271)
    This PR implements the SMV unit generation for all the arithmetic MLIR
    handshake operations. The implemented units are: `absf`, `addf`, `addi`,
    `andi`, `cmpf`, `cmpi`, `divf`, `divsi`, `divui`, `extf`, `extsi`,
    `extui`, `fptosi`, `maximumf`, `minimumf`, `mulf`, `muli`, `negf`,
    `not`, `ori`, `shli`, `shrsi`, `shrui`, `sitofp`, `subf`, `subi`,
    `truncf`, `trunci`, `xori`. They directly correspond to the respective
    MLIR handshake operations.
    
    This PR stems from the discussion in
    https://github.com/EPFL-LAP/dynamatic/issues/235 and it extends
    https://github.com/EPFL-LAP/dynamatic/pull/270.
    
    The integer arithmetic operations are modeled precisely, by applying the
    respective operation to the input(s) (e.g. `addi` performs an addition
    of the inputs). On the other hand floating point operation just
    propagate one of the inputs to the output. We chose to do so because
    there is no simple way to model floating point operations in SMV. This
    doesn't impact the correctness of the model, as all decisions based on
    floating point operations will be modeled by a non-deterministic value.

---
+++ experimental/tools/unit-generators/smv/generators/support/nondeterministic_comparator.py
@@ -0,0 +1,29 @@
+from generators.support.utils import *
+from generators.support.arith_utils import *
+
+
+def generate_nondeterministic_comparator(name, params):
+  latency = params[ATTR_LATENCY]
+  data_type = SmvScalarType(params[ATTR_PORT_TYPES]["lhs"])
+
+  return _generate_nondeterministic_comparator(name, latency, data_type)
+
+
+def _generate_nondeterministic_comparator(name, latency, data_type):
+  return f"""
+MODULE {name}(lhs, lhs_valid, rhs, rhs_valid, outs_ready)
+  VAR inner_handshake_manager : {name}__handshake_manager(lhs_valid, rhs_valid, outs_ready);
+  VAR rand : boolean;
+
+  ASSIGN
+  init(rand) := {{TRUE, FALSE}};
+  next(rand) := outs_valid & outs_ready ? {{TRUE, FALSE}} : rand;
+
+  // output
+  DEFINE lhs_ready := inner_handshake_manager.lhs_ready;
+  DEFINE rhs_ready := inner_handshake_manager.rhs_ready;
+  DEFINE outs_valid := inner_handshake_manager.outs_valid;
+  DEFINE outs := rand;
+  
+  {generate_binary_op_handshake_manager(f"{name}__handshake_manager", {ATTR_LATENCY: latency})}
+"""
