87a1eac070f4219e5574636f0199130bb9aa3b1e
shundroid <shundroid@users.noreply.github.com>
2025-03-17 09:53:01 +0100

    implemented spec_v2

--- experimental/tools/unit-generators/vhdl/generators/handshake/spec_v2/speculator_v2.py
+++ experimental/tools/unit-generators/vhdl/generators/handshake/spec_v2/speculator_v2.py
@@ -38,16 +38,59 @@ architecture arch of {name} is
   signal loop_mode : LOOP_STATE;
   signal spec_mode : SPEC_STATE;
 begin
+  condition_ready <= (condition_spec and (spec_mode = MISSPEC)) or
+                     (commitCtrl_ready and (loop_mode = LOOP));
+
   trigger_ready <= muxCtrl_ready and (loop_mode = IDLE);
+
+  ctrlOut_valid <= (loop_mode = EXIT) and muxCtrl_ready;
+
   muxCtrl_valid <= (trigger_valid and (loop_mode = IDLE)) or
                     (ctrlOut_ready and (loop_mode = EXIT)) or
                     (loop_mode = LOOP);
   muxCtrl <= "01" when (loop_mode = IDLE) else
              "00" when (loop_mode = EXIT) else
              "10";
-  ctrlOut_valid <= (loop_mode = EXIT) and muxCtrl_ready;
 
-  condition_ready <= (condition_spec and (spec_mode = MISSPEC)) or
-                     (commitCtrl_ready and (loop_mode = LOOP));
+  commitCtrl_valid <= condition_valid and ((spec_mode = DEFAULT) or not condition_spec(0)) and (loop_mode = LOOP);
+  commitCtrl <= condition;
+
+  loop_mode_proc : process (clk)
+  begin
+    if rising_edge(clk) then
+      if rst = '1' then
+        loop_mode <= IDLE;
+      else
+        if (trigger_ready and trigger_valid) then
+          loop_mode <= LOOP;
+        elsif ((not condition) and condition_valid and condition_ready) then
+          loop_mode <= EXIT;
+        elsif (ctrlOut_valid and ctrlOut_ready) then
+          loop_mode <= IDLE;
+        else
+          loop_mode <= loop_mode;
+        end if;
+      end if;
+    end if;
+  end process;
+
+  spec_mode_proc : process (clk)
+  begin
+    if rising_edge(clk) then
+      if rst = '1' then
+        spec_mode <= DEFAULT;
+      else
+        if (condition_valid and condition_ready) then
+          if (condition_spec(0)) then
+            spec_mode <= MISSPEC;
+          else
+            spec_mode <= DEFAULT;
+          end if;
+        else
+          spec_mode <= spec_mode;
+        end if;
+      end if;
+    end if;
+  end process;
 end architecture;
 """
