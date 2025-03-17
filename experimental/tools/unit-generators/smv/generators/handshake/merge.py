7fb9d5663f75d1bdd9bb2789130455a305c2713b
Gioele Gottardo <giogot2000@gmail.com>
2025-03-17 18:28:38 +0100

    Fix bugs with test generator

--- experimental/tools/unit-generators/smv/generators/handshake/merge.py
+++ experimental/tools/unit-generators/smv/generators/handshake/merge.py
@@ -17,14 +17,14 @@ def generate_merge(name, params):
 
 def _generate_merge_dataless(name, size):
   return f"""
-MODULE {name}({", ".join([f"ins_valid_{n}" for n in range(size)])}, outs_ready)
+MODULE {name}({", ".join([f"ins_{n}_valid" for n in range(size)])}, outs_ready)
   VAR
   inner_tehb : {name}__tehb_dataless(inner_merge.outs_valid, outs_ready);
-  inner_merge : {name}__merge_notehb_dataless({", ".join([f"ins_valid_{n}" for n in range(size)])}, inner_tehb.ins_ready);
+  inner_merge : {name}__merge_notehb_dataless({", ".join([f"ins_{n}_valid" for n in range(size)])}, inner_tehb.ins_ready);
 
-  // output
+  -- output
   DEFINE
-  {"\n  ".join([f"ins_ready_{n} := inner_merge.ins_ready_{n};" for n in range(size)])}
+  {"\n  ".join([f"ins_{n}_ready := inner_merge.ins_{n}_ready;" for n in range(size)])}
   outs_valid := inner_tehb.outs_valid;
 
 {generate_merge_notehb(f"{name}__merge_notehb_dataless", {ATTR_SIZE: size, ATTR_DATA_TYPE: HANDSHAKE_CONTROL_TYPE.mlir_type})}
@@ -34,17 +34,17 @@ MODULE {name}({", ".join([f"ins_valid_{n}" for n in range(size)])}, outs_ready)
 
 def _generate_merge(name, size, data_type):
   return f"""
-MODULE {name}({", ".join([f"ins_{n}, ins_valid_{n}" for n in range(size)])}, outs_ready)
+MODULE {name}({", ".join([f"ins_{n}, ins_{n}_valid" for n in range(size)])}, outs_ready)
   VAR
   inner_tehb : {name}__tehb(inner_merge.outs, inner_merge.outs_valid, outs_ready);
-  inner_merge : {name}__merge_notehb({", ".join([f"ins_{n}" for n in range(size)])}, {", ".join([f"ins_valid_{n}" for n in range(size)])}, inner_tehb.ins_ready);
+  inner_merge : {name}__merge_notehb({", ".join([f"ins_{n}, ins_{n}_valid" for n in range(size)])}, inner_tehb.ins_ready);
 
-  // output
+  -- output
   DEFINE
-  {"\n  ".join([f"ins_ready_{n} := inner_merge.ins_ready_{n};" for n in range(size)])}
+  {"\n  ".join([f"ins_{n}_ready := inner_merge.ins_{n}_ready;" for n in range(size)])}
   outs := inner_tehb.outs;
   outs_valid := inner_tehb.outs_valid;
 
 {generate_merge_notehb(f"{name}__merge_notehb", {ATTR_SIZE: size, ATTR_DATA_TYPE: data_type.mlir_type})}
-{generate_tehb(f"{name}__tehb_dataless", {ATTR_DATA_TYPE: data_type.mlir_type})}
+{generate_tehb(f"{name}__tehb", {ATTR_DATA_TYPE: data_type.mlir_type})}
 """
--- experimental/tools/unit-generators/smv/generators/handshake/mux.py
+++ experimental/tools/unit-generators/smv/generators/handshake/mux.py
@@ -15,19 +15,19 @@ def generate_mux(name, params):
 
 def _generate_mux_dataless(name, size, select_type):
   return f"""
-MODULE {name}({", ".join([f"ins_valid_{n}" for n in range(size)])}, index, index_valid, outs_ready)
+MODULE {name}({", ".join([f"ins_{n}_valid" for n in range(size)])}, index, index_valid, outs_ready)
   VAR
   inner_tehb : {name}__tehb_dataless(tehb_ins_valid, outs_ready);
 
   DEFINE
   tehb_ins_valid := case
-    {"\n    ".join([f"index = {select_type.format_constant(n)} : index_valid & ins_valid_{n};" for n in range(size)])}
+    {"\n    ".join([f"index = {select_type.format_constant(n)} : index_valid & ins_{n}_valid;" for n in range(size)])}
     TRUE : FALSE;
   esac;
 
-  // output
+  -- output
   DEFINE
-  {"\n  ".join([f"ins_ready_{n} := index = {select_type.format_constant(n)} & index_valid & inner_tehb.ins_ready & ins_valid_{n} | !ins_valid_{n};" for n in range(size)])}
+  {"\n  ".join([f"ins_{n}_ready := index = {select_type.format_constant(n)} & index_valid & inner_tehb.ins_ready & ins_{n}_valid | !ins_{n}_valid;" for n in range(size)])}
   index_ready := !index_valid | tehb_ins_valid & inner_tehb.ins_ready;
   outs_valid := inner_tehb.outs_valid;
 
@@ -37,26 +37,26 @@ MODULE {name}({", ".join([f"ins_valid_{n}" for n in range(size)])}, index, index
 
 def _generate_mux(name, size, data_type, select_type):
   return f"""
-MODULE {name}({", ".join([f"ins_{n}, ins_valid_{n}" for n in range(size)])}, index, index_valid, outs_ready)
+MODULE {name}({", ".join([f"ins_{n}, ins_{n}_valid" for n in range(size)])}, index, index_valid, outs_ready)
   VAR
   inner_tehb : {name}__tehb(tehb_ins, tehb_ins_valid, outs_ready);
 
   DEFINE
   tehb_ins := case
-    {"\n    ".join([f"index = {select_type.format_constant(n)} & index_valid & ins_valid_{n} : ins_{n};" for n in range(size)])}
+    {"\n    ".join([f"index = {select_type.format_constant(n)} & index_valid & ins_{n}_valid : ins_{n};" for n in range(size)])}
     TRUE : ins_0;
   esac;
   tehb_ins_valid := case
-    {"\n    ".join([f"index = {select_type.format_constant(n)} : index_valid & ins_valid_{n} | !ins_valid_{n};" for n in range(size)])}
+    {"\n    ".join([f"index = {select_type.format_constant(n)} : index_valid & ins_{n}_valid;" for n in range(size)])}
     TRUE : FALSE;
   esac;
 
-  // output
+  -- output
   DEFINE
-  {"\n  ".join([f"ins_ready_{n} := index = {select_type.format_constant(n)} & index_valid & inner_tehb.ins_ready & ins_valid_{n} | !ins_valid_{n};" for n in range(size)])}
+  {"\n  ".join([f"ins_{n}_ready := index = {select_type.format_constant(n)} & index_valid & inner_tehb.ins_ready & ins_{n}_valid | !ins_{n}_valid;" for n in range(size)])}
   index_ready := !index_valid | tehb_ins_valid & inner_tehb.ins_ready;
   outs_valid := inner_tehb.outs_valid;
   outs := inner_tehb.outs;
 
-{generate_tehb(f"{name}__tehb_dataless", {ATTR_DATA_TYPE: data_type.mlir_type})}
+{generate_tehb(f"{name}__tehb", {ATTR_DATA_TYPE: data_type.mlir_type})}
 """
