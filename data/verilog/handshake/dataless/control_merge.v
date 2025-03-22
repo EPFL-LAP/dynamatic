f80ac2a86222d81506c666651428b35977fced88
QinYuan2000 <qinyuan123123@gmail.com>
2025-03-22 02:23:12 +0100

    Remove merge_notehb

--- data/verilog/handshake/dataless/control_merge.v
+++ data/verilog/handshake/dataless/control_merge.v
@@ -35,8 +35,8 @@ module control_merge_dataless #(
   end
 
   // Instantiate Merge_dataless
-  merge_notehb_dataless #(
-    .INPUTS(SIZE)
+  merge_dataless #(
+    .SIZE(SIZE)
   ) merge_ins (
     .clk        (clk          ),
     .rst        (rst          ),
