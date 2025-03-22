768d363beff19ea60ccf5a76631a3dfa078a7557
QinYuan2000 <qinyuan123123@gmail.com>
2025-03-22 21:07:07 +0100

    Remove all logics considering internal buffers

--- lib/Transforms/BufferPlacement/HandshakeSetBufferingProperties.cpp
+++ lib/Transforms/BufferPlacement/HandshakeSetBufferingProperties.cpp
@@ -107,14 +107,6 @@ static void setLSQControlConstraints(handshake::LSQOp lsqOp) {
 }
 
 void dynamatic::buffer::setFPGA20Properties(handshake::FuncOp funcOp) {
-  // Merges with more than one input should have at least a transparent slot
-  // at their output
-  for (handshake::MergeOp mergeOp : funcOp.getOps<handshake::MergeOp>()) {
-    if (mergeOp->getNumOperands() > 1) {
-      Channel channel(mergeOp.getResult(), true);
-      channel.props->minTrans = std::max(channel.props->minTrans, 1U);
-    }
-  }
 
   // Memrefs are not real edges in the graph and are therefore unbufferizable
   for (BlockArgument arg : funcOp.getArguments())
