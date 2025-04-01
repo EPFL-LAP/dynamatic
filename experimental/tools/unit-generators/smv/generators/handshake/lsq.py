cec47824f88d89b47be8ec221047a1cd08deadb5
Gioele Gottardo <giogot2000@gmail.com>
2025-04-01 16:51:47 +0200

    Add mc_control util

--- experimental/tools/unit-generators/smv/generators/handshake/lsq.py
+++ experimental/tools/unit-generators/smv/generators/handshake/lsq.py
@@ -24,26 +24,37 @@ MODULE {name} ({lsq_in_ports})
 
   {"\n  ".join([f"VAR inner_load_port_{n} : nd_load_port_{n}(io_ctrl_{n}_valid, io_ldAddr_{n}_bits, io_ldAddr_{n}_valid, io_ldData_{n}_ready, io_loadData)" for n in range(num_load_ports)])}
   {"\n  ".join([f"VAR inner_store_port_{n} : nd_store_port_{n}(io_ctrl_{n + num_load_ports}_valid, io_stAddr_{n}_bits, io_stAddr_{n}_valid, io_stData_{n}_bits, io_stData_{n}_valid)" for n in range(num_load_ports)])}
+  VAR inner_mc_control : {name}__mc_control(io_memStart_valid, io_ctrlEnd_valid, io_memEnd_ready, all_requests_done);
+
+  -- all_requests_done is true when all the load and store ports have completed the operation
+  DEFINE all_requests_done := {" & ".join([f"io_ctrl_{n}_ready" for n in range(num_load_ports + num_store_ports)])};
 
   -- output
   DEFINE
-  -- this comes from an mc control 
+  -- We decide not to model these signals as they are connected only to memory, but
+  -- we are now simulating the memory behavior through ndwires
   io_storeData := {data_type.format_constant(0)};
   io_storeAddress := {addr_type.format_constant(0)};
   io_storeEn := FALSE;
   io_loadAddr := {addr_type.format_constant(0)};
   io_loadEn := FALSE;
-  -- this we create nondeterministic component like in the paper for each port
-  {"\n  ".join([f"io_ctrl_{n}_ready := TRUE;" for n in range(num_load_ports + num_store_ports)])}
-  {"\n  ".join([f"io_ldAddr_{n}_ready := TRUE;" for n in range(num_load_ports)])}
-  {"\n  ".join([f"io_ldData_{n}_bits := {data_type.format_constant(0)};" for n in range(num_load_ports)])}
-  {"\n  ".join([f"io_ldData_{n}_valid := TRUE;" for n in range(num_load_ports)])}
-  {"\n  ".join([f"io_stAddr_{n}_ready := TRUE;" for n in range(num_store_ports)])}
-  {"\n  ".join([f"io_stData_{n}_ready := TRUE;" for n in range(num_store_ports)])}
-  io_memStart_ready :=
-  io_ctrlEnd_ready :=
-  io_memEnd_valid :=
+
+  -- non-deterministic signals from the non-deterministic ports
+  {"\n  ".join([f"io_ctrl_{n}_ready := inner_load_port_{n}.ctrl_ready;" for n in range(num_load_ports)])}
+  {"\n  ".join([f"io_ldAddr_{n}_ready := inner_load_port_{n}.ldAddr_ready;" for n in range(num_load_ports)])}
+  {"\n  ".join([f"io_ldData_{n}_bits := inner_load_port_{n}.ldData;" for n in range(num_load_ports)])}
+  {"\n  ".join([f"io_ldData_{n}_valid := inner_load_port_{n}.ldData_valid;" for n in range(num_load_ports)])}
+
+  {"\n  ".join([f"io_ctrl_{n + num_load_ports}_ready := inner_store_port_{n}.ctrl_ready;" for n in range(num_store_ports)])}
+  {"\n  ".join([f"io_stAddr_{n}_ready := inner_store_port_{n}.stAddr_ready;;" for n in range(num_store_ports)])}
+  {"\n  ".join([f"io_stData_{n}_ready := inner_store_port_{n}.stData_ready;;" for n in range(num_store_ports)])}
+
+  -- control signals from mc_control
+  io_memStart_ready := inner_mc_control.memStart_ready;
+  io_ctrlEnd_ready := inner_mc_control.ctrlEnd_ready;
+  io_memEnd_valid := inner_mc_control.memEnd_valid;
 """
+
 
 def _generate_nd_load_port(name, capacity, addr_type, data_type):
     return f"""
@@ -52,11 +63,16 @@ MODULE {name} (ctrl_valid, ldAddr, ldAddr_valid, ldData_ready, data_from_mem)
   VAR inner_capacity : capacity(inner_input_ndw.outs, inner_input_ndw.outs_valid, inner_output_ndw.ins_ready);
   VAR inner_output_ndw : {name}__out_ndwire(data_from_mem, inner_capacity.outs_valid, ldData_ready);
 
+  -- ctrl_valid tells the port when it can start running and ctrl_ready
+  -- informs the outside that the operation is done. This is equivalent 
+  -- to the memory controller ctrl signal of size 1
+
   -- output
   DEFINE
-  ldAddr_ready := inner_input_ndw.ins_ready;
+  ldAddr_ready := inner_input_ndw.ins_ready & ctrl_valid;
   ldData := inner_output_ndw.outs;
-  ldData_valid := inner_output_ndw.outs_valid;
+  ldData_valid := inner_output_ndw.outs_valid & ctrl_valid;
+  ctrl_ready := ldData_valid;
 
   {generate_ndwire(f"{name}__in_ndwire", {ATTR_PORT_TYPES: {"ins": addr_type.mlir_type}})}
   {generate_ndwire(f"{name}__out_ndwire", {ATTR_PORT_TYPES: {"ins": data_type.mlir_type}})}
@@ -75,8 +91,9 @@ MODULE {name} (ctrl_valid, stAddr, stAddr_valid, stData, stData_valid)
 
   -- output
   DEFINE
-  stAddr_ready := inner_addr_ndw.ins_ready;
-  stData_ready := inner_data_ndw.ins_ready;
+  stAddr_ready := inner_addr_ndw.ins_ready & ctrl_valid;
+  stData_ready := inner_data_ndw.ins_ready & ctrl_valid;
+  ctrl_ready := stAddr_ready & stData_ready;
 
   {generate_ndwire(f"{name}__addr_ndwire", {ATTR_PORT_TYPES: {"ins": addr_type.mlir_type}})}
   {generate_ndwire(f"{name}__data_ndwire", {ATTR_PORT_TYPES: {"ins": data_type.mlir_type}})}
