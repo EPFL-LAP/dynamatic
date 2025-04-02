7fddf3c850f42ae084cb44f4f4c2ac3c23ca5a42
Gioele Gottardo <giogot2000@gmail.com>
2025-04-02 11:17:45 +0200

    Add ofifo

--- experimental/tools/unit-generators/smv/generators/handshake/lsq.py
+++ experimental/tools/unit-generators/smv/generators/handshake/lsq.py
@@ -1,4 +1,5 @@
 from generators.support.utils import *
+from generators.support.oehb import generate_oehb
 from generators.handshake.ndwire import generate_ndwire
 import json
 
@@ -10,11 +11,12 @@ def generate_lsq(name, params):
   data_type = SmvScalarType(params[ATTR_PORT_TYPES]["ldData_0"])
   num_load_ports = config["numLoadPorts"]
   num_store_ports = config["numStorePorts"]
+  capacity = config["fifoDepth"]
   if config["master"]:
-    return _generate_lsq_master(name, addr_type, data_type, num_load_ports, num_store_ports)
+    return _generate_lsq_master(name, addr_type, data_type, num_load_ports, num_store_ports, capacity)
 
 
-def _generate_lsq_master(name, addr_type, data_type, num_load_ports, num_store_ports):
+def _generate_lsq_master(name, addr_type, data_type, num_load_ports, num_store_ports, capacity):
   load_addr = [f"io_ctrl_{n}_valid, io_ldAddr_{n}_bits, io_ldAddr_{n}_valid" for n in range(num_load_ports)]
   store_addr = [f"io_ctrl_{n + num_load_ports}_valid, io_stAddr_{n}_bits, io_stAddr_{n}_valid, io_stData_{n}_bits, io_stData_{n}_valid" for n in range(num_store_ports)]
   load_data = [f"io_ldData_{n}_ready" for n in range(num_load_ports)]
@@ -22,8 +24,8 @@ def _generate_lsq_master(name, addr_type, data_type, num_load_ports, num_store_p
   return f"""
 MODULE {name} ({lsq_in_ports})
 
-  {"\n  ".join([f"VAR inner_load_port_{n} : nd_load_port_{n}(io_ctrl_{n}_valid, io_ldAddr_{n}_bits, io_ldAddr_{n}_valid, io_ldData_{n}_ready, io_loadData)" for n in range(num_load_ports)])}
-  {"\n  ".join([f"VAR inner_store_port_{n} : nd_store_port_{n}(io_ctrl_{n + num_load_ports}_valid, io_stAddr_{n}_bits, io_stAddr_{n}_valid, io_stData_{n}_bits, io_stData_{n}_valid)" for n in range(num_load_ports)])}
+  {"\n  ".join([f"VAR inner_load_port_{n} : {name}__nd_load_port(io_ctrl_{n}_valid, io_ldAddr_{n}_bits, io_ldAddr_{n}_valid, io_ldData_{n}_ready, io_loadData)" for n in range(num_load_ports)])}
+  {"\n  ".join([f"VAR inner_store_port_{n} : {name}__nd_store_port(io_ctrl_{n + num_load_ports}_valid, io_stAddr_{n}_bits, io_stAddr_{n}_valid, io_stData_{n}_bits, io_stData_{n}_valid)" for n in range(num_load_ports)])}
   VAR inner_mc_control : {name}__mc_control(io_memStart_valid, io_ctrlEnd_valid, io_memEnd_ready, all_requests_done);
 
   -- all_requests_done is true when all the load and store ports have completed the operation
@@ -53,14 +55,18 @@ MODULE {name} ({lsq_in_ports})
   io_memStart_ready := inner_mc_control.memStart_ready;
   io_ctrlEnd_ready := inner_mc_control.ctrlEnd_ready;
   io_memEnd_valid := inner_mc_control.memEnd_valid;
+
+  {_generate_nd_load_port(f"{name}__nd_load_port", capacity, addr_type, data_type)}
+  {_generate_nd_store_port(f"{name}__nd_store_port", capacity, addr_type, data_type)}
 """
 
 
 def _generate_nd_load_port(name, capacity, addr_type, data_type):
     return f"""
 MODULE {name} (ctrl_valid, ldAddr, ldAddr_valid, ldData_ready, data_from_mem)
+  -- what is the capacity: elastic fifo, ofifo, tfifo???
   VAR inner_input_ndw : {name}__in_ndwire(ldAddr, ldAddr_valid, inner_capacity.ins_ready);
-  VAR inner_capacity : capacity(inner_input_ndw.outs, inner_input_ndw.outs_valid, inner_output_ndw.ins_ready);
+  VAR inner_capacity : {name}__ofifo(inner_input_ndw.outs, inner_input_ndw.outs_valid, inner_output_ndw.ins_ready);
   VAR inner_output_ndw : {name}__out_ndwire(data_from_mem, inner_capacity.outs_valid, ldData_ready);
 
   -- ctrl_valid tells the port when it can start running and ctrl_ready
@@ -74,17 +80,18 @@ MODULE {name} (ctrl_valid, ldAddr, ldAddr_valid, ldData_ready, data_from_mem)
   ldData_valid := inner_output_ndw.outs_valid & ctrl_valid;
   ctrl_ready := ldData_valid;
 
-  {generate_ndwire(f"{name}__in_ndwire", {ATTR_PORT_TYPES: {"ins": addr_type.mlir_type}})}
-  {generate_ndwire(f"{name}__out_ndwire", {ATTR_PORT_TYPES: {"ins": data_type.mlir_type}})}
+  {generate_ndwire(f"{name}__in_ndwire", {ATTR_PORT_TYPES: {"outs": addr_type.mlir_type}})}
+  {generate_oehb(f"{name}__ofifo", {ATTR_DATA_TYPE: addr_type.mlir_type})}
+  {generate_ndwire(f"{name}__out_ndwire", {ATTR_PORT_TYPES: {"outs": data_type.mlir_type}})}
 """
 
 def _generate_nd_store_port(name, capacity, addr_type, data_type):
     return f"""
 MODULE {name} (ctrl_valid, stAddr, stAddr_valid, stData, stData_valid)
   VAR inner_addr_ndw : {name}__addr_ndwire(stAddr, stAddr_valid, inner_addr_capacity.ins_ready);
-  VAR inner_addr_capacity : capacity(inner_addr_ndw.stAddr, inner_addr_ndw.outs_valid, inner_join.ins_0_ready);
+  VAR inner_addr_capacity : {name}__addr_ofifo(inner_addr_ndw.stAddr, inner_addr_ndw.outs_valid, inner_join.ins_0_ready);
   VAR inner_data_ndw : {name}__data_ndwire(stData, stData_valid, inner_data_capacity.ins_ready);
-  VAR inner_data_capacity : capacity(inner_data_ndw.stAddr, inner_data_ndw.outs_valid, inner_join.ins_1_ready);
+  VAR inner_data_capacity : {name}__data_ofifo(inner_data_ndw.stAddr, inner_data_ndw.outs_valid, inner_join.ins_1_ready);
   VAR inner_join : join_dataless(inner_addr_capacity.outs_valid, inner_data_capacity.outs_valid);
   VAR inner_sink_ndw : {name}__data_ndwire(inner_data_ndw.outs, inner_join.outs_valid, sink_ins_ready);
   VAR inner_sink : sink(inner_data_ndw.outs, inner_sink_ndw.outs_valid);
@@ -95,6 +102,8 @@ MODULE {name} (ctrl_valid, stAddr, stAddr_valid, stData, stData_valid)
   stData_ready := inner_data_ndw.ins_ready & ctrl_valid;
   ctrl_ready := stAddr_ready & stData_ready;
 
-  {generate_ndwire(f"{name}__addr_ndwire", {ATTR_PORT_TYPES: {"ins": addr_type.mlir_type}})}
-  {generate_ndwire(f"{name}__data_ndwire", {ATTR_PORT_TYPES: {"ins": data_type.mlir_type}})}
+  {generate_ndwire(f"{name}__addr_ndwire", {ATTR_PORT_TYPES: {"outs": addr_type.mlir_type}})}
+  {generate_oehb(f"{name}__addr_ofifo", {ATTR_DATA_TYPE: addr_type.mlir_type})}
+  {generate_ndwire(f"{name}__data_ndwire", {ATTR_PORT_TYPES: {"outs": data_type.mlir_type}})}
+  {generate_oehb(f"{name}__data_ofifo", {ATTR_DATA_TYPE: data_type.mlir_type})}
 """
