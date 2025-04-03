7d3d289478c92dbc21884c4573203a84074c2350
shundroid <shundroid@users.noreply.github.com>
2025-04-03 17:26:32 +0200

    small fix

--- integration-test/nested_loop/nested_loop_original.c
+++ integration-test/nested_loop/nested_loop_original.c
@@ -1,9 +1,9 @@
-#include "nested_loop.h"
 #include "dynamatic/Integration.h"
+#include "nested_loop.h"
 #include "stdlib.h"
 
 void nested_loop(in_int_t a[N], in_int_t b[N], inout_int_t c[N]) {
-  for(int j = 0; j < 2; j++){
+  for (int j = 0; j < 2; j++) {
     int i = 0;
     int bound = 1000;
     int sum = 0;
