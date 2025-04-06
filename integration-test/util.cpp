1667a826fc0f0ceaf5eed574ffb9c261a55f1820
Eugen Bosnjak <eugen.bosnjak@epfl.ch>
2025-04-06 17:31:37 +0200

    Fixed style issues

--- integration-test/util.cpp
+++ integration-test/util.cpp
@@ -37,22 +37,22 @@ std::vector<fs::path> findTests(const fs::path& start) {
 
 int runIntegrationTest(const fs::path& path) {
   std::string name = path.stem();
-  std::cout << "Running " << name << std::endl;
+  std::cout << "[INFO] Running " << name << std::endl;
   std::string tmpFilename = "tmp_" + name + ".dyn";
-  std::ofstream script_file(tmpFilename);
-  if (!script_file.is_open()) {
-    std::cout << "Failed to create .dyn script file" << std::endl;
+  std::ofstream scriptFile(tmpFilename);
+  if (!scriptFile.is_open()) {
+    std::cout << "[ERROR] Failed to create .dyn script file" << std::endl;
     return -1;
   } 
 
-  script_file << "set-dynamatic-path " << DYNAMATIC_ROOT << std::endl
+  scriptFile << "set-dynamatic-path " << DYNAMATIC_ROOT << std::endl
     << "set-src " << path.string() << std::endl
     << "compile" << std::endl
     << "write-hdl" << std::endl
     << "simulate" << std::endl
     << "exit" << std::endl;
   
-  script_file.close();
+  scriptFile.close();
 
   fs::path dynamaticPath = fs::path(DYNAMATIC_ROOT) / "bin" / "dynamatic";
   std::string cmd = dynamaticPath.string() + " --exit-on-failure --run ";
