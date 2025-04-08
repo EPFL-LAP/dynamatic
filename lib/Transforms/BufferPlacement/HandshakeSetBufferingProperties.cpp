d5926683177f73b486ec0072c0b90ca3317033f9
QinYuan2000 <qinyuan123123@gmail.com>
2025-04-08 03:33:36 +0200

    clean up and add R buffer

--- lib/Transforms/BufferPlacement/HandshakeSetBufferingProperties.cpp
+++ lib/Transforms/BufferPlacement/HandshakeSetBufferingProperties.cpp
@@ -114,12 +114,7 @@ void dynamatic::buffer::setFPGA20Properties(handshake::FuncOp funcOp) {
     if (mergeOp->getNumOperands() > 1) {
       for (OpResult mergeRes : mergeOp->getResults()) {
         Channel channel(mergeRes, true);
-        // if (!mergeRes.use_empty()) {
-        //   Operation* nextOp = mergeRes.getUses().begin()->getOwner();
-          // if (isa<handshake::ForkOp>(nextOp)) {
         channel.props->minSlots = std::max(channel.props->minSlots, 1U);
-          // }
-        // }
       }
     }
   }
