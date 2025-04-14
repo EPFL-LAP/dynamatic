6501716843a85b2e40d0eb8708fbd6e92e700112
Gioele Gottardo <giogot2000@gmail.com>
2025-04-14 10:54:27 +0200

    Fix

--- data/verilog/support/merge_notehb.v
+++ data/verilog/support/merge_notehb.v
@@ -17,20 +17,20 @@ module merge_notehb #(
   reg [INPUTS - 1 : 0] tmp_ready_out;
   reg [DATA_TYPE - 1 : 0] tmp_data_out;
 	integer i;
-  integer cont;
+  integer cnt;
 
 	always @(*) begin
 		tmp_valid_out = 0;
     tmp_ready_out = {INPUTS{1'b0}}; 
     tmp_data_out = data_in_bus[0 * DATA_TYPE +: DATA_TYPE];
 
-    cont = 1;
+    cnt = 1;
 		for (i = 0; i < INPUTS; i = i + 1) begin
-			if (cont == 1 && ins_valid[i]) begin
+			if (cnt == 1 && ins_valid[i]) begin
         tmp_data_out = ins[i * DATA_TYPE +: DATA_TYPE];
 				tmp_valid_out = 1;
         tmp_ready_out[i] = outs_ready;
-        cont = 0;
+        cnt = 0;
 			end
 		end
 	end
