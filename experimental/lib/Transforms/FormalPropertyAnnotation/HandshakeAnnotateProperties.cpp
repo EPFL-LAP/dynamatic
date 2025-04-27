72acd67349a1b4b4ff6c86c92e1b8704984a103c
Gioele Gottardo <giogot2000@gmail.com>
2025-04-27 19:14:41 +0200

    Pass skeleton

---
+++ experimental/lib/Transforms/FormalPropertyAnnotation/HandshakeAnnotateProperties.cpp
@@ -0,0 +1,65 @@
+//===- HandshakeSizeLSQs.cpp - LSQ Sizing -----------------------*- C++ -*-===//
+//
+// Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the --handshake-annotate-properties pass.
+//
+//===----------------------------------------------------------------------===//
+
+#include "experimental/Transforms/FormalPropertyAnnotation/HandshakeAnnotateProperties.h"
+#include "dynamatic/Dialect/Handshake/HandshakeAttributes.h"
+#include "dynamatic/Dialect/Handshake/HandshakeDialect.h"
+#include "dynamatic/Dialect/Handshake/HandshakeInterfaces.h"
+#include "dynamatic/Dialect/Handshake/HandshakeOps.h"
+#include "dynamatic/Dialect/Handshake/MemoryInterfaces.h"
+#include "dynamatic/Support/Attribute.h"
+#include "dynamatic/Support/Backedge.h"
+#include "dynamatic/Support/CFG.h"
+#include "dynamatic/Support/DynamaticPass.h"
+#include "dynamatic/Support/TimingModels.h"
+#include "dynamatic/Transforms/BufferPlacement/CFDFC.h"
+#include "mlir/IR/Value.h"
+#include "mlir/Pass/PassManager.h"
+#include "llvm/Support/Debug.h"
+
+using namespace llvm;
+using namespace mlir;
+using namespace dynamatic;
+using namespace dynamatic::buffer;
+using namespace dynamatic::handshake;
+using namespace dynamatic::experimental;
+using namespace dynamatic::experimental::formalprop;
+
+namespace {
+
+struct HandshakeAnnotatePropertiesPass
+    : public dynamatic::experimental::formalprop::impl::
+          HandshakeAnnotatePropertiesBase<HandshakeAnnotatePropertiesPass> {
+
+  HandshakeAnnotatePropertiesPass(const std::string &jsonPath = "") {
+    this->jsonPath = jsonPath;
+  }
+
+  // HandshakeAnnotatePropertiesPass(StringRef timingModels,
+  //                                 StringRef collisions) {
+  //   this->timingModels = timingModels.str();
+  //   this->collisions = collisions.str();
+  // }
+
+  void runDynamaticPass() override;
+
+private:
+};
+} // namespace
+
+void HandshakeAnnotatePropertiesPass::runDynamaticPass() {}
+
+std::unique_ptr<dynamatic::DynamaticPass>
+dynamatic::experimental::formalprop::createAnnotateProperties(
+    const std::string &jsonPath) {
+  return std::make_unique<HandshakeAnnotatePropertiesPass>(jsonPath);
+}
