804b61d80b3bee5a101e83ddf5142ea64d0ef933
QinYuan2000 <qinyuan123123@gmail.com>
2025-05-01 01:56:22 +0200

    update

--- lib/Transforms/BufferPlacement/FPL22Buffers.cpp
+++ lib/Transforms/BufferPlacement/FPL22Buffers.cpp
@@ -67,14 +67,16 @@ void FPL22BuffersBase::extractResult(BufferPlacement &placement) {
         result.numOneSlotDV = 1;
         result.numOneSlotR = 1;
       } else {
-        result.numFifoDV = numSlotsToPlace - 1;
+        result.numOneSlotDV = 1;
+        result.numFifoNone = numSlotsToPlace - 2;
         result.numOneSlotR = 1;
       }
     } else if (forceBreakDV) {
       if (numSlotsToPlace == 1) {
         result.numOneSlotDV = 1;
       } else {
-        result.numFifoNone = numSlotsToPlace;
+        result.numOneSlotDV = 1;
+        result.numFifoNone = numSlotsToPlace - 1;
       }
     } else {
       if (numSlotsToPlace == 1) {
