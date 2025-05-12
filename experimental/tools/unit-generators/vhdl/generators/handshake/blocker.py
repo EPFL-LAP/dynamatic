3c2e24ef9ffd99acb6755a19b99a2bc60ff777da
Dana Kossaybati <danak.2003@icloud.com>
2025-05-12 17:26:56 +0200

    Removed data-less blocker

--- experimental/tools/unit-generators/vhdl/generators/handshake/blocker.py
+++ experimental/tools/unit-generators/vhdl/generators/handshake/blocker.py
@@ -1,6 +1,7 @@
 from generators.support.signal_manager import generate_signal_manager
 from generators.handshake.join import generate_join
 
+
 def generate_blocker(name, params):
     # Number of input ports
   size = params["size"]
@@ -10,60 +11,14 @@ def generate_blocker(name, params):
 
   if extra_signals:
     return _generate_blocker_signal_manager(name, size, bitwidth, extra_signals)
-  elif bitwidth == 0:
-    return _generate_blocker_dataless(name, size)
   else:
     return _generate_blocker(name, size, bitwidth)
-
-def _generate_blocker_dataless(name, size):
-  join_name = f"{name}_join"
-
-  dependencies = generate_join(join_name, {"size": size})
-
-  entity = f"""
-library ieee;
-use ieee.std_logic_1164.all;
-use work.types.all;
-
--- Entity of blocker
-entity {name} is
-  port (
-    clk          : in std_logic;
-    rst          : in std_logic;
-    -- input channels
-    ins_valid  : in std_logic_vector({size} - 1 downto 0);
-    outs_ready : in std_logic;
-    -- output channel
-    outs_valid : out std_logic;
-    ins_ready  : out std_logic_vector({size} - 1 downto 0)
-  );
-end entity;
-"""
-
-  architecture = f"""
--- Architecture of blocker
-architecture arch of {name} is
-begin
-  join_inputs : entity work.{join_name}(arch)
-    port map(
-      -- inputs
-      ins_valid    => ins_valid,
-      outs_ready   => outs_ready,
-      -- outputs
-      outs_valid   => outs_valid,
-      ins_ready    => ins_ready
-    );
-end architecture;
-"""
-
-  return dependencies + entity + architecture
 
 
 def _generate_blocker(name, size, bitwidth):
   join_name = f"{name}_join"
 
   dependencies = generate_join(join_name, {"size": size})
-
 
   entity = f"""
 library ieee;
@@ -125,5 +80,4 @@ def _generate_blocker_signal_manager(name, size, bitwidth, extra_signals):
           "extra_signals": extra_signals
       }],
       "extra_signals": extra_signals
-  }, lambda name: _generate_blocker_dataless(name, size) if bitwidth == 0
-      else _generate_blocker(name, size, bitwidth))
+  }, lambda name:  _generate_blocker(name, size, bitwidth))
