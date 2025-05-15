97530885e375ede1cb8ce57502b088646f1fe7ab
Jiahui Xu <jxu@ethz.ch>
2025-05-15 14:12:54 +0200

    update test.sh

--- experimental/tools/vivado-ip-adapter/test.sh
+++ experimental/tools/vivado-ip-adapter/test.sh
@@ -1,2 +1,7 @@
 build/bin/vivado-ip-adapter \
-  "integration-test/iir/out/comp/handshake_export.mlir"
+  "integration-test/iir/out/comp/handshake_export.mlir" \
+  > /tmp/wrapper.v
+
+
+/data/verilator/bin/verilator \
+  -Wall --lint-only /tmp/wrapper.v
