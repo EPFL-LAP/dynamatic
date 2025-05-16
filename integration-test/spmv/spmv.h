9149e7315790206b1a0f5089984463018f4709ea
Jiahui Xu <jxu@ethz.ch>
2025-05-16 20:07:33 +0200

    Fix CSR format for spmv

--- integration-test/spmv/spmv.h
+++ integration-test/spmv/spmv.h
@@ -4,7 +4,7 @@
 typedef int in_int_t;
 typedef int inout_int_t;
 
-int spmv(in_int_t n, inout_int_t row[10], inout_int_t col[10],
+int spmv(in_int_t n, inout_int_t row[11], inout_int_t col[10],
          inout_int_t val[10], inout_int_t vec[10], inout_int_t out[10]);
 
 #endif // SPMV_SPMV_H
