361bfe4b7c63674d67988788c13b46f6dc2864a0
Gioele Gottardo <giogot2000@gmail.com>
2025-05-21 10:23:40 +0200

    Fix file headers

--- experimental/include/experimental/Transforms/FormalPropertyAnnotation/HandshakeAnnotateProperties.h
+++ experimental/include/experimental/Transforms/FormalPropertyAnnotation/HandshakeAnnotateProperties.h
@@ -1,4 +1,4 @@
-//===- HandshakeSpeculation.h - Speculation units placement -----*- C++ -*-===//
+//===- HandshakeAnnotateProperties.h - Property annotation ------*- C++ -*-===//
 //
 // Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- experimental/lib/Transforms/FormalPropertyAnnotation/HandshakeAnnotateProperties.cpp
+++ experimental/lib/Transforms/FormalPropertyAnnotation/HandshakeAnnotateProperties.cpp
@@ -1,4 +1,4 @@
-//===- HandshakeSizeLSQs.cpp - LSQ Sizing -----------------------*- C++ -*-===//
+//===- HandshakeAnnotateProperties.cpp - Property annotation ----*- C++ -*-===//
 //
 // Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
