178746219422cbc90a3990a2752e52bc56f06179
Gioele Gottardo <giogot2000@gmail.com>
2025-06-05 16:13:04 +0200

    Reuse dataless models

--- experimental/tools/unit-generators/smv/generators/handshake/merge.py
+++ experimental/tools/unit-generators/smv/generators/handshake/merge.py
@@ -30,11 +30,10 @@ MODULE {name}({", ".join([f"ins_{n}_valid" for n in range(size)])}, outs_ready)
 def _generate_merge(name, size, data_type):
     return f"""
 MODULE {name}({", ".join([f"ins_{n}" for n in range(size)])}, {", ".join([f"ins_{n}_valid" for n in range(size)])}, outs_ready)
+  VAR
+  inner_merge : {name}__merge_dataless({", ".join([f"ins_{n}_valid" for n in range(size)])}, outs_ready);
 
   DEFINE
-  one_valid := {' | '.join([f'ins_{i}_valid' for i in range(size)])};
-  in_ins_0_ready := ins_0_valid ? outs_ready : FALSE;
-  {"\n  ".join([f"in_ins_{n + 1}_ready := (ins_{n + 1}_valid & !({' | '.join([f'in_ins_{i}_ready' for i in range(n + 1)])})) ? outs_ready : FALSE;" for n in range(size - 1)])}
   data := case
     {"\n    ".join([f"ins_{n}_valid : ins_{n};" for n in range(size)])}
     TRUE : {data_type.format_constant(0)};
@@ -42,7 +41,9 @@ MODULE {name}({", ".join([f"ins_{n}" for n in range(size)])}, {", ".join([f"ins_
 
   -- output
   DEFINE
-  {"\n  ".join([f"ins_{n}_ready := in_ins_{n}_ready;" for n in range(size)])}
-  outs_valid := one_valid;
+  {"\n  ".join([f"ins_{n}_ready := inner_merge.ins_{n}_ready;" for n in range(size)])}
+  outs_valid := inner_merge.outs_valid;
   outs := data;
+
+{_generate_merge_dataless(f"{name}__merge_dataless", size)}
 """
--- experimental/tools/unit-generators/smv/generators/handshake/mux.py
+++ experimental/tools/unit-generators/smv/generators/handshake/mux.py
@@ -33,20 +33,21 @@ MODULE {name}(index, index_valid, {", ".join([f"ins_{n}_valid" for n in range(si
 def _generate_mux(name, size, data_type, select_type):
     return f"""
 MODULE {name}(index, index_valid, {", ".join([f"ins_{n}" for n in range(size)])}, {", ".join([f"ins_{n}_valid" for n in range(size)])}, outs_ready)
+  VAR
+  inner_mux : {name}__mux_dataless(index, index_valid, {", ".join([f"ins_{n}_valid" for n in range(size)])}, outs_ready);
+
   DEFINE
   inner_outs := case
     {"\n    ".join([f"index = {select_type.format_constant(n)} & index_valid & ins_{n}_valid : ins_{n};" for n in range(size)])}
     TRUE : ins_0;
   esac;
-  inner_outs_valid := case
-    {"\n    ".join([f"index = {select_type.format_constant(n)} : index_valid & ins_{n}_valid;" for n in range(size)])}
-    TRUE : FALSE;
-  esac;
 
   -- output
   DEFINE
-  {"\n  ".join([f"ins_{n}_ready := index = {select_type.format_constant(n)} & index_valid & outs_ready & ins_{n}_valid | !ins_{n}_valid;" for n in range(size)])}
-  index_ready := !index_valid | inner_outs_valid & outs_ready;
-  outs_valid := inner_outs_valid;
+  {"\n  ".join([f"ins_{n}_ready := inner_mux.ins_{n}_ready;" for n in range(size)])}
+  index_ready := !inner_mux.index_ready;
+  outs_valid := inner_mux.outs_valid;
   outs := inner_outs;
+
+{_generate_mux_dataless(f"{name}__mux_dataless", size, select_type)}
 """
