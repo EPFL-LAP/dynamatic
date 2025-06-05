0b27696f4bcd4231f66f4e80b8820ad0f7846033
Gioele Gottardo <giogot2000@gmail.com>
2025-06-05 11:05:33 +0200

    Use formatv

--- experimental/lib/Support/CreateSmvFormalTestbench.cpp
+++ experimental/lib/Support/CreateSmvFormalTestbench.cpp
@@ -42,10 +42,12 @@ static std::string instantiateModuleUnderTest(
 
     llvm::TypeSwitch<Type>(argumentType)
         .Case<handshake::ChannelType>([&](auto) {
-          inputVariables.push_back("seq_generator_" + argumentName + "." +
-                                   SEQUENCE_GENERATOR_DATA_NAME.str());
-          inputVariables.push_back("seq_generator_" + argumentName + "." +
-                                   SEQUENCE_GENERATOR_VALID_NAME.str());
+          inputVariables.push_back(
+              llvm::formatv("seq_generator_{0}.{1}", argumentName,
+                            SEQUENCE_GENERATOR_DATA_NAME.str()));
+          inputVariables.push_back(
+              llvm::formatv("seq_generator_{0}.{1}", argumentName,
+                            SEQUENCE_GENERATOR_VALID_NAME.str()));
         })
         .Case<handshake::ControlType>([&](auto) {
           // TODO: remove this if statement when updating the elastic-miter to
@@ -56,21 +58,24 @@ static std::string instantiateModuleUnderTest(
           // is always true). This hack can be fixed as soon as elastic-miter is
           // updated.
           if (syncOutput) {
-            inputVariables.push_back("seq_generator_" + argumentName + "." +
-                                     SEQUENCE_GENERATOR_VALID_NAME.str());
+            inputVariables.push_back(
+                llvm::formatv("seq_generator_{0}.{1}", argumentName,
+                              SEQUENCE_GENERATOR_VALID_NAME.str()));
           } else {
-            inputVariables.push_back("seq_generator_" + argumentName + "." +
-                                     SEQUENCE_GENERATOR_DATA_NAME.str());
-            inputVariables.push_back("seq_generator_" + argumentName + "." +
-                                     SEQUENCE_GENERATOR_VALID_NAME.str());
+            inputVariables.push_back(
+                llvm::formatv("seq_generator_{0}.{1}", argumentName,
+                              SEQUENCE_GENERATOR_DATA_NAME.str()));
+            inputVariables.push_back(
+                llvm::formatv("seq_generator_{0}.{1}", argumentName,
+                              SEQUENCE_GENERATOR_VALID_NAME.str()));
           }
         })
         .Case<IntegerType>([&](IntegerType intType) {
           // This is the case for data coming from memory (it has no handshake
           // signals)
           if (argumentName != "clk" && argumentName != "rst") {
-            inputVariables.push_back("0ud" +
-                                     std::to_string(intType.getWidth()) + "_0");
+            inputVariables.push_back(
+                llvm::formatv("0ud{0}_0", intType.getWidth()));
           }
         })
         .Default([](Type) {});
@@ -80,14 +85,13 @@ static std::string instantiateModuleUnderTest(
     for (const auto &[i, result] : llvm::enumerate(results)) {
       const auto &[resultName, type] = result;
       if (type.isa<handshake::ControlType, handshake::ChannelType>())
-        inputVariables.push_back("join_global.ins_" + std::to_string(i) +
-                                 "_ready");
+        inputVariables.push_back(llvm::formatv("join_global.ins_{0}_ready", i));
     }
   else
     for (const auto &[resultName, type] : results) {
       if (type.isa<handshake::ControlType, handshake::ChannelType>())
-        inputVariables.push_back("sink_" + resultName + "." +
-                                 SINK_READY_NAME.str());
+        inputVariables.push_back(
+            llvm::formatv("sink_{0}.{1}", resultName, SINK_READY_NAME.str()));
     }
 
   return llvm::formatv("VAR {0} : {1} ({2});\n", moduleName, moduleName,
@@ -113,7 +117,8 @@ static std::optional<std::string> convertMLIRTypeToSMV(const Type &type) {
       .Case<handshake::ChannelType>([&](handshake::ChannelType cType) {
         if (cType.getDataBitWidth() == 1)
           return std::string("boolean");
-        return "signed word [" + std::to_string(cType.getDataBitWidth()) + "]";
+        return llvm::formatv("signed word [{0}]", cType.getDataBitWidth())
+            .str();
       });
 }
 
@@ -121,51 +126,55 @@ static std::optional<std::string> convertMLIRTypeToSMV(const Type &type) {
 // actual number of generated tokens is non-determinstically set between 0
 // and (inclusive) max_tokens.
 std::string smvInput(const Type &type) {
-  return "MODULE " + *getPrefixTypeName(type) +
-         "_input(nReady0, max_tokens)\n"
-         "  VAR outs : " +
-         *convertMLIRTypeToSMV(type) +
-         ";\n"
-         "  VAR counter : 0..31;\n"
-         "  FROZENVAR exact_tokens : 0..max_tokens;\n"
-         "  ASSIGN\n"
-         "    init(counter) := 0;\n"
-         "    next(counter) := case\n"
-         "      nReady0 & counter < exact_tokens : counter + 1;\n"
-         "      TRUE : counter;\n"
-         "    esac;\n"
-         "\n"
-         "  DEFINE outs_valid := counter < exact_tokens;\n\n";
+  return llvm::formatv(R"DELIM(
+MODULE {0}_input(nReady0, max_tokens)"
+  VAR outs : {1};
+  VAR counter : 0..31;
+  FROZENVAR exact_tokens : 0..max_tokens;
+  ASSIGN
+    init(counter) := 0;
+    next(counter) := case
+      nReady0 & counter < exact_tokens : counter + 1;
+      TRUE : counter;
+    esac;
+
+  DEFINE outs_valid := counter < exact_tokens;
+
+)DELIM",
+                       *getPrefixTypeName(type), *convertMLIRTypeToSMV(type));
 }
 
 // SMV module for a sequence generator with a finite number of tokens. The
 // number of generated tokens is exact_tokens.
 std::string smvInputExact(const Type &type) {
-  return "MODULE " + *getPrefixTypeName(type) +
-         "_input_exact(nReady0, exact_tokens)\n"
-         "  VAR outs : " +
-         *convertMLIRTypeToSMV(type) +
-         ";\n"
-         "  VAR counter : 0..31;\n"
-         "  ASSIGN\n"
-         "    init(counter) := 0;\n"
-         "    next(counter) := case\n"
-         "      nReady0 & counter < exact_tokens : counter + 1;\n"
-         "      TRUE : counter;\n"
-         "    esac;\n"
-         "\n"
-         "  DEFINE outs_valid := counter < exact_tokens;\n\n";
+  return llvm::formatv(R"DELIM(
+MODULE {0}_input_exact(nReady0, exact_tokens)"
+  VAR outs : {1};
+  VAR counter : 0..31;
+  ASSIGN
+    init(counter) := 0;
+    next(counter) := case
+      nReady0 & counter < exact_tokens : counter + 1;
+      TRUE : counter;
+    esac;
+
+  DEFINE outs_valid := counter < exact_tokens;
+
+)DELIM",
+                       *getPrefixTypeName(type), *convertMLIRTypeToSMV(type));
 }
 
 // SMV module for a sequence generator with an infinite number of tokens
 std::string smvInputInf(const Type &type) {
-  return "MODULE " + *getPrefixTypeName(type) +
-         "_input_inf(nReady0)\n"
-         "  VAR outs : " +
-         *convertMLIRTypeToSMV(type) +
-         ";\n"
-         "    -- make sure outs is persistent\n"
-         "    DEFINE outs_valid := TRUE;\n\n";
+  return llvm::formatv(R"DELIM(
+MODULE {0}_input_inf(nReady0)"
+  VAR outs : {1};
+
+  -- make sure outs is persistent
+  DEFINE outs_valid := TRUE;
+
+)DELIM",
+                       *getPrefixTypeName(type), *convertMLIRTypeToSMV(type));
 }
 
 static std::string
@@ -201,22 +210,22 @@ static std::string createTBJoin(size_t nrOfOutputs) {
   std::ostringstream tbJoin;
   std::vector<std::string> insValids;
   for (size_t i = 0; i < nrOfOutputs; i++)
-    insValids.push_back("ins_" + std::to_string(i) + "_valid");
+    insValids.push_back(llvm::formatv("ins_{0}_valid", i));
 
-  tbJoin << "MODULE tb_join (";
-  tbJoin << llvm::join(insValids, ", ");
-  tbJoin << ", outs_ready)\n";
+  tbJoin << llvm::formatv("MODULE tb_join ({0}, outs_ready)\n",
+                          llvm::join(insValids, ", "))
+                .str();
 
-  tbJoin << "  DEFINE\n  outs_valid := ";
-  tbJoin << llvm::join(insValids, " & ");
-  tbJoin << ";\n";
+  tbJoin << llvm::formatv("  DEFINE\n  outs_valid := {0};\n",
+                          llvm::join(insValids, " & "))
+                .str();
 
   for (size_t i = 0; i < nrOfOutputs; i++) {
-    tbJoin << "  ins_" << i << "_ready := ";
     std::vector<std::string> tmp = insValids;
     tmp.erase(tmp.begin() + i);
-    tbJoin << llvm::join(tmp, " & ");
-    tbJoin << " & outs_ready;\n";
+    tbJoin << llvm::formatv("  ins_{0}_ready := {1} & outs_ready;\n", i,
+                            llvm::join(tmp, " & "))
+                  .str();
   }
   tbJoin << "\n\n";
   return tbJoin.str();
@@ -320,8 +329,9 @@ instantiateSinks(const std::string &moduleName,
 
   for (const auto &[resultName, type] : results) {
     if (type.isa<handshake::ControlType, handshake::ChannelType>())
-      sinks << "  VAR sink_" << resultName << " : sink_main(" << moduleName
-            << "." << resultName << "_valid);\n";
+      sinks << llvm::formatv("  VAR sink_{0} : sink_main({1}.{0}_valid);\n",
+                             resultName, moduleName)
+                   .str();
   }
   return sinks.str();
 }
@@ -336,7 +346,8 @@ instantiateJoin(const std::string &moduleName,
   str << "  VAR join_global : tb_join(";
   for (const auto &[resultName, type] : results) {
     if (type.isa<handshake::ControlType, handshake::ChannelType>())
-      outputValids.push_back(moduleName + "." + resultName + "_valid");
+      outputValids.push_back(
+          llvm::formatv("{0}.{1}_valid", moduleName, resultName));
   }
   str << llvm::join(outputValids, ", ") << ", global_ready);\n";
 
