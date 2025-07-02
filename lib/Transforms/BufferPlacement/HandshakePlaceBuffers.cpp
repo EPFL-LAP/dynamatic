dc3eefe3cd344ae551af72b76f1737d540b2499f
Emmet Murphy <murphe67@tcd.ie>
2025-07-02 16:54:40 +0200

    fix format 5

--- lib/Transforms/BufferPlacement/HandshakePlaceBuffers.cpp
+++ lib/Transforms/BufferPlacement/HandshakePlaceBuffers.cpp
@@ -163,7 +163,7 @@ void HandshakePlaceBuffersPass::runDynamaticPass() {
   if (failed(TimingDatabase::readFromJSON(timingModels, timingDB)))
     llvm::errs() << "=== TimindDB read failed ===\n";
   modOp.walk([&](mlir::Operation *op) {
-    if (auto arithInterface = 
+      if (auto arithInterface = 
             llvm::dyn_cast<dynamatic::handshake::ArithOpInterface>(op)) {
       double delay;
       if (!failed(timingDB.getInternalCombinationalDelay(op, SignalType::DATA,
