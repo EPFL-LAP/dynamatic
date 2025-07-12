20a6be2be5c8c6aefa3eced61119c71b46895724
Emmet Murphy <murphe67@tcd.ie>
2025-07-12 15:24:18 +0200

    add missing files

---
+++ experimental/ftd/InitAllPasses.h
@@ -0,0 +1,31 @@
+//===- InitAllPasses.h - Experimental passes registration --------*- C++-*-===//
+//
+// Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines a helper to trigger the registration of all ftd
+// passes defined in the Dynamatic.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef FTD_INITALLPASSES_H
+#define FTD_INITALLPASSES_H
+
+#include "experimental/ftd/Transforms/Passes.h"
+
+namespace dynamatic {
+namespace experimental {
+namespace ftd {
+
+inline void registerAllPasses() {
+  registerHandshakeCombineSteeringLogic();
+}
+
+} // namespace ftd
+} // namespace experimental
+} // namespace dynamatic
+
+#endif // FTD_INITALLPASSES_H
