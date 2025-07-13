8ce1d535692a96921f42dfd2c20267c86c18df5f
shundroid <shundroid@users.noreply.github.com>
2025-07-13 15:55:19 +0200

    fixed #529

--- lib/Transforms/BufferPlacement/BufferPlacementMILP.cpp
+++ lib/Transforms/BufferPlacement/BufferPlacementMILP.cpp
@@ -423,13 +423,6 @@ void BufferPlacementMILP::
     // Get the ports the channels connect and their retiming MILP variables
     Operation *dstOp = *channel.getUsers().begin();
 
-    // No throughput constraints on channels going to stores
-    /// TODO: this is from legacy implementation, we should understand why we
-    /// really do this and figure out if it makes sense (@lucas-rami: I don't
-    /// think it does)
-    if (isa<handshake::StoreOp>(dstOp))
-      continue;
-
     /// TODO: The legacy implementation does not add any constraints here for
     /// the input channel to select operations that is less frequently
     /// executed. Temporarily, emulate the same behavior obtained from passing
@@ -508,13 +501,6 @@ void BufferPlacementMILP::
   for (Value channel : cfdfc.channels) {
     // Get the ports the channels connect and their retiming MILP variables
     Operation *dstOp = *channel.getUsers().begin();
-
-    // No throughput constraints on channels going to stores
-    /// TODO: this is from legacy implementation, we should understand why we
-    /// really do this and figure out if it makes sense (@lucas-rami: I don't
-    /// think it does)
-    if (isa<handshake::StoreOp>(dstOp))
-      continue;
 
     /// TODO: The legacy implementation does not add any constraints here for
     /// the input channel to select operations that is less frequently
