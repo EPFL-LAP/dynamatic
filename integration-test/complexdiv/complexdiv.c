8a02e78c73a8480101e2b24290dca5ccc6f6edfe
Emmet Murphy <murphe67@tcd.ie>
2025-07-21 15:12:02 +0200

    text abs float

--- integration-test/complexdiv/complexdiv.c
+++ integration-test/complexdiv/complexdiv.c
@@ -2,24 +2,24 @@
 #include "dynamatic/Integration.h"
 #include <stdlib.h>
 
-void complexdiv(in_int_t a_i[1000], in_int_t a_r[1000], in_int_t b_i[1000],
-                in_int_t b_r[1000], out_int_t c_i[1000], out_int_t c_r[1000]) {
-  int i;
+void complexdiv(in_float_t a_i[1000], in_float_t a_r[1000], in_float_t b_i[1000],
+                in_float_t b_r[1000], out_float_t c_i[1000], out_float_t c_r[1000]) {
+  float i;
 
   for (i = 0; i < 1000; i++) {
-    int bi = b_i[i];
-    int br = b_r[i];
-    int ai = a_i[i];
-    int ar = a_r[i];
-    int cr, ci;
+    float bi = b_i[i];
+    float br = b_r[i];
+    float ai = a_i[i];
+    float ar = a_r[i];
+    float cr, ci;
     if (abs(br) >= abs(bi)) {
-      int r = bi / br;
-      int den = br + r * bi;
+      float r = bi / br;
+      float den = br + r * bi;
       cr = (ar + r * ai) / den;
       ci = (ai - r * ar) / den;
     } else {
-      int r = br / bi;
-      int den = bi + r * br;
+      float r = br / bi;
+      float den = bi + r * br;
       cr = (ar * r + ai) / den;
       ci = (ai * r - ar) / den;
     }
@@ -28,15 +28,15 @@ void complexdiv(in_int_t a_i[1000], in_int_t a_r[1000], in_int_t b_i[1000],
   }
 }
 
-int main(void) {
-  in_int_t a_i[1000];
-  in_int_t a_r[1000];
-  in_int_t b_i[1000];
-  in_int_t b_r[1000];
-  out_int_t c_i[1000];
-  out_int_t c_r[1000];
+float main(void) {
+  in_float_t a_i[1000];
+  in_float_t a_r[1000];
+  in_float_t b_i[1000];
+  in_float_t b_r[1000];
+  out_float_t c_i[1000];
+  out_float_t c_r[1000];
 
-  for (int j = 0; j < 1000; ++j) {
+  for (float j = 0; j < 1000; ++j) {
     a_i[j] = 1;
     a_r[j] = 1;
     b_i[j] = 1;
--- integration-test/complexdiv/complexdiv.h
+++ integration-test/complexdiv/complexdiv.h
@@ -1,9 +1,9 @@
 #ifndef COMPLEXDIV_COMPLEXDIV_H
 #define COMPLEXDIV_COMPLEXDIV_H
 
-typedef int in_int_t;
-typedef int out_int_t;
-void complexdiv(in_int_t a_i[1000], in_int_t a_r[1000], in_int_t b_i[1000],
-                in_int_t b_r[1000], out_int_t c_i[1000], out_int_t c_r[1000]);
+typedef float in_float_t;
+typedef float out_float_t;
+void complexdiv(in_float_t a_i[1000], in_float_t a_r[1000], in_float_t b_i[1000],
+                in_float_t b_r[1000], in_float_t c_i[1000], in_float_t c_r[1000]);
 
 #endif // COMPLEXDIV_COMPLEXDIV_H
