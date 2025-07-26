32c347557381d8b5ba019bb1e97b604faef1824a
Jiahui Xu <jxu@ethz.ch>
2025-07-26 12:03:07 +0200

    Dependence analysis via index analysis worked

--- tools/array-partition/ArrayPartition.cpp
+++ tools/array-partition/ArrayPartition.cpp
@@ -2,6 +2,7 @@
 #include "polly/ScopInfo.h"
 #include "polly/ScopPass.h"
 
+#include "llvm/ADT/MapVector.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Analysis/AliasAnalysis.h"
 #include "llvm/IR/Function.h"
@@ -21,20 +22,97 @@
 #include "dynamatic/Analysis/NameAnalysis.h"
 #include "dynamatic/Support/MemoryDependency.h"
 
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/detail/adjacency_list.hpp>
+
 using namespace llvm;
 using namespace polly;
 
+using Graph =
+    boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS>;
+using Vertex = boost::graph_traits<Graph>::vertex_descriptor;
+using Vertex_size_t = boost::graph_traits<Graph>::vertices_size_type;
+using vertex_index_map =
+    boost::property_map<Graph, boost::vertex_index_t>::const_type;
+
+Value *findBaseInternal(Value *addr) {
+  if (auto *arg = dyn_cast<Argument>(addr)) {
+    if (!arg->getType()->isPointerTy())
+      llvm_unreachable("Only pointer arguments are considered addresses");
+    return addr;
+  }
+
+  if (isa<Constant>(addr))
+    llvm_unreachable("Cannot determine base address of Constant");
+
+  if (auto *inst = dyn_cast_or_null<Instruction>(addr)) {
+    if (isa<AllocaInst>(inst))
+      return addr;
+    if (auto *gepi = dyn_cast<GetElementPtrInst>(inst))
+      return findBaseInternal(gepi->getPointerOperand());
+    if (auto *si = dyn_cast<SelectInst>(inst)) {
+      auto *trueBase = findBaseInternal(si->getTrueValue());
+      auto *falseBase = findBaseInternal(si->getFalseValue());
+
+      // Select must choose pointers to same array. Otherwise cannot
+      // choose relevant arrayRAM in elastic circuit
+      assert(trueBase == falseBase);
+      return trueBase;
+    }
+  }
+
+  // We try to find a few known cases of pointer expression. For others,
+  // implement when you come across them
+  llvm_unreachable("Cannot  determine base array, aborting...");
+}
+
+Value *findBase(Instruction *inst) {
+  Value *addr;
+  if (auto *loadInst = dyn_cast<LoadInst>(inst)) {
+    addr = loadInst->getPointerOperand();
+  } else if (auto *storeInst = dyn_cast<StoreInst>(inst)) {
+    addr = storeInst->getPointerOperand();
+  } else {
+    llvm_unreachable("Instruction is not a memory access");
+  }
+
+  return findBaseInternal(addr);
+}
+
 namespace {
 
-/// \brief: an LLVM pass that combines polyhedral and alias analysis to compute
-/// a set of dependency edges from the LLVM IR. It further uses dataflow
-/// analysis to eliminate dependency edges enforced by the dataflow.
+struct AccessInfo {
+  std::map<Instruction *, isl::set> accessMaps;
+  std::map<Instruction *, Value *> instToBase;
+
+  std::map<Instruction *, unsigned> instToScopId;
+
+  // Base to the set of instructions storing to this base
+  std::map<Value *, std::set<Instruction *>> baseToInsts;
+
+  bool sameScop(Instruction *i, Instruction *j) {
+    if (!instToScopId.count(i))
+      return false;
+
+    if (!instToScopId.count(j))
+      return false;
+
+    return instToScopId.count(i) == instToScopId.count(j);
+  }
+};
+
 struct ArrayPartition : PassInfoMixin<ArrayPartition> {
 
   unsigned memCount = 0;
 
   PreservedAnalyses run(Function &f, FunctionAnalysisManager &fam);
 };
+
+void getAllRegions(llvm::Region &r, std::deque<llvm::Region *> &rq) {
+  rq.push_back(&r);
+  for (const auto &e : r)
+    getAllRegions(*e, rq);
+}
 
 PreservedAnalyses ArrayPartition::run(Function &f,
                                       FunctionAnalysisManager &fam) {
@@ -46,7 +124,104 @@ PreservedAnalyses ArrayPartition::run(Function &f,
   auto &scopInfoAnalysis = fam.getResult<ScopInfoAnalysis>(f);
 
   auto &loopAnalysis = fam.getResult<LoopAnalysis>(f);
+
+  auto &aliasAnalysis = fam.getResult<AAManager>(f);
+
   llvm::errs() << "Hello!\n";
+
+  AccessInfo info;
+
+  std::deque<Region *> rq;
+  getAllRegions(*regionInfoAnalysis.getTopLevelRegion(), rq);
+
+  for (auto &bb : f) {
+    for (auto &inst : bb) {
+      if (isa<llvm::LoadInst, llvm::StoreInst>(&inst)) {
+        Value *base = findBase(&inst);
+        info.baseToInsts[base].insert(&inst);
+      }
+    }
+  }
+
+  Scop *s;
+  unsigned scopId = 0;
+  for (Region *r : rq) {
+    if ((s = scopInfoAnalysis.getScop(r))) {
+      for (auto &stmt : *s) {
+        for (auto *memAccess : stmt) {
+          auto *inst = memAccess->getAccessInstruction();
+
+          info.instToScopId[inst] = scopId;
+
+          // Find the access
+          auto &memoryAccess = stmt.getArrayAccessFor(inst);
+
+          // Maps iteration indices to array access indices:
+          // example:
+          // stmt[i, j] -> A[i, j]
+          // stmt[i, j] -> B[i, j - 1]
+          // NOTE:
+          // - The base address might be different for different maps.
+          isl::map currentMap = memoryAccess.getLatestAccessRelation();
+
+          // The domain of the map, e.g., the loop bounds for the iterators.
+          isl::set domain = stmt.getDomain();
+
+          // The range of the map over the domain
+          // e.g.,
+          // - input: stmt[i, j] -> A[i, j] | i \in [0, N] and j \in [0, M]
+          // - output: A[i, j] | i \in [0, N] and j \in [0, M]
+          info.accessMaps[inst] = currentMap.intersect_domain(domain).range();
+        }
+      }
+      scopId += 1;
+    }
+  }
+
+  for (auto [base, insts] : info.baseToInsts) {
+
+    // TODO: create graph
+
+    for (Instruction *inst1 : insts) {
+      for (Instruction *inst2 : insts) {
+
+        if (inst1 == inst2)
+          continue;
+
+        bool isDependent = true;
+
+        // If their are in the same scop
+        if (info.sameScop(inst1, inst2)) {
+
+          auto inst1Map = info.accessMaps[inst1];
+          auto inst2Map = info.accessMaps[inst2];
+
+          // If the two instructions might access the same index:
+          isl::set intersect = inst1Map.intersect(inst2Map);
+
+          isDependent = intersect.is_empty().is_false();
+        } else {
+          // Use the result from alias analysis to determine if the intructions
+          // are dependent:
+          // Otherwise, use results from alias analysis:
+          AliasResult aliasResult = aliasAnalysis.alias(
+              MemoryLocation::get(inst1), MemoryLocation::get(inst2));
+
+          isDependent = aliasResult != AliasResult::NoAlias;
+        }
+
+        if (isDependent) {
+          llvm::errs() << "Dependency found between: " << *inst1 << " and "
+                       << *inst2 << "\n";
+          info.instToBase[inst1] = base;
+          info.instToBase[inst2] = base;
+        } else {
+          llvm::errs() << "No dependency between: " << *inst1 << " and "
+                       << *inst2 << "\n";
+        }
+      }
+    }
+  }
 
   return PreservedAnalyses::all();
 }
