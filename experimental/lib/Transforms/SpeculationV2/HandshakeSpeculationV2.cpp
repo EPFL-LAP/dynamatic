25f71f50a6c9cfe6d374ac87a7e5c19b4433eee0
shundroid <shundroid@users.noreply.github.com>
2025-07-27 20:35:11 +0200

    move passers past PM first

--- experimental/lib/Transforms/SpeculationV2/HandshakeSpeculationV2.cpp
+++ experimental/lib/Transforms/SpeculationV2/HandshakeSpeculationV2.cpp
@@ -951,6 +951,28 @@ void HandshakeSpeculationV2Pass::runDynamaticPass() {
   // Storing repeating inits for post-processing.
   SmallVector<SpecV2RepeatingInitOp> repeatingInits(n);
   Value specLoopContinue = loopContinue;
+
+  for (Operation *op : iterateOverPossiblyIndirectUsers(loopContinue)) {
+    if (auto passer = dyn_cast<PasserOp>(op)) {
+      frontiers.insert(passer);
+    }
+  }
+
+  bool frontiersUpdated;
+  do {
+    frontiersUpdated = false;
+    for (auto passerOp : frontiers) {
+      if (isEligibleForPasserMotionOverPM(passerOp)) {
+        performPasserMotionPastPM(passerOp, frontiers);
+        frontiersUpdated = true;
+        // If frontiers are updated, the iterator is outdated.
+        // Break and restart the loop.
+        break;
+      }
+    }
+    // If no frontiers were updated, we can stop.
+  } while (frontiersUpdated);
+
   // Repeatedly move passers past Muxes and PMSC.
   for (unsigned i = 0; i < n; i++) {
     frontiers.clear();
