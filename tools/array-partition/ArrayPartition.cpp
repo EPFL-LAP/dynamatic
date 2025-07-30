21802a45b278be032f101df8babaa059a6a7e835
Jiahui Xu <jxu@ethz.ch>
2025-07-30 18:41:39 +0800

    extract the compact dimension info

--- tools/array-partition/ArrayPartition.cpp
+++ tools/array-partition/ArrayPartition.cpp
@@ -15,7 +15,9 @@
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
+#include "isl/point.h"
 #include <boost/graph/connected_components.hpp>
+#include <cstdint>
 #include <iostream>
 #include <stdexcept>
 #include <stdlib.h>
@@ -39,6 +41,23 @@ using Vertex = boost::graph_traits<Graph>::vertex_descriptor;
 using Vertex_size_t = boost::graph_traits<Graph>::vertices_size_type;
 using vertex_index_map =
     boost::property_map<Graph, boost::vertex_index_t>::const_type;
+
+/// \brief: Analysis information for reducing the array size if the accesses
+/// range is affine
+///
+/// \example: suppose you have an array v[10], and you only access the old
+/// number: [1, 3, 5, 7, 9]. This holds the information about what indices are
+/// accessed.
+/// (firstIndex, stepSize, numElements). In this case, we can redefine the array
+/// as v[1].
+///
+/// After redefining the array, we still need to update the GEP to point to the
+/// new addresses.
+/// We assume that the GEP is already instcombined.
+///
+/// The access index of the new GEP would be (oldIdx - firstIndex) / stepSize.
+using DimInfo = std::tuple<unsigned, unsigned, unsigned>;
+using ArraySquashingInfo = std::vector<DimInfo>;
 
 Value *findBaseInternal(Value *addr) {
   if (auto *arg = dyn_cast<Argument>(addr)) {
@@ -131,6 +150,20 @@ void changeGEPBasePtr(Instruction *gepInst, Value *newBasePtr) {
   }
 }
 
+/// \brief: After shrinking the array to an optimal size, we also need to update
+/// the GEP ops to the new array.
+///
+/// 1. Change the arrayType
+/// 2. Change the indices to point to the new array
+///
+/// Note: this pass assumes that the GEPs are already instcombined. So one GEP
+/// manages the indexing of all dimensions.
+void changeGEPOperands(Instruction *gep, Value *newArrayType,
+                       const ArraySquashingInfo &info) {
+
+  //
+}
+
 namespace {
 
 struct AccessInfo {
@@ -151,6 +184,65 @@ struct AccessInfo {
     return instToScopId.count(i) == instToScopId.count(j);
   }
 };
+
+ArraySquashingInfo extractDimInfo(isl::set range) {
+  ArraySquashingInfo info;
+
+  llvm::errs() << "Enumerating points! with num dims\n";
+
+  // example: A[N][M] gives you 2 dimensions
+  auto numDims = unsignedFromIslSize(range.as_set().dim(isl::dim::set));
+
+  for (unsigned i = 0; i < numDims; i++) {
+    auto dim0 = range.as_set().project_out(
+        isl::dim::set, /*starting from which dimension? */ i,
+        /* how many dimensions? */ 1);
+
+    std::vector<int> reachableIndices;
+    dim0.foreach_point([&reachableIndices](isl::point p) {
+      auto val = p.coordinate_val(
+          isl::dim::set,
+          /* dim only has one dimension so the position of the dim is 0 */
+          0);
+
+      int actualVal = val.get_num_si();
+      reachableIndices.push_back(actualVal);
+      return isl::stat::ok();
+    });
+    llvm::errs() << "Number of indices! " << reachableIndices.size() << "\n";
+    std::sort(reachableIndices.begin(), reachableIndices.end());
+    std::set<int> diffs;
+    for (size_t i = 0; i + 1 < reachableIndices.size(); ++i) {
+      int diff = reachableIndices[i] - reachableIndices[i + 1];
+      diffs.insert(diff);
+    }
+
+    if (diffs.size() != 1) {
+      info.emplace_back(reachableIndices.front(),
+                        /* step = 1 indicates that we can't squash the array
+                           into a smaller one currently */
+                        1, reachableIndices.size());
+    } else {
+      info.emplace_back(reachableIndices.front(), abs(*diffs.begin()),
+                        reachableIndices.size());
+    }
+  }
+
+  return info;
+}
+
+/// \brief: Construct the type needed for the new allocaOp
+///
+/// \note: dims is the number of elements in each dimension. Caveat:
+/// - suppose that you declared A[3][4][5], then dims should be {5, 4, 3}
+llvm::Type *getAllocaElemType(Type *baseElementType,
+                              const ArraySquashingInfo &dims) {
+  Type *ElemTy = baseElementType;
+  for (auto [init, step, elems] : dims) {
+    ElemTy = ArrayType::get(ElemTy, elems);
+  }
+  return ElemTy;
+}
 
 struct ArrayPartition : PassInfoMixin<ArrayPartition> {
 
@@ -273,9 +365,9 @@ PreservedAnalyses ArrayPartition::run(Function &f,
 
           isDependent = intersect.is_empty().is_false();
         } else {
-          // Use the result from alias analysis to determine if the intructions
-          // are dependent:
-          // Otherwise, use results from alias analysis:
+          // Use the result from alias analysis to determine if the
+          // intructions are dependent: Otherwise, use results from alias
+          // analysis:
           AliasResult aliasResult = aliasAnalysis.alias(
               MemoryLocation::get(inst1), MemoryLocation::get(inst2));
 
@@ -297,8 +389,8 @@ PreservedAnalyses ArrayPartition::run(Function &f,
         boost::connected_components(g, &nodeToComponentId[0]);
 
     for (size_t i = 1; i < numComponents; i++) {
-      // Make an empty set (note: somehow if you just do "isl::union_set range;"
-      // it wouldn't work)
+      // Make an empty set (note: somehow if you just do "isl::union_set
+      // range;" it wouldn't work)
       isl::union_set range = isl::union_set::empty(islCtx);
       for (size_t j = 0; j < nodeToComponentId.size(); j++) {
         if (nodeToComponentId[j] == static_cast<int>(i)) {
@@ -310,11 +402,8 @@ PreservedAnalyses ArrayPartition::run(Function &f,
       // Enumerate points
 
       llvm::errs() << "Enumerating points! with num dims\n";
-      dumpPw(range);
-      range.foreach_point([](isl::point p) {
-        llvm::errs() << "A point!\n";
-        return isl::stat::ok();
-      });
+
+      auto dimInfo = extractDimInfo(range.as_set());
 
       llvm::errs() << "Creating new alloca to improve parallelism...\b";
       // Make a new alloca
