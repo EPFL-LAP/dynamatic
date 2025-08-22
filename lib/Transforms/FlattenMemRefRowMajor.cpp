62c45d3f2896b1042495a15cec88ca844b01ea45
A-Alosius <akontehalosius@gmail.com>
2025-08-22 14:22:22 +0200

    Fixed cpp format with clang-format

--- lib/Transforms/FlattenMemRefRowMajor.cpp
+++ lib/Transforms/FlattenMemRefRowMajor.cpp
@@ -122,7 +122,7 @@ struct LoadOpConversion : public OpConversionPattern<memref::LoadOp> {
 
   LoadOpConversion(NameAnalysis &namer, TypeConverter &converter,
                    MLIRContext *ctx)
-      : OpConversionPattern(converter, ctx), namer(namer){};
+      : OpConversionPattern(converter, ctx), namer(namer) {};
 
   LogicalResult
   matchAndRewrite(memref::LoadOp loadOp, OpAdaptor adaptor,
@@ -151,7 +151,7 @@ struct StoreOpConversion : public OpConversionPattern<memref::StoreOp> {
 
   StoreOpConversion(NameAnalysis &namer, TypeConverter &converter,
                     MLIRContext *ctx)
-      : OpConversionPattern(converter, ctx), namer(namer){};
+      : OpConversionPattern(converter, ctx), namer(namer) {};
 
   LogicalResult
   matchAndRewrite(memref::StoreOp storeOp, OpAdaptor adaptor,
