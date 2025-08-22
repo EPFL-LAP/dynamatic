c7e51c747eb112320e9703d6bedc550b27de6155
Emmet Murphy <murphe67@tcd.ie>
2025-08-22 13:49:54 +0200

    first changes

--- tools/backend/lsq-generator-python/src/config.py
+++ tools/backend/lsq-generator-python/src/config.py
@@ -6,20 +6,8 @@
 
 import math
 import json
-import sys
 
-# read and parse the json file
-# example json format in README
-
-
-def GetConfigs(config_json_path: str):
-    with open(config_json_path, 'r') as file:
-        configString = file.read()
-        configs = json.loads(configString)
-        return Configs(configs)
-
-
-class Configs:
+class Config:
     """
     Configuration object for LSQ code generation.
 
@@ -65,47 +53,50 @@ class Configs:
     stResp:        bool = False     # Whether store response channel in store access port is enabled
     gaMulti:       bool = False     # Whether multiple groups are allowed to request an allocation at the same cycle
 
-    def __init__(self, config: dict) -> None:
-        self.name = config["name"]
-        self.dataW = config["dataWidth"]
-        self.addrW = config["addrWidth"]
-        self.idW = config["indexWidth"]
-        self.numLdqEntries = config["fifoDepth_L"]
-        self.numStqEntries = config["fifoDepth_S"]
-        self.numLdPorts = config["numLoadPorts"]
-        self.numStPorts = config["numStorePorts"]
-        self.numGroups = config["numBBs"]
-        self.numLdMem = config["numLdChannels"]
-        self.numStMem = config["numStChannels"]
-        self.master = bool(config["master"])
+    def __init__(self, json_file) -> None:
+        with open(json_file, "r") as f:
+            obj = json.load(f)
+            
+            self.name = obj["name"]
+            self.dataW = obj["dataWidth"]
+            self.addrW = obj["addrWidth"]
+            self.idW = obj["indexWidth"]
+            self.numLdqEntries = obj["fifoDepth_L"]
+            self.numStqEntries = obj["fifoDepth_S"]
+            self.numLdPorts = obj["numLoadPorts"]
+            self.numStPorts = obj["numStorePorts"]
+            self.numGroups = obj["numBBs"]
+            self.numLdMem = obj["numLdChannels"]
+            self.numStMem = obj["numStChannels"]
+            self.master = bool(obj["master"])
 
-        self.stResp = bool(config["stResp"])
-        self.gaMulti = bool(config["groupMulti"])
+            self.stResp = bool(obj["stResp"])
+            self.gaMulti = bool(obj["groupMulti"])
 
-        self.gaNumLoads = config["numLoads"]
-        self.gaNumStores = config["numStores"]
-        self.gaLdOrder = config["ldOrder"]
-        self.gaLdPortIdx = config["ldPortIdx"]
-        self.gaStPortIdx = config["stPortIdx"]
+            self.gaNumLoads = obj["numLoads"]
+            self.gaNumStores = obj["numStores"]
+            self.gaLdOrder = obj["ldOrder"]
+            self.gaLdPortIdx = obj["ldPortIdx"]
+            self.gaStPortIdx = obj["stPortIdx"]
 
-        self.ldqAddrW = math.ceil(math.log2(self.numLdqEntries))
-        self.stqAddrW = math.ceil(math.log2(self.numStqEntries))
-        self.emptyLdAddrW = math.ceil(math.log2(self.numLdqEntries+1))
-        self.emptyStAddrW = math.ceil(math.log2(self.numStqEntries+1))
-        # Check the number of ports, if num*Ports == 0, set it to 1
-        self.ldpAddrW = math.ceil(math.log2(self.numLdPorts if self.numLdPorts > 0 else 1))
-        self.stpAddrW = math.ceil(math.log2(self.numStPorts if self.numStPorts > 0 else 1))
+            self.ldqAddrW = math.ceil(math.log2(self.numLdqEntries))
+            self.stqAddrW = math.ceil(math.log2(self.numStqEntries))
+            self.emptyLdAddrW = math.ceil(math.log2(self.numLdqEntries+1))
+            self.emptyStAddrW = math.ceil(math.log2(self.numStqEntries+1))
+            # Check the number of ports, if num*Ports == 0, set it to 1
+            self.ldpAddrW = math.ceil(math.log2(self.numLdPorts if self.numLdPorts > 0 else 1))
+            self.stpAddrW = math.ceil(math.log2(self.numStPorts if self.numStPorts > 0 else 1))
 
-        self.pipe0 = bool(config["pipe0En"])
-        self.pipe1 = bool(config["pipe1En"])
-        self.pipeComp = bool(config["pipeCompEn"])
-        self.headLag = bool(config["headLagEn"])
+            self.pipe0 = bool(obj["pipe0En"])
+            self.pipe1 = bool(obj["pipe1En"])
+            self.pipeComp = bool(obj["pipeCompEn"])
+            self.headLag = bool(obj["headLagEn"])
 
-        assert (self.idW >= self.ldqAddrW)
+            assert (self.idW >= self.ldqAddrW)
 
-        # list size checking
-        assert (len(self.gaNumLoads) == self.numGroups)
-        assert (len(self.gaNumStores) == self.numGroups)
-        assert (len(self.gaLdOrder) == self.numGroups)
-        assert (len(self.gaLdPortIdx) == self.numGroups)
-        assert (len(self.gaStPortIdx) == self.numGroups)
+            # list size checking
+            assert (len(self.gaNumLoads) == self.numGroups)
+            assert (len(self.gaNumStores) == self.numGroups)
+            assert (len(self.gaLdOrder) == self.numGroups)
+            assert (len(self.gaLdPortIdx) == self.numGroups)
+            assert (len(self.gaStPortIdx) == self.numGroups)
