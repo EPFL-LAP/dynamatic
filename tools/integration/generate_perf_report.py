142a0b78e9165548c589ed4235c974ecbf9be80c
Eugen Bosnjak <eugen.bosnjak@epfl.ch>
2025-08-23 14:16:41 +0200

    Cleaned up code

--- tools/integration/generate_perf_report.py
+++ tools/integration/generate_perf_report.py
@@ -2,25 +2,37 @@ def parse_results():
     return [
         {"name": "test1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaathis is a very long name",
             "cycles": 1234, "result": "pass"},
-        {"name": "test2", "cycles": -1, "result": "fail"},
-        {"name": "test3", "cycles": -1, "result": "timeout"}
+        {"name": "test2", "result": "fail"},
+        {"name": "test3", "result": "timeout"}
     ]
 
 
+def table(header, data):
+    res = ""
+    for elem in header:
+        res += f"| {elem} "
+    res += "|\n"
+
+    for elem in header:
+        res += "| ---- "
+    res += "|\n"
+
+    for row in data:
+        for elem in header:
+            if elem in row:
+                res += f"| {row[elem]} "
+            else:
+                res += f"| N/A "
+        res += "|\n"
+
+    return res
+
+
 def main():
-    print("## Performance Report")
-    print()
-    print("| Name | Result | Num. cycles |")
-    print("| ---- | ------ | ----------- |")
-    t = parse_results()
-    for d in t:
-        print(f"| {d["name"]} ", end="")
-        if d["result"] == "pass":
-            print(f"| {d["cycles"]} ", end="")
-        else:
-            print(f"| N/A ", end="")
-        print(f"| {d["result"]} ", end="")
-        print("|")
+    print(table(
+        ["name", "cycles", "result"],
+        parse_results()
+    ))
 
 
 if __name__ == "__main__":
