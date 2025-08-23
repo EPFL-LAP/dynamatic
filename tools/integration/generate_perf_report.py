b9fe81f1b4e977672769c74488aa3f7bc46fa372
Eugen Bosnjak <eugen.bosnjak@epfl.ch>
2025-08-23 13:27:50 +0200

    Added dummy report

---
+++ tools/integration/generate_perf_report.py
@@ -0,0 +1,26 @@
+def parse_results():
+    return [
+        {"name": "test1", "cycles": 1234, "result": "pass"},
+        {"name": "test2", "cycles": -1, "result": "fail"},
+        {"name": "test3", "cycles": -1, "result": "timeout"}
+    ]
+
+
+def main():
+    print("## Performance Report")
+    print()
+    print("| Name | Result | Num. cycles |")
+    print("| ---- | ------ | ----------- |")
+    t = parse_results()
+    for d in t:
+        print(f"| {d["name"]} ", end="")
+        if d["result"] == "pass":
+            print(f"| {d["cycles"]} ", end="")
+        else:
+            print(f"| N/A ", end="")
+        print(f"| {d["result"]} ", end="")
+        print("|")
+
+
+if __name__ == "__main__":
+    main()
