8369939087b10ab306caef303ba4fe4982029517
Eugen Bosnjak <eugen.bosnjak@epfl.ch>
2025-08-24 14:08:18 +0200

    Added perf report

--- tools/integration/generate_perf_report.py
+++ tools/integration/generate_perf_report.py
@@ -1,10 +1,74 @@
-def parse_results():
-    return [
-        {"name": "test1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaathis is a very long name",
-            "cycles": 1234, "result": "pass"},
-        {"name": "test2", "result": "fail"},
-        {"name": "test3", "result": "timeout"}
-    ]
+import os
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+
+DYNAMATIC_ROOT = Path(__file__).parent.parent.parent
+RESULTS_DIR = DYNAMATIC_ROOT / "build" / "tools" / "integration" / "results"
+
+
+def find_files_ext(directory, ext):
+    """
+    Finds all files with a given extension in a directory.
+
+    Arguments:
+    `directory` -- Path of the directory
+    `ext` -- File extension to be searched for
+
+    Returns: List of paths to all files in directory with extension ext
+    """
+    c_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith(ext):
+                c_files.append(os.path.join(root, file))
+    return c_files
+
+
+def parse_xml(path):
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    res = []
+    for test in root.iter("testcase"):
+        row = {}
+
+        test_name = test.attrib["classname"] + "." + test.attrib["name"]
+
+        fail_messages = []
+        for f in test.findall("failure"):
+            fail_messages.append(f.attrib["message"])
+
+        if len(fail_messages) > 0:
+            result = "fail"
+        else:
+            result = "pass"
+
+        row["name"] = test_name
+        row["result"] = result
+
+        for property in test.iter("property"):
+            prop_name = property.attrib["name"]
+            prop_val = property.attrib["value"]
+
+            if prop_name != "cycles" or prop_val != "-1":
+                row[prop_name] = prop_val
+
+        res.append(row)
+
+    return res
+
+
+def parse_results(path):
+    xml_files = find_files_ext(path, ".xml")
+
+    columns = ["name", "result", "cycles"]
+    data = []
+
+    for file in xml_files:
+        data += parse_xml(file)
+
+    return data
 
 
 def table(header, data):
@@ -31,7 +95,7 @@ def table(header, data):
 def main():
     print(table(
         ["name", "cycles", "result"],
-        parse_results()
+        parse_results(RESULTS_DIR)
     ))
 
 
