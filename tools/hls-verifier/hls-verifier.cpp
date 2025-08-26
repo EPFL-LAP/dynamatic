d8db5c43b044794ea5ec32157598d0b331f8e3f2
Jiahui Xu <jxu@ethz.ch>
2025-08-26 18:42:56 +0200

    memref dialect is not needed in the verifier

--- tools/hls-verifier/hls-verifier.cpp
+++ tools/hls-verifier/hls-verifier.cpp
@@ -191,7 +191,7 @@ int main(int argc, char **argv) {
 
   // We only need the Handshake dialect
   MLIRContext context;
-  context.loadDialect<handshake::HandshakeDialect, memref::MemRefDialect>();
+  context.loadDialect<handshake::HandshakeDialect>();
   context.allowUnregisteredDialects();
 
   auto fileOrErr = MemoryBuffer::getFileOrSTDIN(mlirPathName.c_str());
