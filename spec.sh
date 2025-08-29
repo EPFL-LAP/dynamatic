9a44fd654dafc2c59197e588407921707c2d3d9a
shundroid <shundroid@users.noreply.github.com>
2025-08-29 22:07:43 +0900

    added spec v1 shell script

---
+++ spec.sh
@@ -0,0 +1,9 @@
+python3 tools/integration/run_spec_integration.py single_loop
+python3 tools/integration/run_spec_integration.py loop_path
+python3 tools/integration/run_spec_integration.py subdiag --transformed-code subdiag_v1.c
+python3 tools/integration/run_spec_integration.py subdiag_fast --transformed-code subdiag_fast_v1.c
+python3 tools/integration/run_spec_integration.py fixed
+python3 tools/integration/run_spec_integration.py sparse
+python3 tools/integration/run_spec_integration.py nested_loop
+python3 tools/integration/run_spec_integration.py if_convert
+
