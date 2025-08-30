4a4c6c745e8708cdbcc9b932171493c7622f1946
shundroid <shundroid@users.noreply.github.com>
2025-08-30 13:22:06 +0900

    removed weird constraint on argument length

--- include/dynamatic/Integration.h
+++ include/dynamatic/Integration.h
@@ -31,44 +31,10 @@
 // where arguments' value will be stored (`HLS_VERIFICATION_PATH/INPUT_VECTORS`
 // for values before the kernel call and `HLS_VERIFICATION_PATH/C_OUT` for
 // values after the kernel call).
-//
-//===- IMPORTANT NOTE -----------------------------------------------------===//
-//
-// There are two "limitations" when instrumenting a kernel call using
-// CALL_KERNEL. However, they can easily be lifted should there be a need.
-// 1. Statically-sized arrays are supported only up to 5 dimensions. Adding
-// support for higher-dimensional arrays is as simple as adding more `dumpArg`
-// functions similar to those already present.
-// 2. Kernels must have a maximum of 16 arguments. Adding support for kernels
-// with more arguments requires modifying a couple macros.
-//   - HAS_ARGS_IMPL (before argument N, add _17, _18, ... )
-//   - HAS_ARGS (add ARGS as many times as the number of extra arguments you
-//   want to support before NO_ARGS)
-//   - VA_NUM_ARGS_IMPL (before argument N, add _17, _18, ... )
-//   - VA_NUM_ARGS (after variadic argument in expansion, add ..., 18, 17)
-//   - DUMP_ARG_* (add DUMP_ARG_17, DUMP_ARG_18, ...)
-//
 //===----------------------------------------------------------------------===//
 
 #ifndef DYNAMATIC_INTEGRATION_H
 #define DYNAMATIC_INTEGRATION_H
-
-#define STRINGIFY_IMPL(str) #str
-#define STRINGIFY(str) STRINGIFY_IMPL(str)
-
-#define CONCAT_IMPL(x, y) x##y
-#define CONCAT(x, y) CONCAT_IMPL(x, y)
-
-/// Expands to the 18th macro argument.
-#define HAS_ARGS_IMPL(_kernel, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11,   \
-                      _12, _13, _14, _15, _16, N, ...)                         \
-  N
-
-/// HAS_ARGS will expand to NO_ARGS if kernelAndArgs has size 1 (just the kernel
-/// name). Otherwise it will expand to ARGS.
-#define HAS_ARGS(kernelAndArgs...)                                             \
-  HAS_ARGS_IMPL(kernelAndArgs, ARGS, ARGS, ARGS, ARGS, ARGS, ARGS, ARGS, ARGS, \
-                ARGS, ARGS, ARGS, ARGS, ARGS, ARGS, ARGS, ARGS, NO_ARGS)
 
 //===----------------------------------------------------------------------===//
 // PRINT_PROFILING_INFO/HLS_VERIFICATION - Common code
@@ -201,15 +167,7 @@ static Res callKernel(Res (*kernel)(FunArgs...), RealArgs &&...args) {
   return kernel(std::forward<RealArgs>(args)...);
 }
 
-template <typename Res>
-static Res callKernel(Res (*kernel)(void)) {
-  return kernel();
-}
-
-#define CALL_NO_ARGS(kernel) callKernel(kernel)
-#define CALL_ARGS(kernel, args...) callKernel(kernel, args)
-#define CALL_KERNEL(kernelAndArgs...)                                          \
-  CONCAT(CALL_, HAS_ARGS(kernelAndArgs))(kernelAndArgs)
+#define CALL_KERNEL(kernelAndArgs...) callKernel(kernelAndArgs)
 #endif // PRINT_PROFILING_INFO
 
 //===----------------------------------------------------------------------===//
@@ -226,6 +184,7 @@ static Res callKernel(Res (*kernel)(void)) {
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <vector>
 
 /// Whenever HLS_VERIFICATION is defined, this macro must contain the path to
 /// the directory where the INPUT_VECTORS and C_OUT directories have been
@@ -317,89 +276,59 @@ void dumpHLSArg(const T &arg, const char *argName) {
   outFile.close();
 }
 
-/// Calls the kernel with the provided arguments.
-template <typename... FunArgs, typename... RealArgs>
-static void callKernel(void (*kernel)(FunArgs...), RealArgs &&...args) {
-  return kernel(std::forward<RealArgs>(args)...);
+template <typename Func, typename... Args>
+void dumpArgsImpl(const char *names, Func, Args &&...args) {
+  std::string s(names); // "fir, a, b, c"
+  std::stringstream ss(s);
+  std::string name;
+
+  // split on ',' and dump alongside each arg
+  std::vector<std::string> nameList;
+  // drop the first item
+  std::getline(ss, name, ',');
+  while (std::getline(ss, name, ',')) {
+    // trim spaces
+    name.erase(0, name.find_first_not_of(" \t"));
+    name.erase(name.find_last_not_of(" \t") + 1);
+    nameList.push_back(name);
+  }
+
+  size_t i = 0;
+  ((dumpHLSArg(args, nameList[i++].c_str())), ...);
 }
 
-/// Calls the kernel.
-static void callKernel(void (*kernel)(void)) { return kernel(); }
+/// Calls the kernel with the provided arguments.
+template <typename... FunArgs, typename... RealArgs>
+static void callKernelImpl(void (*kernel)(FunArgs...), RealArgs &&...args) {
+  return kernel(std::forward<RealArgs>(args)...);
+}
 
 /// Calls the kernel with the provided arguments and dumps the function's result
 /// to a file.
 template <typename Res, typename... FunArgs, typename... RealArgs>
-static void callKernel(Res (*kernel)(FunArgs...), RealArgs &&...args) {
+static void callKernelImpl(Res (*kernel)(FunArgs...), RealArgs &&...args) {
   Res res = kernel(std::forward<RealArgs>(args)...);
   dumpHLSArg(res, "out0");
 }
 
-/// Calls the kernel and dumps the function's result to a file.
-template <typename Res>
-static void callKernel(Res (*kernel)(void)) {
-  Res res = kernel();
-  dumpHLSArg(res, "out0");
-}
+#define STRINGIFY_IMPL(str) #str
+#define STRINGIFY(str) STRINGIFY_IMPL(str)
 
-// Following macro definitions strongly inspired by
-// https://stackoverflow.com/questions/46725369/how-to-get-name-for-each-argument-in-variadic-macros
-
-// This works for kernels with at most 16 arguments, but can be trivially
-// extended to more if needed.
-
-#define VA_NUM_ARGS_IMPL(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12,    \
-                         _13, _14, _15, _16, N, ...)                           \
-  N
-#define VA_NUM_ARGS(...)                                                       \
-  VA_NUM_ARGS_IMPL(__VA_ARGS__, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4,  \
-                   3, 2, 1, 0)
-
-#define HLS_DUMP(arg) dumpHLSArg(arg, #arg);
-#define DUMP_ARG_0
-#define DUMP_ARG_1(arg) HLS_DUMP(arg)
-#define DUMP_ARG_2(arg, ...) HLS_DUMP(arg) DUMP_ARG_1(__VA_ARGS__)
-#define DUMP_ARG_3(arg, ...) HLS_DUMP(arg) DUMP_ARG_2(__VA_ARGS__)
-#define DUMP_ARG_4(arg, ...) HLS_DUMP(arg) DUMP_ARG_3(__VA_ARGS__)
-#define DUMP_ARG_5(arg, ...) HLS_DUMP(arg) DUMP_ARG_4(__VA_ARGS__)
-#define DUMP_ARG_6(arg, ...) HLS_DUMP(arg) DUMP_ARG_5(__VA_ARGS__)
-#define DUMP_ARG_7(arg, ...) HLS_DUMP(arg) DUMP_ARG_6(__VA_ARGS__)
-#define DUMP_ARG_8(arg, ...) HLS_DUMP(arg) DUMP_ARG_7(__VA_ARGS__)
-#define DUMP_ARG_9(arg, ...) HLS_DUMP(arg) DUMP_ARG_8(__VA_ARGS__)
-#define DUMP_ARG_10(arg, ...) HLS_DUMP(arg) DUMP_ARG_9(__VA_ARGS__)
-#define DUMP_ARG_11(arg, ...) HLS_DUMP(arg) DUMP_ARG_10(__VA_ARGS__)
-#define DUMP_ARG_12(arg, ...) HLS_DUMP(arg) DUMP_ARG_11(__VA_ARGS__)
-#define DUMP_ARG_13(arg, ...) HLS_DUMP(arg) DUMP_ARG_12(__VA_ARGS__)
-#define DUMP_ARG_14(arg, ...) HLS_DUMP(arg) DUMP_ARG_13(__VA_ARGS__)
-#define DUMP_ARG_15(arg, ...) HLS_DUMP(arg) DUMP_ARG_14(__VA_ARGS__)
-#define DUMP_ARG_16(arg, ...) HLS_DUMP(arg) DUMP_ARG_15(__VA_ARGS__)
-#define DUMP_ARGS(args...) CONCAT(DUMP_ARG_, VA_NUM_ARGS(args))(args)
-
-#define CALL_NO_ARGS(kernel)                                                   \
+#define CALL_KERNEL(kernelAndArgs...)                                          \
   {                                                                            \
-    _outPrefix_ = std::string{STRINGIFY(HLS_VERIFICATION_PATH)} +              \
-                  std::filesystem::path::preferred_separator + "C_OUT" +       \
-                  std::filesystem::path::preferred_separator + "output_";      \
-    callKernel(kernel);                                                        \
-    ++_transactionID_;                                                         \
-  }
-
-#define CALL_ARGS(kernel, args...)                                             \
-  {                                                                            \
-    _outPrefix_ = std::string{STRINGIFY(HLS_VERIFICATION_PATH)} +              \
+    _outPrefix_ = std::string{(STRINGIFY(HLS_VERIFICATION_PATH))} +            \
                   std::filesystem::path::preferred_separator +                 \
                   "INPUT_VECTORS" +                                            \
                   std::filesystem::path::preferred_separator + "input_";       \
-    DUMP_ARGS(args)                                                            \
-    _outPrefix_ = std::string{STRINGIFY(HLS_VERIFICATION_PATH)} +              \
+    dumpArgsImpl(#kernelAndArgs, kernelAndArgs);                               \
+    _outPrefix_ = std::string{(STRINGIFY(HLS_VERIFICATION_PATH))} +            \
                   std::filesystem::path::preferred_separator + "C_OUT" +       \
                   std::filesystem::path::preferred_separator + "output_";      \
-    callKernel(kernel, args);                                                  \
-    DUMP_ARGS(args)                                                            \
+    callKernelImpl(kernelAndArgs);                                             \
+    dumpArgsImpl(#kernelAndArgs, kernelAndArgs);                               \
     ++_transactionID_;                                                         \
   }
 
-#define CALL_KERNEL(kernelAndArgs...)                                          \
-  CONCAT(CALL_, HAS_ARGS(kernelAndArgs))(kernelAndArgs)
 #endif // HLS_VERIFICATION
 
 //===----------------------------------------------------------------------===//
