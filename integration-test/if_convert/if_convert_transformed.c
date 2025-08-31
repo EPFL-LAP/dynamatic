bbee4442e9df7764407c9ba5a4319b9e5f53ca02
shundroid <shundroid@users.noreply.github.com>
2025-08-31 22:14:59 +0900

    if convert of smaller impl

--- integration-test/if_convert/if_convert_transformed.c
+++ integration-test/if_convert/if_convert_transformed.c
@@ -5,25 +5,22 @@
 
 void if_convert(in_int_t a[N], inout_int_t b[N]) {
   int i = 1;
-  while (i < N2) {
+  do {
     int pred_i = i;
-    while (pred_i < N2 && pred_i == i) {
+    do {
       int tmp = a[pred_i];
 
       // Manual CSE
       int plus_1 = pred_i + 1;
 
-      if (pred_i * tmp < 10000) {
-        i = pred_i + 2;
-      } else {
-        i = plus_1;
-      }
+      int true_addition = pred_i * tmp < 10000 ? 2 : 1;
+      i = pred_i + true_addition;
       b[i] = 1;
 
       // Prediction
       pred_i = plus_1;
-    }
-  }
+    } while (pred_i < N2 && pred_i == i);
+  } while (i < N2);
 }
 
 int main(void) {
