ae1ece3beab86dae8edfbc0d2632503d51be8ba6
Emmet Murphy <murphe67@tcd.ie>
2025-09-06 18:39:27 +0200

    barrel shifters

--- tools/backend/lsq-generator-python/LSQ/generators/barrel_shifter.py
+++ tools/backend/lsq-generator-python/LSQ/generators/barrel_shifter.py
@@ -78,7 +78,7 @@ class BarrelShifterBody():
 """.removeprefix("\n")
             for j in range(num_shifts):
                 self.item += f"""
-  {shift_outs[i]}({j}) <= {shift_ins[i]}({(j + 2**i) % to_shift.size.number}); 
+  {shift_outs[i]}({(j + 2**i) % num_shifts}) <= {shift_ins[i]}({0}); 
 """.removeprefix("\n")
             self.item += f"""
 
