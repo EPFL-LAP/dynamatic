e65fe000aed53a2ce4f9bf3a4e3229a7f38c7261
Emmet Murphy <murphe67@tcd.ie>
2025-09-08 18:26:06 +0200

    refactor

--- tools/backend/lsq-generator-python/LSQ/generators/__init__.py
+++ tools/backend/lsq-generator-python/LSQ/generators/__init__.py
@@ -1,10 +1,8 @@
 # vhdl_gen/generators/__init__.py
 from LSQ.generators.dispatchers import PortToQueueDispatcher, QueueToPortDispatcher
-from LSQ.generators.group_allocator.group_allocator import GroupAllocator
 from LSQ.generators.core import LSQ
 
 __all__ = [
     "PortToQueueDispatcher", "QueueToPortDispatcher",
-    "GroupAllocator",
     "LSQ",
 ]
