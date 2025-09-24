7a30c699aab5b9b2155713ae16058173e60cf44c
Jiahui Xu <jxu@ethz.ch>
2025-09-24 16:03:05 +0200

    update tester

--- tools/integration/TEST_SUITE.cpp
+++ tools/integration/TEST_SUITE.cpp
@@ -49,6 +49,20 @@ TEST_P(BasicFixture, basic) {
 //   RecordProperty("cycles", std::to_string(simTime));
 // }
 
+TEST_P(MemoryFixture, basic) {
+  IntegrationTestData config{
+      // clang-format off
+      .name = GetParam(),
+      .benchmarkPath = fs::path(DYNAMATIC_ROOT) / "integration-test" / "memory",
+      .useVerilog = false,
+      .useSharing = false,
+      .simTime = -1
+      // clang-format on
+  };
+  EXPECT_EQ(runIntegrationTest(config), 0);
+  RecordProperty("cycles", std::to_string(config.simTime));
+}
+
 /// This testing fixture runs the test with and without sharing. It checks
 /// whenever the sharing option is enabled, the pass can run without any
 /// interruption and does not penalize the latency.
@@ -124,55 +138,40 @@ TEST_P(SpecFixture, spec_NoCI) {
   RecordProperty("cycles", std::to_string(simTime));
 }
 
-static auto miscBenchmarkNames = testing::Values(
-    "single_loop", "atax", "atax_float", "bicg", "bicg_float", "binary_search",
-    "factorial", "fir", "float_basic", "gaussian", "gcd", "gemm", "gemm_float",
-    "gemver", "gemver_float", "gesummv_float", "get_tanh", "gsum", "gsumif",
-    "histogram", "if_loop_1", "if_loop_2", "if_loop_3", "if_loop_add",
-    "if_loop_mul", "iir", "image_resize", "insertion_sort",
-    "iterative_division", "iterative_sqrt", "jacobi_1d_imper", "kernel_2mm",
-    "kernel_2mm_float", "kernel_3mm", "kernel_3mm_float", "kmp", "loop_array",
-    "lu", "matching", "matching_2", "matrix", "matrix_power", "matvec",
-    "mul_example", "mvt_float", "pivot", "polyn_mult", "simple_example_1",
-    "sobel", "spmv", "stencil_2d", "sumi3_mem", "symm_float", "syr2k_float",
-    "test_stdint", "threshold", "triangular", "vector_rescale", "video_filter",
-    "while_loop_1", "while_loop_3");
+INSTANTIATE_TEST_SUITE_P(
+    MiscBenchmarks, BasicFixture,
+    testing::Values("single_loop", "atax", "atax_float", "bicg", "bicg_float",
+                    "binary_search", "factorial", "fir", "float_basic",
+                    "gaussian", "gcd", "gemm", "gemm_float", "gemver",
+                    "gemver_float", "gesummv_float", "get_tanh", "gsum",
+                    "gsumif", "histogram", "if_loop_1", "if_loop_2",
+                    "if_loop_3", "if_loop_add", "if_loop_mul", "iir",
+                    "image_resize", "insertion_sort", "iterative_division",
+                    "iterative_sqrt", "jacobi_1d_imper", "kernel_2mm",
+                    "kernel_2mm_float", "kernel_3mm", "kernel_3mm_float", "kmp",
+                    "loop_array", "lu", "matching", "matching_2", "matrix",
+                    "matrix_power", "matvec", "mul_example", "mvt_float",
+                    "pivot", "polyn_mult", "simple_example_1", "sobel", "spmv",
+                    "stencil_2d", "sumi3_mem", "symm_float", "syr2k_float",
+                    "test_stdint", "threshold", "triangular", "vector_rescale",
+                    "video_filter", "while_loop_1", "while_loop_3"),
+    [](const auto &info) { return info.param; });
 
-static auto memoryBenchmarkNames = testing::Values(
-    // clang-format off
-    "test_flatten_array",
-    "test_constant_array",
-    "test_internal_array",
-    "test_memory_1",
-    "test_memory_2",
-    "test_memory_3",
-    "test_memory_4",
-    "test_memory_5",
-    "test_memory_6",
-    "test_memory_7",
-    "test_memory_8",
-    "test_memory_9",
-    "test_memory_10",
-    "test_memory_11",
-    "test_memory_12",
-    "test_memory_13",
-    "test_memory_14",
-    "test_memory_15",
-    "test_memory_16",
-    "test_memory_17",
-    "test_memory_18",
-    "test_smallbound"
-    // clang-format on
-);
+INSTANTIATE_TEST_SUITE_P(
+    MemoryBenchmarks, MemoryFixture,
+    testing::Values("test_flatten_array", "test_memory_1", "test_memory_2",
+                    "test_memory_3", "test_memory_4", "test_memory_5",
+                    "test_memory_6", "test_memory_7", "test_memory_8",
+                    "test_memory_9", "test_memory_10", "test_memory_11",
+                    "test_memory_12", "test_memory_13", "test_memory_14",
+                    "test_memory_15", "test_memory_16", "test_memory_17",
+                    "test_memory_18", "test_smallbound"),
+    [](const auto &info) { return "memory_" + info.param; });
 
-static auto sharingBenchmarkNames =
-    testing::Values("share_test_1", "share_test_2");
-
-INSTANTIATE_TEST_SUITE_P(MiscBenchmarks, BasicFixture, miscBenchmarkNames,
-                         [](const auto &info) { return info.param; });
-INSTANTIATE_TEST_SUITE_P(MemoryBenchmarks, MemoryFixture, memoryBenchmarkNames,
+INSTANTIATE_TEST_SUITE_P(SharingUnitTests, SharingUnitTestFixture,
+                         testing::Values("share_test_1", "share_test_2"),
                          [](const auto &info) {
-                           return "memory_" + info.param;
+                           return "sharing_" + info.param;
                          });
 
 // clang-format off
