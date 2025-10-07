9d9f6d8d0de0a8f3b0e948bbe713828dcbeb49cf
Jiahui Xu <jxu@ethz.ch>
2025-10-07 19:52:09 +0200

    fix comments

--- include/dynamatic/Support/ConstraintProgramming/ConstraintProgramming.h
+++ include/dynamatic/Support/ConstraintProgramming/ConstraintProgramming.h
@@ -3,6 +3,9 @@
 /// - Users can use overloaded '+', '*', '-', '<', etc. to construct constraints
 /// and objectives.
 /// - It is designed to be solver agnostic.
+///
+/// For example usage of this API, please refer to
+/// `dynamatic/unittests/Support/ConstraintProgramming/CPTest.cpp`
 #pragma once
 #include "llvm/Support/ErrorHandling.h"
 #include <map>
@@ -31,7 +34,7 @@ struct Var {
   VarType type;
   // Null value of lowerBound would be -inf
   std::optional<double> lowerBound;
-  // Null value of upperBound would be -inf
+  // Null value of upperBound would be +inf
   std::optional<double> upperBound;
   // Using Var as a key
   bool operator<(const Var &other) const noexcept { return name < other.name; }
@@ -171,14 +174,21 @@ inline Constraint operator==(const LinearExpr &lhs, double rhs) {
 /// This is overloaded for the Gurobi solver and the Google's OR Tools API.
 class CPSolver {
 public:
+  enum Status { OPTIMAL, NONOPTIMAL, INFEASIBLE, UNBOUNDED, UNKNOWN, ERROR };
+  Status status = UNKNOWN;
   virtual ~CPSolver() = default;
-
+  // Virtual class methods: they provide a unified interface for all available
+  // solvers.
   virtual Var addVariable(const Var &var) = 0;
+  // Create var, add gurobi var, and then return the created variable
+  virtual Var addVariable(const std::string &name, Var::VarType type,
+                          std::optional<double> lb,
+                          std::optional<double> ub) = 0;
   virtual void addLinearConstraint(const Constraint &constraint) = 0;
   virtual void setMaximizeObjective(const LinearExpr &expr) = 0;
-  virtual bool solve() = 0;
-  virtual double getValue(const Var &var) const = 0;
-  virtual double getObjective() const = 0;
+  virtual void optimize() = 0;
+  virtual std::optional<double> getValue(const Var &var) const = 0;
+  virtual std::optional<double> getObjective() const = 0;
 };
 
 #ifndef DYNAMATIC_GUROBI_NOT_INSTALLED
@@ -186,12 +196,12 @@ class GurobiSolver : CPSolver {
 
   std::unique_ptr<GRBEnv> env;
   std::map<Var, GRBVar> variables;
+  std::unique_ptr<GRBModel> model;
 
   // Track the added names: prevent adding variables with duplicated names
   std::set<std::string> names;
 
 public:
-  std::unique_ptr<GRBModel> model;
   GurobiSolver() {
     env = std::make_unique<GRBEnv>(true);
     env->start();
@@ -199,16 +209,12 @@ public:
   }
 
   Var addVariable(const Var &var) override {
-
     if (names.count(var.name)) {
       llvm::report_fatal_error("Adding variable with duplicated names is not "
                                "permitted! Aborting...");
     }
-
-    double lb =
-        var.lowerBound.has_value() ? var.lowerBound.value() : -GRB_INFINITY;
-    double ub =
-        var.upperBound.has_value() ? var.upperBound.value() : GRB_INFINITY;
+    double lb = var.lowerBound.value_or(-GRB_INFINITY);
+    double ub = var.upperBound.value_or(GRB_INFINITY);
     char type;
     switch (var.type) {
     case Var::REAL:
@@ -227,7 +233,7 @@ public:
 
   // Create var, add gurobi var, and then return
   Var addVariable(const std::string &name, Var::VarType type,
-                  std::optional<double> lb, std::optional<double> ub) {
+                  std::optional<double> lb, std::optional<double> ub) override {
     auto var = Var(name, type, lb, ub);
     return addVariable(var);
   }
@@ -256,20 +262,41 @@ public:
     model->setObjective(obj, GRB_MAXIMIZE);
   }
 
-  bool solve() override {
+  void optimize() override {
     model->optimize();
-    return model->get(GRB_IntAttr_Status) == GRB_OPTIMAL;
+
+    switch (model->get(GRB_IntAttr_Status)) {
+    case GRB_OPTIMAL:
+      status = OPTIMAL;
+      break;
+    case GRB_SUBOPTIMAL:
+    case GRB_TIME_LIMIT:
+      status = NONOPTIMAL;
+      break;
+    case GRB_UNBOUNDED:
+      status = UNBOUNDED;
+      break;
+    case GRB_INFEASIBLE:
+      status = INFEASIBLE;
+      break;
+    default:
+      status = ERROR;
+    }
   }
 
   /// Retrieve the value from the solved MILP
   ///
   /// Example:
   /// auto resultMyVar = solver.getValue(myVar);
-  double getValue(const Var &var) const override {
+  std::optional<double> getValue(const Var &var) const override {
+    if (status != OPTIMAL && status != NONOPTIMAL)
+      return std::nullopt;
     return variables.at(var).get(GRB_DoubleAttr_X);
   }
 
-  double getObjective() const override {
+  std::optional<double> getObjective() const override {
+    if (status != OPTIMAL && status != NONOPTIMAL)
+      return std::nullopt;
     return model->get(GRB_DoubleAttr_ObjVal);
   }
 };
--- unittests/Support/ConstraintProgramming/CPTest.cpp
+++ unittests/Support/ConstraintProgramming/CPTest.cpp
@@ -4,8 +4,7 @@
 using namespace dynamatic::cp;
 
 namespace {
-// Tests factorial of negative numbers.
-TEST(BasicTests, test1) {
+TEST(BasicTests, basicMILPTest) {
 
   // [Using our API to solve the result]
   auto solver = GurobiSolver();
@@ -15,11 +14,9 @@ TEST(BasicTests, test1) {
   solver.addLinearConstraint(3 * x - y >= 0);
   solver.addLinearConstraint(x - y <= 2);
   solver.setMaximizeObjective(3 * x + 4 * y);
-  solver.solve();
+  solver.optimize();
 
   // [Using Gurobi's API to solve the result]
-  solver.model->write("model.lp");
-
   GRBEnv env = GRBEnv(true);
   env.start();
 
@@ -27,10 +24,8 @@ TEST(BasicTests, test1) {
   GRBModel model = GRBModel(env);
 
   // Create variables x and y (continuous by default)
-  GRBVar a =
-      model.addVar(-GRB_INFINITY, GRB_INFINITY, 0.0, GRB_CONTINUOUS, "x");
-  GRBVar b =
-      model.addVar(-GRB_INFINITY, GRB_INFINITY, 0.0, GRB_CONTINUOUS, "y");
+  GRBVar a = model.addVar(0, GRB_INFINITY, 0.0, GRB_CONTINUOUS, "x");
+  GRBVar b = model.addVar(0, GRB_INFINITY, 0.0, GRB_CONTINUOUS, "y");
 
   // Add constraints
   model.addConstr(a + 2 * b <= 14, "c1");
@@ -49,4 +44,45 @@ TEST(BasicTests, test1) {
   EXPECT_EQ(a.get(GRB_DoubleAttr_X), solver.getValue(x));
   EXPECT_EQ(b.get(GRB_DoubleAttr_X), solver.getValue(y));
 }
+
+TEST(BasicTests, basicIntegerProgramTest) {
+  // [Using our API to solve the result]
+  auto solver = GurobiSolver();
+  auto x = solver.addVariable("x", Var::INTEGER, /* lb */ 0,
+                              /* infinity */ std::nullopt);
+  auto y = solver.addVariable("y", Var::INTEGER, /* lb */ 0,
+                              /* infinity */ std::nullopt);
+  auto z = solver.addVariable("z", Var::INTEGER, /* lb */ 0,
+                              /* infinity */ std::nullopt);
+  solver.addLinearConstraint(2 * x + 7 * y + 3 * z <= 50);
+  solver.addLinearConstraint(3 * x + 5 * y + 7 * z <= 45);
+  solver.addLinearConstraint(5 * x + 2 * y - 6 * z <= 37);
+  solver.setMaximizeObjective(2 * x + 2 * y + 3 * z);
+  solver.optimize();
+
+  // [Using Gurobi's API to solve the result]
+  GRBEnv env = GRBEnv(true);
+  env.start();
+  GRBModel model = GRBModel(env);
+  //
+  // Create variables x and y (continuous by default)
+  GRBVar a = model.addVar(0, GRB_INFINITY, 0.0, GRB_INTEGER, "a");
+  GRBVar b = model.addVar(0, GRB_INFINITY, 0.0, GRB_INTEGER, "b");
+  GRBVar c = model.addVar(0, GRB_INFINITY, 0.0, GRB_INTEGER, "c");
+
+  model.addConstr(2 * a + 7 * b + 3 * c <= 50);
+  model.addConstr(3 * a + 5 * b + 7 * c <= 45);
+  model.addConstr(5 * a + 2 * b - 6 * c <= 37);
+
+  model.setObjective(2 * a + 2 * b + 3 * c, GRB_MAXIMIZE);
+  model.optimize();
+
+  // Check: The result from our API and from Gurobi's API must be exactly the
+  // same.
+  EXPECT_EQ(model.get(GRB_DoubleAttr_ObjVal), solver.getObjective());
+  EXPECT_EQ(a.get(GRB_DoubleAttr_X), solver.getValue(x));
+  EXPECT_EQ(b.get(GRB_DoubleAttr_X), solver.getValue(y));
+  EXPECT_EQ(c.get(GRB_DoubleAttr_X), solver.getValue(z));
+}
+
 } // namespace
