0370d0a178c88c29127900b21b3f2fb4110ab29d
Emmet Murphy <murphe67@tcd.ie>
2025-10-14 16:19:36 +0200

    remove port namer

--- experimental/lib/Support/FormalProperty.cpp
+++ experimental/lib/Support/FormalProperty.cpp
@@ -128,9 +128,6 @@ AbsenceOfBackpressure::AbsenceOfBackpressure(unsigned long id, TAG tag,
   Operation *ownerOp = res.getOwner();
   Operation *userOp = *res.getUsers().begin();
 
-  handshake::PortNamer ownerNamer(ownerOp);
-  handshake::PortNamer userNamer(userOp);
-
   unsigned long operandIndex = userOp->getNumOperands();
   for (auto [j, arg] : llvm::enumerate(userOp->getOperands())) {
     if (arg == res) {
@@ -144,9 +141,8 @@ AbsenceOfBackpressure::AbsenceOfBackpressure(unsigned long id, TAG tag,
   userChannel.operationName = getUniqueName(userOp).str();
   ownerChannel.channelIndex = res.getResultNumber();
   userChannel.channelIndex = operandIndex;
-  ownerChannel.channelName =
-      ownerNamer.getOutputName(res.getResultNumber()).str();
-  userChannel.channelName = userNamer.getInputName(operandIndex).str();
+  ownerChannel.channelName = getOutputPortName(ownerOp, res.getResultNumber());
+  userChannel.channelName = getInputPortName(userOp, operandIndex);
 }
 
 llvm::json::Value AbsenceOfBackpressure::extraInfoToJSON() const {
@@ -184,18 +180,16 @@ ValidEquivalence::ValidEquivalence(unsigned long id, TAG tag,
     : FormalProperty(id, tag, TYPE::VEQ) {
   Operation *op1 = res1.getOwner();
   unsigned int i = res1.getResultNumber();
-  handshake::PortNamer namer1(op1);
 
   Operation *op2 = res2.getOwner();
   unsigned int j = res2.getResultNumber();
-  handshake::PortNamer namer2(op2);
 
   ownerChannel.operationName = getUniqueName(op1).str();
   targetChannel.operationName = getUniqueName(op2).str();
   ownerChannel.channelIndex = i;
   targetChannel.channelIndex = j;
-  ownerChannel.channelName = namer1.getOutputName(i).str();
-  targetChannel.channelName = namer2.getOutputName(j).str();
+  ownerChannel.channelName = getOutputPortName(op1, i);
+  targetChannel.channelName = getOutputPortName(op2, j);
 }
 
 llvm::json::Value ValidEquivalence::extraInfoToJSON() const {
--- include/dynamatic/Dialect/Handshake/HandshakeInterfaces.h
+++ include/dynamatic/Dialect/Handshake/HandshakeInterfaces.h
@@ -49,6 +49,30 @@ std::string getInputPortName(Operation *op, size_t portIdx);
 unsigned getNumOutputPorts(Operation *op);
 std::string getOutputPortName(Operation *op, size_t portIdx);
 
+/// Provides an opaque interface for generating the port names of an operation;
+
+// class PortNamer {
+// public:
+//   /// Does nothing; no port name will be generated.
+//   PortNamer() = default;
+
+//   /// Derives port names for the operation on object creation.
+//   PortNamer(Operation *op);
+
+//   /// Returs the port name of the input at the specified index.
+//   StringRef getInputName(unsigned idx) const { return inputs[idx]; }
+
+//   /// Returs the port name of the output at the specified index.
+//   StringRef getOutputName(unsigned idx) const { return outputs[idx]; }
+
+// private:
+
+//   /// List of input port names.
+//   SmallVector<std::string> inputs;
+//   /// List of output port names.
+//   SmallVector<std::string> outputs;
+// };
+
 class ControlType;
 
 namespace detail {
