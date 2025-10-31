d1b6115d0989e546aa02763bb1dd11a610c4f552
Jiahui Xu <jxu@ethz.ch>
2025-10-31 17:46:17 +0100

    add more passes to remove loops completely

--- tools/dynamatic/scripts/compile.sh
+++ tools/dynamatic/scripts/compile.sh
@@ -157,7 +157,7 @@ sed -i "s/^target triple = .*$//g" "$F_CLANG"
 # ------------------------------------------------------------------------------
 
 $LLVM_BINS/opt -S \
-  -passes="inline,mem2reg,consthoist,instcombine,function(loop-mssa(licm<no-allowspeculation>)),simplifycfg,loop-rotate,simplifycfg,sink,lowerswitch,simplifycfg" \
+  -passes="inline,mem2reg,consthoist,instcombine,function(loop-mssa(licm<no-allowspeculation>)),function(loop(loop-idiom,indvars,loop-deletion,loop-unroll-full)),simplifycfg,loop-rotate,simplifycfg,sink,lowerswitch,simplifycfg" \
   "$F_CLANG" \
   > "$F_CLANG_OPTIMIZED"
 exit_on_fail "Failed to apply optimization to LLVM IR" \
