6ba8beb460fd01ccbc88c98cf24c01b9993a79b8
Ian Ganz <aganz@student.ethz.ch>
2025-11-04 23:08:23 +0100

    commit squash

--- tools/export-rtl/export-rtl.cpp
+++ tools/export-rtl/export-rtl.cpp
@@ -81,7 +81,9 @@ static cl::opt<std::string> propertyFilename("property-database", cl::Optional,
 static cl::opt<HDL>
     hdl("hdl", cl::Optional, cl::desc("<hdl to use>"), cl::init(HDL::VHDL),
         cl::values(clEnumValN(HDL::VHDL, "vhdl", "VHDL"),
+                   clEnumValN(HDL::VHDL, "vhdl-beta", "VHDL Beta"),
                    clEnumValN(HDL::VERILOG, "verilog", "Verilog"),
+                   clEnumValN(HDL::VERILOG, "verilog-beta", "Verilog Beta"),
                    clEnumValN(HDL::SMV, "smv", "SMV")),
         cl::cat(mainCategory));
 
@@ -135,7 +137,7 @@ struct ExportInfo {
   /// Creates export information for the given module and RTL configuration.
   ExportInfo(mlir::ModuleOp modOp, RTLConfiguration &config,
              StringRef outputPath)
-      : modOp(modOp), config(config), outputPath(outputPath){};
+      : modOp(modOp), config(config), outputPath(outputPath) {};
 
   /// Associates every external hardware module to its match according to the
   /// RTL configuration and concretizes each of them inside the output
@@ -158,7 +160,7 @@ struct FormalPropertyInfo {
   StringRef outputPath;
 
   FormalPropertyInfo(FormalPropertyTable &table, StringRef outputPath)
-      : table(table), outputPath(outputPath){};
+      : table(table), outputPath(outputPath) {};
 };
 } // namespace
 
@@ -331,7 +333,7 @@ public:
 
   /// Creates the RTL writer.
   RTLWriter(ExportInfo &exportInfo, FormalPropertyInfo &propertyInfo, HDL hdl)
-      : exportInfo(exportInfo), propertyInfo(propertyInfo), hdl(hdl){};
+      : exportInfo(exportInfo), propertyInfo(propertyInfo), hdl(hdl) {};
 
   /// Writes the RTL implementation of the module to the output stream. On
   /// failure, the RTL implementation should be considered invalid and/or
