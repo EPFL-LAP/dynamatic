86ece53fd69b3d9b7370d4ccca967c526d3a3e27
Ian-Ganz <151950296+Ian-Ganz@users.noreply.github.com>
2025-12-03 15:46:45 +0100

    beta-backend verilog (#628)
    Added basic units for verilog for the beta-backend.
    The option verilog-beta is available.
    
    ---------
    
    Co-authored-by: Ian Ganz <iaganz@student.ethz.ch>

---
+++ experimental/tools/unit-generators/verilog/generators/handshake/cond_br.py
@@ -0,0 +1,121 @@
+from generators.handshake.join import generate_join
+
+
+def generate_cond_br(name, params):
+    bitwidth = params["bitwidth"]
+    extra_signals = params.get("extra_signals", None)
+
+    if bitwidth == 0:
+        return _generate_cond_br_dataless(name)
+    else:
+        return _generate_cond_br(name, bitwidth)
+
+
+def _generate_cond_br(name, bitwidth):
+
+    cond_br_dataless_name = name + "_cond_br_dataless"
+    cond_br_dataless = _generate_cond_br_dataless(cond_br_dataless_name)
+
+    body_cond_br = f"""
+// Module of cond_br
+module {name}(
+  input clk,
+  input rst,
+  // Data Input Channel
+  input [{bitwidth} - 1 : 0] data,        
+  input data_valid,
+  output data_ready,
+  // Condition Input Channel
+  input condition,    
+  input condition_valid,
+  output condition_ready,
+  // True Output Channel
+  output [{bitwidth} - 1 : 0] trueOut,
+  output trueOut_valid,
+  input trueOut_ready,
+  // False Output Channel
+  output [{bitwidth} - 1 : 0] falseOut,
+  output falseOut_valid,
+  input falseOut_ready
+);
+  {cond_br_dataless_name} control (
+    .clk             (clk             ),
+    .rst             (rst             ),
+    .data_valid       (data_valid     ),
+    .data_ready       (data_ready     ),
+    .condition       (condition       ),
+    .condition_valid (condition_valid),
+    .condition_ready (condition_ready),
+    .trueOut_valid   (trueOut_valid   ),
+    .trueOut_ready   (trueOut_ready   ),
+    .falseOut_valid   (falseOut_valid ),
+    .falseOut_ready   (falseOut_ready )
+  );
+
+  assign trueOut = data;
+  assign falseOut = data;
+
+endmodule
+
+"""
+
+    return cond_br_dataless + body_cond_br
+
+
+def _generate_cond_br_dataless(name):
+
+    join_name = name + "_join"
+
+    join_instance = generate_join(join_name, {"size": 2})
+
+    body_cond_br_dataless = f"""
+// Module of cond_br_dataless
+
+// In the original implementation
+// Data Organization: out2-:32, out1+:32
+// condition is implemeted as [0:0], I removed the redudancy
+module {name} (
+  input  clk,
+  input  rst,
+  // Data Input Channel
+  input  data_valid,
+  output data_ready,
+  // Condition Input Channel
+  input  condition,       
+  input  condition_valid,
+  output condition_ready,
+  // True Output Channel, Channel 1
+  output trueOut_valid,
+  input  trueOut_ready,
+  // False Output Channel, Channel 2
+  output falseOut_valid,
+  input  falseOut_ready
+);  
+  wire [1 : 0] ins_valid_vec; // Vector for the join's ins_valid
+  wire [1 : 0] ins_ready_vec; // Vector for the join's ins_ready
+  
+  wire branchInputs_valid;
+  wire branch_ready;
+
+  // Assign individual signals to the vector
+  assign ins_valid_vec = {{condition_valid, data_valid}};
+
+  {join_name} join_branch (
+    .ins_valid  (ins_valid_vec     ),
+    .outs_ready (branch_ready      ),
+    .ins_ready  (ins_ready_vec     ),
+    .outs_valid (branchInputs_valid)
+  );
+
+  // Connect the outputs of the join module to appropriate external signals
+  assign data_ready = ins_ready_vec[0];
+  assign condition_ready = ins_ready_vec[1];
+
+  assign trueOut_valid  = condition & branchInputs_valid;
+  assign falseOut_valid = ~condition & branchInputs_valid;
+  assign branch_ready = (falseOut_ready & ~condition) | (trueOut_ready & condition);
+
+endmodule
+"""
+
+    return join_instance + body_cond_br_dataless
---
+++ experimental/tools/unit-generators/verilog/generators/support/unary.py
@@ -0,0 +1,156 @@
+from generators.support.utils import ExtraSignals
+from generators.support.utils import ExtraSignals
+from generators.handshake.buffer import generate_valid_propagation_buffer
+
+
+def generate_unary(
+    name: str,
+    handshake_op: str,
+    body: str,
+    extra_signals: ExtraSignals,
+    signals: str = "",
+    dependencies: str = "",
+    latency: int = 0,
+    bitwidth: int = None,
+    input_bitwidth: int = None,
+    output_bitwidth: int = None,
+) -> str:
+    """
+    Generates boilerplate Verilog entity and handshaking code for unary units
+    (units with one input and one output).
+
+    If latency = 0:
+      Output ready is directly forwarded up from input ready.
+      Output valid is directly forwarded down from input valid.
+    Else:
+      Handshaking signals are passed through a valid propagation buffer.
+      Which is either a one slot break dv, or a shift register buffer
+
+    Args:
+        name: Unique name based on MLIR op name (e.g. adder0).
+        handshake_op: Which handshake operation this module corresponds to, used only in comments
+        signals: Local signal declarations used in body.
+        body: VHDL body of the unit, excluding handshaking.
+        dependencies: Dependencies, excluding handshaking.
+        latency:
+        bitwidth: Unit bitwidth (if input/output are the same).
+        input_bitwidth: Input bitwidth (used if asymmetric).
+        output_bitwidth: Output bitwidth (used if asymmetric).
+        extra_signals: Extra signals on input/output channels, from IR.
+
+
+    Returns:
+        Verilog code as a string.
+    """
+    if bitwidth is not None:
+        if input_bitwidth is not None or output_bitwidth is not None:
+            raise RuntimeError(
+                "If bitwidth is specified, input and output bitwidth must not be specified")
+
+        input_bitwidth = bitwidth
+        output_bitwidth = bitwidth
+
+    elif input_bitwidth is None or output_bitwidth is None:
+        raise RuntimeError(
+            "If bitwidth is not specified, both input and output bitwidth must be specified")
+
+    # generate inner function takes a name parameter
+    # since the top level name is used for the signal manager wrapper
+    #
+    # the signal manager wrapper will make a new name for the inner unit
+    def generate_inner(name): return _generate_unary(
+        name,
+        handshake_op,
+        input_bitwidth,
+        output_bitwidth,
+        signals,
+        body,
+        dependencies,
+        latency
+    )
+
+    # if no signal manager,
+    # the unit uses the top level name
+    def generate(): return generate_inner(name)
+
+    return generate()
+
+
+def _generate_unary(
+        name,
+        handshake_op,
+        input_bitwidth,
+        output_bitwidth,
+        signals,
+        body,
+        dependencies,
+        latency
+):
+
+    # all unary units have the same entity
+    entity = f"""
+// Module of {handshake_op}
+module {name} (
+    input wire clk,
+    input wire rst,
+    // input channel
+    input wire [{input_bitwidth - 1}:0] ins,
+    input wire ins_valid,
+    output wire ins_ready,
+    // output channel
+    output wire [{output_bitwidth - 1}:0] outs,
+    output wire outs_valid,
+    input wire outs_ready
+  );
+"""
+    signals = signals.lstrip()
+    body = body.lstrip()
+
+    # but the architecture differs depending
+    # on the latency
+
+    # Handshaking is directly forwarded
+    if latency == 0:
+        architecture = f"""
+  // Signals
+  {signals}
+
+  // Body
+  {body}
+
+  // combinatorial unit forwards handshaking
+  assign outs_valid = ins_valid;
+  assign ins_ready = outs_ready;
+
+endmodule
+"""
+    # otherwise, we need a buffer to propagate the valid
+    else:
+        valid_buffer_name = f"{name}_valid_buffer"
+        dependencies += generate_valid_propagation_buffer(
+            valid_buffer_name, latency)
+
+        architecture = f"""
+  // Signals
+  {signals}
+  wire valid_buffer_ready;
+  {valid_buffer_name} valid_buffer (
+    .clk        (clk),
+    .rst        (rst),
+    // input channel from "ins"
+    .ins_valid  (ins_valid),
+    .ins_ready  (valid_buffer_ready),
+    // output channel to "outs"
+    .outs_valid (outs_valid),
+    .outs_ready (outs_ready)
+  );
+
+  // expose to allow use as a clock enable signal
+  assign ins_ready = valid_buffer_ready;
+
+  {body}
+
+end architecture;
+"""
+
+    return dependencies + entity + architecture
