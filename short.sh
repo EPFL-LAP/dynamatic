331710554a9ff42cb3479e235f1538f72b7e5632
Emmet Murphy <murphe67@tcd.ie>
2025-12-03 13:19:00 +0100

    short test script

---
+++ short.sh
@@ -0,0 +1,7 @@
+python3 experimental/tools/integration/run_specv2_integration.py single_loop --baseline --out out_baseline_7ns --cp 7.00
+python3 experimental/tools/integration/run_adapted_spec_integration.py single_loop --out out_v1_7ns --cp 7.00
+python3 experimental/tools/integration/run_specv2_integration.py single_loop --n 1 --out out_1_7ns --cp 7.00 --resolver
+
+timeout --kill-after=10s 900s ./tools/dynamatic/scripts/synthesize.sh $DYNAMATIC_DIR $DYNAMATIC_DIR/integration-test/single_loop/out_baseline_7ns single_loop 7.000 3.500
+timeout --kill-after=10s 900s ./tools/dynamatic/scripts/synthesize.sh $DYNAMATIC_DIR $DYNAMATIC_DIR/integration-test/single_loop/out_v1_7ns single_loop 7.000 3.500
+timeout --kill-after=10s 900s ./tools/dynamatic/scripts/synthesize.sh $DYNAMATIC_DIR $DYNAMATIC_DIR/integration-test/single_loop/out_4_7ns single_loop 7.000 3.500
