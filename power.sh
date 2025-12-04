5b09753f3cfe76ea97de42523a6daafa93a30e7c
Emmet Murphy <murphe67@tcd.ie>
2025-12-04 13:43:43 +0100

    draft power script

---
+++ power.sh
@@ -0,0 +1,2 @@
+python tools/dynamatic/estimate_power/estimate_power.py --output_dir $DYNAMATIC_DIR/integration-test/single_loop/out_baseline_7ns --kernel_name single_loop --cp 7
+python tools/dynamatic/estimate_power/estimate_power.py --output_dir $DYNAMATIC_DIR/integration-test/single_loop/out_v1_7ns --kernel_name single_loop --cp 7
