57b19dfd87a12bbf48e03f6f765d928ebd8d91e2
shundroid <shundroid@users.noreply.github.com>
2025-12-05 10:13:31 +0900

    decide n for large single loop

--- main.sh
+++ main.sh
@@ -620,7 +620,7 @@ timeout --kill-after=10s 900s ./tools/dynamatic/scripts/synthesize.sh $DYNAMATIC
 
 # prof-cache is not used
 python3 experimental/tools/integration/run_specv2_large_integration.py single_loop_unrolled_160 --min-buffering --baseline --use-prof-cache --out out_baseline_7ns --factor 160
-python3 experimental/tools/integration/run_specv2_large_integration.py single_loop_unrolled_160 --min-buffering --n=4 --resolver --use-prof-cache --out out_4_7ns --factor 160
+python3 experimental/tools/integration/run_specv2_large_integration.py single_loop_unrolled_160 --min-buffering --decide-n 0 --resolver --use-prof-cache --out out_4_7ns --factor 160
 rm integration-test/single_loop_unrolled_160/specv2_*
 
 # Longer timeout
