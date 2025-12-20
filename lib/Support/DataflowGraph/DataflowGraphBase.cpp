2adcea3c2aa748ea58840f498c1425cc99b6c762
Ziad Malik <me@ziadmalik.com>
2025-12-21 00:50:30 +0100

    chore: refactor dataflow graph & separate the logic from the pathfinding

---
+++ lib/Support/DataflowGraph/DataflowGraphBase.cpp
@@ -0,0 +1,15 @@
+//-*-===//
+//
+// Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// A base class for dataflow graphs used in circuit analysis & optimization.
+//
+//===----------------------------------------------------------------------===//
+
+#include "dynamatic/Support/DataflowGraph/DataflowGraphBase.h"
+
+namespace dynamatic {} // namespace dynamatic
