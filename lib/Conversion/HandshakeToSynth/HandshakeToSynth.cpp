2cbb07ed83b8682eacb676b1cf677983117bb8a3
Carmine50 <crizzi@ethz.ch>
2026-01-14 18:12:08 +0100

    Added new option to pass the directory containing blif files and hard-coded switch to retrieve the blif path of each operation

--- lib/Conversion/HandshakeToSynth/HandshakeToSynth.cpp
+++ lib/Conversion/HandshakeToSynth/HandshakeToSynth.cpp
@@ -59,6 +59,7 @@
 #include <bitset>
 #include <cctype>
 #include <cstdint>
+#include <filesystem>
 #include <fstream>
 #include <iterator>
 #include <regex>
@@ -1370,13 +1371,206 @@ LogicalResult SignalRewriter::rewriteAllSignals(mlir::ModuleOp modOp) {
 }
 
 // ------------------------------------------------------------------
+// Functions to mark handshake operations with blif path
+// ------------------------------------------------------------------
+
+// Function to combine parameter values, module type and blif directory path
+// to create the blif file path
+std::string combineBlifFilePath(std::string blifDirPath, std::string moduleType,
+                                std::vector<std::string> paramValues,
+                                std::string extraSuffix = "") {
+  std::string blifFilePath = blifDirPath + "/" + moduleType + extraSuffix;
+  for (const auto &paramValue : paramValues) {
+    blifFilePath += "/" + paramValue;
+  }
+  blifFilePath += "/" + moduleType + extraSuffix + ".blif";
+  return blifFilePath;
+}
+
+// Function to retrieve the path of the blif file for a given handshake
+// operation
+std::string getBlifFilePathForHandshakeOp(Operation *op,
+                                          std::string blifDirPath) {
+  // Depending on the operation type, there is a different number and type of
+  // parameters. For now, we use a switch hard-coded on the operation type. In
+  // the future, this should be improved by allowing extraction of parameter
+  // information from the operation itself
+  std::string moduleType = op->getName().getStringRef().str();
+  // Erase the dialect name from the moduleType
+  moduleType = moduleType.substr(moduleType.find('.') + 1);
+  std::string blifFileName;
+  llvm::TypeSwitch<Operation *>(op)
+      .Case<handshake::AddIOp, handshake::AndIOp, handshake::CmpIOp,
+            handshake::OrIOp, handshake::ShLIOp, handshake::ShRSIOp,
+            handshake::ShRUIOp, handshake::SubIOp, handshake::XOrIOp,
+            handshake::MulIOp, handshake::DivSIOp, handshake::DivUIOp,
+            handshake::SelectOp, handshake::SIToFPOp, handshake::FPToSIOp,
+            handshake::ExtFOp, handshake::TruncFOp>([&](auto) {
+        unsigned dataWidth =
+            handshake::getHandshakeTypeBitWidth(op->getOperand(0).getType());
+        blifFileName = combineBlifFilePath(blifDirPath, moduleType,
+                                           {std::to_string(dataWidth)});
+      })
+      .Case<handshake::ConstantOp>([&](handshake::ConstantOp constOp) {
+        handshake::ChannelType cstType = constOp.getResult().getType();
+        // Get the data width of the constant operation
+        unsigned dataWidth = cstType.getDataBitWidth();
+        blifFileName = combineBlifFilePath(blifDirPath, moduleType,
+                                           {std::to_string(dataWidth)});
+      })
+      .Case<handshake::BranchOp, handshake::SinkOp>([&](auto) {
+        unsigned dataWidth =
+            handshake::getHandshakeTypeBitWidth(op->getOperand(0).getType());
+        if (dataWidth == 0) {
+          blifFileName =
+              combineBlifFilePath(blifDirPath, moduleType, {}, "_dataless");
+        } else {
+          blifFileName = combineBlifFilePath(blifDirPath, moduleType,
+                                             {std::to_string(dataWidth)});
+        }
+      })
+      .Case<handshake::BufferOp>([&](handshake::BufferOp op) {
+        std::string bufferType =
+            handshake::stringifyEnum(op.getBufferType()).str();
+        unsigned dataWidth =
+            handshake::getHandshakeTypeBitWidth(op->getOperand(0).getType());
+        if (dataWidth == 0) {
+          blifFileName =
+              combineBlifFilePath(blifDirPath, bufferType, {}, "_dataless");
+        } else {
+          blifFileName = combineBlifFilePath(blifDirPath, bufferType,
+                                             {std::to_string(dataWidth)});
+        }
+      })
+      .Case<handshake::ConditionalBranchOp>(
+          [&](handshake::ConditionalBranchOp cbrOp) {
+            unsigned dataWidth = handshake::getHandshakeTypeBitWidth(
+                cbrOp.getDataOperand().getType());
+            if (dataWidth == 0) {
+              blifFileName =
+                  combineBlifFilePath(blifDirPath, moduleType, {}, "_dataless");
+            } else {
+              blifFileName = combineBlifFilePath(blifDirPath, moduleType,
+                                                 {std::to_string(dataWidth)});
+            }
+          })
+      .Case<handshake::ControlMergeOp>([&](handshake::ControlMergeOp cmergeOp) {
+        unsigned size = cmergeOp.getDataOperands().size();
+        unsigned dataWidth =
+            handshake::getHandshakeTypeBitWidth(cmergeOp.getResult().getType());
+        unsigned indexType =
+            handshake::getHandshakeTypeBitWidth(cmergeOp.getIndex().getType());
+        if (dataWidth == 0) {
+          blifFileName = combineBlifFilePath(
+              blifDirPath, moduleType,
+              {std::to_string(size), std::to_string(indexType)}, "_dataless");
+        } else {
+          assert(false && "ControlMerge with data not supported yet");
+        }
+      })
+      .Case<handshake::ExtSIOp, handshake::ExtUIOp, handshake::ExtFOp,
+            handshake::TruncIOp, handshake::TruncFOp>([&](auto op) {
+        unsigned inputWidth =
+            handshake::getHandshakeTypeBitWidth(op.getOperand().getType());
+        unsigned outputWidth =
+            handshake::getHandshakeTypeBitWidth(op.getResult().getType());
+        blifFileName = combineBlifFilePath(
+            blifDirPath, moduleType,
+            {std::to_string(inputWidth), std::to_string(outputWidth)});
+      })
+      .Case<handshake::ForkOp, handshake::LazyForkOp>([&](auto) {
+        unsigned size = op->getNumResults();
+        unsigned dataWidth =
+            handshake::getHandshakeTypeBitWidth(op->getOperand(0).getType());
+        if (dataWidth == 0) {
+          blifFileName = combineBlifFilePath(
+              blifDirPath, moduleType, {std::to_string(size)}, "_dataless");
+        } else {
+          blifFileName = combineBlifFilePath(
+              blifDirPath, moduleType,
+              {std::to_string(size), std::to_string(dataWidth)}, "_type");
+        }
+      })
+      .Case<handshake::MuxOp>([&](handshake::MuxOp muxOp) {
+        unsigned size = muxOp.getDataOperands().size();
+        unsigned dataWidth =
+            handshake::getHandshakeTypeBitWidth(muxOp.getResult().getType());
+        unsigned selectType = handshake::getHandshakeTypeBitWidth(
+            muxOp.getSelectOperand().getType());
+        blifFileName = combineBlifFilePath(blifDirPath, moduleType,
+                                           {std::to_string(size),
+                                            std::to_string(dataWidth),
+                                            std::to_string(selectType)});
+      })
+      .Case<handshake::MergeOp>([&](handshake::MergeOp mergeOp) {
+        unsigned size = mergeOp.getDataOperands().size();
+        unsigned dataWidth = handshake::getHandshakeTypeBitWidth(
+            mergeOp.getDataOperands()[0].getType());
+        blifFileName = combineBlifFilePath(
+            blifDirPath, moduleType,
+            {std::to_string(size), std::to_string(dataWidth)});
+      })
+      .Case<handshake::LoadOp, handshake::StoreOp>([&](auto op) {
+        unsigned dataWidth =
+            handshake::getHandshakeTypeBitWidth(op.getDataInput().getType());
+        unsigned addrType =
+            handshake::getHandshakeTypeBitWidth(op.getAddressInput().getType());
+        blifFileName = combineBlifFilePath(
+            blifDirPath, moduleType,
+            {std::to_string(dataWidth), std::to_string(addrType)});
+      })
+      .Case<handshake::SourceOp>([&](auto) {
+        blifFileName = combineBlifFilePath(blifDirPath, moduleType, {});
+      })
+      .Default([&](auto) { blifFileName = ""; });
+  // Check blifFileName path exists
+  if (blifFileName != "") {
+    // Check if the file exists
+    if (!std::filesystem::exists(blifFileName)) {
+      llvm::errs() << "BLIF file for operation " << getUniqueName(op)
+                   << "does not exist: " << blifFileName << "\n";
+      assert(false && "Check the file path specified for this operation is "
+                      "built correctly");
+    }
+  }
+  return blifFileName;
+}
+
+// Function to add blif path attribute to each handshake operation
+LogicalResult markHandshakeOpsWithBlifPath(handshake::FuncOp funcOp,
+                                           std::string blifDirPath,
+                                           OpBuilder &builder) {
+  // Check blifDirPath is not empty
+  if (blifDirPath.empty()) {
+    llvm::errs() << "BLIF directory path is empty\n";
+    return failure();
+  }
+  // Walk through all handshake operations in the function
+  funcOp.walk([&](Operation *op) {
+    // Skip function operations
+    if (isa<handshake::FuncOp>(op))
+      return;
+    // Create the blif path by combining the directory path and the op name
+    // depending on the operation type
+    std::string blifFilePath = getBlifFilePathForHandshakeOp(op, blifDirPath);
+    // Create a string attribute for the blif path
+    mlir::StringAttr blifPathAttr =
+        mlir::StringAttr::get(builder.getContext(), blifFilePath);
+    // Set the blif path attribute on the operation
+    op->setAttr("blif_path", blifPathAttr);
+  });
+  return success();
+}
+
+// ------------------------------------------------------------------
 // Main pass definition
 // ------------------------------------------------------------------
 
 namespace {
-
 // The following pass converts handshake operations into synth operations
 // It executes in multiple steps:
+// 0) Mark each handshake operation with the path of the blif file where its
+//    definition is located
 // 1) Unbundle all handshake types used in the handshake function
 // 2) Invert the direction of ready signals in all hw modules and hw instances
 //    to follow the standard handshake protocol where ready signals go in the
@@ -1387,6 +1581,9 @@ namespace {
 class HandshakeToSynthPass
     : public dynamatic::impl::HandshakeToSynthBase<HandshakeToSynthPass> {
 public:
+  HandshakeToSynthPass(std::string blifDirPath);
+  using HandshakeToSynthBase::HandshakeToSynthBase;
+
   void runDynamaticPass() override {
     mlir::ModuleOp modOp = getOperation();
     MLIRContext *ctx = &getContext();
@@ -1408,6 +1605,11 @@ public:
     if (!funcOp)
       return;
 
+    // Step 0: Mark each handshake operation with the path of the blif file
+    // where its definition is located
+    if (failed(markHandshakeOpsWithBlifPath(funcOp, blifDirPath, builder)))
+      return signalPassFailure();
+
     // Step 1: unbundle all handshake types in the handshake function
     if (failed(unbundleAllHandshakeTypes(modOp, ctx)))
       return signalPassFailure();
@@ -1415,6 +1617,7 @@ public:
     // Step 2: invert the direction of all ready signals in the hw modules
     // created from handshake operations. Additionally, unbundle data signals
     // into single-bit signals.
+    //
     // Create on object of the SignalRewriter class to manage the
     // inversion
     SignalRewriter signalRewriter;
