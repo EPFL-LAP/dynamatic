05e17c6852c74c69fe2e99c862a3a0a4e239474d
Jiantao Liu <jianliu@ethz.ch>
2026-01-27 16:11:39 +0100

    Initial commit for the power_evaluation tcl generation script

--- tools/dynamatic/power/estimate_power.py
+++ tools/dynamatic/power/estimate_power.py
@@ -10,8 +10,6 @@ import argparse
 from pdb import run
 import shutil
 
-from numpy import power
-
 # Import file templates
 from script_templates import *
 
@@ -271,7 +269,7 @@ if __name__ == "__main__":
         ),
         default="pre"
     )
-    p.add_argument("--vivado_cmd", type=str, required=False, help="Vivado command", default="vivado")
+    p.add_argument("--vivado_cmd", type=str, required=False, help="Vivado command", default="vivado-2019.1.1 vivado")
     p.add_argument("--cp", type=float, required=True, help="Clock period for synthesis")
 
     args = p.parse_args()
