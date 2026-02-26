fc74105e2b1916e2a6165814d28df27e3fadf8b4
Markus Böck <markus.boeck02@gmail.com>
2026-02-26 15:47:07 +0100

    Use plain MLIR `Pass`

--- lib/Transforms/HandshakeRemoveUnusedMemRef.cpp
+++ lib/Transforms/HandshakeRemoveUnusedMemRef.cpp
@@ -17,30 +17,31 @@ namespace dynamatic {
 #include "dynamatic/Transforms/Passes.h.inc"
 } // namespace dynamatic
 
+using namespace dynamatic;
+
 namespace {
 
 struct HandshakeRemoveUnusedMemRefsPass
     : dynamatic::impl::HandshakeRemoveUnusedMemRefsBase<
           HandshakeRemoveUnusedMemRefsPass> {
 
-  void runDynamaticPass() override {
-    for (auto func : getOperation().getOps<dynamatic::handshake::FuncOp>()) {
-      if (func.isExternal())
+  void runOnOperation() override {
+    handshake::FuncOp func = getOperation();
+    if (func.isExternal())
+      return;
+
+    // Iterate through the arguments backwards, this ensures that the block
+    // argument indices remain stable after deletion.
+    auto argNames = to_vector(func.getArgNames().getValue());
+    for (BlockArgument arg : llvm::reverse(func.getArguments())) {
+      if (!arg.use_empty() || !isa<MemRefType>(arg.getType()))
         continue;
 
-      // Iterate through the arguments backwards, this ensures that the block
-      // argument indices remain stable after deletion.
-      auto argNames = to_vector(func.getArgNames().getValue());
-      for (BlockArgument arg : llvm::reverse(func.getArguments())) {
-        if (!arg.use_empty() || !isa<MemRefType>(arg.getType()))
-          continue;
-
-        func.eraseArgument(arg.getArgNumber());
-        argNames.erase(argNames.begin() + arg.getArgNumber());
-      }
-      // Update the argument names as well.
-      func->setAttr("argNames", ArrayAttr::get(&getContext(), argNames));
+      func.eraseArgument(arg.getArgNumber());
+      argNames.erase(argNames.begin() + arg.getArgNumber());
     }
+    // Update the argument names as well.
+    func->setAttr("argNames", ArrayAttr::get(&getContext(), argNames));
   }
 };
 } // namespace
