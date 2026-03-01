017ebdffbe955417210d4eda8a5cabdb43c5f411
Markus Böck <markus.boeck02@gmail.com>
2026-03-01 23:00:07 +0100

    Add reference to the C23 standard

--- tools/translate-llvm-to-std/InferArgTypes.cpp
+++ tools/translate-llvm-to-std/InferArgTypes.cpp
@@ -29,7 +29,8 @@ Type ArgType::getMlirType(OpBuilder &builder, bool flattenArray) const {
       case Void:       baseMLIRElemType = builder.getNoneType(); break;
       case Bool:
         // The in-memory representation of a bool type coming from C is a full
-        // byte.
+        // byte (See https://www.open-std.org/Jtc1/sc22/wg14/www/docs/n3220.pdf
+        // C23 6.2.6.2).
         baseMLIRElemType = (arrayDimensions.empty() ? builder.getI1Type() : builder.getI8Type());
         break;
       case Int8:       baseMLIRElemType = builder.getI8Type();   break;
