0acc219a28b62ae9416f9701c17c6d2c87b2fdc8
Markus Böck <markus.boeck02@gmail.com>
2026-03-10 10:27:25 +0100

    add missing dependencies, try out relative path in build.sh

--- build.sh
+++ build.sh
@@ -361,7 +361,7 @@ if should_run_cmake ; then
             $CMAKE_DYNAMATIC_ENABLE_CBC \
             $CMAKE_DYNAMATIC_ENABLE_LEQ_BINARIES
 
-    LLVM_DIR=$(pwd)/llvm-project/bin
+    LLVM_DIR="../build/llvm-project/bin"
   else
     cmake -G Ninja .. \
         -DMLIR_DIR="$LLVM_DIR/lib/cmake/mlir" \
