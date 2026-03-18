4d0f1910ae8ea5d5c5365d3d2205f73d8fe559bf
Max Wipfli <mwipfli@ethz.ch>
2026-03-18 12:52:36 +0100

    [integration-test] Make atax actually not use floats

--- integration-test/atax/atax.h
+++ integration-test/atax/atax.h
@@ -1,12 +1,12 @@
-#ifndef ATAX_ATAX_FLOAT
-#define ATAX_ATAX_FLOAT
+#ifndef ATAX_ATAX
+#define ATAX_ATAX
 
-typedef float in_int_t;
-typedef float out_int_t;
-typedef float inout_int_t;
+typedef int in_int_t;
+typedef int out_int_t;
+typedef int inout_int_t;
 
 #define N 20
 
 void atax(in_int_t A[N][N], in_int_t x[N], inout_int_t y[N],
           inout_int_t tmp[N]);
-#endif // ATAX_ATAX_FLOAT
+#endif // ATAX_ATAX
