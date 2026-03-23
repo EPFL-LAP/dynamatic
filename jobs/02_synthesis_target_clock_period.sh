3ee9c6cbd5719f900313ae95ecc793bbcb8048f6
Max Wipfli <mwipfli@ethz.ch>
2026-03-23 08:46:02 +0100

    [jobs] Clean up experiment 2

--- jobs/02_synthesis_target_clock_period.sh
+++ jobs/02_synthesis_target_clock_period.sh
@@ -6,31 +6,26 @@ RUN_EVALUATION_PY="${SCRIPT_DIR}/../tools/evaluation/run_evaluation.py"
 GIT_REVISION="$(git rev-parse --short HEAD)"
 
 # HACK: only run remaining
-# CLOCK_PERIODS=(6 5 4 3 2.5 2)
-CLOCK_PERIODS=(2.5 2)
+CLOCK_PERIODS=(6 5 4 3 2.5 2)
 LSQ_SIZES=(4 6 8 10 12 16 20)
 
 OUTPUT_DIR="${SCRIPT_DIR}/../eval_results/02_synthesis_target_clock_period_${GIT_REVISION}"
 mkdir -p "${OUTPUT_DIR}"
 cat <<EOF >"${OUTPUT_DIR}/README.txt"
 Experiment 2: Synthesis Target Clock Period
-- remaining run only: 2.5ns with size 20, 2ns full run
+- target clock periods: ${CLOCK_PERIODS[*]} ns
+- LSQ sizes: ${LSQ_SIZES[*]} entries
 - git revision: ${GIT_REVISION}
 - full pipelining
 EOF
 
 for CP in "${CLOCK_PERIODS[@]}"; do
-    CP_FORMATTED="${CP//./p}"
+	CP_FORMATTED="${CP//./p}"
 	OUTPUT_DIR_CP="${OUTPUT_DIR}/${CP_FORMATTED}ns"
 	echo "Output directory for clock period = ${CP} ns: ${OUTPUT_DIR_CP}"
 	mkdir -p "${OUTPUT_DIR_CP}"
 
 	for LSQ_SIZE in "${LSQ_SIZES[@]}"; do
-    	if [[ "$CP" == "2.5" && "$LSQ_SIZE" != "20" ]]; then
-            # HACK: only run remaining
-    		continue
-    	fi
-
 		echo "Running evaluation with clock period = ${CP} ns; LSQ size = $LSQ_SIZE"
 		export SYNTHESIS_CLOCK_PERIOD_NS="${CP}"
 		export LSQ_NUM_LDQ_ENTRIES=$LSQ_SIZE
@@ -41,6 +36,6 @@ for CP in "${CLOCK_PERIODS[@]}"; do
 		export LSQ_HEAD_LAG_EN=1
 
 		"$RUN_EVALUATION_PY" --synth-lsqs -j 16 \
-		    --json "${OUTPUT_DIR_CP}/lsq_${LSQ_SIZE}_${LSQ_SIZE}.json"
+			--json "${OUTPUT_DIR_CP}/lsq_${LSQ_SIZE}_${LSQ_SIZE}.json"
 	done
 done
