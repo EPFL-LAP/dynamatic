c07002eed852e7cd7f19c3ed8fcb565b333b8712
Elena Morbach <emorbach@ethz.ch>
2026-04-30 15:34:28 +0200

    update finite loop files

--- integration-test/finite_loop/finite_loop.c
+++ integration-test/finite_loop/finite_loop.c
@@ -1,20 +1,20 @@
 #include "finite_loop.h"
 #include "dynamatic/Integration.h"
 
-void finite_loop(inout_int_t a[N], inout_int_t b[N]) {
+void finite_loop(inout_float_t a[N], inout_int_t b[N]) {
   int x = 0;
   for (unsigned i = 1; i < N; ++i) {
-    a[i] = a[i-1] *2+2;
-    // x = a[i] + x;
-    b[i] = i;
+    a[i] = a[i-1] * -1.0+6.0;
+    // x = a[i]*4 + 8 - x;
+    // b[i] = i;
   } 
 }
 
 int main(void) {
-  in_int_t a[N];
+  in_float_t a[N];
   in_int_t b[N];
   for (unsigned j = 0; j < N; ++j) {
-    a[j] = -2;
+    a[j] = 1.0;
     b[j] = j;
   }
 
--- integration-test/finite_loop/finite_loop.h
+++ integration-test/finite_loop/finite_loop.h
@@ -1,7 +1,10 @@
 #define N 100
 
+typedef float in_float_t;
+typedef float out_float_t;
+typedef float inout_float_t;
 typedef int in_int_t;
 typedef int out_int_t;
 typedef int inout_int_t;
-void finite_loop(inout_int_t a[N], inout_int_t b[N]);
+void finite_loop(inout_float_t a[N], inout_int_t b[N]);
 
