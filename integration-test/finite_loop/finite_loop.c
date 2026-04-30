aed83aabaa091ba31258735e43b66ca1775b8d5d
Elena Morbach <emorbach@ethz.ch>
2026-04-30 16:10:15 +0200

    finite loop without osci

--- integration-test/finite_loop/finite_loop.c
+++ integration-test/finite_loop/finite_loop.c
@@ -4,7 +4,7 @@
 void finite_loop(inout_float_t a[N], inout_int_t b[N]) {
   int x = 0;
   for (unsigned i = 1; i < N; ++i) {
-    a[i] = a[i-1] * -1.0+6.0;
+    a[i] = a[i-1] * -2.0+15.0;
     // x = a[i]*4 + 8 - x;
     // b[i] = i;
   } 
@@ -14,7 +14,7 @@ int main(void) {
   in_float_t a[N];
   in_int_t b[N];
   for (unsigned j = 0; j < N; ++j) {
-    a[j] = 1.0;
+    a[j] = 5.0;
     b[j] = j;
   }
 
