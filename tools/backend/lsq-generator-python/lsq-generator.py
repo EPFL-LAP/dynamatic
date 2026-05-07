fcfd354a08b7e337d981459a320af8b146033272
Jiahui Xu <jxu@ethz.ch>
2026-05-07 15:56:25 +0200

    revert changes: LSQ is directly conn. to the BRAM

--- tools/backend/lsq-generator-python/lsq-generator.py
+++ tools/backend/lsq-generator-python/lsq-generator.py
@@ -428,13 +428,11 @@ class LSQWrapper:
             self.lsq_wrapper_str += OpTab(wreq_ready[i], (self.tab_level + 2), "'1'")
 
 
-        self.lsq_wrapper_str += "\t-- Signals the LSQ core that the store is completed if the MC is ready.\n"
-        self.lsq_wrapper_str += "\t-- NOTE: we assume that MC joins the addr and data, so here we only check the addr ready .\n"
         self.lsq_wrapper_str += (
             "\n"
             + "\t" * (self.tab_level + 2)
             + "if "
-            + io_storeEn.getNameWrite() + " = '1' " + " and " + io_stAddrToMC_ready.getNameRead() + " = '1' then\n"
+            + io_storeEn.getNameWrite() + " = '1' then\n"
         )
 
         for i in range(self.lsq_config.numStMem):
