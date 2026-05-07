c1ad1fc216737e8ea38a3e9602f2897869255f54
Bas Niekel <bniekel@ethz.ch>
2026-05-07 10:26:55 +0200

    fix errors

--- experimental/tools/rigidification/rigidification.sh
+++ experimental/tools/rigidification/rigidification.sh
@@ -58,7 +58,7 @@ show_property -o $F_NUXMV_PROP;
 time;
 quit"
 else
-  ANNOTATE_FLAGS="annotate-properties annotate-invariants"
+  ANNOTATE_FLAGS="annotate-properties"
   NUXMV_SCRIPT="set verbose_level 0;
 set pp_list cpp;
 set counter_examples 0;
