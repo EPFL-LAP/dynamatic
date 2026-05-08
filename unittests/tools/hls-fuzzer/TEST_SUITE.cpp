a802ed6b3330acbd7c5b0205ea6bf22ceffa645e
Markus Böck <markus.boeck02@gmail.com>
2026-05-08 14:09:38 +0200

    fix unit tests

--- unittests/tools/hls-fuzzer/TEST_SUITE.cpp
+++ unittests/tools/hls-fuzzer/TEST_SUITE.cpp
@@ -52,7 +52,7 @@ public:
         /*rhs=*/
         TransferFn<ast::BinaryExpression, ast::BinaryExpression::LHS>(
             PlusOfTwoState::ExistingParamNeeded),
-        /*output=*/copyFromParent<ast::BinaryExpression>(),
+        /*output=*/copyOutputFromParent<ast::BinaryExpression>(),
     };
   }
 
@@ -108,7 +108,7 @@ public:
     return {
         /*array parameter=*/TransferFn<ast::ArrayReadExpression>(false),
         /*index=*/TransferFn<ast::ArrayReadExpression>(false),
-        /*output=*/copyFromParent<ast::ArrayReadExpression>(),
+        /*output=*/copyOutputFromParent<ast::ArrayReadExpression>(),
     };
   }
 
