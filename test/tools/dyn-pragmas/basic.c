77fe6a4450af59ff7c6d616b5610e76f36f62b86
Emmet Murphy <murphe67@tcd.ie>
2026-05-16 14:22:58 +0200

    fix unit test to generic

--- test/tools/dyn-pragmas/basic.c
+++ test/tools/dyn-pragmas/basic.c
@@ -1,8 +1,8 @@
 // RUN: %dyn-clang-pragmas %s | FileCheck %s
 
 // CHECK: c"standard\00"
-// CHECK: call i32 @__dyn_speculate(double {{.+}}, i32 noundef 8, ptr noundef {{.+}})
-// CHECK: declare i32 @__dyn_speculate(double noundef, i32 noundef, ptr noundef)
+// CHECK: call i32 @__dyn_speculate_i(i32 noundef {{.+}}, i32 noundef 8, ptr noundef {{.+}})
+// CHECK: declare i32 @__dyn_speculate_i(i32 noundef, i32 noundef, ptr noundef)
 
 int compute(void);
 int use(int);
