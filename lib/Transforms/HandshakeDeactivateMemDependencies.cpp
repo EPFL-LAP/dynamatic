6c01a6a5f8e96792c6655af693f64b38c983f69a
Jasmijn Bookelmann <bookelmannjasmijn@gmail.com>
2026-05-20 12:09:26 +0000

    Remove !isActive check from deactiveEnforcedWAWs

--- lib/Transforms/HandshakeDeactivateMemDependencies.cpp
+++ lib/Transforms/HandshakeDeactivateMemDependencies.cpp
@@ -114,8 +114,6 @@ static void deactivateEnforcedWAWs(DenseSet<handshake::StoreOp> &storeOps) {
       StringRef storeName = getUniqueName(storeOp);
 
       for (MemDependenceAttr dep : deps.getDependencies()) {
-        if (!dep.getIsActive())
-          continue;
         // set the dependency as inactive if the source and destination of the
         // dependency are the same store operation
         newDeps.push_back(
