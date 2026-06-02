0d7d07ea1a197a5b42c8201e561e58122466a762
Max Wipfli <mwipfli@ethz.ch>
2026-06-02 16:38:28 +0200

    [jobs] Experiment 7a: Test no-compare store issue

---
+++ jobs/07a_store_no_compare.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+RUN_EVALUATION_PY="${SCRIPT_DIR}/../tools/evaluation/run_evaluation.py"
+GIT_REVISION="$(git rev-parse --short HEAD)"
+
+LSQ_SIZE=20
+PIPELINE_CONFIGS=(
+	"headlag"
+	"headlag_pipe0"
+)
+
+export SYNTHESIS_CLOCK_PERIOD_NS="2.5"
+export LSQ_NO_BYPASS=1
+export LSQ_NUM_LDQ_ENTRIES=$LSQ_SIZE
+export LSQ_NUM_STQ_ENTRIES=$LSQ_SIZE
+export LSQ_ST_ISSUE_NO_COMPARE=1
+
+EXPERIMENT_NAME="$(basename "$0" .sh)"
+OUTPUT_DIR="${SCRIPT_DIR}/../eval_results/${EXPERIMENT_NAME}_${GIT_REVISION}"
+mkdir -p "${OUTPUT_DIR}"
+cat <<EOF >"${OUTPUT_DIR}/README.txt"
+Experiment 7a: Store issue without store-load comparison
+- synthesis target clock period: ${SYNTHESIS_CLOCK_PERIOD_NS} ns
+- pipeline configurations: ${PIPELINE_CONFIGS[*]}
+- LSQ size: ${LSQ_SIZE} entries
+- git revision: ${GIT_REVISION}
+EOF
+
+for PIPELINE_CONFIG in "${PIPELINE_CONFIGS[@]}"; do
+	export LSQ_PIPE_COMP_EN=0
+	export LSQ_PIPE0_EN=0
+	export LSQ_PIPE1_EN=0
+	export LSQ_HEAD_LAG_EN=0
+	if [[ "$PIPELINE_CONFIG" == *"pipecomp"* ]]; then
+		export LSQ_PIPE_COMP_EN=1
+	fi
+	if [[ "$PIPELINE_CONFIG" == *"pipe0"* ]]; then
+		export LSQ_PIPE0_EN=1
+	fi
+	if [[ "$PIPELINE_CONFIG" == *"pipe1"* ]]; then
+		export LSQ_PIPE1_EN=1
+	fi
+	if [[ "$PIPELINE_CONFIG" == *"headlag"* ]]; then
+		export LSQ_HEAD_LAG_EN=1
+	fi
+
+	OUTPUT_SUBDIR="${OUTPUT_DIR}/${PIPELINE_CONFIG}"
+	echo "Output directory for pipeline configuration = ${PIPELINE_CONFIG}: ${OUTPUT_SUBDIR}"
+	mkdir -p "${OUTPUT_SUBDIR}"
+
+	echo "Running evaluation with pipeline configuration = ${PIPELINE_CONFIG}"
+	"$RUN_EVALUATION_PY" --no-synth -j 16 \
+		--json "${OUTPUT_SUBDIR}/data.json"
+done
