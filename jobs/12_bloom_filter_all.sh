583137fc5fba9a61018587e112d9af852055866d
Max Wipfli <mwipfli@ethz.ch>
2026-06-15 14:41:03 +0200

    [jobs] Update experiment 12 to support multiple LSQ sizes

--- jobs/12_bloom_filter_all.sh
+++ jobs/12_bloom_filter_all.sh
@@ -5,14 +5,18 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 RUN_EVALUATION_PY="${SCRIPT_DIR}/../tools/evaluation/run_evaluation.py"
 GIT_REVISION="$(git rev-parse --short HEAD)"
 
-LSQ_SIZE=16
+LSQ_SIZES=(
+	12
+	16
+)
 
 # k, log2(m)
 BLOOM_FILTER_HASH_COUNTS_WIDTHS=(
+	"1 2"
 	"1 3"
 	"2 4"
 	"3 5"
-	"4 6"
+	"3 6"
 )
 
 PIPELINE_CONFIGS=(
@@ -22,8 +26,6 @@ PIPELINE_CONFIGS=(
 
 export SYNTHESIS_CLOCK_PERIOD_NS="2.5"
 export LSQ_NO_BYPASS=1
-export LSQ_NUM_LDQ_ENTRIES=$LSQ_SIZE
-export LSQ_NUM_STQ_ENTRIES=$LSQ_SIZE
 export LSQ_BLOOM_FILTER_LOAD=1
 export LSQ_ST_ISSUE_NO_COMPARE=1
 export LSQ_BLOOM_FILTER_SEQUENTIAL=1
@@ -34,19 +36,15 @@ OUTPUT_DIR="${SCRIPT_DIR}/../eval_results/${EXPERIMENT_NAME}_${GIT_REVISION}"
 mkdir -p "${OUTPUT_DIR}"
 cat <<EOF >"${OUTPUT_DIR}/README.txt"
 Experiment 12: Bloom filters (all kernels)
-- Bloom filter type: sequential
+- Bloom filter type: sequential, conservative store issue
 - synthesis target clock period: ${SYNTHESIS_CLOCK_PERIOD_NS} ns
-- LSQ size: ${LSQ_SIZE} entries
+- LSQ sizes: ${LSQ_SIZE[*]}
 - pipeline configurations: ${PIPELINE_CONFIGS[*]}
 - bloom filter hash counts and widths: ${BLOOM_FILTER_HASH_COUNTS_WIDTHS[*]}
 - git revision: ${GIT_REVISION}
 EOF
 
 for PIPELINE_CONFIG in "${PIPELINE_CONFIGS[@]}"; do
-	OUTPUT_SUBDIR="${OUTPUT_DIR}/${PIPELINE_CONFIG}"
-	echo "Output directory for pipeline configuration = ${PIPELINE_CONFIG}: ${OUTPUT_SUBDIR}"
-	mkdir -p "${OUTPUT_SUBDIR}"
-
 	export LSQ_PIPE_COMP_EN=0
 	export LSQ_PIPE0_EN=0
 	export LSQ_PIPE1_EN=0
@@ -70,11 +68,19 @@ for PIPELINE_CONFIG in "${PIPELINE_CONFIGS[@]}"; do
 		HASH_WIDTH=$2
 		export LSQ_BLOOM_FILTER_HASH_COUNT=$HASH_COUNT
 		export LSQ_BLOOM_FILTER_HASH_WIDTH=$HASH_WIDTH
-
 		FILTER_WIDTH=$((2 ** HASH_WIDTH))
 
-		echo "Running evaluation with m=${FILTER_WIDTH}, k=${HASH_COUNT}..."
-		"$RUN_EVALUATION_PY" --synth-lsqs -j 8 \
-			--json "${OUTPUT_SUBDIR}/bf_m${FILTER_WIDTH}_k${HASH_COUNT}.json"
+		OUTPUT_SUBDIR="${OUTPUT_DIR}/${PIPELINE_CONFIG}/bf_m${FILTER_WIDTH}_k${HASH_COUNT}"
+		echo "Output directory for pipeline configuration = ${PIPELINE_CONFIG}, m=${FILTER_WIDTH}, k=${HASH_COUNT}: ${OUTPUT_SUBDIR}"
+		mkdir -p "${OUTPUT_SUBDIR}"
+
+		for LSQ_SIZE in "${LSQ_SIZES[@]}"; do
+			export LSQ_NUM_LDQ_ENTRIES=$LSQ_SIZE
+			export LSQ_NUM_STQ_ENTRIES=$LSQ_SIZE
+
+			echo "Running evaluation LSQ size ${LSQ_SIZE}..."
+			"$RUN_EVALUATION_PY" --synth-lsqs -j 8 \
+				--json "${OUTPUT_SUBDIR}/lsq_${LSQ_SIZE}_${LSQ_SIZE}.json"
+		done
 	done
 done
