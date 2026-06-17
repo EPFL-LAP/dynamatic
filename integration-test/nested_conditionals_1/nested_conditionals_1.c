839c0228cd079b284cf0b69c8b3ca6d0eaf37e5d
Elena Morbach <elena.morbach@gmail.com>
2026-06-17 16:34:22 +0200

    new testcase

---
+++ integration-test/nested_conditionals_1/nested_conditionals_1.c
@@ -0,0 +1,38 @@
+#include "dynamatic/Integration.h"
+#include "nested_conditionals_1.h"
+
+void nested_conditionals_1(inout_int_t a[N], in_int_t c) {
+  for (unsigned i = 0; i < N; ++i) {
+    int x = a[i];
+#pragma DYN predict variable = x values = [2] location = start marker =        \
+                                               0 type = int
+    int y = x + 2;
+    if (y > 10) {
+      if (c > 5) {
+        y = y * 2;
+      } else {
+        y = y + c;
+      }
+    } else {
+      if (c == 0) {
+        y = y - 3;
+      } else {
+        y = y / c;
+      }
+    }
+    int z = y + 3;
+#pragma DYN predict variable = z location = end marker = 0
+    a[i] = z;
+  }
+}
+
+int main(void) {
+  in_int_t a[N];
+  int c = 7;
+  for (unsigned j = 0; j < N; ++j) {
+    a[j] = 10;
+  }
+
+  CALL_KERNEL(nested_conditionals_1, a, c);
+  return 0;
+}
