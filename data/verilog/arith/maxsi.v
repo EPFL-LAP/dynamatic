74b31454d90b167bb28e8ce0f03d44e8ee139424
josh-anderegg <josh.anderegg@gmail.com>
2026-08-20 17:29:20 +0200

    [Verilog] Enable correct `min-si/ui` `max-si/ui` translation
    - `data/verilog`: Included implementations for the verilog handshake
      modules.
    
    - `include/dynamatic/Dialect/Handshake`: Tablegen entries for all min-max
      instructions
    
    - `data/rtl-config-verilog.json`: Addition of `minsi`, `minui` handskake
      entries and correction of `maxsi` handshake entry

---
+++ data/verilog/arith/maxsi.v
@@ -0,0 +1,22 @@
+module maxsi #(
+    parameter DATA_TYPE = 32
+)(
+    input  wire                     clk,
+    input  wire                     rst,
+    input  wire [DATA_TYPE-1:0]     lhs,
+    input  wire                     lhs_valid,
+    input  wire [DATA_TYPE-1:0]     rhs,
+    input  wire                     rhs_valid,
+    input  wire                     result_ready,
+
+    output wire [DATA_TYPE-1:0]     result,
+    output wire                     result_valid,
+    output wire                     lhs_ready,
+    output wire                     rhs_ready
+);
+    assign result = ($signed(lhs) > $signed(rhs)) ? lhs : rhs;
+
+    assign result_valid = lhs_valid & rhs_valid;
+    assign lhs_ready     = result_ready & rhs_valid;
+    assign rhs_ready     = result_ready & lhs_valid;
+endmodule
